ee886f74b58f8e63ea3adad5f5c674a1a8b78635
jakebrady5 <jakebrady5@gmail.com>
2015-11-23 21:05:59 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,32 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    #twitter, linkedin, github, blog, profile_quote, bio
+    index = open(profile_url)
+    info = {}
+    social_links = []
+    doc = Nokogiri::HTML(index)
+    social_links = doc.css(".social-icon-container a")
+    quote = doc.css(".profile-quote").text
+    bio_text = doc.css(".description-holder p").text
+
+    social_links.each do |type|
+      link = type.attribute("href").value
+      #seems like i should be able to refactor this into a more meta an flexible process?
+      if link.match(/(linked)/) != nil
+        info[:linkedin] = link
+      elsif link.match(/(twitter)/) != nil
+        info[:twitter] = link
+      elsif link.match(/(github)/) != nil
+        info[:github] = link
+      else
+        info[:blog] = link
+      end
+    end
     
+    info[:profile_quote] = quote
+    info[:bio] = bio_text
+    info
   end
 
 end
