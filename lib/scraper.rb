e4193cf18935f0b6ce2c114cd603d9cc5b919aac
mmmeyers <marsi286@gmail.com>
2015-11-30 17:46:34 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,69 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
+  # #scrape_index_page is a class method that scrapes the student index page and 
+  # returns an array of hashes in which each hash represents one student
+  
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    scraped_students = []
+    students = doc.css(".student-card")
     
+    students.each do |student|
+        name = student.css(".student-name").text
+        location = student.css(".student-location").text
+        profile_url = "http://students.learn.co/#{student.css("a").attribute("href").value}"
+        scraped_students << {:name => name, :location => location, :profile_url => profile_url}
+    end
+    scraped_students
   end
 
+  # scrape_profile_page is a class method that scrapes a student's profile page and returns a 
+  # hash of attributes describing an individual student
+
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+  
+
+    student_info = {}
+     
+    doc.css(".social-icon-container").each do |social_icon|
+
+      if social_icon.include?("twitter")
+        twitter = doc.css(".social-icon-container a")[0].attribute("href").value
+      else
+        ""
+      end
+
+      if social_icon.include?("linkedin")
+        linkedin = doc.css(".social-icon-container a")[1].attribute("href").value
+      else
+        ""
+      end
+
+      if social_icon.include?("github")
+        github = doc.css(".social-icon-container a")[2].attribute("href").value
+      else
+        ""
+      end
+
+      if social_icon.include?("rss")
+        blog = doc.css(".social-icon-container a")[3].attribute("href").value
+      else
+        ""
+      end
+
+      profile_quote = doc.css(".profile-quote").text
+      bio = doc.css(".description-holder p").text
+      student_info = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog, 
+        :profile_quote => profile_quote, :bio => bio}
+   end
+    student_info
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    # self.name = student_hash.values_at(:name).join(" ")
+    # self.location = student_hash.values_at(:location).join(" ")
+    # self.profile_url = student_hash.values_at(:profile_url).join(" ")
+    @@all <<  self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) # array of hashes
+    # create a new individual student from each hash
+    students_array.each {|student| puts student}
   end
 
   def add_student_attributes(attributes_hash)
-    
+
+    attributes_hash.each do 
+      attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
