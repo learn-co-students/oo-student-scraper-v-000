648493ef8cfd719b0a31dc1f8555980c324bb7f6
Justin Thomas <jthomas0209@gmail.com>
2015-12-01 11:50:15 -0500

    Defined Scraper and Student classes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,36 @@
+require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    scraped_students = []
+    doc = Nokogiri::HTML(open("#{index_url}"))
+    students = doc.css(".student-card")
+    students.each do |student|
+      hash = { name: student.css(".card-text-container h4").text,
+               location: student.css(".card-text-container p").text,
+               profile_url: "http://students.learn.co/#{student.css("a")[0]["href"]}"
+      }
+      scraped_students << hash
+    end
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open("#{profile_url}"))
+    social_links = doc.css(".vitals-container .social-icon-container a")
+    student_hash = {name: doc.css(".vitals-container .vitals-text-container .profile-name").text,
+            quote: doc.css(".vitals-container .vitals-text-container .profile-quote").text,
+            bio: doc.css(".details-container .bio-block .bio-content .description-holder p").text,
+            twitter: social_links[0]["href"],
+            linkedin: social_links[1]["href"],
+            github: social_links[2]["href"],
+    }
+    if social_links.length == 4
+      student_hash[:blog] = social_links[3]["href"]
+    end
+    student_hash
   end
-
 end
 
