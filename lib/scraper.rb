2e582db3cc03de66dac8df1f508db3dcdb093357
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-17 08:29:33 +0000

    completes scrape_index_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,24 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
+  def self.get_page(index_url)
+    html = open(index_url)
+    Nokogiri::HTML(html)
+  end
+  
   def self.scrape_index_page(index_url)
-    
+    students = []
+    self.get_page(index_url).css('.student-card').each do |s|
+      student = {}
+      student[:name] = s.css('.card-text-container h4.student-name').text
+      student[:location] = s.css('.card-text-container p.student-location').text
+      student[:profile_url] = index_url + '/' + s.css('a').attribute('href').value
+      students << student
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
