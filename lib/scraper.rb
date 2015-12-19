c6120f240608fd98fc95c9ee1c623caa58156805
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-19 08:33:05 +0000

    completes student scraper tests

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,13 +17,44 @@ class Scraper
       student[:location] = s.css('.card-text-container p.student-location').text
       student[:profile_url] = index_url + '/' + s.css('a').attribute('href').value
       students << student
+      
     end
     students
   end
 
   def self.scrape_profile_page(profile_url)
+    student = {}
+    social_links = []
     
+    # Grabs links in the .social-icon container div and adds their href
+    # attribute value to an array of social_links
+    self.get_page(profile_url).css('.social-icon-container a').each do |s|
+      social_links << s.attribute('href').value
+    end
+    
+    # iterates over social links and assigns them to the appropriate
+    # key-value pairs in the student hash
+    social_links.each do |s|
+      if s.match(/twitter\.com/)
+        student[:twitter] = s
+      elsif s.match(/linkedin\.com/)
+        student[:linkedin] = s
+      elsif s.match(/github\.com/)
+        student[:github] = s
+      else
+        student[:blog] = s
+      end
+    end
+    
+    student[:profile_quote] = self.get_page(profile_url).css('.profile-quote').text
+    student[:bio] = self.get_page(profile_url).css('.bio-content p').text
+    
+    student
   end
 
 end
 
+# joe = Scraper.scrape_profile_page('http://students.learn.co/students/joe-burgess.html')
+# joe.each do |k,v|
+#   puts "#{k} => #{v}"
+# end
--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,6 +1,7 @@
 VCR.configure do |c|  
   #the directory where your cassettes will be saved
-  c.cassette_library_dir = 'spec/vcr'
+  c.cassette_library_dir = 'spec/vcr/'
+  c.default_cassette_options = { :record => :new_episodes, :erb => true }
   # your HTTP request service. 
   c.hook_into :webmock
 end 
