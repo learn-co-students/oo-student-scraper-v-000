bb28f62959621a3d0a085e7d1c0681560236eb81
authorbeard <msnolan@gmail.com>
2016-01-12 23:45:26 -0500

    scraper tests all passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,22 +6,32 @@ class Scraper
   # attr_accessor :name, :location, :profile_url
 
   def self.scrape_index_page(index_url)
-# binding.pry    
     Nokogiri::HTML(open(index_url)).css(".student-card").collect {|c|
         { 
         name: c.css("h4").text,
         location: c.css(".student-location").text,
         profile_url: "http://students.learn.co/#{c.css("a").attribute("href").value}"
-      }
-    }
-    # scraped_students
-
-    
-# binding.pry
+        }}
   end
 
   def self.scrape_profile_page(profile_url)
-    
+  # binding.pry
+    profile = Nokogiri::HTML(open(profile_url)).css(".profile > div:not(:empty)")
+# binding.pry    
+    social_details = profile.css(".social-icon-container a").collect{|s|
+      s.attribute("href").value}.flatten
+    social_hash = 
+      {
+        twitter: social_details.detect{|d| d.include?("twitter")},
+        linkedin: social_details.detect{|d| d.include?("linkedin")},
+        github: social_details.detect{|d| d.include?("github")},
+        blog: social_details.last,
+        profile_quote: profile.css(".profile_quote").text,
+        bio: profile.css(".description-holder p").text
+      }
+     
+
+# binding.pry
   end
 
 end
