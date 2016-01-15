89c2dbe4f213dadc3f2bf0b0bbb4275e6e3d1772
Marco Tsui <marcotsui2003@yahoo.com>
2016-01-15 20:11:20 +0000

    scrape_index_page passed

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,21 @@
 require 'open-uri'
 require 'pry'
+#require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+  	index_page = Nokogiri::HTML(open(index_url))
+  	student_info = index_page.css("div.student-card")
+  	student_array =[]
+  	student_info.each do |student|
+  		student_hash = {}
+  		student_hash[:name] = student.css("h4.student-name").text
+  		student_hash[:location] = student.css("p.student-location").text
+  		student_hash[:profile_url] = "http://students.learn.co/" + student.css("a")[0]["href"]
+  		student_array << student_hash
+  	end
+  	student_array
   end
 
   def self.scrape_profile_page(profile_url)
