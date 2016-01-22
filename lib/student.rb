b14ec561bb4cf5f8f42573d3c6395eb5c816d689
pajamaw <pjpeterww@gmail.com>
2016-01-22 16:05:36 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'nokogiri'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +6,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k,v|
+      self.send("#{k}=", v)
+    end
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |k,v|
+      self.send("#{k}=", v)
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
