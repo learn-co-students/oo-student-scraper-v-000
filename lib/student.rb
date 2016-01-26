a90b0bc5703e2c382c43fe270c90355a97f8b259
James Novak <jamesnovak90@gmail.com>
2016-01-25 23:46:52 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
