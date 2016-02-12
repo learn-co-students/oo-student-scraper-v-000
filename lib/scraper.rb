1c514e79fbf72104081609ea1de038bcdd7b15fb
thegands <thegands@gmail.com>
2016-02-12 03:57:37 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,28 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    array = []
+    Nokogiri::HTML(open(index_url)).css(".student-card").each { |student_card| array << {name: student_card.css(".student-name").text, location: student_card.css(".student-location").text, profile_url: "http://127.0.0.1:4000/#{student_card.css("a").attribute("href").value}"} }
+    array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+    hash = {profile_quote: profile.css(".profile-quote").text, bio: profile.css(".bio-content p").text}
+    profile.css(".social-icon-container a").each do |social_item|
+      link = social_item.attribute("href").value
+      case social_item.css("img").attribute("src").value
+      when "../assets/img/twitter-icon.png"
+        hash[:twitter] = link
+      when "../assets/img/linkedin-icon.png"
+        hash[:linkedin] = link
+      when "../assets/img/github-icon.png"
+        hash[:github] = link
+      when "../assets/img/rss-icon.png"
+        hash[:blog] = link
+      end
+    end
+    hash
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,50 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each { |student| self.new(student) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each { |key, value| self.send key, value}
+    self
   end
 
   def self.all
-    
+    @@all
+  end
+
+  def twitter(link = nil)
+    link ? @twitter = link : @twitter
+  end
+
+  def linkedin(link = nil)
+    link ? @linkedin = link : @linkedin
+  end
+
+  def github(link)
+    @github = link
+  end
+
+  def blog(link = nil)
+    link ? @blog = link : @blog
+  end
+
+  def profile_quote(link = nil)
+    link ? @profile_quote = link : @profile_quote
+  end
+
+  def bio(link = nil)
+    link ? @bio = link : @bio
   end
 end
-
