baf4e7336aa6e33e1fc17ed0fc80412cae14b5cd
aprietof <adrianprieto00@gmail.com>
2016-02-21 12:11:37 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -23,7 +25,7 @@ class Student
   end
 
   def self.all
-    
+    @all
   end
 end
 
