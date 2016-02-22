9334594955f0e36f51dab8afd400f2772b205f69
shinyrachel <rachel.kory@gmail.com>
2016-02-22 13:48:46 -0500

    index page scraping css works

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,34 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    binding.pry
+    # 
+    students = []
+    doc.css("div.student-card").each do |student|
+      students << {
+        :name => student.css("a div.card-text-container h4.student-name").text,
+        :location => student.css("a div.card-text-container p.student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      }
+    end
+    students
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    student_data = {}
+    doc.css() do |attribute|
+      student_data[attribute.css().to_sym] = attribute.css()
+    end
+    student_data
   end
 
 end
 
+Scraper.scrape_index_page("http://127.0.0.1:4000/")
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,25 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+  
+    @@all << self  
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attribute|
+
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
