51f43ce9ca0dc444b318be8a2a39811dbd998a4a
Brian Holland <brian.holland@proatria.com>
2016-02-27 18:13:39 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,22 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    add_student_attributes(student_hash)
+    @@all.push(self)
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each{|key, val| self.send(("#{key}="), val)}
   end
 
   def self.all
-    
+    @@all
   end
 end
 
