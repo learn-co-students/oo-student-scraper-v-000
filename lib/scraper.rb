1c388ffffa5d2113707a3ff5ab1b8abb14865b98
reifnotreef <bridgerrhammond@gmail.com>
2016-03-05 11:47:53 -0600

    sync to github

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,19 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = Array.new
+    index_page.css("div.roster-cards-containter").each do |card|
+      card.css(".student-card a").each do |student|
+        profile_url = "http://127.0.0.1:4000/#{student.attr('href')}"
+        location = student.css('.student-location').text
+        name = student.css('.student-name').text
+      end
+    end
+    # :name, :location, :profile_url
   end
 
   def self.scrape_profile_page(profile_url)
-    
   end
-
 end
-
