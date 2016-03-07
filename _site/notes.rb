f130ee0b25d7310c993213b473a6ff018249dfb0
smithWEBtek <brad@smithwebtek.com>
2016-03-07 11:08:15 -0500

    pushing for tutoring purposes

---
+++ _site/notes.rb
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+################################
+################################
+
+
+doc = Nokogiri::HTML(open("http://localhost:4000"))
+doc.css(".student-card")
+binding.pry
+
+
+(".student-card")
+
+
+(".card-text-container h4 .student-name")
+
+(".student-card .card-text-container h4 .student-name")
+
+(".card-text-container h4 .student-name")
+
+  #   html = open(index_url)
+  #   doc = Nokogiri::HTML(html)
+  #   puts doc.css("roster-cards-container")
+
+("roster-cards-container")
+
+require 'nokogiri'
+require 'open-uri'
+doc = Nokogiri::HTML(open("http://localhost:4000")
+doc.css(".card-text-container h4 .student-name")
+
+
+
+name = ("h4.student-name")
+location = ("p.student-location")
+profile_url = ("students/(name).html")
+
+
+
+("div.student-card #{fname}-#{lname}-card")
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,22 @@
-require 'open-uri'
 require 'pry'
+require 'open-uri'
+require 'nokogiri' 
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css("div.roster-cards-container").each do |student| 
+        binding.pry
+        
+       name = student.css(".student-name").text,
+       location = student.css(".student-location").text, 
+       profile_url = "http://127.0.0.1:4000/#{student.css("a").first["href"]}"
+    students << {name: name, location: location, profile_url: profile_url}
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +24,3 @@ class Scraper
   end
 
 end
-
