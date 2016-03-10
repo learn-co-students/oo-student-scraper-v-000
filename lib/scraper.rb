ec95ed63e47c9d065bc531e658c483bb7b106f39
agdavid <antonio.g.david@gmail.com>
2016-03-10 12:49:48 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,47 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    students_array = []
+
+    doc.search("div.student-card").each do |student|
+      name = student.search("h4.student-name").text
+      location = student.search("p.student-location").text
+      sub = student.search("a").attribute("href").value
+      students_array << {
+        :name => name,
+        :location => location,
+        :profile_url => "#{index_url}#{sub}"
+      }
+    end
+    students_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    #the easy grabs using .search method and direct element/class relationship
+    student = {}
+    student[:profile_quote] = doc.search("div.profile-quote").text
+    student[:bio] = doc.search("div.description-holder p").text
+    #iterate over each 'a' element and set the 'href' attribute value equal to sociallink
+    doc.search("div.social-icon-container a").each do |socialnodes|
+      sociallink = socialnodes.attribute("href").value
+      if sociallink.include?("twitter")
+        student[:twitter] = sociallink
+      elsif sociallink.include?("linkedin")
+        student[:linkedin] = sociallink
+      elsif sociallink.include?("github")
+        student[:github] = sociallink
+      else
+        student[:blog] = sociallink
+      end
+    end
+    student
   end
-
 end
+
+
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,37 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    #should this call on #add_student_attibutes?
+    student_hash.each do |key, value|
+      self.send(("#{key}="), value)
+      #name of the key becomes our setter method
+      #the value becomes the value of the setter method
+      #self.send(key=,value)
+      #same as instance.key= value
+    end
+    add_student_attributes(student_hash)
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #is this even part of the test suite?
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+      #name of the key becomes our setter method
+      #the value becomes the value of the setter method
+      #self.send(key=,value)
+      #same as instance.key= value
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
