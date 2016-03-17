7770542f411e83112bed09a08384b19adc5767d4
hide83 <hiral.desai83@gmail.com>
2016-03-17 20:52:48 +0000

    Done.

--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -5,41 +5,40 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student_arr = []
-    student_hash = {}
-    doc = Nokogiri::HTML(open("http://students.learn.co/"))
+
+    Nokogiri::HTML(open(index_url)).css(".student-card").collect {|card|
+
+    {
+    name: card.css("h4").text,
     #binding.pry
+    #student_names = doc.css(".student-card").css("h4")
+    location: card.css("p").text,
 
-    student_name = doc.css(".student-card").css("h4")
-    #binding.pry
-    student_names = doc.css(".student-card").css("h4").text
-    student_location  = doc.css(".student-card").css("p").text
-
-    student_profile = doc.css(".student-card").each{|a| a.css('a').attribute('href').value}
-
-
-  (0..student_name.size).each do |i|
-      student_names.each do |st_names|
-      student_location.each do |st_location|
-      student_profile.each do |st_profile|
-    student_hash = {
-      :name => st_names,
-      :location => st_location,
-      :profile_url => st_profile
-      }
-
-
-    student_arr << student_hash
-      end
-    end
-  end
+    profile_url: "http://students.learn.co/#{card.css("a").attribute("href").value}"
+      }}
 end
 
 
-    end
+
 
 def self.scrape_profile_page(profile_url)
+ #binding.pry
+  doc = Nokogiri::HTML(open(profile_url))
+  #binding.pry
+   student_social_links = doc.css(".social-icon-container").css("a").collect do |s|
+      s.attribute("href").value
+   end #["https://twitter.com/jmburges", "https://www.linkedin.com/in/jmburges", "https://github.com/jmburges", "http://joemburgess.com/"]
 
+  #twitter: post.css(".social-icon-container").css('a').attribute("href").value
+  student_social_hash =
+      {
+        :twitter => student_social_links[0],
+        :linkedin => student_social_links[1],
+        github: student_social_links[2],
+        blog: student_social_links.last,
+        profile_quote: doc.css(".profile-quote").text,
+        bio: doc.css(".description-holder").css("p").text
+      }
   end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,42 +5,79 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    student_arr = []
-    student_hash = {}
-    doc = Nokogiri::HTML(open("http://students.learn.co/"))
+
+    Nokogiri::HTML(open(index_url)).css(".student-card").collect {|card|
+
+    {
+    name: card.css("h4").text,
     #binding.pry
+    #student_names = doc.css(".student-card").css("h4")
+    location: card.css("p").text,
 
-    student_name = doc.css(".student-card").css("h4")
-    #binding.pry
-    student_names = doc.css(".student-card").css("h4").text
-    student_location  = doc.css(".student-card").css("p").text
-
-    student_profile = doc.css(".student-card").each{|a| a.css('a').attribute('href').value}
-
-
-  (0..student_name.size).each do |i|
-      student_names.each do |st_names|
-      student_location.each do |st_location|
-      student_profile.each do |st_profile|
-    student_hash = {
-      :name => st_names,
-      :location => st_location,
-      :profile_url => st_profile
-      }
-
-
-    student_arr << student_hash
-      end
-    end
-  end
+    profile_url: "http://students.learn.co/#{card.css("a").attribute("href").value}"
+      }}
 end
 
 
-    end
+
 
 def self.scrape_profile_page(profile_url)
 
-  end
+  social_link_hash={}
+ #binding.pry
+  doc = Nokogiri::HTML(open(profile_url))
+  #doc = Nokogiri::HTML(open("http://students.learn.co/students/david-kim.html"))
+  #binding.pry
+   student_social_links = doc.css(".social-icon-container").css("a").collect {|s|
+      s.attribute("href").value }
+  #binding.pry
+   #["https://twitter.com/jmburges", "https://www.linkedin.com/in/jmburges", "https://github.com/jmburges", "http://joemburgess.com/"]
+#For another student my array is ["https://www.linkedin.com/in/david-kim-38221690", "https://github.com/davdkm"]
+  #twitter: post.css(".social-icon-container").css('a').attribute("href").value
+#social_link_hash={
+  #:twitter => student_social_links.each{|links| links.include?("twitter")},
+  #:linkedin => student_social_links.each{|links| links.include?("linkedin")},
+ # :github => student_social_links.each{|links| links.include?("github")},
+ # :blog => student_social_links.last,
+ # :profile_quote => doc.css(".profile-quote").text,
+ # :bio => doc.css(".description-holder").css("p").text
+ # }
 
+student_social_links.collect do |links|
+    if links.include?("twitter")
+      social_link_hash[:twitter] = links
+      else social_link_hash[:twitter] = nil
+    end
+     if links.include?("linkedin")
+        social_link_hash[:linkedin] = links
+       else  social_link_hash[:linkedin] = nil
+     end
+       if links.include?("github")
+      social_link_hash[:github] = links
+         else social_link_hash[:github] = nil
+       end
 end
+      social_link_hash[:blog] = student_social_links.last
+      social_link_hash[:profile_quote] = doc.css(".profile-quote").text,
+      social_link_hash[:bio] = doc.css(".description-holder").css("p").text
+
+  social_link_hash
+end
+end
+  #}
+      #  bio: doc.css(".description-holder").css("p").text
+
+
+  #student_social_hash =
+    #  {
+     #   :twitter => student_social_links[0],
+      #  :linkedin => student_social_links[1],
+      #  github: student_social_links[2],
+      #  blog: student_social_links.last,
+      #  profile_quote: doc.css(".profile-quote").text,
+      #  bio: doc.css(".description-holder").css("p").text
+     # }
+
+
+
 
