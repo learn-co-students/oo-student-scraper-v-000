89a5397b66d30c9c38f6647672c48f885d4ba479
hide83 <hiral.desai83@gmail.com>
2016-03-17 05:05:59 +0000

    Done.

--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -4,46 +4,41 @@ require 'pry'
 
 class Scraper
 
-
-
-
-
   def self.scrape_index_page(index_url)
     student_arr = []
     student_hash = {}
-doc = Nokogiri::HTML(open("http://students.learn.co/"))
-    binding.pry
+    doc = Nokogiri::HTML(open("http://students.learn.co/"))
+    #binding.pry
 
-    student_names = doc.css(".student-card").css("h4")
-    student_location  = doc.css(".student-card").css("p")
-    #profile_url = []
+    student_name = doc.css(".student-card").css("h4")
+    #binding.pry
+    student_names = doc.css(".student-card").css("h4").text
+    student_location  = doc.css(".student-card").css("p").text
 
-     i=0
+    student_profile = doc.css(".student-card").each{|a| a.css('a').attribute('href').value}
+
+
+  (0..student_name.size).each do |i|
+      student_names.each do |st_names|
+      student_location.each do |st_location|
+      student_profile.each do |st_profile|
     student_hash = {
-      :name => student_names.text,
-      :location => student_location.text
+      :name => st_names,
+      :location => st_location,
+      :profile_url => st_profile
       }
-      while i <= student_names.size
 
-        i+=1
-      end
+
     student_arr << student_hash
+      end
+    end
+  end
+end
+
 
     end
-#student_arr
 
-
-
-   # student_hash={
-     # :name => doc.css(".student-card").css("h4").text ,
-    #  :location => doc.css(".student-card").css("p").text ,
-    #  :profile_url => doc.css(".student-card").css("a")[0]["href"]
-    #  }
-
-
-
-
-  def self.scrape_profile_page(profile_url)
+def self.scrape_profile_page(profile_url)
 
   end
 
