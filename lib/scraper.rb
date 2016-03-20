56e2a1547bd09b8beca979d389a006dfdc291b6e
jbok4 <jbok4@aol.com>
2016-03-20 15:32:28 -0400

    name and location of students scraped

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,28 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css(".roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+      name = student.css(".student-name").text.strip
+      location = student.css(".student-location").text.strip
+      link = student.css("a href")
+      students << {name: name, location: location, profile_url: link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
     
   end
+
 
 end
 
