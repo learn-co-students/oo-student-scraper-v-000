e56a685ade69ad2ba6eb917545bbcc5e8df28ff3
gnfisher <me@gnfisher.com>
2016-03-21 16:04:46 -0300

    profile scrape passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,29 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    profile_page = Nokogiri::HTML(open(profile_url))
 
+    student = {
+      profile_quote: profile_page.css('.profile-quote').text,
+      bio: profile_page.css('.bio-content .description-holder p').text
+    }
+
+    # Iterate through social icons and grab any links to twitter, linkedin
+    # github or their blog (rss).
+    profile_page.css('.social-icon-container a').each do |link|
+      link_href = link.attribute('href').value
+      link_img_src = link.css('img').attribute('src').value
+      if link_img_src[/twitter/]
+        student[:twitter] = link_href
+      elsif link_img_src[/linkedin/]
+        student[:linkedin] = link_href
+      elsif link_img_src[/github/]
+        student[:github] = link_href
+      elsif link_img_src[/rss/]
+        student[:blog] = link_href
+      end
+    end
+    student
   end
 
 end
