02412fcdcf40f0e0f6bb277fd156d6bb30922df3
gnfisher <me@gnfisher.com>
2016-03-21 15:27:47 -0300

    scrape_index_page passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css('.roster-cards-container .student-card').each do |student|
+      students << {
+        name: student.css('a .card-text-container h4.student-name').text,
+        location: student.css('a .card-text-container p.student-location').text,
+        profile_url: "http://127.0.0.1:4000/#{student.css('a').attribute('href').value}"
+      }
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
