abec71a0f2caec6e038485668630b04f01d91683
LonghornRach <LonghornRach@users.noreply.github.com>
2016-03-25 16:50:49 -0500

    revised main scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,19 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    site = Nokogiri::HTML(index_url)
+    site = Nokogiri::HTML(open(index_url))
+    binding.pry
     student_arr = []
 
     all_students = site.css("div.roster-cards-container")
+    binding.pry
     students = all_students.css("div.student-card")
     students.each do |student|
+      binding.pry
       profile = {
         :name => student.css("div.card-text-container h4.student-name").text,
         :location => student.css("div.card-text-container p.student-location").text,
-        :profile_url => student.css("a").attr('href').value
+        :profile_url => (index_url + student.css("a").attr('href').value)
         }
       student_arr << profile
     end
@@ -34,5 +37,5 @@ end
 #:profile_url: student.css('a').attr('href').value
 
 # index_url = File.read('fixtures/student-site/_site/index.html')
-# scraped_students = Scraper.scrape_index_page("https://0.0.0.0:4000")
+# scraped_students = Scraper.scrape_index_page('http://0.0.0.0:4000')
 # binding.pry
