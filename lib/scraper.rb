ac04650b244af7262569ba756c3f8270b05a7495
ldgarber <leah.garber88@gmail.com>
2016-03-25 10:06:38 -0400

    student page scraper test passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,18 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     @doc = Nokogiri::HTML(open("#{index_url}"))    
+    @students = []
+    @student_html = @doc.css("div.student-card")
+    @student_html.each do |student|
+      @student_hash ={}
+      @student_hash[:name] = student.css(".student-name").text
+      @student_hash[:location] = student.css(".student-location").text
+      base_url = "http://127.0.0.1:4000/"
+      student_url = student.css("a").map{|link| link['href']}.first.to_s
+      @student_hash[:profile_url] = base_url + student_url
+      @students << @student_hash
+    end
+    @students
   end
 
   def self.scrape_profile_page(profile_url)
