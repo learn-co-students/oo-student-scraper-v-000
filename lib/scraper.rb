e83757e09067a2f2a0eb77668399540d3939e24e
jbok4 <jbok4@aol.com>
2016-03-26 15:33:25 -0400

    scraper done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,25 +4,38 @@ require 'nokogiri'
 
 class Scraper
 
-
-
-  def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
-    students = []
-    doc.css(".roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-      name = student.css(".student-name").text.strip
-      location = student.css(".student-location").text.strip
-      link = "http://127.0.0.1:4000/#{student.attr('href')}"
-      students << {name: name, location: location, profile_url: link}
-      end
+ def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css('.roster-cards-container .student-card')
+    student_array = []
+    students.each do |student|
+      student_array << {name: student.css('h4').text, location: student.css('p').text, profile_url: "#{index_url}#{student.css('a')[0]['href']}"}
     end
-    students
+    student_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    profiles = doc.css('.vitals-container .social-icon-container')
+    profile_hash = {}
+    profiles.css('a').each do |profile|
+      if profile['href'].include?('twitter')
+        profile_hash[:twitter] = profile['href']
+      elsif profile['href'].include?('linkedin')
+        profile_hash[:linkedin] = profile['href']
+      elsif profile['href'].include?('github')
+        profile_hash[:github] = profile['href']
+      else
+        profile_hash[:blog] = profile['href']
+      end
+    end
+      profile_hash[:profile_quote] = doc.css('.vitals-container .vitals-text-container .profile-quote').text.strip
+      profile_hash[:bio] = doc.css('.description-holder p').text.strip
+      profile_hash
   end
+
 
 
 end
