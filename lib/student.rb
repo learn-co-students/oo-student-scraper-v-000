4c8bf8e3c1ee190c64d0da142f710faa1f739aa7
jbok4 <jbok4@aol.com>
2016-03-28 20:09:18 -0400

    tests passing

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(student_array)
+    student_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
