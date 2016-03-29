87b0e973a510be647f4ecd28c9e3725b3c6af6e9
Joe <user@Users-Computer-5.local>
2016-03-28 23:00:37 -0400

    updated scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,50 @@
 require 'open-uri'
-require 'pry'
+require 'nokogiri'
+#require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
+    Nokogiri::HTML(open(index_url)).css(".student-card").collect do |chunk|
+
+      {name: chunk.css("h4").text,
+      location: chunk.css("p").text,
+      profile_url: "http://127.0.0.1:4000/#{chunk.css("a").attribute("href").value}"}
+    end
   end
+
+#Scraper.scrape_index_page("http://127.0.0.1:4000") 
+
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+   
+    doc = Nokogiri::HTML(open(profile_url))
+
+    chunk = doc.css(".vitals-container .social-icon-container")
+    student = {}
+
+    chunk.css("a").each do |chunk|
+
+      if chunk["href"].include?("twitter")
+        student[:twitter] = chunk["href"]
+      elsif chunk["href"].include?("linkedin")
+        student[:linkedin] = chunk["href"]
+      elsif chunk["href"].include?("github")
+        student[:github] = chunk["href"]
+      else
+        student[:blog] = chunk["href"]
+      end
+    end
+      student[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text.gsub(/\s+/, " ")
+      student[:bio] = doc.css(".description-holder p").text.gsub(/\s+/, " ")
+      student
+    end
 
 end
+
+
+#Scraper.scrape_profile_page("http://127.0.0.1:4000/students/eric-chu.html")
+
+
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,18 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    self.send("name=", self[:name])
+    self.send("location=", self[:location])
+    self.send("profile_url=", self[:profile_url])
+
+    @@all << self
     
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |e|
+      e = Student.new
+    end   
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +24,25 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
+
 end
 
+
+#Student.create_from_collection(Scraper.scrape_index_page("http://127.0.0.1:4000"))
+
+
+=begin
+  
+rescue Exception => e
+  
+end
+
+    student = Student.new
+    students_array.each do |student|
+      student.send("name=", student[:name])
+      student.send("location=", student[:location])
+      student.send("profile_url=", student[:profile_url])
+
+=end      
