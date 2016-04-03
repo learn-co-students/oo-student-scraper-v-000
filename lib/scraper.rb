a6a256aa7154e8a61c776caba4cfa03493d96439
McN33ly <rmcneely23@gmail.com>
2016-04-03 12:48:36 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,26 +6,36 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-      index_url = "http://127.0.0.1:4000/"
-      doc = Nokogiri::HTML(open(index_url))
-      binding.pry
-    #div .student-card
-    # => .student-name text
-    # =>  .student-location text
-    # => .student-card a .attribute('href').value
+    index_url = "http://127.0.0.1:4000/"
+    doc = Nokogiri::HTML(open(index_url))
     student_index_array = []
     doc.css(".student-card").each do |student_card|
-  student_index_array = [{
-    :name => doc.css(".student-card .student-name").text,
-    :location => doc.css(".student-card .student-location").text,
-    :profile_url => doc.css(".student-card a").attribute("href").value
-  }  ]
+      student_index_array <<
+        {:name => student_card.css(".student-name").text,
+          :location => student_card.css(".student-location").text,
+          :profile_url => index_url+(student_card.css("a").attribute("href").value)
+        }
     end
     student_index_array
   end
 
   def self.scrape_profile_page(profile_url)
-
+    student_profile = Nokogiri::HTML(open(profile_url))
+    student_info = {}
+    student = []
+    student_profile.css(".main-wrapper").each do |div|
+      student = student_profile.css("a").collect {|link| href = link.attribute("href").value  }
+      student.delete_if{|link| "../" == link }
+            student_info[:twitter] = student.grep(/twitter/)[0]
+              student.delete_if{|link| student_info[:twitter] == link }
+            student_info[:linkedin] = student.grep(/linkedin/)[0]
+              student.delete_if{|link| student_info[:linkedin] == link }
+            student_info[:github] = student.grep(/github/)[0]
+              student.delete_if{|link| student_info[:github] == link }
+            student_info[:blog] = student.last
+            student_info[:profile_quote] = div.css(".profile-quote").text
+            student_info[:bio] = div.css(".description-holder p").text
+          end
+student_info.reject{|key, value| value == nil}
   end
-#binding.pry
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,26 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(hash)
+    hash.each {|key, value| self.send(("#{key}="), value) }
+    @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(student_index_array)
+    student_index_array.each do |hash|
+      Student.new(hash)
+    end
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(student_info)
+    student_info.each {|key, value| self.send(("#{key}="), value) }
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
-
