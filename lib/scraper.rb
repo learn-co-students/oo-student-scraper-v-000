f13eb02f2e91d1daeb3429020027fb8040a1fb5d
tddycks <tadzio.smith@gmail.com>
2016-04-06 12:23:40 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,30 +4,46 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
-      #This is a class method that should take in an argument of the URL of the index page. 
-    #It should use nokogiri and Open-URI to access that page. The return value of this method 
-    #should be an array of hashes in which each hash represents a single student. 
-    #http://127.0.0.1:4000/
-    doc = Nokogiri::HTML(open("http://127.0.0.1:4000/"))
-
-    students = {}
-
-    #The keys of the individual student hashes should be :name, :location and :profile_url
-    #:name = (".roster-cards-container.card-text-container h4.student-name").text
-    #:location = .card-text-container p.student-location).text
-    #profile_url = roster-cards-container.student-card).attribute("src").value
-    name = doc.css(".roster-cards-container.student-card").first.css(".card-text-container h4.student-name").text
-
-    binding.pry
-    
-   
-  
-
+    doc = Nokogiri::HTML(open(index_url))
+    #the first student's info is:
+    #name: doc.css(".student-card").first.css(".student-name").text
+    #location: doc.css(".student-card").first.css(".student-location").text
+    #url: doc.css(".student-card a").first.attribute("href").value
+    doc.css(".student-card").collect do |student|
+        {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => index_url + student.css("a").attribute("href").value
+         }
+     end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    #The return value of this method should be a hash in which the key/value pairs describe an individual 
+    #student. Some students don't have a twitter or some other social link. 
+    profile = {}
+    #iterate through social-icon-container class to input social links if they exist
+    #twitter = doc.css(".social-icon-container a").first.attribute("href").value
+    doc.css(".social-icon-container a").each do |info| 
+  
+      image = info.css("img").attribute("src").value
+
+        if image[/twitter/]
+          profile[:twitter] = info.attribute("href").value
+        elsif image[/linkedin/]
+          profile[:linkedin] = info.attribute("href").value
+        elsif image[/github/]
+          profile[:github] = info.attribute("href").value
+        elsif image[/rss/]
+          profile[:blog] = info.attribute("href").value
+        end
+      end
+
+      profile[:profile_quote] = doc.css(".profile-quote").text.strip
+      profile[:bio] = doc.css(".description-holder p").text.strip
+
+      profile
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,32 @@
 class Student
 
+  #we don't want our Student model to be dependent on how it gets information regarding 
+  #the students it creates
+
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each{|key, value| self.send("#{key}=", value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
+    #The #create_from_collection method should iterate over the array of hashes and create 
+    #a new individual student using each hash
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
     
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send("#{key}=", value)}
   end
 
   def self.all
-    
+    @@all
   end
 end
 
