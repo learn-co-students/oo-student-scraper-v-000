f7583adcebc895080cb8f82ef8890c6ca84d7eb8
David Ward <david_ward@Users-MacBook-Pro.local>
2016-04-07 23:02:09 -0400

    worked on scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,26 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+
+    doc.css("div.student-card").collect do |student|
+      {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => "http://127.0.0.1:4000/" + student.css("a").attribute("href").value
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = Nokogiri::HTML(open(profile_url))
+
+
+
   end
 
 end
-
