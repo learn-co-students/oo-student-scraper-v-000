8ef1d3578e77f7b0df907fbbab98b80118d96675
adriandave13 <adrian@paradoxlogic.net>
2016-04-08 17:18:13 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,37 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = []
+    page.css("div.roster-cards-container").each do |cards|
+      cards.css(".student-card a").each do |student|
+        url = "http://127.0.0.1:4000/#{student.attr('href')}"
+        location = student.css('.student-location').text
+        name = student.css('.student-name').text
+        students << {name: name, location: location, profile_url: url}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    page = Nokogiri::HTML(open(profile_url))
+    links = page.css(".social-icon-container").css("a").map {|link| link.attr('href')}
+    links.each do |link|
+      if link.include?("github")
+        student[:github] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+    student[:profile_quote] = page.css(".profile-quote").text if page.css(".profile-quote")
+    student[:bio] = page.css("div.bio-content.content-holder div.description-holder p").text if page.css("div.bio-content.content-holder div.description-holder p")
+    student
   end
-
 end
 
