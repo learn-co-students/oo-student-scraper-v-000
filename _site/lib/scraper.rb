1039fc90a852cc68ffbb0a0541b49517a5c901e0
johnsontaiwo <josseytee@gmail.com>
2016-04-27 19:34:08 -0400

    Done.

--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -4,8 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = NOKOGIRI::HTML(index_url)
+    students = []
+   index_page = NOKOGIRI::HTML(open(index_url))
+   students_idex_page = index_page.css(".roaster-cards-container")
+   students_idex_page.each do |student|
     binding.pry
+
   end
 
   def self.scrape_profile_page(profile_url)
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,49 @@
 require 'open-uri'
 require 'pry'
-
-class Scraper
+require 'nokogiri'
+ 
+ class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+  doc = Nokogiri::HTML(open(index_url))
+   students = []
+   
+   
+      doc.css(".student-card").each do |student|
+        name = student.css(".student-name").text
+        location = student.css(".student-location").text
+        profile_url = "#{index_url}#{student.css("a").attribute("href").value}"
+       
+    students << {:name => name, :location => location, :profile_url => profile_url}
+   
+     
+    end
+    students
+     
+ end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+     students = {}
+   doc.css(".social-icon-container a").each do |student_url|
     
-  end
+      
+        if student_url["href"].match("twitter")
+          students[:twitter] = student_url["href"]
+         
+          elsif student_url["href"].match("linkedin")
+          students[:linkedin] = student_url["href"]
+          elsif student_url["href"].match("github")
+          students[:github] = student_url["href"]
+        elsif student_url["href"].match("http")
+           students[:blog] = student_url["href"]
+         
 
+         end
+       students[:profile_quote] = doc.css(".profile-quote").text
+       students[:bio] = doc.css(".description-holder p").text
+     end
+     students
+  end
 end
 
