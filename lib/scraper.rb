35c6f8f0669cb10e942ba716a2f763892f7aa3d5
Christian Whitesides <whitesidescr@gmail.com>
2016-05-20 15:09:50 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+
+    students = doc.css("div.student-card").map.with_index do |student, i|
+      name = doc.css("div.student-card div.card-text-container h4.student-name")[i].text
+      location = doc.css("div.student-card div.card-text-container p.student-location")[i].text
+      url = index_url + doc.css("div.student-card a")[i]["href"]
+
+      {:name => name, :location => location, :profile_url => url}
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
