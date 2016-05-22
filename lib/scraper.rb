3bcaef95e0937ec20e570ff268ed34c372fa8eb1
Xiao Ling Zhu <xiaoeyzhu@gmail.com>
2016-05-22 16:12:17 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #responsible for scraping the index page that lists all of the students
+    doc :nokogiri::HTML(open("#{index_url}"))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    #responsible for scraping an individual student's profile page to get further information about that student
+    doc :nokogiri::HTML(open("#{index_url}"))
+    binding.pry
   end
 
 end
-
