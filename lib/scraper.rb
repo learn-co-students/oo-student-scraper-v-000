34db9ebe9f41cb12c52f8c71ba02abfa945c6d78
ebanh <ebanhernandez@gmail.com>
2016-05-25 12:01:20 +0900

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,43 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    page = Nokogiri::HTML(open(index_url))
+
+    page.css(".roster-cards-container").each do |card|
+      card.css(".student-card").each do |student|
+        student_profile_link = "http://127.0.0.1:4000/#{student.css("a")[0]['href']}"
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    page = Nokogiri::HTML(open(profile_url))
+
+    links = page.css(".social-icon-container a").map { |n| n.attribute('href').value }
+
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+    student[:profile_quote] = page.css(".profile-quote").text if page.css(".profile-quote")
+    student[:bio] = page.css(".description-holder p").text if page.css(".description-holder p")
+    student
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,55 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
+  # def send(student_hash)
+  #   student_hash.each do |key, value|
+  #     # binding.pry
+  #     if key == :name
+  #       @name = value
+  #     elsif key == :location
+  #       @location = value
+  #     elsif key == :twitter
+  #       @twitter = value
+  #     elsif key == :linkedin
+  #       @linkedin = value
+  #     elsif key == :github
+  #       @github = value
+  #     elsif key == :blog
+  #       @blog = value
+  #     elsif key == :profile_quote
+  #       @profile_quote = value
+  #     elsif key == :bio
+  #       @bio = value
+  #     elsif key == :profile_quote
+  #       @profile_url = value
+  #     end
+  #   end
+  # end
+
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
-
