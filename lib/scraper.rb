da1f0839c314f486c89987f7d9bca86bcd351294
lizdenhup <lizdenhup@gmail.com>
2016-05-26 14:00:09 -0500

    404 open-uri error

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,21 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css('div.student-card').each do |card|
+    name = card.css('.student-name').text
+    profile_url = index_url + '/' + card.css('a').attribute('href').value
+    location =  card.css('p.student-location').text
+    student = {name: name, location: location, profile_url: profile_url}
+    students << student 
+    end 
+    students
+#   binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +24,4 @@ class Scraper
 
 end
 
+# Scraper.scrape_index_page("http://localhost:4000")
--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
@@ -17,7 +17,7 @@ class Student
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
