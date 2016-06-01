99fd9202d3a578e339bf85cebd9b75675c293f45
Savannah Scott <sascott624@gmail.com>
2016-06-01 14:35:00 -0400

    Done.

--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -7,8 +7,14 @@ class Scraper
   # :location
   # :profile_url
 
+
+
   def self.scrape_index_page(index_url)
+    scraped_students = []
     student_index = Nokogiri::HTML(open(index_url))
+    student_index.css("div.roster-cards-container").each do |card|
+      card.css("div.student-card").each do |student|
+        student.css("")
 
   end
 
