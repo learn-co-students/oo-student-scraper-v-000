267f9901583d682b5557922c9ed9f79a4116ceb9
Savannah Scott <sascott624@gmail.com>
2016-06-02 08:49:57 -0400

    all tests passing

--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
 #   card.css('div.card-text-container').each do |student|       
 #     puts student.css('h4.student-name').text
 #     end
-#   end  =====> Ryan Johnson etc...
+#   end  =====> New York, NY etc...
 
 
 #------- location ------->
@@ -29,23 +29,53 @@ class Scraper
 #     end
 #   end  =====> Ryan Johnson etc...
 
+#student_url = student_index.css('.student-card a')[1]['href'] 
+#   => "students/eric-chu.html" 
+
   def self.scrape_index_page(index_url)
     scraped_students = []
     student_index = Nokogiri::HTML(open(index_url))
-    binding.pry
-#    student_index.css("div.roster-cards-container").each do |card|
-#      card.css("div.student-card a").each do |entry|
-#        entry.css("div.card-text-container").map do |student|
-#          student_name = student.css("h4.student-name").text
-#          student_location = student.css("p.student-location").text
-#          scraped_students << {name: student_name, location: student_location}
-#        end
-#      end
+    student_index.css("div.roster-cards-container").each do |card|
+      card.css('.student-card a').each do |student|
+        student_url = "http://127.0.0.1:4000/#{student['href']}"
+        card.css("div.card-text-container").each do |student|
+          student_name = student.css("h4.student-name").text
+          student_location = student.css("p.student-location").text
+          scraped_students << {name: student_name, location: student_location, profile_url: student_url}
+        end
+      end
+    end
     scraped_students
-  end
+  end  #------ scrape index page -------->
 
   def self.scrape_profile_page(profile_url)
+    scraped_student = {}
+    social_links = []
+    student_profile = Nokogiri::HTML(open(profile_url))
 
-  end
+    student_profile.css('.social-icon-container a').each do |social|
+      social_links << social['href']
+    end
+
+    social_links.each do |site|
+      if site.include?("twitter")
+        scraped_student[:twitter] = site
+      elsif site.include?("linkedin")
+        scraped_student[:linkedin] = site
+      elsif site.include?("github")
+        scraped_student[:github] = site
+      else
+        scraped_student[:blog] = site
+      end
+    end
+
+    scraped_student[:profile_quote] = student_profile.css('.profile-quote').text
+    scraped_student[:bio] = student_profile.css('.description-holder p').text
+
+    scraped_student
+
+  end #-------- scrape profile page ---------->
+
+
 
 end
