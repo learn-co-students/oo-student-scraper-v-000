27b83dde01e8c13f9c76f19d2dca77a0657b1659
interestinall <justin.lefurjah@gmail.com>
2016-06-05 14:14:07 -0500

    make scraper class methods work

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    sh = student_hash
+    @name = sh[:name]
+    @location = sh[:location]
+    @twitter = sh[:twitter]
+    @linkedin = sh[:linkedin]
+    @github = sh[:github]
+    @blog = sh[:blog]
+    @profile_quote = sh[:profile_quote]
+    @bio = sh[:bio]
+    @profile_url = sh[:profile_url] 
   end
 
   def self.create_from_collection(students_array)
