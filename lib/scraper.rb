6fc37e3c344b6f9ff87891ade35f73c345a5619a
kaileeagray <kaileeagray@gmail.com>
2016-06-06 19:27:12 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,15 +16,16 @@ end
   end
 
   def self.create_student_hash
+    all = []
     get_page.css("div.student-card").each do |student|
       student_hash = {}
-      Student.new(student_hash)
       student_hash[:name] = student.css("h4").text
       student_hash[:location] = student.css("p.student-location").text
       student_hash[:profile_url] = "http://127.0.0.1:4000/students/" + student.css("h4").text.downcase.gsub(" ", "-") + ".html"
       Student.new(student_hash)
+      all << student_hash
     end
-    Student.all
+    all
   end
 
   def self.scrape_profile_page(profile_url)
--- lib/student.rb
+++ lib/student.rb
@@ -5,18 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @@all << student_hash
-    binding.pry
     student_hash.each do |key,value|
+      @name = value if key.to_s == "name"
+      @location = value if key.to_s == "location"
+      @profile_url = value if key.to_s == "profile_url"
     end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |hash|
+      Student.new(hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key,value|
+      @twitter = value if key.to_s == "twitter"
+      @github = value if key.to_s == "github"
+      @bio = value if key.to_s == "bio"
+      @blog = value if key.to_s == "blog"
+      @profile_quote = value if key.to_s == "profile_quote"
+      @linkedin = value if key.to_s == "linkedin"
+    end
   end
 
   def self.all
