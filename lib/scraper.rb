789bf71ca494a0359f1d88e0144ea73ed97d176f
karuna24s <karuna24s@gmail.com>
2016-06-06 21:52:27 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,13 +7,24 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     scraped_students = []
-    doc.css("div.student-card").collect do |student|
-       {
-       :name => student.css("h4.student-name").text,
-       :location => student.css("p.student-location").text,
-       :profile_url => index_url + student.css("a").attribute("href").value
-       }
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "http://127.0.0.1:4000/#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        scraped_students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
     end
+    scraped_students
+    # doc = Nokogiri::HTML(open(index_url))
+    # scraped_students = []
+    # doc.css("div.student-card").collect do |student|
+    #    {
+    #    :name => student.css("h4.student-name").text,
+    #    :location => student.css("p.student-location").text,
+    #    :profile_url => index_url + student.css("a").attribute("href").value
+    #    }
+    # end
   end
 
   def self.scrape_profile_page(profile_url)
