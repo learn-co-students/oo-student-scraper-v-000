1d160b9146118d51867b0adbfcda6eaf2a72ee8b
Andrew Papacostas <a_papacostas@yahoo.com>
2016-06-27 22:25:11 +0000

    Done.

--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -10,21 +10,21 @@ class Scraper
     # binding.pry
     doc.css("div.roster-cards-container").each do |all_cards|
       all_cards.css(".student-card").each do |student|
-        student_url = "http://46.101.242.134:3202/#{student.attr("href")}"
+        student_url = "http://46.101.242.134:3202/#{student.css("a").attr("href").value}"
         student_name = student.css(".student-name").text
         student_location = student.css(".student-location").text
 
         scraped_students << {name: student_name, location: student_location, profile_url: student_url}
       end
     end
-    #scraped_students.map do |student|
-    #  puts student[:profile_url]
-
 
     scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
+    scraped_students = []
+    doc = Nokogiri::HTML(open())
+    binding.pry
 
   end
 
