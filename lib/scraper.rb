ed31250ba254b7c95b160e4f47379723810bc8a5
bmolina-nyc <bruce.d.molina@gmail.com>
2016-07-10 13:31:16 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,28 +4,88 @@ require 'pry'
 
 class Scraper
 
-  def scrape_index_page(index_url)
-    html = Nokogiri::HTML(open(index_url))
-
-    #other option 
-    #html = File.open(index_url)
-    #scraped_page = Nokogiri::HTML(html)
-
-    result = []
-    students = {} 
-
-    binding.pry
+  def self.scrape_index_page(index_url)
+    #html = Nokogiri::HTML(open(index_url))
+    html = File.open(index_url)
+    scraped_page = Nokogiri::HTML(html)
+    
+    results = []
+    
+    scraped_page.css(".roster-cards-container .student-card").each do |student|
+      student_hash = {
+        :name => student.css("h4").text,
+        :location => student.css("p").text,
+        :profile_url => "./fixtures/student-site/#{student.css("a").attribute("href").value}"
+      } 
+      results << student_hash
+    end
+    results
   end
 
   #name = html.css(".student-card").first.css("h4").text
   #location = html.css(".student-card").first.css("p").text
+  #url html.css(".student-card").first.css("a").attribute("href").value
+
 
   def self.scrape_profile_page(profile_url)
-    
+    html = File.open(profile_url)
+    scraped_page = Nokogiri::HTML(html)
+
+    #grab social media elements and add them to an array 
+    scraped_page_social_elements = []
+
+    scraped_page.css(".social-icon-container").css("a").each do |el|
+      scraped_page_social_elements <<  el.attribute("href").value
+    end
+
+    # start a hash - result asks for a hash 
+    student = {}
+
+    # iterate through the social elements array and create hash keys and values
+    # if the element string, contains the appropriate info, it becomes the value of a hash key
+    scraped_page_social_elements.each do |element|
+      student[:twitter] = element if element.include?("twit")
+      student[:linkedin] = element if element.include?("link")
+      student[:github] = element if element.include?("git")
+      student[:blog] = element if element.include?((scraped_page.css("div.vitals-container h1").text.downcase.split.first)) unless element.include?("link") 
+    end
+
+    #add the rest of the items 
+      student[:profile_quote] = scraped_page.css("div.profile-quote").text
+      student[:bio] = scraped_page.css("div.bio-content.content-holder div.description-holder p").text
+
+      student  #return the student hash
   end
 
 end
 
 
-Scraper.new.scrape_index_page("./fixtures/student-site/index.html")
+# notes from scraping 
+
+
+#div.vitals-container
+# :twitter = scraped_page.css(".social-icon-container a").attribute("href").value
+#scraped_page.css(".social-icon-container").css("a").each do |el|
+# puts  el.attribute("href").value 
+#end  
+
+
+# :linkedin = above comboed with twitter 
+# :github = above comboed with twitter 
+# :blog = above comboed with twitter 
+# :profile_quote = scraped_page.css("div.profile-quote").text
+
+#div.details-container
+# :bio = scraped_page.css("div.bio-content.content-holder div.description-holder").text
+# dont need but its the school - scraped_page.css("div.education-content.content-holder div.description-holder h4").text
+
+
+    #student = {
+     #   :twitter => scraped_page_social_elements[0] 
+      #  :linkedin => scraped_page_social_elements[1], 
+      #  :github => scraped_page_social_elements[2], 
+      #  :blog => scraped_page_social_elements[3]
+      #}
+
+# p Scraper.scrape_profile_page("./fixtures/student-site/students/david-kim.html")
 
