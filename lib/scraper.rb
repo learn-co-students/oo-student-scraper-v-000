32b08fb2d3f4010c40465892035c3c0af4c34fda
Jesse Novotny <jessenovotny@gmail.com>
2016-07-15 15:40:21 -0700

    Scraper.scrape_index_page seems to be working

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,21 @@ require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+    student_cards = Nokogiri::HTML(open(index_url)).css(".student-card")
+    students = []
+    student_cards.map do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile = student.css("a")[0]["href"]
+      students << {name: name, location: location, profile_url: profile}
+    end
+    students.flatten
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
   end
 
 end
