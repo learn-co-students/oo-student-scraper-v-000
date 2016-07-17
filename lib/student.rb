27b9f868db7c070c347552fe412f7d371ffcccab
Ben Norris <bennorrismisc@gmail.com>
2016-07-17 14:10:07 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,38 @@
+require 'pry'
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+    # @name = student_hash[:name]
+    # @location = student_hash[:location]
+    # @twitter = student_hash[:twitter]
+    # @linkedin = student_hash[:linkedin]
+    # @github = student_hash[:github]
+    # @blog = student_hash[:blog]
+    # @profile_quote = student_hash[:profile_quote]
+    # @bio = student_hash[:bio]
+    # @profile_url = student_hash[:profile_url]
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |x|
+      Student.new(x)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
-
