16ea027607276ebda92fafd957acedbbefab709f
Richard-Frost <frost.rchrd@gmail.com>
2016-07-18 17:56:25 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,41 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    scraped_students = []
+    student_doc = Nokogiri::HTML(open("#{index_url}"))
+    student_doc.css(".student-card").collect do |student|
+      scraped_students << {
+        name: student.search(".student-name").text,
+        location: student.search(".student-location").text,
+        profile_url: "./fixtures/student-site/#{student.css("a").attribute("href").text}"
+      }
+    end
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
+    scraped_student = {}
+    links = []
     
+    student_doc = Nokogiri::HTML(open("#{profile_url}"))
+    student_doc.css("div .social-icon-container a").each do |link|
+      links << link["href"]
+      
+      links.each do |social_link|
+        if social_link.include?("twitter")
+          scraped_student[:twitter] = social_link
+        elsif social_link.include?("linkedin")
+          scraped_student[:linkedin] = social_link
+        elsif social_link.include?("github")
+          scraped_student[:github] = social_link
+        else
+          scraped_student[:blog] = social_link
+        end
+        scraped_student[:profile_quote] = student_doc.css(".profile-quote").text
+        scraped_student[:bio] = student_doc.css("p").text
+      end 
+    end
+    scraped_student
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,34 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio]
+    @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+    self.new(student_hash)
+  end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attr_name, attr_value| 
+    self.send("#{attr_name}=", attr_value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
+
 end
 
