f5808e062957106dc015427292918205a9d4cba2
mmelmer <mikemelmer@gmail.com>
2016-07-27 18:09:11 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
+    @name = [:name]
+    @location = [:location]
     @@all << self
   end
 
