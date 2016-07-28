942d1a8248e0a927dc88a059d373b2441c126d21
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-28 23:15:19 +0000

    did the scrape_profile_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     # stores the HTML of the URL into a variable called html
     # take the string of HTML returned by open-uri's open method and convert it into a NodeSet (aka, a bunch of nested "nodes")
-    doc = Nokogiri::HTML(open("http://159.203.117.55:3537/fixtures/student-site/"))
+    doc = Nokogiri::HTML(open(index_url))
    # selector will allow us to grab index page that lists all of the students
     student_profile=[]
     hash={}
@@ -15,7 +15,7 @@ class Scraper
       student.css(".student-card a").each do |student_detail|
          hash={name: student_detail.css("h4.student-name").text,
                location: student_detail.css("p.student-location").text,
-               profile_url: "./fixtures/student-site/#{student_detail.attr('href')}"}
+               profile_url: "./fixtures/student-site/#{student_detail['href']}"}
               # binding.pry
          student_profile << hash
       end
@@ -25,26 +25,33 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     # take the string of HTML returned by open-uri's open method and convert it into a NodeSet (aka, a bunch of nested "nodes")
-    doc = Nokogiri::HTML(open("http://159.203.117.55:3537/fixtures/student-site/"))
+    doc = Nokogiri::HTML(open(profile_url))
     student_profile=[]
-    hash={}
-    doc.css(".roster-cards-container").each do |student|
-      student.css("div.student-card a").each do |url|
-        url
-    # 1. holds all the students profiles (div.roster-cards-container)
-    # 2. iterate over each student card equal to (div.student-card a)
-    # 3. student-profile-key for hash = after iterating take the href of each ("./fixtures/student-site/#{student_detail.attr('href')}")
-    # 4. we will be using the href from above to get all our keys which are equal to each students profile
-    # 5. div.main-wrapper profile -> vitals container -> social icon container-> hrefs
+    social_hash={}
 
-    # index = doc.css(".roster-cards-container").each do |student|
-    #   student.css(".social-icon-container a")["href"]
+    doc.css(".main-wrapper profile").each do |social|
 
-         binding.pry
-        #  student_profile << hash
-      end
-    end
-    # hash
+      social.css(".social-icon-container").detect do |url|
+        if url.include?( "twitter")
+          social_hash[:twitter] = "#{url['href']}"
+
+        elsif url.include?("linkedin")
+          social_hash[:linkedin] = "#{url['href']}"
+
+        elsif url.include?("github")
+          social_hash[:github] = "#{url['href']}"
+
+        else social_hash[:blog] = "#{url['href']}"
+
+        end
+      end # end of second enumurable
+
+      social_hash[:bio] = social.css("p")
+      social_hash[:profile_quote] = social.css(".profile-quote")
+      binding.pry
+
+    end # end of first enumerable
+    social_hash
   end
 
 end
