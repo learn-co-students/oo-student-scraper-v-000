de26a4f9841de8e2d91a9f6e6cbbc814dbf8e5f9
SnowboardTechie <bryan.thompson518@gmail.com>
2016-08-09 15:21:29 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+
+    end
   end
 
   def add_student_attributes(attributes_hash)
