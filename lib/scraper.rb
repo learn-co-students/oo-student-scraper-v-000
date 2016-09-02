9ac2065b3c9af1d4bbd51d6194b71c8bef10b928
Kenlyn Terai <kkterai@gmail.com>
2016-09-02 03:52:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,20 +12,19 @@ class Scraper
 
     doc = Nokogiri::HTML(open(index_url))
 
-    students = {}
+    students = []
 
     doc.css(".student-card").each do |student|
-      students = [
+      students << {
         :name => student.css(".student-name").text,
         :location => student.css(".student-location").text,
         :profile_url => student.css("a").attr("href").value
-        ]
+        }
       end
     #responsible for scraping the index page that lists all of the students
 
     #This is a class method that should take in an argument of the URL of the index page. It should use nokogiri and Open-URI to access that page. The return value of this method should be an array of hashes in which each hash represents a single student. The keys of the individual student hashes should be `:name`, `:location` and `:profile_url`.
     students
-    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
