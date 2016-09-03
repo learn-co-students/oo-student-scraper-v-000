d949bed464466e1137e570b7bf338856f01ef52b
Kenlyn Terai <kkterai@gmail.com>
2016-09-03 22:12:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,9 @@ require 'pry'
 
 class Scraper
 
-  attr_accessor :name , :location , :profile_url
+  attr_accessor :name , :location , :profile_url, :twitter, :linkedin, :github, :blog, :profile_quote, :bio
 
-  def self.scrape_index_page(index_url) #= "/fixtures/student-site/index.html")
-    #html = File.read(index_url)
-    #doc = Nokogiri::HTML(html)
+  def self.scrape_index_page(index_url)
 
     doc = Nokogiri::HTML(open(index_url))
 
@@ -18,7 +16,7 @@ class Scraper
       students << {
         :name => student.css(".student-name").text,
         :location => student.css(".student-location").text,
-        :profile_url => student.css("a").attr("href").value
+        :profile_url => "./fixtures/student-site/#{student.css("a").attr("href").value}"
         }
       end
     #responsible for scraping the index page that lists all of the students
@@ -27,13 +25,28 @@ class Scraper
     students
   end
 
-  def self.scrape_profile_page(profile_url)
-    #is responsible for scraping an individual student's profile page to get further information about that student.
-
-    #This is a class method that should take in an argument of a student's profile URL. It should use nokogiri and Open-URI to access that page. The return value of this method should be a hash in which the key/value pairs describe an individual student. Some students don't have a twitter or some other social link. Be sure to be able to handle that (mass assignment!). Here is what the hash should look like: #example#
-
-    #The only attributes you need to scrape from a student's profile page are the ones listed above: twitter url, linkedin url, github url, blog url, profile quote, and bio. The hash you build using those attributes should be formatted like the one in the example above.
-
+  def attribute(attribute)
+    attribute.each {|key, value| self.send(("#{key}="), value)}
+    attribute
   end
 
+  def self.scrape_profile_page(profile_url)
+
+    doc = Nokogiri::HTML(open(profile_url))
+
+    attributes_hash = {:twitter => nil, :linkedin => nil, :github => nil, :blog => nil, :profile_quote => nil, :bio => nil}
+
+    doc.css("body").each do |profile|
+      attributes_hash = {
+        :twitter => profile.css("[href*=twitter]").attr("href").value,
+        :linkedin => profile.css("[href*=linkedin]").attr("href").value,
+        :github => profile.css("[href*=github]").attr("href").value,
+        :blog => profile.css(".social-icon-container a")[3].attr("href"),
+        :profile_quote => profile.css("div.profile-quote").text,
+        :bio => profile.css(".description-holder p").text
+      }
+      end
+    #attribute(student)
+    attributes_hash
+  end
 end
