03bfc629936ed15ea3384a77ef36695a111ea044
Kenlyn Terai <kkterai@gmail.com>
2016-09-13 03:52:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,26 +19,29 @@ class Scraper
         :profile_url => "./fixtures/student-site/#{student.css("a").attr("href").value}"
         }
       end
-    #responsible for scraping the index page that lists all of the students
-
-    #This is a class method that should take in an argument of the URL of the index page. It should use nokogiri and Open-URI to access that page. The return value of this method should be an array of hashes in which each hash represents a single student. The keys of the individual student hashes should be `:name`, `:location` and `:profile_url`.
     students
   end
 
-  def self.scrape_profile_page(profile_url)
+def self.scrape_profile_page(profile_url)
+    attributes_hash = {}
     doc = Nokogiri::HTML(open(profile_url))
 
-    attributes_hash = {}
-    
-    attributes_hash = {}.tap do |hash|
-      hash[:twitter] = doc.css("[href*=twitter]").attr("href").value unless doc.css("[href*=twitter]").empty?
-      hash[:linkedin] = doc.css("[href*=linkedin]").attr("href").value unless :linkedin == "" || nil
-      hash[:github] = doc.css("[href*=github]").attr("href").value unless :github == "" || nil
-      hash[:blog] = doc.css(".social-icon-container a")[3].attr("href") unless doc.css(".social-icon-container a")[3].nil?
-      hash[:profile_quote] = doc.css("div.profile-quote").text unless :profile_quote == "" || nil
-      hash[:bio] = doc.css(".description-holder p").text unless :bio == "" || nil
-    end
+    attributes_hash[:profile_quote]= doc.css("div.profile-quote").text unless :profile_quote == "" || nil
+    attributes_hash[:bio]= doc.css(".description-holder p").text unless :bio == "" || nil
 
+    links = doc.css(".social-icon-container").children.css("a").map { |el| el['href']}
+
+    links.each do |link|
+      if link.include? ("twitter")
+        attributes_hash[:twitter]= link
+      elsif link.include? ("linkedin")
+        attributes_hash[:linkedin]= link
+      elsif link.include?("github")
+        attributes_hash[:github]= link
+      elsif link.include?(/.*/)
+        attributes_hash[:blog]= link
+      end
+    end
     attributes_hash
   end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,5 @@
 class Student
 
-  #In this lab, you'll be scraping your Learn.co student website. You'll use the index page to grab a list of current students and instantiate a series of `Student` objects. You'll scrape the individual profile pages of each student to add attributes to each individual student.
-
-  #The student class will use the information returned by the above methods from our `Scraper` class in order to create students and add attributes to individual students. However, the `Student` class shouldn't know about the `Scraper` class. This means that the `Student` class shouldn't directly interact with the `Scraper` class––it shouldn't call on the `Scraper` class in any of its methods or take in the `Scraper` class itself as an argument.
-
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
@@ -13,23 +9,13 @@ class Student
     @@all << self
   end
 
-  def self.create_from_collection(index_url = BASE_PATH + 'index.html')
-      students = self.scrape_index_page(index_url)
-      student = self.new(students)
-    #This class method should take in an array of hashes. In fact, we will call `Student.create_from_collection` with the return value of the `Scraper.scrape_index_page` method as the argument. The `#create_from_collection` method should iterate over the array of hashes and create a new individual student using each hash. This brings us to the `#initialize` method on our `Student` class.
-
+  def self.create_from_collection(students)
+    students.each {|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-
-    attributes_hash= self.scrape_profile_page(profile_url)
     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
     self
-    #This instance method should take in a hash whose key/value pairs describe additional attributes of an individual student. In fact, we will be calling `student.add_student_attributes` with the return value of the `Scraper.scrape_profile_page` method as the argument.
-
-    #The `#add_student_attributes` method should iterate over the given hash and use metaprogramming to dynamically assign the student attributes and values in accordance with the key/value pairs of the hash. Use the `#send` method to achieve this.
-
-    #**Important:** The return value of this method should be the student itself. Use the `self` keyword.
   end
 
   def self.all
