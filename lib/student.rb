c7f5adb2ae4536061a77db84b57f1f772edbc388
Scott Savage <snsavage@gmail.com>
2016-09-14 16:27:00 -0400

    Passing OO Student Srcaper Lab.

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,22 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all
   end
 end
 
