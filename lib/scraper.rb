e2051e1faf5939dda97cd4bf3129f5fd7c31b3df
eleanorgraham <eleanorcjkgraham@gmail.com>
2016-09-19 15:21:35 -0400

    refactor #scrape-index, write #scrape_profile, add colorize gem to fix cli class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+require_relative "../lib/command_line_interface.rb"
 require 'open-uri'
 require 'pry'
 
@@ -5,17 +6,40 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
-    doc = Nokogiri::HTML(html)
-    students = doc.css(".student-card")
-    s = []
+    students = Nokogiri::HTML(html).css(".student-card")
+    students_array = []
     students.each do |student|
-      s << {:name => student.css(".card-text-container .student-name").text ,:location => student.css(".card-text-container .student-location").text,:profile_url => "./fixtures/student-site/"+student.css("a").first.attributes["href"].value}
+      students_array << {
+        :name => student.css(".card-text-container .student-name").text,
+        :location => student.css(".card-text-container .student-location").text,
+        :profile_url => CommandLineInteface::BASE_PATH+student.css("a").first.attributes["href"].value
+      }
     end
-    s
+    students_array
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    student = {}
+    social = doc.css(".social-icon-container a")
+    social.each do |s|
+      if s.attributes["href"].value.include? "twitter"
+        student[:twitter] = s.attributes["href"].value
+      elsif s.attributes["href"].value.include? "linkedin"
+        student[:linkedin] = s.attributes["href"].value
+      elsif s.attributes["href"].value.include? "github"
+        student[:github] = s.attributes["href"].value
+      elsif s.attributes["href"].value.include? "facebook"
+        student[:facebook] = s.attributes["href"].value
+      else
+        student[:blog] = s.attributes["href"].value
+      end
+    end
 
+    student[:profile_quote] = doc.css(".profile-quote").text
+    student[:bio] = doc.css(".bio-content .description-holder").text.strip
+    student
   end
 
 end
