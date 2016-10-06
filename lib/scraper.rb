bd939dfb9be838c1dad7f706ba77a91302e09fa3
Janine Rosen <janinemrosen@gmail.com>
2016-10-06 23:23:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_name = student.css("h4.student-name").text
+        student_location = student.css("p.student-location").text
+        student_profile_url = "./fixtures/student-site/#{student.attr('href')}"
+        students << {name: student_name, location: student_location, profile_url: student_profile_url}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
+    social_media_links = doc.css(".social-icon-container").children.css("a").collect {|link| link.attribute("href").value}
+
+    social_media_links.each do |link|
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else link.include?("blog")
+        student[:blog] = link
+      end
+    end
+
+    student[:profile_quote] = doc.css(".profile-quote").text
+    student[:bio] = doc.css(".description-holder p").text
+
+    student
+    # binding.pry
   end
 
 end
-
