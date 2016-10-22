49a8a5192d6787e52e56130126d0ca0d2b60a7fc
zszalas2 <zszalas@gmail.com>
2016-10-22 15:43:30 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,17 +7,31 @@ class Scraper
   attr_accessor :index_url, :profile_url
 
   def self.scrape_index_page(index_url)
+    binding.pry
     #responsible for scraping the index page that lists all of the students. This gets a list of all the profiles
-    doc = Nokogiri::HTML(open(index_url))    
-    Pry.start(binding)
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css(".student-card").each do |student|
+      students << {
+        :name => students.css("h4").children.each {|child| puts child},
+        :location => students.css("p.student-location").children.each {|child| puts child},
+        :profile_url => stud.css("div.roster-cards-container").each {|card| card.css(".student-card a").each {|student| puts student.attr("href")}}
+      }
+
+  
   end
 
 
 
   def self.scrape_profile_page(profile_url)
     #this scrapes the individual profiles
+
+    doc= Nokogiri::HTML(open(profile_url))
+    profiles = []
+
     
   end
 
+  end
 end
 
