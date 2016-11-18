aa02fa8e065dd17241a98c414cd05dd5ff735631
Justin  Gore <justin.gore@outlook.com>
2016-11-18 21:30:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,42 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    scraped_student = {}
 
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
+
+    profile.css(".vitals-container").each do |info|
+      if info.css(".social-icon-container a").attr("href").text.include?("twitter")
+        twitter = info.css(".social-icon-container a").attr("href").text
+        scraped_student[:twitter] = twitter
+      end
+
+      if info.css(".social-icon-container a").attr("href").text.include?("linkedin")
+        linkedin = info.css(".social-icon-container a").attr("href")
+        scraped_student[:linkedin] = linkedin
+      end
+
+      if info.css(".social-icon-container a").attr("href").text.include?("github")
+        github = info.css(".social-icon-container a").attr("href")
+        scraped_student[:github] = github
+      end
+
+      if info.css(".social-icon-container a").attr("href").text.include?("flatiron")
+        blog = info.css(".social-icon-container a").attr("href")
+        scraped_student[:blog] = blog
+      end
+
+      quote = info.css(".vitals-text-container .profile-quote").text
+      scraped_student[:profile_quote] = quote
+    end
+
+    profile.css(".details-container").each do |info|
+      bio = info.css(".description-holder p").text
+      scraped_student[:bio] = bio
+    end
+
+  scraped_student
   end
 
 end
