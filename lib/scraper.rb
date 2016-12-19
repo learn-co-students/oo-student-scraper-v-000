ddf49d657e12fd2bb45d09fa5d901511fc9efedf
vanita eckert <vanita112003@gmail.com>
2016-12-19 03:31:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,29 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+html = File.read(index_url)
+    learn = Nokogiri::HTML(html)
+     binding.pry
+
+
+  #   students = {}
+  #
+    #  students[:name] = learn.search(  learn.css(".student-name").first.text  ).text
+  # students[:location] = learn.search(  learn.css(".student-location").first.text  ).text
+  # students[:profile_url] = learn.search().text
+
   end
 
+
+
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+Scraper.scrape_index_page("./fixtures/student-site/index.html")
