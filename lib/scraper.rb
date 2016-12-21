f839ea8a4caed13cca65a4f2defe77050d2b8fb4
vanita eckert <vanita112003@gmail.com>
2016-12-21 04:02:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,31 +5,41 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-html = File.read(index_url)
+    html = File.read(index_url)
     learn = Nokogiri::HTML(html)
-     binding.pry
-# profile_url: what has been tried so far
-# learn.css("div.student-card a[href]") 
-# learn.css('div.student-card a').map{|link| link ['href']}  
-# learn.css("a[href]").map{|link| [link["href"]]} 
-# learn.css('div.heat a[href]').map { |link| link['href'] }
-#  learn.css('a[href]').map{|link| link['href']}  
 
 
-  #   students = {}
-  #
-    #  students[:name] = learn.search(  learn.css(".student-name").first.text  ).text
-  # students[:location] = learn.search(  learn.css(".student-location").first.text  ).text
-  # students[:profile_url] = learn.search(   ).text
+      scraper_array = []
+      scraper = {}
 
+      learn.css(".student-name").map do |each_name|
+        name = scraper[:name] = each_name.text
+      end
+
+      learn.css(".student-location").map do |each_location|
+        location = scraper[:location] = each_location.text
+      end
+
+      names = learn.css('a[href]').map{|link| link['href']}
+        names.map do |each_url|
+          url = scraper[:profile_url] = "./fixtures/student-site/" + each_url
+        end
+
+          scraper_array << scraper
+          # binding.pry
   end
 
 
-
   def self.scrape_profile_page(profile_url)
+    html_file = File.read(profile_url)
+    doc = Nokogiri::HTML(html_file)
 
+
+
+ # binding.pry
   end
 
 end
 
 Scraper.scrape_index_page("./fixtures/student-site/index.html")
+Scraper.scrape_profile_page("./fixtures/student-site/students/joe-burgess.html")
