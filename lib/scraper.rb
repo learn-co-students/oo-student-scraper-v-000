0fa52873338772bb96bb5bf4063481de7c8c83f8
M. Simon Borg <msimonborg@gmail.com>
2016-12-30 01:09:22 -0500

    pass first test

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,14 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    page.css('div.student-card').map do |s|
+      {
+          name:        s.css('.student-name').text,
+          location:    s.css('.student-location').text,
+          profile_url: "./fixtures/student-site/#{s.css('a').attribute('href').value}"
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +19,3 @@ class Scraper
   end
 
 end
-
