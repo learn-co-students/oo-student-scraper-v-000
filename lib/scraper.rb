00789cab9251fe03c691c4a2af4865c245888faf
M. Simon Borg <msimonborg@gmail.com>
2016-12-30 01:35:07 -0500

    scraper tests passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,28 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    page = Nokogiri::HTML(open(profile_url))
+    socials = page.css('.social-icon-container a')
+    quote = page.css('.profile-quote').text.strip
+    bio = page.css('.bio-content.content-holder').css('.description-holder').text.strip
+    profile = {}
+
+    socials.each do |s|
+      s = s.attribute('href').value
+      if s.match(/twitter/)
+        profile[:twitter] = s
+      elsif s.match(/linkedin/)
+        profile[:linkedin] = s
+      elsif s.match(/github/)
+        profile[:github] = s
+      else
+        profile[:blog] = s
+      end
+    end
+
+    profile[:profile_quote] = quote
+    profile[:bio]           = bio
+    profile
   end
 
 end
