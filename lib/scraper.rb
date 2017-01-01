98a020360439ea1a6cca0b61733186e5609e5a7e
vanita eckert <vanita112003@gmail.com>
2017-01-01 23:11:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,38 +10,59 @@ class Scraper
 
     scraper_array = []
 
-    learn.css(".student-card").map do |elements|
+    learn.css(".student-card").each do |elements|
 
       hash = {}
-
-
        hash[:name] = elements.css(".student-name").text
        hash[:location] = elements.css(".student-location").text
        hash[:profile_url] = "./fixtures/student-site/" + elements.css('a[href]').map{|link| link['href']}.join
 
-     scraper_array << hash
-scraper_array
-    #  binding.pry
+       scraper_array << hash
     end
- end
-
-
-      # names = learn.css('a[href]').map{|link| link['href']}
-      #   names.map do |each_url|
-      #     url = {:profile_url = "./fixtures/student-site/" + each_url}
-      #   end
-      #
-      #     scraper_array << scraper
+      scraper_array
+    end
 
 
   def self.scrape_profile_page(profile_url)
-    html_file = File.read(profile_url)
-    doc = Nokogiri::HTML(html_file)
+
+     doc = Nokogiri::HTML(open(profile_url))
+        url_hash = {}
+
+          doc.css(".main-wrapper").detect do |each_profile|
+
+            url = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}
+            url.each do |each_link|
+          # binding.pry
+          if each_link == url[0]
+            url_hash[:twitter] = url[0]
+            elsif
+              each_link == url[1]
+                url_hash[:linkedin] = url[1]
+              elsif
+                each_link == url[2]
+                  url_hash[:github] = url[2]
+                elsif
+                  each_link == url[3]
+                    url_hash[:blog] = url[3]
+          end
+        end
 
 
+            #  url_hash[:twitter] = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[0]
+            #  url_hash[:linkedin] = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[1]
+            #  url_hash[:github] = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[2]
+            #  url_hash[:blog] =  each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[3]
+
+             url_hash[:profile_quote] = each_profile.css(".vitals-text-container").css(".profile-quote").text
+             url_hash[:bio] = each_profile.css(".description-holder").css("p").text
+
+         end
+      url_hash
   end
+
+
 
 end
 
 Scraper.scrape_index_page("./fixtures/student-site/index.html")
-Scraper.scrape_profile_page("./fixtures/student-site/students/joe-burgess.html")
+Scraper.scrape_profile_page("./fixtures/student-site/index.html")
