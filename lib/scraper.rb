569bcde06c4d9778cced5d2f0b6858cda0bfa80c
vanita eckert <vanita112003@gmail.com>
2017-01-03 18:46:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = File.read(index_url)
-    learn = Nokogiri::HTML(html)
+    learn = Nokogiri::HTML(open(index_url))
 
     scraper_array = []
 
@@ -24,7 +23,6 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
-
      doc = Nokogiri::HTML(open(profile_url))
         url_hash = {}
 
@@ -32,26 +30,20 @@ class Scraper
 
             url = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}
               url.each do |each_link|
-            # binding.pry
-                if each_link == url[0]
-                  url_hash[:twitter] = url[0]
+                  if each_link.include?("twitter")
+                    url_hash[:twitter] = each_link
+
                   elsif
-                    each_link == url[1]
-                      url_hash[:linkedin] = url[1]
+                    each_link.include?("linkedin")
+                      url_hash[:linkedin] = each_link
+
                     elsif
-                      each_link == url[2]
-                        url_hash[:github] = url[2]
-                      elsif
-                        each_link == url[3]
-                          url_hash[:blog] = url[3]
+                      each_link.include?("github")
+                        url_hash[:github] = each_link
+                      else
+                        url_hash[:blog] = each_link
+                  end
               end
-        end
-
-
-            #  url_hash[:twitter] = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[0]
-            #  url_hash[:linkedin] = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[1]
-            #  url_hash[:github] = each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[2]
-            #  url_hash[:blog] =  each_profile.css(".social-icon-container").css('a[href]').map{|link| link['href']}[3]
 
              url_hash[:profile_quote] = each_profile.css(".vitals-text-container").css(".profile-quote").text
              url_hash[:bio] = each_profile.css(".description-holder").css("p").text
@@ -60,9 +52,8 @@ class Scraper
       url_hash
   end
 
-
-
 end
+
 
 Scraper.scrape_index_page("./fixtures/student-site/index.html")
 Scraper.scrape_profile_page("./fixtures/student-site/index.html")
