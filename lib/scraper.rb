d989a634a91aa613bf7b5e8cee050c5d8663900c
Brett Cline <btcline04@gmail.com>
2017-01-10 20:55:50 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,11 +21,24 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
-
+    student = {}
+    page = Nokogiri::HTML(open(profile_url))
+      page.css(".social-icon-container a").each do |p|
+        if p.attribute("href").text.include?("linkedin") then student[:linkedin] = p.attribute("href").text
+        elsif p.attribute("href").text.include?("twitter") then student[:twitter] = p.attribute("href").text
+        elsif p.attribute("href").text.include?("github") then student[:github] = p.attribute("href").text
+        else student[:blog] = p.attribute("href").text
+      end
+    end
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("p").text
+      student
   end
-
 end
 
-#student name: flatiron.css("h4.student-name").text
-#location: flatiron.css("p.student-location").text
-#profile_url: flatiron.css("a").attribute("href").text
+# iterate over .social-icon-container a
+# linkedin: a.attribute("href").text
+# github: a.attribute("href").text
+# blog: a.attribute("href").text
+# profile_quote: page.css(".profile-quote").text
+# bio: page.css("p").text
