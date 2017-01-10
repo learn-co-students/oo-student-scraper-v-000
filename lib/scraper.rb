5bbe4b3dee7037307a243f0cad60d9e3b3cf5417
Brett Cline <btcline04@gmail.com>
2017-01-10 16:20:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,21 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    flatiron = Nokogiri::HTML(open(index_url))
+
+    binding.pry
+
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+Scraper.scrape_index_page(index_url)
