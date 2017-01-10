4fea42af1d9973bf63e860c180516d3268336cb5
Brett Cline <btcline04@gmail.com>
2017-01-10 18:26:35 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,19 @@ require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
     flatiron = Nokogiri::HTML(open(index_url))
+      students = []
 
-    binding.pry
-
+      flatiron.css(".student-card").each do |f|
+        students << {
+          :name => f.css("h4.student-name").text,
+          :location => f.css("p.student-location").text,
+          :profile_url => f.css("a").attribute("href").text.prepend("./fixtures/student-site/")
+        }
+      end
+      students
   end
 
 
@@ -18,4 +26,6 @@ class Scraper
 
 end
 
-Scraper.scrape_index_page(index_url)
+#student name: flatiron.css("h4.student-name").text
+#location: flatiron.css("p.student-location").text
+#profile_url: flatiron.css("a").attribute("href").text
