a13e1543f5c91473de4db6bf9d42aa8c925ba9e1
David Lewitan <dovidlwtn@gmail.com>
2017-01-29 22:10:08 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,33 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = open(index_url)
+    html = Nokogiri::HTML(page)
+    arr = html.css('.student-card').collect do |student|
+      {
+        name: student.css('.student-name').text,
+        location: student.css('.student-location').text,
+        profile_url: './fixtures/student-site/' + student.css('a')[0]['href']
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    page = open(profile_url)
+    html = Nokogiri::HTML(page)
+    attributes = {}
+    #add URLs
+    html.css('.social-icon-container').css('a').each do |a|
+      str = a['href'].gsub("https://", "").gsub("www.", "").gsub("http://", "")
+      arr = str.split('.com')
+      str = ['twitter', 'linkedin', 'github'].index(arr[0]) != nil ? arr[0] : 'blog'
+      attributes[str.to_sym] = a['href']
+    end
+    #add text
+    attributes[:profile_quote] = html.css('.profile-quote').text
+    attributes[:bio] = html.css('.bio-content').css('p').text
+    #return
+    attributes
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    add_student_attributes(student_hash)
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    @name = attributes_hash[:name] if attributes_hash[:name]
+    @location = attributes_hash[:location] if attributes_hash[:location]
+    @twitter = attributes_hash[:twitter] if attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin] if attributes_hash[:linkedin]
+    @github = attributes_hash[:github] if attributes_hash[:github]
+    @blog = attributes_hash[:blog] if attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote] if attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio] if attributes_hash[:bio]
+    @profile_url = attributes_hash[:profile_url] if attributes_hash[:profile_url]
   end
 
   def self.all
-    
+    @@all
   end
 end
-
