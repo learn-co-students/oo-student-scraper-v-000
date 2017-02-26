c73b234fa559f8aca1128607eef857b9737905f5
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-02-26 08:47:23 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,30 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+    attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
-  @@all = []
+    @@all = []
 
-  def initialize(student_hash)
-    
-  end
+    def initialize(student_hash)
+        student_hash.each { |k,v|
+            send("#{k}=",v)
+            }
+        @@all << self
+    end
 
-  def self.create_from_collection(students_array)
-    
-  end
+    def self.create_from_collection(students_array)
+        students_array.each { |student|
+            s = Student.new(student)
+            }
+    end
 
-  def add_student_attributes(attributes_hash)
-    
-  end
+    def add_student_attributes(attributes_hash)
+      attributes_hash.each { |k,v|
+            send("#{k}=",v)
+            }
+    end
 
-  def self.all
-    
-  end
+    def self.all
+        @@all
+    end
 end
 
