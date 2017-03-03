67385936dd13cfe99f2737f2200acf56ee74b649
Daniel Lempesis <danlrcn@hotmail.com>
2017-03-03 21:36:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,25 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location] #binding.pry
+    @@all << self   #good lord I forgot all about this.
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student| Student.new(student) } #omg. forgot about class.new too. wow. need to get more than 3 hours of sleep D:
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(attributes_hash)     #binding.pry
+    attributes_hash.each {|personal, value| self.send(("#{personal}="), value)}
+    self #@linkedin = attributes_hash[:linkedin]
   end
 
   def self.all
-    
+    @@all
   end
 end
-
