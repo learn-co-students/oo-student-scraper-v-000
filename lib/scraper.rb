ed444dffba6eee1bf0aee65c1f4bc2308742b602
Annette  Drapalski <annette.drapalski@gmail.com>
2017-03-05 23:46:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,14 @@ require 'pry'
 
 class Scraper
 
+  # Scrapes the Learn student page for the names, locations and profile urls of each student
+  # taken from their student card
   def self.scrape_index_page(index_url)
-    page = Nokogiri::HTML(open(index_url))
+    page = Nokogiri::HTML(open(index_url))  # Nokogiri with Open-uri makes the http request to open the page
     scraped_students= []
-    student = {}
     site = index_url.split("index.html")
+    # For each student card collect the student names, locations and profile urls into
+    # a hash for each student. Then place each individual hash into the students array
     page.css("div.student-card a").each do |student|
       scraped_students << {:name => student.css("h4").text,
         :location => student.css("p").text, :profile_url => site[0] + student.attribute("href").value}
@@ -15,12 +18,19 @@ class Scraper
     scraped_students
   end
 
+  # Scrapes a students profile page for their atttibutes:
+  # Their twitter url, facebook url, github url, blog url, profile quote
+  # and biography info.
+  # Allows for a student not to have all the available social links on their profile
   def self.scrape_profile_page(profile_url)
     page = Nokogiri::HTML(open(profile_url))
     social = page.css("div.social-icon-container a")
     student = {}
     array = []
+    # Collect all the social links for the student
     array = social.collect { |a| a.attribute("href").value}
+    # Checks which social links a student has and assigns a key and
+    # value for their student hash
     array.each do |site|
       if site.include?("twitter")
         student[:twitter] = site
@@ -32,8 +42,9 @@ class Scraper
         student[:blog] = site
       end
     end
+    # Adds the key and value for the students profile quote and biography info
     student[:profile_quote] = page.css("div.profile-quote").text
-    student[:bio] = page.css("div.description-holder p").text 
+    student[:bio] = page.css("div.description-holder p").text
     student
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,22 +1,31 @@
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
+  # Array to contain all the students and their attributes hashes
   @@all = []
 
+  # Initialize a new student by being able to mass assign their
+  # attributes a key and value according to what the hash argument contains
   def initialize(student_hash)
     student_hash.each {|key,value| self.send(("#{key}="), value)}
-    @@all << self
+    @@all << self  # Adds each new student hash to the @@all array
   end
 
+  # Creates new student hashes from an array of students
+  # Calls on initialize to create the hashes
   def self.create_from_collection(students_array)
     students_array.each {|student| self.new(student)}
   end
 
-  def add_student_attributes(attributes_hash)  
+  # Adds a students attributes to their hash
+  # Uses mass assignment to only add the attributes a student has
+  def add_student_attributes(attributes_hash)
     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
+  # Allows the student array to be called
   def self.all
     @@all
   end
