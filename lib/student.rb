6a3bc53ec74481c7ceff8ce7c753acc9b99e0e9c
Scott Atkins <satkins16@gmail.com>
2017-03-14 01:45:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,34 @@
+require "pry"
+
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+      self.new(hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      if self.send("#{key}") == nil
+        self.send("#{key}=", value)
+      end
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
-
