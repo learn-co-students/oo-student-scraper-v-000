71c4eb9b972b8d04f6debb8ff1e5881e18f7136e
Scott Wakeman <swakeman@ca.rr.com>
2017-03-17 19:16:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,36 +27,37 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = File.read(profile_url)
     profile_page = Nokogiri::HTML(html)
-
+#array
     scraped_students = []
 
       profile_page.css('div.social-icon-container').each do |social|
+        #social.css('a').first[href].each do ||
 
-      binding.pry
-      end
-    end
-        #scraped_students << {
-          #:twitter => social.css('a')[0]['href']  or social.css('a').first['href'], => "https://twitter.com/jmburges"
-          #:linkedin => social.css('a')[1]['href'], => "https://www.linkedin.com/in/jmburges"
-          #:github => social.css('a')[2]['href'], => "https://github.com/jmburges"
+#hash
+        scraped_students << {
+          :twitter => social.css('a')[0]['href'],  #or social.css('a').first['href'], => "https://twitter.com/jmburges"
+          :linkedin => social.css('a')[1]['href'], #=> "https://www.linkedin.com/in/jmburges"
+          :github => social.css('a')[2]['href'], #=> "https://github.com/jmburges"
 
           #John Anthony Rivera
-          #  :blog => profile_page.css('.social-icon').attribute ('src').value,
+          :blog => profile_page.css('.social-icon').attribute('src').value,
                       #profile_page.css('.social-icon').attribute("src").value
                         #=> "../assets/img/twitter-icon.png"
           #<img class="social-icon" src="../assets/img/rss-icon.png">http://johnanthony-dev.com/blog/
           #<img class="social-icon" src="../assets/img/rss-icon.png"> http://joemburgess.com/
           #<img class="social-icon" src="../assets/img/rss-icon.png">
           #<img class="social-icon" src="../assets/img/rss-icon.png"> http://dannydawson.io/
-          #:profile_quote => profile_page.css('.profile-quote').text, => "\"Reduce to a previously solved problem\""
-          #  :bio => profile_page.css('.description-holder p').text
+          :profile_quote => profile_page.css('.profile-quote').text,# => "\"Reduce to a previously solved problem\""
+          :bio => profile_page.css('.description-holder p').text
 #=> "I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Ora
 #cle consultant for IBM for a bit and now I teach here at The Flatiron School."
 
-        #}
-
+        }
+      end
+    end
+    scraped_students
 end
 
 # output = Scraper.scrape_index_page('./fixtures/student-site/index.html')
 output = Scraper.scrape_profile_page("./fixtures/student-site/students/joe-burgess.html")
-binding.pry
+binding.pry 
