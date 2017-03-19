e79cd6d9b6a74e9157ea53574db7479fc8528783
Scott Wakeman <swakeman@ca.rr.com>
2017-03-19 22:39:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -19,8 +19,21 @@ class Student
 
   def add_student_attributes(attributes_hash)
     binding.pry
+#(attributes_hash)
+#=> {:twitter=>"someone@twitter.com",
+ #:linkedin=>"someone@linkedin.com",
+ #:github=>"someone@github.com",
+ #:blog=>"someone@blog.com",
+ #:profile_quote=>
+  #{}"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+ #:bio=>
+  #{}"I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and
+  #Tokyo for elementary/middle school."}
+    attributes_hash.each do |k,v|
+      self.send("#{k}=", v)
+    end
   end
-
+  
   def self.all
     @@all
   end
