1f3d39518199469f833ee7fc84bc3624eb6559e4
Seth <itz.saga@gmail.com>
2017-03-20 22:51:13 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card")
+    students.each do |student|
+      puts student.css(".student-name").text
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
