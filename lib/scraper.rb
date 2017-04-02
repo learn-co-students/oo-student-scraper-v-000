bb76ecf20c3d763cc86143faf6906e94f5f008ce
johnfelixespinosa <johnfelix.espinosa@icloud.com>
2017-04-02 01:23:54 -0400

    finished self.scrape_profile_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,9 +8,9 @@ class Scraper
     doc = Nokogiri::HTML(open(index_url))
     students = []
   
-    doc.css("div.roster-cards-container").each do |card|
+    doc.css('div.roster-cards-container').each do |card|
       students =
-      card.css(".student-card a").map do |student|
+      card.css('.student-card a').map do |student|
         {
           :name => student.css('.student-name').text,
           :location => student.css('.student-location').text,
@@ -22,8 +22,20 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    attributes = Hash.new
+
+    doc.css('.social-icon-container a').each do |social|
+      attributes[:twitter] = social['href'] if social['href'].include?('twitter') 
+      attributes[:linkedin] = social['href'] if social['href'].include?('linkedin') 
+      attributes[:github] = social['href'] if social['href'].include?('github') 
+      attributes[:blog] = social['href'] if 
+        (doc.css('.social-icon-container img').map do |image|
+          image['src'].include?('rss-icon')
+        end).include?(true) 
+    end
+      attributes[:bio] = doc.css('.details-container .description-holder p').text
+      attributes[:profile_quote] = doc.css('.profile-quote').text
+      attributes
   end
-
 end
-
