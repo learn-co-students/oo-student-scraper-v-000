c7829f55d64ebe1d44e4ee7906fe0eca45919999
Kyle Hughes <blinded93@gmail.com>
2017-04-09 06:28:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,21 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  @@students = []
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    name = doc.css(".student-name").text
+    name.collect do |name|
+      hash[:name] = name
+      self.students << hash
+    end
   end
 
   def self.scrape_profile_page(profile_url)
     
   end
 
-end
+  def self.students
+    @@students
+  end
 
