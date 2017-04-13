22e2eab8485365ffb1b818ffa7f84b6e74e6491d
Kyle Hughes <blinded93@gmail.com>
2017-04-13 04:48:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     cards = doc.css('div.student-card')
-    array = cards.collect do |card|
-      hash = {name: card.css('h4.student-name').text, location: card.css('p.student-location').text, profile_url: card.at('a')['href']}
+    cards.collect do |card|
+      {name: card.css('h4.student-name').text, location: card.css('p.student-location').text, profile_url: card.at('a')['href']}
     end
   end
 
@@ -32,9 +32,9 @@ class Scraper
       hash = {sym => link}
       @hash.merge!(hash)
     end
-    quote = doc.css('.profile-quote')
-    bio = doc.css('div.bio-content.content-holder div.description-holder p')
-    hash1 = {:profile_quote => quote.text, :bio => bio.text}
+    quote = doc.css('.profile-quote').text
+    bio = doc.css('div.bio-content.content-holder div.description-holder p').text
+    hash1 = {:profile_quote => quote, :bio => bio}
      #binding.pry
     @hash.merge!(hash1)
     @hash
