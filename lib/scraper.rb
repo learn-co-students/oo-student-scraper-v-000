fda145f30e5dd3e29a022819b29dcb6962daedaa
Laamia Islam <laamia.islam@gmail.com>
2017-04-15 19:30:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,26 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    learn_student_page = Nokogiri::HTML(html)
+
+    student_list = learn_student_page.css("student-card card-text-container")
+    binding.pry
+    student_array = []
+    #
+    # learn_student_page.css("student-card").each do |student|
+    #   name = student.css("h4.student-name").text
+    #
+    # end
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
