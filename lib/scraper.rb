0281ae926c00d5c2b9746de4164cdc8af8993ef2
Laamia Islam <laamia.islam@gmail.com>
2017-04-16 00:20:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,6 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    #html = open(index_url)
     learn_student_page = Nokogiri::HTML(open(index_url))
 
     student_array = []
@@ -22,42 +21,22 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     learn_profile_page = Nokogiri::HTML(open(profile_url))
-    social_media_links = learn_profile_page.css("div.social-icon-container a").map { |link| link['href'] }
-
-    twitter_url = ""
-    linkedin_url = ""
-    social_media_links.each do |link|
-      if link.include?("twitter.com")
-        twitter_url = link
-      elsif link.include?("linkedin.com")
-        linkedin_url = link
-      elsif link.include?("github.com")
-        github_url = link
-      else
-        blog_url = link
-      end
-    end
     student = {
-      social_media_links.each do |link|
-        if link.include?("twitter.com")
-          :twitter => link
-        # elsif link.include?("linkedin.com")
-        #   linkedin_url = link
-        # elsif link.include?("github.com")
-        #   github_url = link
-        # else
-        #   blog_url = link
-        # end
-      end,
-      # :linkedin =>
-      # :github =>
-      # :blog =>
       :profile_quote => learn_profile_page.css("div.profile-quote").text,
       :bio => learn_profile_page.css("div.bio-content.content-holder div.description-holder p").text
-
     }
-    binding.pry
+    social_media_links = learn_profile_page.css("div.social-icon-container a").map { |link| link['href'] }
+    social_media_links.each do |link|
+      if link.include?("twitter.com")
+        student[:twitter] = link
+      elsif link.include?("linkedin.com")
+        student[:linkedin] = link
+      elsif link.include?("github.com")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+    end
     student
   end
-
 end
