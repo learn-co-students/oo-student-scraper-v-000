0111c568a1e749d71c8c4e518cc435e7bbd80ff5
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-04-21 21:53:52 +0100

    Updates Scraper class.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,17 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card .card-text-container")
+
+    students.each do |student|
+      puts student.css(".student-name").text
+    end
     
   end
 
