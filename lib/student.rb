0e13e6d7d074f9fe3b157f5e864dcb0e8bb9dced
Matteo Piccini <gnappo.1@live.it>
2017-04-22 06:14:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,34 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @student_hash = student_hash
+    @name = @student_hash[:name]
+    @location = @student_hash[:location]
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    #modo migliore per risolverlo: MASS ASSIGNMENT (create obj from different environments). Assegna le coppie key/value all'instance della classe usando send
+    #con un key= method and an attribute.
+    attributes_hash.each{|key, value| self.send("#{key}=", value)}
+    #attributes_hash.each do |attribute|
+      #@bio = attributes_hash[:bio]
+      #@blog = attributes_hash[:blog]
+      #@linkedin = attributes_hash[:linkedin]
+      #@profile_quote = attributes_hash[:profile_quote]
+      #@twitter = attributes_hash[:twitter]
+    #end
   end
 
   def self.all
-    
+    @@all
   end
 end
-
