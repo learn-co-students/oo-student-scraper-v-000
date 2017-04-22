b4b6a71856eb9426ef2035570bcdf5c563b564ac
David Mars <david.w.mars@my.occc.edu>
2017-04-22 08:46:36 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,44 @@
+#   ~~~HAVICK WAS HERE~~~
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    # binding.pry
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "./fixtures/student-site/#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end
+    students
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_profile_page(profile)
+    student = {}
+    doc = Nokogiri::HTML(open(profile))
+    links = doc.css(".social-icon-container").children.css("a").map { |x| x.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+
+    student[:profile_quote] = doc.css(".profile-quote").text if doc.css(".profile-quote")
+    student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text if doc.css("div.bio-content.content-holder div.description-holder p")
+
+    student
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,34 @@
+#   ~~~HAVICK WAS HERE~~~
+
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |k, val|
+      self.send("#{k}=", val)
+      @@all << self
+      end
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+      Student.new(hash)
+
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |k, v|
+      self.send("#{k}=", v)
+
+    end
+    self
   end
 
   def self.all
-    
-  end
+    @@all
+    end
 end
-
