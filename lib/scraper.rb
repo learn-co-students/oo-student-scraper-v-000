24b27604108b63fe605a66296442c1f353bb0ad7
Aditi Green <asaki444@gmail.com>
2017-04-22 18:00:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,20 +19,31 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    social = doc.css(".social-icon-container")
-    linkedin =social.css("a:nth-of-type(2)")[0].attributes["href"].value
-    github =social.css("a:nth-of-type(3)")[0].attributes["href"].value
-    blog=social.css("a:nth-of-type(4)")[0].attributes["href"].value
-    profile = {:twitter =>social.css("a:first-of-type")[0].attributes["href"].value,
-    :profile_quote=> doc.css(".vitals-text-container .profile-quote").text,
-    :bio=> doc.css(".details-container p").text  }
-    if linkedin
-      profile[:linkedin] = linkedin
-    elsif github
-      profile[:github] = github
-    elsif blog
-      profile[:blog] = blog
+    social = doc.css(".social-icon-container a")
+      profile = {}
+    social.each do |link|
+      if link.attributes["href"].value.match(/twitter/)
+        profile[:twitter] = link.attributes["href"].value
+      elsif link.attributes["href"].value.match(/github/)
+        profile[:github] = link.attributes["href"].value
+      elsif link.attributes["href"].value.match(/linkedin/)
+        profile[:linkedin] = link.attributes["href"].value
+      else
+        profile[:blog] = link.attributes["href"].value
+      end
     end
+    linkedin = social.css("a:nth-of-type(2)")#[0].attributes["href"].value
+    github =social.css("a:nth-of-type(3)")#[0].attributes["href"].value
+    blog=social.css("a:nth-of-type(4)")#[0].attributes["href"].value
+    twitter = social.css("a:first-of-type")#[0].attributes["href"].value
+    profile[:profile_quote] = doc.css(".vitals-text-container .profile-quote").text
+    profile[:bio] = doc.css(".details-container p").text
+
+    # profile[:linkedin] = linkedin[0].attributes["href"].value if linkedin != nil
+    # profile[:github]   = github[0].attributes["href"].value if github !=nil
+    # profile[:blog]     = blog[0].attributes["href"].value if blog != nil
+    # profile[:twitter]  = twiiter[0].attributes["href"].value if twitter != nil
+
     profile
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,33 @@
+require 'pry'
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+     @name = student_hash[:name]
+     @location = student_hash[:location]
+     @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+      students_array.each do |student|
+         Student.new(student)
+      end
   end
 
   def add_student_attributes(attributes_hash)
-    
+      attributes_hash.each do |key,value|
+        @bio = value if key == :bio
+        @blog = value if key == :blog
+        @linkedin = value if key == :linkedin
+        @profile_quote = value if key == :profile_quote
+        @twitter = value if key == :twitter
+      end
   end
 
   def self.all
-    
+   @@all
   end
 end
-
