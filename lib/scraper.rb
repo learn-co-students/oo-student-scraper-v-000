35eee3c580ecc34871d25bb1bcec6bded622ae0e
Alexis Dorn <alexisadorn@gmail.com>
2017-04-22 21:21:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     students = []
     html = open(index_url)
-    doc = Nokogiri::HTML(html)
-    doc.css("div.student-card").each do |student|
+    index = Nokogiri::HTML(html)
+    index.css("div.student-card").each do |student|
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
@@ -19,7 +19,17 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
+    binding.pry
+    profile.css("div.main-wrapper.profile").each do |student_detail|
+    :twitter => student_detail.css(".social-icon-container a").first.attribute("href").value
+    :linkedin => student_detail.css(".social-icon-container a:nth-child(2)").attribute("href").value
+    :github => student_detail.css(".social-icon-container a:nth-child(3)").attribute("href").value
+    :blog => student_detail.css(".social-icon-container a").last.attribute("href").value
+    :profile_quote => student_detail.css(".vitals-text-container .profile-quote").text
+    :bio => student_detail.css(".description-holder p").text
   end
 
 end
