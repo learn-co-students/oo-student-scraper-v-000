3423da06dea7af1d31eb972808fada1a1bf3d222
Alexis Dorn <alexisadorn@gmail.com>
2017-04-22 14:49:09 -0700

    Update scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,18 +18,27 @@ class Scraper
     students
   end
 
-  def self.scrape_profile_page(profile_url)
+ def self.scrape_profile_page(profile_url)
     student_profile = {}
     html = open(profile_url)
     profile = Nokogiri::HTML(html)
-    binding.pry
-    profile.css("div.main-wrapper.profile").each do |student_detail|
-    :twitter => student_detail.css(".social-icon-container a").first.attribute("href").value
-    :linkedin => student_detail.css(".social-icon-container a:nth-child(2)").attribute("href").value
-    :github => student_detail.css(".social-icon-container a:nth-child(3)").attribute("href").value
-    :blog => student_detail.css(".social-icon-container a").last.attribute("href").value
-    :profile_quote => student_detail.css(".vitals-text-container .profile-quote").text
-    :bio => student_detail.css(".description-holder p").text
-  end
 
+    binding.pry
+
+    twitter = profile.css("div.main-wrapper.profile .social-icon-container a").first.attribute("href").value
+    linkedin = profile.css("div.main-wrapper.profile .social-icon-container a:nth-child(2)").attribute("href").value
+    github = profile.css("div.main-wrapper.profile .social-icon-container a:nth-child(3)").attribute("href").value
+    blog = profile.css("div.main-wrapper.profile .social-icon-container a").last.attribute("href").value
+    profile_quote = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    bio = profile.css("div.main-wrapper.profile .description-holder p").text
+
+    student_profile[:twitter] = twitter  if twitter
+    student_profile[:linkedin] = linkedin if linkedin
+    student_profile[:github] = github if github
+    student_profile[:blog] = blog  if blog
+    student_profile[:profile_quote] = profile_quote if profile_quote
+    student_profile[:bio] = bio if bio
+
+    student_profile
+  end  
 end
