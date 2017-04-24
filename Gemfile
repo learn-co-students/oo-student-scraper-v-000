bbf79a2f313af19d78b747cfa24fd7a98709f9c8
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-04-24 22:20:36 +0100

    Done.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'pry'
 gem 'nokogiri', '1.6.6.2'
 gem 'rspec'
+gem 'colorize'
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,23 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each { |student| self.new(student) }
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    self
   end
 
   def self.all
-    
+    @@all
   end
+
 end
 
