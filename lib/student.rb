b09dc65e89a071ae600633edb40b65db76f3a0e2
Terry Blue <terryblue@comcast.net>
2017-04-25 04:02:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,11 +10,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each {|hash| self.new(hash)}
   end
 
   def add_student_attributes(attributes_hash)
-
+    initialize(attributes_hash)
   end
 
   def self.all
