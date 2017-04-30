0af26dd11188b61e230a72e1a6fe91396adbce19
charlie lee <charlielee@charlies-MacBook-Pro.local>
2017-04-29 23:49:20 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,12 +13,12 @@ class Scraper
  #     expect(scraped_students).to include(student_index_array[0], student_index_array[1], student_index_array[2])
   #  end
   def self.scrape_index_page(index_url)
-  	#scraped_students = []
+  	scraped_students = []
     html = open("fixtures/student-site/index.html")
 	doc = Nokogiri::HTML(html)
 	#doc.search("h1.profile-name h2.profile-location").text
-	puts doc.css(".profile-name .profile-location").text
-
+	scraped_students << doc.css(".profile-name .profile-location").text
+	scraped_students 
   end
 
   def self.scrape_profile_page(profile_url)
