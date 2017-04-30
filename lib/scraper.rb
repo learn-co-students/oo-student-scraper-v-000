acff94ce0e4fd2e421104052b21206b606437b2e
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-30 03:06:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,22 +8,20 @@ class Scraper
     profiles = Nokogiri::HTML(open("http://students.learn.co/"))
 
     profiles_array = []
-    profiles_hash = {}
 
     profiles.css("div.roster-cards-container").each do |profile|
       profile.css("div.student-card").each do |i|
-        binding.pry
       info = profile.css("div.student-card").text
-      profiles_hash[info.to_sym] = {
+      profiles_hash = {
       :name => i.css("a div.card-text-container h4.student-name").text,
       :location => i.css("a div.card-text-container p.student-location").text,
-      :profile_url => i.css("a").attribute("href").text
-    }
-    profiles_array << profiles_hash
-   end
- end
-  profiles_array
-end
+      :profile_url => "./fixtures/student-site/" + i.css("a").attribute("href").text
+      }
+        profiles_array << profiles_hash
+         end
+       end
+      profiles_array
+    end
 
   def self.scrape_profile_page(profile_url)
     #     doc = Nokogiri::HTML.parse(<<-HTML_END)
