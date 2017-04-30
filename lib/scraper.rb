f06d869a17230a896ed8bd4b858712701c090485
Liliana Alonzo <lillyneveu@gmail.com>
2017-04-30 01:37:03 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,23 +7,41 @@ class Scraper
   def self.scrape_index_page(index_url)
     profiles = Nokogiri::HTML(open("http://students.learn.co/"))
 
+    profiles_array = []
     profiles_hash = {}
 
-    profiles_hash.css("div.roster-cards-container").each do |profile|
-      info = profile.css("").text
-      profile[info.to_sym] = {
-      :name => profile.css("a div.card-text-container h4.student-name").text,
-      :location => profile.css("a div.card-text-container p.student-location").text,
-      :profile_url => profile.css("div.student-card a.href").text
-        }
+    profiles.css("div.roster-cards-container").each do |profile|
+      binding.pry
+      info = profile.css("div.student-card").text
+      profiles_hash[info.to_sym] = {
+      :name => profiles.css("a div.card-text-container h4.student-name").text,
+      :location => profiles.css("a div.card-text-container p.student-location").text,
+      :profile_url => profiles.css("a").text
+    }
+
+    profiles_array << profiles_hash
    end
-  profiles_hash
+  profiles_array
   end
 
   def self.scrape_profile_page(profile_url)
+    #     doc = Nokogiri::HTML.parse(<<-HTML_END)
+    # <div class="heat">
+    #    <a href='http://example.org/site/1/'>site 1</a>
+    #    <a href='http://example.org/site/2/'>site 2</a>
+    #    <a href='http://example.org/site/3/'>site 3</a>
+    # </div>
+    # <div class="wave">
+    #    <a href='http://example.org/site/4/'>site 4</a>
+    #    <a href='http://example.org/site/5/'>site 5</a>
+    #    <a href='http://example.org/site/6/'>site 6</a>
+    # </div>
+    # HTML_END
+    #
+    # l = doc.css('div.heat a').map { |link| link['href'] }
 
   end
 
 end
 
-Scraper.new
+# Scraper.new
