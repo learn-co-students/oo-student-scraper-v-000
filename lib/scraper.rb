3d5038ee3d494469a06b3a7e2e77ca8e9183cd50
charlie lee <charlielee@charlies-MacBook-Pro.local>
2017-04-30 13:08:53 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,10 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
+  attr_accessor :first
+  def initialize (first)
+    @first = first
+  end 
 #describe "#scrape_index_page" do
  #   it "is a class method that scrapes the student index page and a returns an array of hashes in which each hash represents one student" do
  #     index_url = "./fixtures/student-site/index.html"
@@ -15,12 +19,14 @@ class Scraper
   def self.scrape_index_page(index_url)
   	scraped_students = []
     html = open("fixtures/student-site/index.html")
-	doc = Nokogiri::HTML(html)
+	   doc = Nokogiri::HTML(html)
 	#doc.search("h1.profile-name h2.profile-location").text
-	scraped_students << doc.css(".profile-name .profile-location").text
-	scraped_students 
+	  scraped_students << doc.css(".profile-name .profile-location").text
+	  scraped_students 
   end
-
+  def first 
+    @first = scraped_students[:location] scraped_students[:name]
+  end 
   def self.scrape_profile_page(profile_url)
     
   end
