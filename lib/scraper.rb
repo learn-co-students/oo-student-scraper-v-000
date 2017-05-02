5d6a326eaefe03aee46c9a79d806cd02135d4933
Alexander Friedland <friedlandaj@gmail.com>
2017-05-02 05:31:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    jon_snow = Nokogiri::HTML(open(index_url))
+    student_index = []
+    students = doc.css("")
+    students.each do |student|
+      student_hash = {
+        
+      }
+      student_index << student_hash
+    end
+    student_index
   end
 
   def self.scrape_profile_page(profile_url)
