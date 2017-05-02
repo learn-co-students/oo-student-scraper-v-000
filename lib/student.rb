9168d22ec8038badd8fc751a2df40076468d5eca
Liliana Alonzo <lillyneveu@gmail.com>
2017-05-02 05:16:20 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,10 +17,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key, value|
+      @@all.send(:key => value)
+    end
   end
 
   def self.all
     @@all
   end
+
 end
