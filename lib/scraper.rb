706779d0a46289388114ed40bef0ebddb01b4ace
Alexander Friedland <friedlandaj@gmail.com>
2017-05-02 23:11:03 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,24 +4,22 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    jon_snow = Nokogiri::HTML(open(index_url))
+    doc = Nokogiri::HTML(open(index_url))
     student_index = []
-    students = jon_snow.css(".student-card")
+    students = doc.css(".student-card")
     students.each do |student|
       student_hash = {
         :name => student.css(".student-name").text,
         :location => student.css(".student-location").text,
-        :profile_url => "./fixtures/student-site/#{student.css("a").attribute("href").value}"
+        :profile_url => "./fixtures/student-site/#{student.attr('href')}"
       }
       student_index << student_hash
     end
     student_index
-    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
