efa0d0a170820a08c9c7110050eb9253947e934b
debjohnson33 <debjohnson_11@yahoo.com>
2017-05-08 19:38:57 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,10 +5,44 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     
+    students = Nokogiri::HTML(open(index_url))
+    scraped_students = []
+   
+    students.css("div.roster-cards-container").each do |card|
+    	card.css(".student-card a").each do |student|
+
+    		student_name = student.css(".student-name").text
+     		student_location = student.css(".student-location").text
+    		student_profile_url = "#{student.attr('href')}"
+    		scraped_students << {name: student_name, location: student_location, profile_url: student_profile_url}
+    	end
+    	
+	end
+  scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+    scraped_profile = {}
+
+   social_links = profile.css("div.social-icon-container a").map { |link| link["href"]}
+
+   social_links.each do |link|
+   		if link.include?("twitter")
+   			scraped_profile[:twitter] = link
+   		elsif link.include?("linkedin")
+   			scraped_profile[:linkedin] = link
+   		elsif link.include?("github")
+   			scraped_profile[:github] = link
+   		else	
+   			scraped_profile[:blog] = link
+   		end
+   	end
+
+   	scraped_profile[:profile_quote] = profile.css("div.profile-quote").text
+   	scraped_profile[:bio] = profile.css("p").text
+
+   	scraped_profile
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+      student_hash.each do |key, value|
+        self.send("#{key}=", value)
+        @@all << self
+      end
   end
 
   def self.create_from_collection(students_array)
-    
+      students_array.each do |hash|
+        self.new(hash)
+      end
   end
 
   def add_student_attributes(attributes_hash)
-    
+      attributes_hash.each do |key, value|
+        self.send("#{key}=", value)
+      end
+      self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
