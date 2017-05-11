d174771d4fae7a4be15d61cc3b6f0cc3c899130b
Sean Quillen <sean.r.quillen@gmail.com>
2017-05-11 19:22:06 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,42 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        student_url = "./fixtures/student-site/#{student.attr('href')}"
+        students << {name: student_name, location: student_location, profile_url: student_url}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map { |i| i.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+    student
   end
 
 end
-
