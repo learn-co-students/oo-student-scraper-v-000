ae3cf3cbdff1335c8b07d346e79edf9a23a90cbc
Dave Schlauderaff <dschlaud@gmail.com>
2017-05-12 12:26:31 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,27 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    self.tap {attributes_hash.each {|key, value| self.send(("#{key}="), value)}}
+
+    # binding.pry
   end
 
   def self.all
-    
+    @@all
   end
 end
-
