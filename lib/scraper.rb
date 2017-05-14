42ea0230fe7a5b234dee4ddf97191b31db0c2668
Elvin <Dimix181@Gmail.com>
2017-05-14 18:30:30 -0400

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,54 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
+    page = Nokogiri::HTML(open(index_url))
+    card_container = page.css('.student-card')
+
+    #iterates throught the student_container create a hash for each.
+    #:name :location and :profile_url
+    card_container.collect do |k|
+          {
+          "name":k.css('.student-name').text, "location":k.css('.student-location').text,
+          "profile_url": k.css("a")[0]["href"]
+            }
+         end
   end
 
+      #iterates throught the student profile adds social media to a hash.
   def self.scrape_profile_page(profile_url)
-    
-  end
+     page  = Nokogiri::HTML(open(profile_url))
+     socialMedia  = page.css("div.social-icon-container")
+     hash  = Hash.new
+      socialMedia.css("a").each do |k| url = k["href"]
+          if url.include?("twitter.com")
+          hash[:"twitter"] =  url
+        elsif url.include?("linkedin.com")
+          hash[:"linkedin"] =  url
+        elsif url.include?("github.com")
+          hash[:"github"] =  url
+        else
+          hash[:"blog"] = url
+        end
+      end
+
+      if  page.css("div.profile-quote").text != nil
+        hash[:"profile_quote"] = page.css("div.profile-quote").text
+      end
+
+      if  page.css("div.description-holder p").text !=nil
+        hash[:"bio"] = page.css("div.description-holder p").text
+      end
+    hash
+ end
+
+
+
+
+
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,26 @@
-class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+class Student
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]  # gets the name from the hash
+    @location = student_hash[:location] #gets the location from the hash
+    @@all << self #adds the student to the all variable
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student_hash| self.new(student_hash)}
+    #creates an obj with each elementary on the array with consist of hashes
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each{|k,v| self.instance_variable_set("@#{k}".to_sym, v) }
+    #sets the value based on the instance name matching the key.
   end
 
   def self.all
-    
+    @@all
   end
 end
-
