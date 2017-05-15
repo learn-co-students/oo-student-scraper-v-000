2148693b5860da1d5cebcf5ac42d925aab09256e
Amanda George <github email address>
2017-05-15 07:47:16 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,14 +16,28 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    media = []
-    doc.css("div.social-icon-container").each do |icon|
-      icon.css("a href").attribute.text
-binding.pry
-      media << {}
+    student_info = {}
 
 
+    doc.css("div.social-icon-container a").each do |icon|
+      if icon.css(".social-icon").attribute("src").value[/twitter/]
+        student_info[:twitter] = icon.attribute("href").value
+      end
+      if icon.css(".social-icon").attribute("src").value[/linkedin/]
+        student_info[:linkedin] = icon.attribute("href").value
+      end
+      if icon.css(".social-icon").attribute("src").value[/github/]
+        student_info[:github] = icon.attribute("href").value
+      end
+      if icon.css(".social-icon").attribute("src").value[/rss/]
+        student_info[:blog] = icon.attribute("href").value
+      end #end if/else
+    end#end each
 
+    student_info[:profile_quote] = doc.css("div.profile-quote").text
+    student_info[:bio] = doc.css("div.description-holder p").text
+
+    student_info
   end
 
 end
