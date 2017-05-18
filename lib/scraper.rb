9b82c2d8dcd284fce98fac86368c990243f29515
Sam Zeidman <sam.zeidman@gmail.com>
2017-05-18 16:17:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    #return array of hashes
+    students = {}
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
-end
+  binding.pry
 
+end
