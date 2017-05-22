75a027191756a1a805cab8c9baca26dde760ebfd
hyoyou <heatherhyou@gmail.com>
2017-05-21 20:58:45 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = open(index_url)
+    doc = Nokogiri::HTML(index_page)
+    doc.css(".roster-cards-container")
+    name : doc.css(".roster-cards-container").first.css("h4").text
+    location : doc.css(".roster-cards-container").first.css("p").text
+    profile_url :
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_url
   end
 
 end
-
