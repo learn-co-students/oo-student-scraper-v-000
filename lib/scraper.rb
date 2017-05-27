a60713ccf0e8febad6f36eb35615d33325363dae
charlie lee <ctlee9096@gmail.com>
2017-05-27 15:55:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,27 +3,39 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-#describe "#scrape_index_page" do
- #   it "is a class method that scrapes the student index page and a returns an array of hashes in which each hash represents one student" do
- #     index_url = "./fixtures/student-site/index.html"
- #     scraped_students = Scraper.scrape_index_page(index_url)
- #     expect(scraped_students).to be_a(Array)
- #     expect(scraped_students.first).to have_key(:location)
- #     expect(scraped_students.first).to have_key(:name)
- #     expect(scraped_students).to include(student_index_array[0], student_index_array[1], student_index_array[2])
-  #  end
   def self.scrape_index_page(index_url)
-  	scraped_students = []
     html = open("fixtures/student-site/index.html")
-	doc = Nokogiri::HTML(html)
-	#doc.search("h1.profile-name h2.profile-location").text
-	scraped_students << doc.css(".profile-name .profile-location").text
-	scraped_students 
+	  doc = Nokogiri::HTML(html)
+	  scraped_students = doc.css(".student-card")
+    nodeset = doc.css('a[href]')
+	  scraped_students.map do |student|
+       student_hash = {}
+       student_hash[:name]= student.css(".student-name").text
+       student_hash[:location] = student.css(".student-location").text
+       student_hash[:profile_url] = student.css("a").attr("href").value
+       student_hash
+    end
   end
-
   def self.scrape_profile_page(profile_url)
-    
-  end
-
+      html = open(profile_url)
+      doc = Nokogiri::HTML(html)
+      #binding.pry
+      scraped_student = {
+        :profile_quote => doc.css(".vitals-container .vitals-text-container .profile-quote").text,
+        :bio => doc.css(".details-container .bio-block .bio-content .description-holder p").text
+      }
+      socials = doc.css(".vitals-container .social-icon-container a")
+      socials.each do |social|
+      if social.attribute("href").value.include?("twitter")
+        scraped_student[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        scraped_student[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        scraped_student[:github] = social.attribute("href").value
+      else social.attribute("href").value.include?("blog")
+        scraped_student[:blog] = social.attribute("href").value
+      end
+      end
+      scraped_student
+    end
 end
-
