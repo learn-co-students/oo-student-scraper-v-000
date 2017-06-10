a10cb8e5ec03975f4ee33ea6c36d64945a327be9
Luisa Scavo <luisascavo@gmail.com>
2017-06-10 03:21:56 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,32 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      student = Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    @bio = attributes_hash[:bio]
+    @profile_quote = attributes_hash[:profile_quote]
+    @twitter = attributes_hash[:twitter]
+    @github = attributes_hash[:github]
+    @linkedin = attributes_hash[:linkedin]
+    @blog = attributes_hash[:blog]
   end
 
   def self.all
-    
+    @@all
   end
 end
-
