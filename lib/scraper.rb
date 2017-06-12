363ded241798713247d8f2dc3d04c9ae287f1689
benhayehudi <bengreenberg@gmail.com>
2017-06-12 16:36:42 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,7 +15,6 @@ class Scraper
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
          :profile_url => ("./fixtures/student-site/" + student.css("a").attribute("href").value)
-#:profile_url => student.css("a").attribute("href").value
       }
     end
     scraped_students
