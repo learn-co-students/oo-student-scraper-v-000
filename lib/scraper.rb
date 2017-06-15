aa7a6aeba57cc329a26d9a245f41c91240059c64
ctab33 <ctab33@me.com>
2017-06-15 18:05:32 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,14 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open("fixtures/student-site/index.html"))
+    doc = Nokogiri::HTML(open(index_url))
     students = []
     doc.css(".student-card").each do |student|
         student_location = student.css("p.student-location").text
         student_name = student.css("div.card-text-container h4.student-name").text
         student_profile_url = student.css("a").attribute("href").value
         students << {:name => student_name, :location => student_location, :profile_url => student_profile_url}
-
       end
-    
     students
   end
 
