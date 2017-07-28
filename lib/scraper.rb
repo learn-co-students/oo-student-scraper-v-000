b02c3e1adcca25521b625d7b8cdd255dc729a849
Chase Paules <cpaules@gmail.com>
2017-07-28 00:06:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,43 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index = Nokogiri::HTML(open(index_url))
+    arr = []
+    index.css(".student-card").each do |student|
+      hash = {}
+      hash[:profile_url] = student.css("a").attr('href').text
+      hash[:name] = student.css(".student-name").text
+      hash[:location] = student.css(".student-location").text
+      arr << hash
+    end
+    arr
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+
+    #puts profile.css('//a[@href*="twitter"]').attr('href').text
+    #puts profile.css('//a[@href*="linkedin"]').attr('href').text
+    #puts profile.css('//a[@href*="github"]').attr('href').text
+    #puts profile.css(".social-icon-container a").last.attr('href')
+    #puts profile.css(".profile-quote").text
+    #puts profile.css(".description-holder p").text
+
+    #puts profile.css(".social-icon-container a").to_s.size
+
+    hash = {}
+    if profile.css(".social-icon-container a").to_s.include?("twitter")
+      hash[:twitter] = profile.css('//a[@href*="twitter"]').attr('href').text
+    end
+    hash[:linkedin] = profile.css('//a[@href*="linkedin"]').attr('href').text
+    hash[:github] = profile.css('//a[@href*="github"]').attr('href').text
+    if profile.css(".social-icon-container a").to_s.include?("rss")
+      hash[:blog] = profile.css(".social-icon-container a").last.attr('href')
+    end
+    hash[:profile_quote] = profile.css(".profile-quote").text
+    hash[:bio] = profile.css(".description-holder p").text
+    hash
+
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,29 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send(("#{key}="), value)
+      @@all << self
+    end
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_h|
+      student = Student.new(student_h)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
-
