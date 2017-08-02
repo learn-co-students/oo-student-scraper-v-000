d337ee8cf4b845fbc3fd2be323d15f965531c1aa
Robert Peckham <peckham.rob@gmail.com>
2017-08-02 10:29:16 +0000

    Scraper.scrape_profile_page works

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,36 +20,33 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
-    links = {}
-    social = profile_page.css('div.social-icon-container')
-    icon = social.css('.social-icon-container')
-    binding.pry
-    icon.css('a').attribute('href').value.each do |site|
-      binding.pry
-      if site.include?('twitter.com')
-        links[:twitter] = icon.css('a').attribute('href').value
+    students = {}
+    social = profile_page.css('div.social-icon-container').children.css('a').map{ |icon| icon.attribute('href').value}
+    social.each do |link|
 
-      # elsif icon.css('a').attribute('href').value.include?('linkedin.com')
-      #   links[:linkedin] = icon.css('a').attribute('href').value
-      #
-      # elsif icon.css('a').attribute('href').value.include?('github.com')
-      #   links[:github] = icon.css('a').attribute('href').value
-      #
-      # elsif !(icon.css('a').attribute('href').value.include?('twitter, github, linkedin'))
-      #   links[:blog] = icon.css('a').attribute('href').value
+      if link.include?('twitter')
+        students[:twitter] = link
+
+      elsif link.include?('linkedin')
+        students[:linkedin] = link
+
+      elsif link.include?('github')
+        students[:github] = link
+
+      else
+        students[:blog] = link
       end
-      # binding.pry
     end
 
     profile_page.css('.vitals-text-container').each do |quote|
-      links[:profile_quote] = quote.css('.profile-quote').text
+      students[:profile_quote] = quote.css('.profile-quote').text
     end
 
     profile_page.css('.bio-content').each do |bio|
-      links[:bio] = bio.css('.description-holder p').text
+      students[:bio] = bio.css('.description-holder p').text
     end
     # binding.pry
-    links.reject{|k,v| v.nil?}
+    students.reject{|k,v| v.nil?}
   end
 
 end
