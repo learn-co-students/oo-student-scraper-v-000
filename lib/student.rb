2295beab4bdf4e545c761539aed2f654227e6a55
Robert Peckham <peckham.rob@gmail.com>
2017-08-02 10:35:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |attribute, value|
-      self.send("#{attribute}=, value")
+      self.send("#{attribute}=", value)
     end
     @@all << self
   end
@@ -20,7 +20,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |attribute, value|
-      self.send("#{attribute}=, value")
+      self.send("#{attribute}=", value)
     end
     self
   end
