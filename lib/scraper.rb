bd9e82619e81ef1e92dea14475cf6090700aa796
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-08-09 01:03:29 +0000

    Save

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
-    page = Nokogiri::HTML(index_url)
+    html = open(index_url)
+    page = Nokogiri::HTML(html)
     students = []
 
     page.css('div.roster-cards-container').each do |card|
@@ -20,7 +20,25 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    page = Nokogiri::HTML(html)
 
+    student_profile = {}
+
+    links = page.css('div.social-icon-container a').collect{ |social_icon| social_icon['href'] }
+    links.each do |link|
+      if link.include?("twitter")
+        student_profile[:twitter] = link
+      elsif link.include?("linkedin")
+        student_profile[:linkedin] = link
+      elsif link.include?("github")
+        student_profile[:github] = link
+      else
+        student_profile[:blog] = link
+      end
+    end
+    student_profile[:profile_quote] = page.css('div.profile-quote').text
+    student_profile[:bio] = page.css('div.bio-content.content-holder p').text
+    student_profile
   end
-
 end
