d815cbaf81876b152e5da47d55453a6daf502881
Chris <cjones999983@gmail.com>
2017-08-11 08:40:45 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,39 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+
+    doc.css(".student-card").each do |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text
+      profile_url = student.css("a").attribute("href").value
+      students << {name: name, location: location, profile_url: profile_url}
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    student_profile_hash = {}
+
+    doc.css(".social-icon-container a").each do |a|
+      link = a.attribute("href").value
+      if !link.match(/[^\/]\/[a-z]+/)
+        symbol = :blog
+      else
+        symbol = link.match(/[a-z0-9]+\.[a-z]{2,5}\b/)[0].gsub(".com", "").to_sym
+      end
+      student_profile_hash[symbol] = link
+    end
+
+    student_profile_hash[:profile_quote] = doc.css(".profile-quote").text
+    student_profile_hash[:bio] = doc.css(".description-holder p").text
+    student_profile_hash
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +7,28 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash.values_at(:name).join
+    @location = student_hash.values_at(:location).join
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    self.twitter = attributes_hash.values_at(:twitter).join
+    self.linkedin = attributes_hash.values_at(:linkedin).join
+    self.github = attributes_hash.values_at(:github).join
+    self.blog = attributes_hash.values_at(:blog).join
+    self.profile_quote = attributes_hash.values_at(:profile_quote).join
+    self.bio = attributes_hash.values_at(:bio).join
   end
 
   def self.all
-    
+    @@all
   end
 end
 
