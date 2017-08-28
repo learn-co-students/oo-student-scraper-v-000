6ec61436f904dc2b2f1d1f07896d33b15cd2d3a2
Amelie Oller <amelieoller@gmail.com>
2017-08-28 15:22:59 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,57 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
+    # Student name: html.css(".card-text-container h4.student-name").text
+    # location: html.css(".card-text-container p.student-location").text
+    # url: html.css("student-card a").attribute('href').value
+    scraped_students = []
+
+    html.css(".student-card").each do |card|
+      student = {}
+      student[:name] = card.css(".card-text-container h4.student-name").text
+      student[:location] = card.css(".card-text-container p.student-location").text
+      student[:profile_url] = card.css("a").attribute('href').value
+      scraped_students << student
+    end
+
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = Nokogiri::HTML(open(profile_url))
+
+    # .vitals-container
+    # Linkedin: card.css(".social-icon-container").attribute('href').value
+    # Github: card.css(".social-icon-container").attribute('href').value
+    # Blog: card.css(".social-icon-container").attribute('href').value
+    # Profile Quote: card.css(".profile-quote").text
+    # Bio: card.css(".bio-content .description-holder").text
+
+    scraped_student = {}
+    social_links = []
+
+    html.css(".social-icon-container a").each do |link|
+      social_links << link.attribute('href').value
+    end
+
+    social_links.each do |link|
+      if link.match(/github\.com/)
+        scraped_student[:github] = link
+      elsif link.match(/twitter\.com/)
+        scraped_student[:twitter] = link
+      elsif link.match(/linkedin\.com/)
+        scraped_student[:linkedin] = link
+      else
+        scraped_student[:blog] = link
+      end
+    end
+
+    scraped_student[:profile_quote] = html.css(".profile-quote").text
+    scraped_student[:bio] = html.css(".bio-content .description-holder p").text
+
+    scraped_student
+
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio]
+    @profile_url = student_hash[:profile_url]
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| self.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send("#{key}=", value)}
   end
 
   def self.all
-    
+    @@all
   end
 end
-
