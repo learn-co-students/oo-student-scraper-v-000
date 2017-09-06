b2b51e758c8ad45f40b6bb924d3fcb5e94d0d7fd
Richard Steinbach <steinbach.dick@gmail.com>
2017-09-06 15:38:35 -0700

    Student Scraper

--- lib/student.rb
+++ lib/student.rb
@@ -6,18 +6,33 @@ class Student
 
   def initialize(student_hash)
     
+      #binding.pry
+      #@name = student_hash[:name]
+      #@location = student_hash[:location]
+
+      student_hash.each {|key, value|
+        self.send(("#{key}="), value)}
+      @@all << self
+    
+
   end
 
   def self.create_from_collection(students_array)
-    
+  
+    students_array.each do |h| Student.new(h)
+    end  
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+      #binding.pry
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
