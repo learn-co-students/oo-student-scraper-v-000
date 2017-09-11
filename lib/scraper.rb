801a6bc50cada4a4886ae8ef43ce67e33832e5d7
churchwerks <steven.lennis@icloud.com>
2017-09-11 10:46:11 -0500

    Scraper tests passing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
-    students = []
-    index_page.css("div.roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-        student_profile_link = "./fixtures/student-site/#{student.attr('href')}"
-        student_location = student.css('.student-location').text
-        student_name = student.css('.student-name').text
+      student_site = Nokogiri::HTML(open(index_url))
+      students = []
+      student_site.css("div.student-card").each do |card|
+        student_profile_link = card.css("a").attribute("href").value
+        student_name = card.css(".student-name").text
+        student_location = card.css(".student-location").text
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
+      students
     end
-    students
-  end
 
   def self.scrape_profile_page(profile_slug)
     student = {}
