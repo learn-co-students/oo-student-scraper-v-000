e0f4c5f41784608856b2a833c80ff8aa3b59f405
Christine Tran <christinetran825@gmail.com>
2017-09-15 07:12:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,46 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #returns an array of hashes in which each hash represents one student
+    index_page = Nokogiri::HTML(open(index_url)) #scraped_students = Scraper.scrape_index_page(index_url)
+    #binding.pry
+    index_page.css(".student-card").collect.with_index do |cards, idx| #list of student profile
+      scraped_students = {} #expect(scraped_students).to be_a(Array)
+      scraped_students[:name] = index_page.css(".student-name")[idx].text #name of individual student
+      scraped_students[:location] = index_page.css(".student-location")[idx].text #location of individual student
+      scraped_students[:profile_url] = index_page.css(".student-card a")[idx]["href"] #profile_url of individual student
+      scraped_students
+    end
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  # index_page.css(".roster-cards-container") # collection of students
+  # index_page.css(".roster-cards-container").first.css(".student-card a") #list of student profile
+  # index_page.css(".roster-cards-container").first.css(".student-card a").attribute("href").value # profile_url of individual student
+  # index_page.css(".roster-cards-container").first.css(".student-card a").first.css("h4.student-name").text #name of individual student
+  # index_page.css(".roster-cards-container").first.css(".student-card a").first.css("p.student-location").text #location of individual student
+
+  def self.scrape_profile_page(profile_url) #is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student
+    student_hash = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    #binding.pry
+    profile_page.css(".social-icon-container a").each_with_index do |links, idx|
+      if links["href"].include?("twitter")
+        student_hash[:twitter] = links["href"]
+      elsif links["href"].include?("linkedin")
+        student_hash[:linkedin] = links["href"]
+      elsif links["href"].include?("github")
+        student_hash[:github] = links["href"]
+      else links["href"].include?("blog")
+        student_hash[:blog] = links["href"]
+      end
+    end
+    student_hash[:profile_quote] = profile_page.css(".profile-quote").text
+    student_hash[:bio] = profile_page.css(".description-holder p").text
+    student_hash
   end
 
 end
 
+# profile_page.css("div.vitals-container").first.css(".social-icon-container a").attribute("href").value #social likes
+# profile_page.css("div.vitals-container").first.css(".vitals-text-container .profile-quote").text #profile quote
+# profile_page.css("div.details-container").first.css(".description-holder p").text #bio
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,40 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(student_hash) #takes in an argument of a hash and sets that new student's attributes using the key/value pairs of that hash.
+    #binding.pry
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)  #"#{key}=" is the method name reflecting the attr_accessor methods
+    end
+     @@all << self #adds that new student to the Student class' collection of all existing students, stored in the `@@all` class variable.
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) #take in an array of hashes from Scraper.scrape_index_page, iterate over the array of hashes and create a new individual student using each hash. Bringing us to Initialize method
+    students_array.each do |student_info|
+      Student.new(student_info)
+    end
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(attributes_hash) # take in a hash whose key/value pairs from Scraper.scrape_profile_page, iterate over the given hash and use metaprogramming to assign student attributes and values in key/value pairs of the hash. Use #send to do this.
+    #binding.pry
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    #return value should be student itself. use self
   end
 
   def self.all
-    
+    @@all
   end
 end
 
+#     student.add_student_attributes(student_hash)
+#     expect(student.bio).to eq("I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school.")
+#     expect(student.blog).to eq("someone@blog.com")
+#     expect(student.linkedin).to eq("someone@linkedin.com")
+#     expect(student.profile_quote).to eq("\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi")
+#     expect(student.twitter).to eq("someone@twitter.com")
+#   end
