2e00a7f4f103acbd272ef315895cf8f2750e1c16
github username <github email address>
2017-09-15 18:36:16 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,55 +5,67 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
-    result = []
+    student_hash_array = []
     html = File.read('fixtures/student-site/index.html')
     doc = Nokogiri::HTML(html)
 
-    student_array = doc.css("div.student-card")
+    #create array of students (as xml elements)
+    student_xml_array = doc.css("div.student-card")
 
-    student_array.each do |student_card|
-      name = student_card.css("h4.student-name").text
-      location = student_card.css("p.student-location").text
-      profile_url = student_card.css("a")[0]["href"]
+    #iterate through array, find attributes, create a hash of attributes for each student_card
+    #return array of hashes
+    student_xml_array.each do |student_attributes|
+      name = student_attributes.css("h4.student-name").text
+      location = student_attributes.css("p.student-location").text
+      profile_url = student_attributes.css("a")[0]["href"]
 
-      student = {:name => name, :location => location, :profile_url => profile_url}
-      result << student
-    end #end each
-    result
-  end
+      student_hash = {:name => name, :location => location, :profile_url => profile_url}
+      student_hash_array << student_hash
+    end
+    student_hash_array
+  end #end method
+
 
   def self.scrape_profile_page(profile_url)
 
     doc = Nokogiri::HTML(open("#{profile_url}"))
 
-    social_media =
+    #scrape for social media links and return as array
+    social_media_array =
     doc.css("div.social-icon-container a").collect do |element|
       element['href']
     end
 
-    twitter_url = social_media.detect do |link|
+    ########################################################################################
+    #iterate over array and assign particular links to a varlable if those links are included.
+    #variables will later be used to set key values for student hash
+
+    twitter_url = social_media_array.detect do |link|
       link.include?("twitter")
     end
 
 
-    linkedin_url = social_media.detect do |link|
+    linkedin_url = social_media_array.detect do |link|
       link.include?("linkedin")
     end
 
-    github_url = social_media.detect do |link|
+    github_url = social_media_array.detect do |link|
       link.include?("github")
     end
 
-    blog_url_array = social_media.reject do |link|
+    #set blog url and re-format to match test expectation
+    blog_url_array = social_media_array.reject do |link|
       link.include?("github") || link.include?("linkedin") ||link.include?("twitter")
     end
-
     blog_url = blog_url_array.join("")
 
+    #scrape and set variables for rest of attributes
     profile_quote = doc.css("div.profile-quote").text
     bio = doc.css("div.description-holder p").text
 
+    ########################################################################################
+
+    #use variables above to set hash key values. Delete particular keys if key value is nil/empty. Return hash
     scraped_student = {
       :twitter => twitter_url,
       :linkedin => linkedin_url,
