bdb5e57d9cb7cbd338f95838ff10738aa2cf1d57
github username <github email address>
2017-09-15 15:48:42 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,29 +25,45 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
 
-
     doc = Nokogiri::HTML(open("#{profile_url}"))
 
-    binding.pry
-    social_media = doc.css("div.social-icon-container a")[0]["href"]
-    # blog_url = ????
+    social_media =
+    doc.css("div.social-icon-container a").collect do |element|
+      element['href']
+    end
+
+    twitter_url = social_media.detect do |link|
+      link.include?("twitter")
+    end
+
+
+    linkedin_url = social_media.detect do |link|
+      link.include?("linkedin")
+    end
+
+    github_url = social_media.detect do |link|
+      link.include?("github")
+    end
+
+    blog_url_array = social_media.reject do |link|
+      link.include?("github") || link.include?("linkedin") ||link.include?("twitter")
+    end
+
+    blog_url = blog_url_array.join("")
+
     profile_quote = doc.css("div.profile-quote").text
     bio = doc.css("div.description-holder p").text
 
+    scraped_student = {
+      :twitter => twitter_url,
+      :linkedin => linkedin_url,
+      :github => github_url,
+      :blog => blog_url,
+      :profile_quote => profile_quote,
+      :bio => bio
+    }.delete_if{ |k,v| v.nil? || v == ""}
 
-
-    # scraped_student = {
-    #   :twitter_url => twitter_url,
-    #   :linkedin_url => linkedin_url,
-    #   :github_url => github_url,
-    #   :blog_url => blog_url,
-    #   :profile_quote => profile_quote,
-    #   :bio => bio
-    # }
-    #
-    # scraped_student
-
-    #Some students don't have a twitter or some other social link. Be sure to be able to handle that.
+    scraped_student
 
   end
 
