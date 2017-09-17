b1cdcfd0352e482131e2b9cbb6bcbb3dbce2f261
Chris Nady <nady.christopher@gmail.com>
2017-09-17 19:30:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,41 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(index_url)
+
+    projects = []
+
+    index_page.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+        :description => project.css("p.bbcard_blurb").text,
+        :location => project.css("ul.project-meta span.location-name").text,
+        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      }
+    end
+
+    # return the projects hash
+    projects
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_page = Nokogiri::HTML(profile_url)
+
+    projects = {}
+
+    profile_page.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+        :description => project.css("p.bbcard_blurb").text,
+        :location => project.css("ul.project-meta span.location-name").text,
+        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      }
+    end
+
+    # return the projects hash
+    projects
   end
 
 end
-
