b17931bf39d283e550efcbf9b81aeb72b96135f3
chrisjamesr <chrisjames.r@gmail.com>
2017-09-17 10:12:06 -0400

    Pass all tests for Scraper & Student

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
     
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|s| Student.new(s)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+
+      self.instance_variable_set("@#{key}","#{value}")
+      # self.class_eval("def #{key}=(#{value}); @#{key}=#{value}; end")
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
-end
+
+end # End of Class
 
