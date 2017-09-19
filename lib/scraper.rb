e419565cc0b75fc3c9b988ed7bb2e730e933c708
Lucas <lqf2b8@gmail.com>
2017-09-19 10:03:46 -0500

    done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,58 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #takes url of inex page and will scrape it
+    students = Nokogiri::HTML(open(index_url)) #sets the nokogiri objects ruturned from the index url to a var
+    students_array = [] #intializes an array to save scraped data
+    students.css('div.roster-cards-container').each do |individual_students| #takes noko var and returns css div then iterates with .each 
+      individual_students.css('.student-card a').each do |student| #takes passed results from 
+        name = student.css('.student-name').text
+        location = student.css('.student-location').text
+        url = student.attr('href')
+        students_array << {:name => name, :location => location, :profile_url => url}
+      end
+    end
+    students_array
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = Nokogiri::HTML(open(profile_url))
+    student_info = {}
+    bio = student_profile.css('.description-holder p').text
+    profile_quote = student_profile.css('.profile-quote').text
+
+    linkedin = ""
+    github = ""
+    blog = ""
+    twitter = ""
+
+    social = student_profile.css('.social-icon-container a')
+        if social[3]
+          blog = social[3].attr('href')
+        end
+      social.each do |social_site|
+        if social_site.attr('href').include?("linkedin")
+          linkedin = social_site.attr('href')
+        elsif social_site.attr('href').include?("github")
+          github = social_site.attr('href')
+        elsif social_site.attr('href').include?("twitter")
+          twitter = social_site.attr('href')
+        end
+      end
+    student_info = {
+      :twitter => twitter,
+      :linkedin => linkedin,
+      :github => github,
+      :blog => blog,
+      :profile_quote => profile_quote,
+      :bio => bio
+    }
+
+    student_info.delete_if { |key, value| value.to_s.strip == '' }
   end
 
 end
-
