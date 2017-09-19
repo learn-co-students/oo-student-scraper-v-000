b0f1ed659b7772c60cce978d88eef16476d668bf
Yehuda Makarov <yehudamakarov@gmail.com>
2017-09-19 19:31:39 -0400

    .

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+
 require 'pry'
 
 class Scraper
--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,14 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name,
+                :location,
+                :twitter,
+                :linkedin,
+                :github,
+                :blog,
+                :profile_quote,
+                :bio,
+                :profile_url
 
   @@all = []
 
