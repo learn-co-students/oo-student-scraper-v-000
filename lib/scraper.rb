81eaab9ba94ca32c50a8807380d23cba28d6828b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-09-19 20:50:06 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = {}
+        # is a class method that scrapes the student index page and a returns an array of hashes in which each hash represents one student (FAILED - 1)
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    # is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student (FAILED - 2)
+    # can handle profile pages without all of the social links (FAILED - 3)
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,23 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # takes in an argument of a hash and sets that new student's attributes using the key/value pairs of that hash. (FAILED - 4)
+    # adds that new student to the Student class' collection of all existing students, stored in the `@@all` class variable. (FAILED - 5)
   end
 
   def self.create_from_collection(students_array)
-    
+        # uses the Scraper class to create new students with the correct name and location. (FAILED - 6)
   end
 
   def add_student_attributes(attributes_hash)
-    
+        # uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student. (FAILED - 7)
   end
 
   def self.all
-    
+        # returns the class variable @@all (FAILED - 8)
   end
 end
-
