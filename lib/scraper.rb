205000d3795794265580d8826915c2db68400a28
Janusz Szubert <theinvalidnonce@gmail.com>
2017-09-20 15:31:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,22 +6,26 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
 
-    scraped_students = []
+    scraped_student_array = []
 
     index_page.css(".student-card").each do |student|
       student_info = {}
       student_info[:name] = student.css(".student-name").text
       student_info[:location] = student.css(".student-location").text
       student_info[:profile_url] = student.css("a").attribute("href").text
-      scraped_students << student_info
+      scraped_student_array << student_info
       #binding.pry
     end
-    scraped_students
+    scraped_student_array
 
   end
 
   def self.scrape_profile_page(profile_url)
     # is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student (FAILED - 2)
+    profile_page = Nokogiri::HTML(open(profile_url))
+
+    profile
+
     # can handle profile pages without all of the social links (FAILED - 3)
   end
 
