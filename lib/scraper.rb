2c0419298b547d8dd51f22b6beb4d10e2c1d4060
GusVilla303 <gusvilla303@gmail.com>
2017-09-26 12:54:04 -0600

    Add self.scrape_index_page to Scraper. Spec passes.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,17 +6,16 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
 
+    students = []
 
-    binding.pry
-
-    # this is just an example: 
-    # doc.css(".post").each do |post|
-    #   student = Student.new
-    #   student.title = post.css("h2").text
-    #   student.schedule = post.css(".date").text
-    #   student.description = post.css("p").text
-    # end
-
+    doc.css(".student-card").each do |student_card|
+    students << {
+        name: student_card.css(".student-name").text,
+        location: student_card.css(".student-location").text,
+        profile_url: student_card.css("a").attribute("href").value
+      }
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
