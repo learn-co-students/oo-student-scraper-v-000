0a20683a8aa16e2b59db4d87df85fdb854b2c4cf
GusVilla303 <gusvilla303@gmail.com>
2017-09-26 09:46:12 -0600

    Start implementing Nokogiri in Scraper.scrape_index_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,23 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+
+
+    binding.pry
+
+    # this is just an example: 
+    # doc.css(".post").each do |post|
+    #   student = Student.new
+    #   student.title = post.css("h2").text
+    #   student.schedule = post.css(".date").text
+    #   student.description = post.css("p").text
+    # end
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
