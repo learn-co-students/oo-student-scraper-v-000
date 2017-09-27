1ab7b287aad82035e1ccbaac0a84c33aadd75f9d
ndalcin <dalcin.nicole@gmail.com>
2017-09-27 06:55:52 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
       students << new_student = {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
-        :profile_url => student.css("a").attr("href").value 
+        :profile_url => student.css("a").attr("href").value
       }
     end
     students
@@ -20,19 +20,18 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student_site = Nokogiri::HTML(open(profile_url))
-
        #gets links:
-      link_array = student_site.css(".social-icon-container").children.css("a").collect {|link| link.attribute('href').value}
+    link_array = student_site.css(".social-icon-container").children.css("a").collect {|link| link.attribute('href').value}
 
-      profile_page = {
-        :twitter => link_array.find {|link| link.include?("twitter")},
-        :linkedin => link_array.find {|link| link.include?("linkedin")},
-        :github => link_array.find {|link| link.include?("github")},
-        :blog => link_array.find {|link| !link.include?("twitter") && !link.include?("linkedin") && !link.include?("github")},
-        :profile_quote => student_site.css(".vitals-text-container .profile-quote").text,
-        :bio => student_site.css(".description-holder p").text
-      }
-      profile_page.delete_if{|k,v| v == nil}
+    profile_page = {
+      :twitter => link_array.find {|link| link.include?("twitter")},
+      :linkedin => link_array.find {|link| link.include?("linkedin")},
+      :github => link_array.find {|link| link.include?("github")},
+      :blog => link_array.find {|link| !link.include?("twitter") && !link.include?("linkedin") && !link.include?("github")},
+      :profile_quote => student_site.css(".vitals-text-container .profile-quote").text,
+      :bio => student_site.css(".description-holder p").text
+    }
+    profile_page.delete_if{|k,v| v == nil}
   end
 
 end
