9e2d30179c7315436aa43319cad57cf4ffb390d4
GusVilla303 <gusvilla303@gmail.com>
2017-09-26 18:11:52 -0600

    Add self.scrape_profile_page to Scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,14 +21,30 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
 
-    student_hash = {
-      twitter: doc.css(".social-icon-container a")[0].attribute("href").value,
-      linkedin: doc.css(".social-icon-container a")[1].attribute("href").value,
-      github: doc.css(".social-icon-container a")[2].attribute("href").value,
-      blog: doc.css(".social-icon-container a")[3].attribute("href").value,
-      profile_quote: doc.css(".vitals-text-container .profile-quote").text,
-      bio: doc.css(".description-holder p").text
-    }
+    student_hash = {}
+
+    links = doc.css(".social-icon-container a")
+
+    links.each do |l|
+      string = l.attribute("href").value
+
+      case
+      when string.include?("twitter")
+        student_hash[:twitter] = string
+      when string.include?("linkedin")
+        student_hash[:linkedin] = string
+      when string.include?("github")
+        student_hash[:github] = string
+      else
+        student_hash[:blog] = string
+      end
+    end
+
+    student_hash[:profile_quote] = doc.css(".vitals-text-container .profile-quote").text
+    student_hash[:bio] = doc.css(".description-holder p").text
+
+    student_hash
+
 
   end
 
