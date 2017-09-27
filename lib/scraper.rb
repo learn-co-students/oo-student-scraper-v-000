1fd7e9275903b31766327359ab62d138e87fb751
ndalcin <dalcin.nicole@gmail.com>
2017-09-26 21:51:39 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,25 +9,30 @@ class Scraper
     website = Nokogiri::HTML(open(index_url))
     students = []
     website.css(".student-card").each do |student|
-      new_student = {
-        :name => student.css(".card-class-container h4.student-name").text,
-        :location => student.css(".card-class-container p.student-location").text,
-        :profile_url => student.css("")
-
-
-
+      students << new_student = {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").attr("href").value 
       }
-
-
-
+    end
+    students
   end
 
-
-
-
-
   def self.scrape_profile_page(profile_url)
+    student_site = Nokogiri::HTML(open(profile_url))
 
+       #gets links:
+      link_array = student_site.css(".social-icon-container").children.css("a").collect {|link| link.attribute('href').value}
+
+      profile_page = {
+        :twitter => link_array.find {|link| link.include?("twitter")},
+        :linkedin => link_array.find {|link| link.include?("linkedin")},
+        :github => link_array.find {|link| link.include?("github")},
+        :blog => link_array.find {|link| !link.include?("twitter") && !link.include?("linkedin") && !link.include?("github")},
+        :profile_quote => student_site.css(".vitals-text-container .profile-quote").text,
+        :bio => student_site.css(".description-holder p").text
+      }
+      profile_page.delete_if{|k,v| v == nil}
   end
 
 end
