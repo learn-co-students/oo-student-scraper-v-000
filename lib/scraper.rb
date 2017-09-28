675a322979a280e54de4b682bf86fed937075de7
Carley Tripp <carley.tripp@outlook.com>
2017-09-28 04:26:36 +0000

    finished scrape_index_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,27 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index = Nokogiri::HTML(open(index_url))
+    students = []
+
+    index.css(".student-card").each do |student|
+      students << {
+        :name => student.css("a .card-text-container .student-name").text,
+        :location => student.css("a .card-text-container .student-location").text,
+        :profile_url => student.css("a").attribute("href").text
+      }
+    end
+
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
 
+  end
 end
 
+# Scraper.scrape_index_page("./fixtures/student-site/index.html")
