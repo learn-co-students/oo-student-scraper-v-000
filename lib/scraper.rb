00c27e9aed17e636a32f516dbb1574aac1c6d0d6
Carley Tripp <carley.tripp@outlook.com>
2017-09-29 03:55:51 +0000

    finished scrape_profile_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
       students << {
         :name => student.css("a .card-text-container .student-name").text,
         :location => student.css("a .card-text-container .student-location").text,
-        :profile_url => student.css("a").attribute("href").text
+        :profile_url => student.css("a").attribute("href").value
       }
     end
 
@@ -20,7 +20,26 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    profile = Nokogiri::HTML(open(profile_url))
+    students_data = {}
 
+    students_data[:profile_quote] = profile.css(".vitals-container .vitals-text-container .profile-quote").text
+    students_data[:bio] = profile.css(".details-container .bio-block .bio-content .description-holder p").text
+
+    profile.css(".social-icon-container a").each do |social|
+      case
+      when social.attributes["href"].value.include?("twitter")
+        students_data[:twitter] = social.attribute("href").value
+      when social.attributes["href"].value.include?("linkedin")
+        students_data[:linkedin] = social.attribute("href").value
+      when social.attributes["href"].value.include?("github")
+        students_data[:github] = social.attribute("href").value
+      else
+        students_data[:blog] = social.attribute("href").value
+      end
+    end
+
+    students_data
   end
 end
 
