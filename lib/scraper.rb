31bd77f0d7e2bcb8ac9361675e01a960cd87f4a5
Melissa Amaya <amaya.melissa@gmail.com>
2017-10-02 16:34:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,60 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    profiles = {}
+    profile = open(profile_url)
+    doc = Nokogiri::HTML(profile)
+    quote = []
+    social = doc.css(".social-icon-container a")
+      social.each do
+        |kid|  value = kid.attributes['href'].value
+        case value
+        when /linkedin/
+          @linkedin = value
+        when /twitter/
+          @twitter = value
+        when /github/
+          @github = value
+        else
+          @blog = value
+        end
+    end
 
-  end
+    @quote = doc.css(".profile-quote").text
+    @bio = doc.css(".description-holder p").text
+
+    profiles = {
+    :twitter => @twitter ,
+    :linkedin => @linkedin,
+    :github => @github,
+    :blog => @blog,
+    :profile_quote => @quote,
+    :bio => @bio
+    }
+
+    profiles
+  
+end
+
+#iterate through url's
+#social.each {|kid| puts kid.attributes['href'].value}
+
+  #1. get into student data
+  #2. establish a temporary variable to store each value while iterating through
+  #3. If .attributes['href'].value includes -- twitter, linkedin, github  == store that value in that variable
+      #case statement for this?
+  #4. target bio directly
+  #5. 2-part process for quote.  Target quote, store in array.  target author of quote, store in array.
+      #--> combine array into string and store in variable
+  #6. put all these items into hash array
+    # student.each do |entry|
+    #   binding.pry
+    #   profile = student.at(".social-icon-container a")
+    #   profile.each {|val| puts val}
+
+# .attributes['href'].value
+
+
+
 
 end
