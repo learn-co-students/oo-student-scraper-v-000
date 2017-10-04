a7dad22321e13714202d4339af244eed8204fadf
Wendy Lau <wlau903@gmail.com>
2017-10-04 17:56:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css("div.student-card")
+    student_hashes = []
+
+    students.each do |student|
+      student_hash = {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").attribute("href").value
+      }
+      student_hashes << student_hash
+    end
+    student_hashes
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    social_links = doc.css("div.social-icon-container a").map { |link| link.attribute("href").value }
+    profile = {}
+
+    social_links.each do |link|
+      if link.include?("twitter")
+        profile[:twitter] = link
+      elsif link.include?("linkedin")
+        profile[:linkedin] = link
+      elsif link.include?("github")
+        profile[:github] = link
+      else link.include?("blog")
+        profile[:blog] = link
+        #binding.pry
+      end
+    end
+    profile[:profile_quote] = doc.css("div.profile-quote").text
+    profile[:bio] = doc.css("div.description-holder p").text
+    profile
   end
 
 end
-
