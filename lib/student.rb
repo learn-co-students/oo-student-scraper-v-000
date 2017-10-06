5ed503e8a58105df45a27ada212ce93b4ba57e08
Lauren Feld <lauren.feld@aol.com>
2017-10-06 01:15:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,30 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
+    @name = student_hash.values_at(:name).join
+    @location = student_hash.values_at(:location).join
+    @profile_url = student_hash.values_at(:profile_url).join
     @@all << self
   end
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      Student.new(student)
+    new_student = self.new(student)
+    new_student
+   end
   end
-end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attributes|
-    @twitter = attributes_hash[:twitter]
-    @linkedin = attributes_hash[:linkedin]
-    @github = attributes_hash[:github]
-    @blog = attributes_hash[:blog]
-    @profile_quote = attributes_hash[:profile_quote]
-    @bio = attributes_hash[:bio]
+   @twitter = attributes_hash.values_at(:twitter).join
+   @linkedin = attributes_hash.values_at(:linkedin).join
+   @github = attributes_hash.values_at(:github).join
+   @blog = attributes_hash.values_at(:blog).join
+   @profile_quote = attributes_hash.values_at(:profile_quote).join
+   @bio = attributes_hash.values_at(:bio).join
   end
-  self
-end
 
   def self.all
-    @@all
+   @@all
   end
 end
