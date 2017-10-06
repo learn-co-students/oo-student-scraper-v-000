5fd3b3ede0b2d2f65e3ae8d74048b31dd12793bb
John Tuttle <thejwt@gmail.com>
2017-10-06 21:59:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,76 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    people = []
+    #binding.pry
+    #doc.css(".student-name").first.text
+    #doc.css(".student-location").first.text
+    #doc.css("div.student-card a").attribute("href").value
+    doc.css(".student-card").each do |person|
+
+      people << {
+          :name => person.css(".student-name").text,
+          :location => person.css(".student-location").text,
+          :profile_url => person.css("a").attribute("href").value
+        }
+    end
+    people
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    student_bio = []
+    twitter = ""
+    linkedin = ""
+    github = ""
+    blog = ""
+    temp_string =""
+    #binding.pry
+    #doc.css(".vitals-text-container .profile-name").text
+    #doc.css(".social-icon-container a img").attribute("src").value
+    doc.css(".social-icon-container a").each do |person|
+      temp_string = person.values
+      if temp_string.to_s.include?("twitter")
+        twitter = temp_string
+      elsif temp_string.to_s.include?("linkedin")
+        linkedin = temp_string
+      elsif temp_string.to_s.include?("github")
+        github = temp_string
+      else
+        blog = temp_string
+      end
+    end
+
+    person_info = {}
+    profile_quote = doc.css(".vitals-text-container div").text.to_s
+    bio = doc.css(".description-holder p").first.text
+
+
+    if twitter != ""
+      person_info[:twitter] = twitter.join
+    end
+    if linkedin != ""
+      person_info[:linkedin] = linkedin.join
+    end
+    if github != ""
+      person_info[:github] = github.join
+    end
+    if blog != ""
+      person_info[:blog] = blog.join
+    end
+    if profile_quote != ""
+      person_info[:profile_quote] = profile_quote
+    end
+    if bio != ""
+      person_info[:bio] = bio
+    end
+
+    person_info
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,80 @@
+require 'pry'
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    if student_hash.has_key?(:name)
+      @name = student_hash[:name]
+    end
+    if student_hash.has_key?(:location)
+      @location = student_hash[:location]
+    end
+    if student_hash.has_key?(:twitter)
+      @twitter = student_hash[:twitter]
+    end
+    if student_hash.has_key?(:linkedin)
+      @linkedin = student_hash[:linkedin]
+    end
+    if student_hash.has_key?(:github)
+      @github = student_hash[:github]
+    end
+    if student_hash.has_key?(:blog)
+      @blog = student_hash[:blog]
+    end
+    if student_hash.has_key?(:profile_url)
+      @profile_url = student_hash[:profile_url]
+    end
+    if student_hash.has_key?(:bio)
+      @bio = student_hash[:bio]
+    end
+    if student_hash.has_key?(:profile_quote)
+      @profile_quote = student_hash[:profile_quote]
+    end
+
+    @@all << self
+
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student = Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    if attributes_hash.has_key?(:name)
+      @name = attributes_hash[:name]
+    end
+    if attributes_hash.has_key?(:location)
+      @location = attributes_hash[:location]
+    end
+    if attributes_hash.has_key?(:twitter)
+      @twitter = attributes_hash[:twitter]
+    end
+    if attributes_hash.has_key?(:linkedin)
+      @linkedin = attributes_hash[:linkedin]
+    end
+    if attributes_hash.has_key?(:github)
+      @github = attributes_hash[:github]
+    end
+    if attributes_hash.has_key?(:blog)
+      @blog = attributes_hash[:blog]
+    end
+    if attributes_hash.has_key?(:profile_url)
+      @profile_url = attributes_hash[:profile_url]
+    end
+    if attributes_hash.has_key?(:bio)
+      @bio = attributes_hash[:bio]
+    end
+    if attributes_hash.has_key?(:profile_quote)
+      @profile_quote = attributes_hash[:profile_quote]
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
-
