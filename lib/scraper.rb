c41dcfa5e49996ce4415c2216efd1801e4445cf3
Susan Fowler <infopro94@gmail.com>
2017-10-09 18:44:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,37 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::Open-URI(open(http://174.138.35.103:48617/fixtures/student-site/))
+    Open-URI = File.read('174.138.35.103:48617/fixtures/student-site/')
+
+    student_scraper('filename').each do |project|
+      title = project.#blahblahblah
+    students = {
+      :name => ,
+      :location => ,
+      :profile_url =>
+    }
+
   end
 
+  # projects
+  # end
+
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::Open-URI(open(
+    ))
+    {
+    :twitter=> #url,
+    :linkedin=> #URL,
+    :github=> #URL,
+    :blog=> #url,
+    :profile_quote=>,
+    :bio=>
+  }
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,23 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all<<self
   end
 
-  def self.create_from_collection(students_array)
-    
+  def self.create_from_collection(students_array) #Student .create_from_collection uses the Scraper class to create new students with the correct name and location.
+
   end
 
-  def add_student_attributes(attributes_hash)
-    
+  def add_student_attributes(attributes_hash) #Student #add_student_attributes uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
+
   end
 
-  def self.all
-    
+  def self.all #Student .all returns the class variable @@all
+    @@all
   end
 end
-
