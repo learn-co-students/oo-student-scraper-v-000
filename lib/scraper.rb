93349ed18826a6474f7c2955a71dd7a10fa1f9f1
Susan Fowler <infopro94@gmail.com>
2017-10-10 02:33:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,27 +24,25 @@ class Scraper
       students
     end
 
-
       def self.scrape_profile_page(profile_url)
-        student = {}
-        doc = Nokogiri::HTML(open(profile_url))
-          links = profile.css(".social-icon-container").children.css("a").map {|e| e.attribute('href').value}
-          links.each.do |link|
-          if link.include?("twitter")
-            student[:twitter] = link
-          elsif link.include?("linkedin")
-            student[:linkedin] = link
-          elsif link.include?("github")
-            student[:github] = link
-          else
-            student[:blog] = link
-          end
-        end
+          student = {}
+          doc = Nokogiri::HTML(open(profile_url))
+          links = doc.css(".social-icon-container").children.css("a").map { |l| l.attribute('href').value }
+          links.each do |link|
+           if link.include?("twitter")
+             student[:twitter] = link
+           elsif link.include?("linkedin")
+             student[:linkedin] = link
+           elsif link.include?("github")
+             student[:github] = link
+           else
+             student[:blog] = link
+           end
+         end
+        
+         student[:profile_quote] = doc.css(".profile-quote").text if doc.css(".profile-quote")
+         student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text if doc.css("div.bio-content.content-holder div.description-holder p")
 
-          student[:profile_quote] = profile.css(".profile-quote").text if profile_page.css(".profile-quote"),
-            student[:bio] = profile.css("div.bio-content.content-holder div.description-holder p").text if profile.css("div.bio-content.content-holder div.description-holder p")
-        }
-
-        student
-      end
+    student
+  end
 end
--- lib/student.rb
+++ lib/student.rb
@@ -9,15 +9,21 @@ class Student
     @@all<<self
   end
 
-  def self.create_from_collection(students_array) #Student .create_from_collection uses the Scraper class to create new students with the correct name and location.
-
+  def self.create_from_collection(students_array) # uses the Scraper class to create new students with the correct name and location.
+    students_array.each do |student_hash|
+      Student.new(student_hash)
   end
 
-  def add_student_attributes(attributes_hash) #Student #add_student_attributes uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
-
+  def add_student_attributes(attributes_hash) # uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
   end
 
   def self.all #Student .all returns the class variable @@all
     @@all
   end
+
+end
 end
