0d40c003385353e629aa8adc189c193934fd595c
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-11 00:28:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,10 +17,25 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    html = File.read(index_url)
+    html = File.read(profile_url)
+    name = profile_url.split("/").pop.gsub(".html", "").split("-").first.to_sym
 
-    student_hash = Nokogiri::HTML(html)
-    student_index_array = {}
+    student = Nokogiri::HTML(html)
+    student_hash = {}
+
+    student.css("div.social-icon-container a").each do |icon|
+      icon_name = icon.attribute("href").value.split(/:\/\//).pop.split(".com").shift.gsub("www.", "")
+      if icon_name == "twitter" || icon_name == "linkedin" || icon_name == "github" || icon_name == "youtube"
+        student_hash[icon_name.to_sym] = icon.attribute("href").value
+      else
+        student_hash[:blog] = icon.attribute("href").value
+      end
+    end
+
+    student_hash[:profile_quote] = student.css("div.profile-quote").text
+    student_hash[:bio] = student.css("div.bio-content.content-holder div.description-holder p").text
+  #  student_hash = {twitter: icon_array[0], linkedin: icon_array[1], github: icon_array[2], blog: icon_array[3]}
+  student_hash
 
   end
 
