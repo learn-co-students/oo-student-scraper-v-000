67ead458a4a4b13ecdbfcb1bce7d349775ec8c4f
Gui Bin Zhang <guibzhang@gmail.com>
2017-10-11 05:05:09 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,52 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #open the URL
+    doc = Nokogiri::HTML(open(index_url))
+    #created an array
+    scraped_students = []
+    #iterate
+    doc.css("div.student-card").each do |info|
+      #hash
+      student_hash = Hash.new
+
+      student_hash[:name] = info.css(".student-name").text
+      student_hash[:location] = info.css(".student-location").text
+      student_hash[:profile_url] = info.css("a").attr("href").value
+
+      #finish iteration and shovels into array
+      scraped_students << student_hash
+    end
+    scraped_students
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    scraped_students = []
+    student_hash = Hash.new
+
+    doc.css('.vitals-container .social-icon-container a').each do |social|
+      if social.css('img').attr('src').value.include?("twitter")
+        student_hash[:twitter] = social.attr('href')
+      elsif social.css('img').attr('src').value.include?("linkedin")
+        student_hash[:linked] = social.attr('href')
+      elsif social.css('img').attr('src').value.include?("github")
+        student_hash[:github] = social.attr('href')
+      elsif social.css('img').attr('src').value.include?("rss")
+        student_hash[:blog] = social.attr('href')
+      end
+
+      doc.css('html').each do |details|
+      student_hash[:profile_quote] = doc.css(".profile-quote").text.strip
+      student_hash[:bio] = doc.css(".description-holder").css("p").text
+
+    end
+
+
+      student_hash
+    end
+
   end
 
 end
-
