66d08a0404a7ec695e8c659cd9fc6b6e9fe2b399
Denis Bondin <dsbondin@gmail.com>
2017-10-12 11:15:08 +0000

    started scrape_profile_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     html = Nokogiri::HTML(open(index_url))
     students = []
 
-    html.css("div.student-card").each do |student|
+    html.css("div.roster-cards-container div.student-card").each do |student|
       students << {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
@@ -20,6 +20,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    profile_page = Nokogiri::HTML(open(profile_url))
 
   end
 
