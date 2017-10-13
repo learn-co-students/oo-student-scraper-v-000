a62af929e29d0a145faed76a29d10adc7925f21c
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-13 00:36:34 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,25 +7,29 @@ require_relative './student.rb'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    get_page
+    get_students
   end
 
   def self.scrape_profile_page(profile_url)
-
+    get_page
+    
   end
 
-    def get_page(url)
-       doc = Nokogiri::HTML(open("url"))
-     end
+  def get_page(url)
+     doc = Nokogiri::HTML(open("url"))
+  end
 
-     def get_courses
-     self.get_page.css(".post")
+  def get_students
+    self.get_page.css(".post")
      # this returns the array
-   end
+  end
+   #
+  #  return array of hashes with
+  #  :name :location, :profile_url
+   #
+  #  return :linkedin  :github  :blog  :profile_quote :bio
 
-   return array of hashes with
-   :name :location, :profile_url
-
-   return :linkedin  :github  :blog  :profile_quote :bio
+    # doc = Nokogiri::HTML(open("index_url"))
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,28 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+ #use metaprogramming to assign the newly created student attributes and values by the key/values pairs of the hash
+ #this will use #send. also, add the newly created student to the @@all array.
+    #@@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #iterates over the array of hashes and crates a new individaul student using each hash.
+    #argument is: the return value of the scraper.scrape_index_page
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
 
+# Scraper.new.print_students
