2b84e30210f858f73237474ace57a00959372b88
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-13 02:33:59 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,32 +2,45 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative './student.rb'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open("./fixtures/student-site/"))
-    info = {}
-    doc.css(".roster-cards-container").each do |details|
-      name = details.css("").text
-        details[name] = {
-          :location => details.css("").text
-          :profile_url => details.css(" ..... a img").attribute("src").value
+    doc = Nokogiri::HTML(open(index.url))#can be refactored later, "./fixtures/student-site/index.html"
+    students = []
+    doc.css("div.roster-cards-container"). each do |c|
+      c.css(".student-card a").each do |student|
+         student_profile = "#{student.attr('href')}"
+         student_name = student.css(".student-name").text
+         student_location = student.css(".student-location").text
+       end
+     end
+   end
 
  end
+ Scraper.new.scrape_index_page
 
-  :name :location, :profile_url
-    get_students
-    #/fixtures/student-site/
-  end
+     #".card-text-container"
+    # info = {}
+    # doc.css(".roster-cards-container").each do |details|
+    #   name = details.css("").text
+    #     details[name] = {
+    #       :location => details.css("").text
+          # :profile_url => details.css(" ..... a img").attribute("src").value
 
 
-
-  def get_students
-    self.get_page.css
-     # this returns the array
-  end
+  #
+  # :name :location, :profile_url
+  #   get_students
+  #   #/fixtures/student-site/
+  # end
+  #
+  #
+  #
+  # def get_students
+  #   self.get_page.css
+  #    # this returns the array
+  # end
    #
   #  return array of hashes with
   #  :name :location, :profile_url
@@ -35,9 +48,7 @@ class Scraper
   #  return :linkedin  :github  :blog  :profile_quote :bio
 
     # doc = Nokogiri::HTML(open("index_url"))
-
-end
-def self.scrape_profile_page(profile_url)
-  get_page
-
-end
+#
+# end
+# def self.scrape_profile_page(profile_url)
+#   get_page
