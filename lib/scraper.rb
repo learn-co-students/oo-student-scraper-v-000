e5c387ccb4ccb3b87723c2ccf5ce520de391fe04
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-13 18:31:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,10 +8,10 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))# "./fixtures/student-site/index.html"
     students = []
-    binding.pry
+  binding.pry
     doc.css("div.roster-cards-container").each do |c|
       c.css(".student-card a").each do |student|
-         student_profile = "#{student.attr('href')}"
+         student_profile = "students/#{student}.htm).attr('href')}"
          student_name = student.css(".student-name").text
          student_location = student.css(".student-location").text
        end
@@ -19,22 +19,30 @@ class Scraper
     students
      # {:name => student_name, :location => student_location, :profile_url => student_profile}
   end
+  #
+  # def self.scrape_profile_page(profile_url)#  #students/fname-lname.html      ./fixtures/student-site/students
+  #   doc = Nokogiri::HTML(open(profile.url))
+  #   binding.pry
+  #   each_student = []
+  #   doc.css(". ").each do |student|
+  #   #   student.css(".social-icon-container ").each do|s|
+    #     a.attr('href')
+    #these 3 are(".social-icon-container").children.map do |social|
+    #      student_twitter =href .src("")
+    #     student_linkedin =  a href  .src("")
+    #     student_github =  a href   .src("")
 
-  def self.scrape_profile_page(profile_url)#./fixtures/student-site/students
-    doc = Nokogiri::HTML(open(profile.url))
-    binding.pry
-    profiles = []
-    doc.css(". ").each do |student|
-    #   student.css(".social-icon-container ").each do|s|
-    #     student_twitter =
-    #     student_linkedin =  a href
-    #     student_github =  a href
     #     student_blog =
+
+    #    (".details-container description-holder")
     #     student_bio =
+
+    #    (".vitals-text-container profile-quote")
     #     student_profile_quote = s.css(".profile-quote").text
-    #     student_blog =
+    #
     #   end
     # end
     #profiles
-  end
+#   end
+# end
 end
