8dc0453808c97cc2a0b4a23f1cd7023bc1b5fe72
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-13 14:20:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,17 +8,37 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index.url))#can be refactored later, "./fixtures/student-site/index.html"
     students = []
-    doc.css("div.roster-cards-container"). each do |c|
+    binding.pry
+    doc.css("div.roster-cards-container").each do |c|
       c.css(".student-card a").each do |student|
          student_profile = "#{student.attr('href')}"
          student_name = student.css(".student-name").text
          student_location = student.css(".student-location").text
        end
      end
+     students
    end
-
  end
- Scraper.new.scrape_index_page
+#   def self.scrape_profile_page(profile_url)#./fixtures/student-site/students
+#     doc = Nokogiri::HTML(open(profile.url))
+  binding.pry
+#     profiles = []
+#     doc.css(" ").each do |student|
+#       student.css
+#         student_linkedin =
+#         student_github =
+#         student_blog =
+#         student_profile_quote =
+#         student_blog =
+# =>    end
+      end
+      students
+    end
+# # return :linkedin  :github  :blog  :profile_quote :bio
+#
+#   end
+
+ # Scraper.new.scrape_index_page
 
      #".card-text-container"
     # info = {}
