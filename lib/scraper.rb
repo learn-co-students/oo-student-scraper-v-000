21dd30726f41c7635567dd98d1e0250b5463d893
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-10-16 23:18:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    result = []
+     doc = Nokogiri::HTML(open(index_url))
+     doc.css(".student-card").each do |tag|
+       hash = {:name => tag.css("h4").text,
+              :location => tag.css("p").text,
+              :profile_url => tag.at_css("a").first[1]}
+       result << hash
+     end
+     result
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
