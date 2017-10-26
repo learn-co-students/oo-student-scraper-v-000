82ee01c0028a4652a4014f430db351946ea72bf7
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-10-26 13:40:59 +0000

    local variable doc created

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
