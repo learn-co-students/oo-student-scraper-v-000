41e90ecb2086bd1ab5b48eb2e0ef1e7030a30331
Adam Fudge <coolfudge@gmail.com>
2017-10-27 16:47:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,28 +2,33 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+# Joe Burgess
+# Mathieu Balez
+# Diane Vu
   def self.scrape_index_page(index_url)
-    # doc = Nokogiri::HTML(open(index_url))
-    # return_array_of_hash = []
-    # students = doc.css(".student-card")
-    #
-    # students.each do |student|
-    #   new_student_hash = {:name=>"", :location=>"", :profile_url=>""}
-    #
-    #   new_student_hash[:name] = student.css("h4").text
-    #   new_student_hash[:location] = student.css("p").text
-    #   new_student_hash[:profile_url] = "./fixtures/student-site/" + student.css("a").attribute("href").value
-    #
-    #   return_array_of_hash << new_student_hash
-    # end
-    #
-    # return_array_of_hash
+    doc = Nokogiri::HTML(open(index_url))
+    return_array_of_hash = []
+    students = doc.css(".student-card")
+
+# index_hash = doc.css(".student-card").map do |student|
+
+    students.each do |student|
+      new_student_hash = {}
+# binding.pry
+      new_student_hash[:name] = student.css("h4").text
+      new_student_hash[:location] = student.css("p").text
+      new_student_hash[:profile_url] = "./fixtures/student-site/" + student.css("a").attribute("href").value
+
+      return_array_of_hash << new_student_hash
+    end
+
+    return_array_of_hash
   end
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     return_hash = {}
+
     social_container = doc.css(".social-icon-container").children.css("a")
 
     social_container.each do |placeholder|
