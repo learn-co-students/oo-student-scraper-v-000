d01ead22469fc89d32c556cb4e618ca5d342ddc8
Kristen Peck <kpeck71@gmail.com>
2017-11-01 00:27:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,51 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
+  attr_accessor :name, :location, :profile_url, :twitter, :linkedin, :github, :profile_quote, :bio, :blog
+
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css("div.student-card").each do |kids|
+       student_data = {
+        :name => kids.css("h4.student-name").text,
+        :location => kids.css("p.student-location").text,
+        :profile_url => kids.css("a").attribute("href").value
+      }
+      students << student_data
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    profile = {}
+    # links = doc.css(".social-icon-container a")
+    #
+    #     profile = {:twitter => links.css("a").attribute("href").value} #only returns twitter url (first in that container)
+    #
+    #     profile = {:linkedin => links["href"]}
+    #
+    #     profile = {:github => links["href"]}
+    #
+    #     profile = {:blog => links["href"]}
+
+      social = doc.css(".social-icon-container a").collect do |link|
+        if link["href"].include?("twitter.com")
+          profile[:twitter] = link["href"]
+        elsif link["href"].include?("linkedin.com")
+          profile[:linkedin] = link["href"]
+        elsif link["href"].include?("github")
+          profile[:github] = link["href"]
+        else
+          profile[:blog] = link["href"]
+        end
+      end
+      profile[:bio] = doc.css(".description-holder p").text
+      profile[:profile_quote] = doc.css(".profile-quote").text
+      profile
   end
-
 end
-
