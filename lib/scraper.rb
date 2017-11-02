7dbadb17bec6c70ecbcd08ef9ae6e853cf043438
Jessica Bensch <github email address>
2017-11-01 20:16:35 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
