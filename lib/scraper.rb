ec7fbd23abd3e0787407e45e4005b51012299735
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-11-22 18:55:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,11 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    
+
+    students = doc.css(".student-card")
+    name = doc.css(".student-name").text
+    location = doc.css(".student-location").text
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
