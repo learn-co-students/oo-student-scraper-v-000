1ebe53a43b0133681ff8cd7ce22ef99762767f7c
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-11-22 19:37:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,35 +20,25 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    twitter = nil
-    linkedin = nil
-    github = nil
-    blog = nil
-    profile_quote = doc.css(".profile-quote").text
-    bio = doc.css(".bio-content p").text
+    attributes = {}
 
     social = doc.css(".social-icon-container a")
     social.each {|link|
       link_value = link.attribute("href").value
       if link_value.include?("twitter")
-        twitter = link_value
+        attributes[:twitter] = link_value
       elsif link_value.include?("linkedin")
-        linkedin = link_value
+        attributes[:linkedin] = link_value
       elsif link_value.include?("github")
-        github = link_value
+        attributes[:github] = link_value
       else
-        blog = link_value
+        attributes[:blog] = link_value
       end
     }
-    {
-      :twitter => twitter,
-      :linkedin => linkedin,
-      :github => github,
-      :blog => blog,
-      :profile_quote => profile_quote,
-      :bio => bio
-    }
-    #binding.pry
+    attributes[:profile_quote] = doc.css(".profile-quote").text
+    attributes[:bio] = doc.css(".bio-content p").text
+
+    attributes
   end
 
 end
