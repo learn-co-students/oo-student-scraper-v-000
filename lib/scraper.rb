433f70b6779052560ffc86e183a91675fda10db2
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2017-11-22 18:42:27 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,16 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
