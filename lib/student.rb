ef8b2e76a19656d5e4334bc8ced7940bc93fdc30
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-01 19:53:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    self
   end
 
   def self.all
