ecbdfb6d56498112bc04fee6edf053708239333f
Blakeley Bond <burrick2003@yahoo.com>
2017-12-06 03:53:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,15 +15,19 @@ class Student
   end
 
   #Student .create_from_collection uses the Scraper class to create new students with the correct name and location.
-  def self.create_from_collection(students_array)
+  def self.create_from_collection(students_array) #array of hashes, 1D eachdo
     students_array.each do |student_hash|
-      Student.new(student_hash)
+      Student.new(student_hash) #generates the partial info on the index page for each student
     end
   end
 
 #Student #add_student_attributes uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
-  def add_student_attributes(attributes_hash)
+  def add_student_attributes(attributes_hash) #hash of hashes, 2D eachdo
+    attributes_hash.each do |newkey, newvalue|
+      self.send(("#{newkey}="), newvalue) #self is preexisting student in the attributes_hash.  Assume this is for the additional info on profile page
+    end
 
+    self #per lesson
   end
 
 #Student .all returns the class variable @@all
