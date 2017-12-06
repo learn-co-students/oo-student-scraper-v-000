bc252c7f43a7f648db2865197e53e9f65ec14bc6
github username <github email address>
2017-12-05 23:09:11 -0500

    create Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,11 +3,10 @@ require 'pry'
 
 class Scraper
 
-
-
   def self.scrape_index_page(index_url)
     page = Nokogiri::HTML(open(index_url))
     students = []
+
     page.css(".student-card").each_with_index do |student_card, i|
       students << {
         :name => student_card.css(".student-name").text,
@@ -15,12 +14,14 @@ class Scraper
         :profile_url => student_card.css("a")[0]["href"]
       }
     end
+
     students
   end
 
   def self.scrape_profile_page(profile_url)
     page = Nokogiri::HTML(open(profile_url))
     student_profile = {}
+
     page.css(".social-icon-container a").each do |link|
       if link["href"].include?("twitter")
         student_profile[:twitter] = link["href"]
@@ -32,10 +33,10 @@ class Scraper
         student_profile[:blog] = link["href"]
       end
     end
+
     student_profile[:profile_quote] = page.css(".profile-quote").text
     student_profile[:bio] = page.css(".bio-content .description-holder").text.strip
     student_profile
-
   end
 
 end
