70c78879f53070647f794a46b7d4283cca0a553c
github username <github email address>
2017-12-09 18:57:35 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,3 +26,12 @@ kickstarter.css("li.project.grid_4").each do |project|
     :location => project.css("ul.project-meta span.location-name").text,
     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   }
+
+  # kickstarter.css("li.project.grid_4").each do |project|
+  #   title = project.css("h2.bbcard_name strong a").text
+  #   projects[title.to_sym] = {
+  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+  #     :description => project.css("p.bbcard_blurb").text,
+  #     :location => project.css("ul.project-meta span.location-name").text,
+  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  #   }
