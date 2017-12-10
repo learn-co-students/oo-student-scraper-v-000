4185b32d12726b0775305580e7065938d18d625e
github username <github email address>
2017-12-09 18:55:21 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,6 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
     
     
   end
@@ -15,3 +14,15 @@ class Scraper
 
 end
 
+scrape information from index and instantiate students
+
+=> {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "students/abby-smith.html"}
+
+kickstarter.css("li.project.grid_4").each do |project|
+  title = project.css("h2.bbcard_name strong a").text
+  projects[title.to_sym] = {
+    :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+    :description => project.css("p.bbcard_blurb").text,
+    :location => project.css("ul.project-meta span.location-name").text,
+    :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  }
