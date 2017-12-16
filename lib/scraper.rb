086720b16cfc0d60f19053d31812c61718a741b5
Chad Montoya <chadmontoya21@yahoo.com>
2017-12-16 06:16:16 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    binding.pry
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css("h4.student-name")
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
