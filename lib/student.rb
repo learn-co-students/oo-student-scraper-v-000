80fd7f61160175bebb02eb6dc6b7e89c94154641
github username <github email address>
2017-12-31 14:12:08 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,9 @@ class Student
 #takes in an argument of a hash and sets that new student's
 #attributes using the key/value pairs of that hash.
     student_hash.each do |key, value|
+#takes in an argument of a hash and uses metaprogramming to assign the
+#newly created student attributes and values in accordance with the
+#key/value pairs of the hash. Uses the #send method to acheive this.
       self.send("#{key}=", value)
     end
 #adds that new student to the Student class' collection of
@@ -19,16 +22,23 @@ class Student
 #uses the Scraper class to create new students with the
 #correct name and location.
   def self.create_from_collection(students_array)
+#iterates over the array of hashes and creates a new
+#individual student using each hash
     students_array.each do |student_hash|
       Student.new(student_hash)
     end
   end
 
-
-
+#uses the Scraper class to get a hash of a given students
+#attributes and uses that hash to set additional attributes for that student.
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+      end
+    self
   end
+
+#returns the class variable @@all  end
 
   def self.all
     @@all
