5a86bfa08e175b4b806da7460dda46047b004da2
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-01-02 19:04:34 -0500

    Implemented Scraper.scrape_index_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    student_cards = index_page.css(".student-card")
+    student_data = []
+    student_cards.each_with_index do |student_card, index|
+      student_data[index] = {
+        :name => student_card.at_css('.student-name').inner_html,
+        :location => student_card.at_css('.student-location').inner_html,
+        :profile_url => student_card.at_css("a").attributes['href'].value
+      }
+    end
+    student_data
   end
 
   def self.scrape_profile_page(profile_url)
