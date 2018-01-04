d5b7ebfd54e5e80fd1a3e22442158d8da2c48368
Adjoa Darien <adjoa.darien@gmail.com>
2018-01-04 16:48:45 +0000

    .scrape_profile_page pulls social links from student site

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     student_index_array = []
     doc = Nokogiri::HTML(open(index_url))
 
-    doc.css("div.student-card").each_with_index do |student, index|
+    doc.css('div.student-card').each_with_index do |student, index|
       name = student.css('.card-text-container .student-name').text
       location = student.css('.card-text-container .student-location').text
       profile_url = student.css('a').attribute('href').value
@@ -24,15 +24,25 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    scraped_student = {}
     doc = Nokogiri::HTML(open(profile_url))
+
+    socials = doc.css('div.social-icon-container').collect do |social|
+      social.css('a').attribute('href').value
+    end
 
     profile_quote = doc.css('div.vitals-text-container .profile-quote').text
     bio = doc.css('div.details-container .bio-block .description-holder').text
 
-    scraped_student = {
-      :profile_quote => profile_quote.strip,
-      :bio           => bio.strip
-    }
+    socials.each do |social|
+      # parse social_sym from social_val
+  		# 	add social_sym to scraped_student hash
+  		# 	assign social_sym = social_val
+      puts "#{social}"
+    end
+
+    scraped_student[:profile_quote] = profile_quote.strip
+    scraped_student[:bio]           = bio.strip
 
     scraped_student
   end
