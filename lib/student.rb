10f9267960331035b1088a507f85f6d65f40c248
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-01-03 19:52:42 -0500

    Rspeclemented Student.new.initialize in a way that does not require the Student class to know the student_hash key names ahead of times, instead dynamically defining getter setter pairs as necessary

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,37 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :twitter, :linkedin, :github, :blog, :profile_quote, :bio
+  # attr_accessor :name, :location, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    own_methods = self.methods - Object.methods
+
+    student_hash.each_pair do |k, v|
+      key_setter = "#{k}=".to_sym
+
+      if !own_methods.include?(key_setter)
+        # thanks to https://gist.github.com/davidbella/6918455,
+        # https://www.leighhalliday.com/ruby-metaprogramming-creating-methods
+
+        instance_var_name  = "@" + k.to_s
+
+        # new getter
+        self.class.send(:define_method, k) do
+          instance_variable_get(instance_var_name)
+        end
+
+        # and a corresponding new setter
+        self.class.send(:define_method, key_setter) do |new_value|
+          instance_variable_set(instance_var_name, new_value)
+        end
+      end
+
+      self.send(key_setter, v)
+    end
+
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -17,7 +43,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
