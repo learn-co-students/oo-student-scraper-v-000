ea11235567ef70b26b369d946ad2d3992775ca03
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-01-03 19:56:49 -0500

    Rspeclemented Student.create_from_collection

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| self.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
