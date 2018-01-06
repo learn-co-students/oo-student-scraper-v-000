b23e9159b064211f83772f3d1cde97cbfe098a9f
Araba Taylor <araba.taylor20@gmail.com>
2018-01-06 19:59:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,12 +11,12 @@ def self.scrape_index_page(index_url)
 
   doc.css("div.roster-cards-container").each do |student_value|
   student = {}
-
+  student_value.css("div.student-card")each do |student_attr|
   #binding.pry
   student = {
-  @name => student_value.css("div.student-card")[0].search("h4.student-name").text,
-  @location => student_value.css("div.student-card")[0].search("p.student-location").text,
-  @profile_url => student_value.css("div.student-card")[0].search("a href")
+  :name => search("h4.student-name").text,
+  :location => student_value.css("div.student-card")each.search("p.student-location").text,
+  :profile_url => student_value.css("div.student-card")each.search("a href")
   }
 
   students<<student
