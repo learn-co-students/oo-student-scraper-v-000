8f1fa23bdd9e6ac13d5f8207b2220415197dcbec
Araba Taylor <araba.taylor20@gmail.com>
2018-01-06 04:33:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,26 +5,45 @@ class Scraper
 
 attr_accessor :name, :location, :profile_url
 
-  def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
-    students = []
+def self.scrape_index_page(index_url)
+  doc = Nokogiri::HTML(open(index_url))
+  students = []
 
-    doc.css("div.roster-cards-container").each do |student_value|
-    student = {}
-    binding.pry
-    student[title.to_sym] = {
-    student[:name] => student_value.css.("div.student-card").each.search("h4.student-name").text,
-    student[:location] => student_value.search("p.student-location").text,
-    student[:profile_url] => student_value.search("a href")
-    }
+  doc.css("div.roster-cards-container").each do |student_value|
+  student = {}
 
-    students<<students
-    end
-    students
+  #binding.pry
+  student = {
+  @name => student_value.css("div.student-card")[0].search("h4.student-name").text,
+  @location => student_value.css("div.student-card")[0].search("p.student-location").text,
+  @profile_url => student_value.css("div.student-card")[0].search("a href")
+  }
+
+  students<<student
   end
+  students
+end
 
   def self.scrape_profile_page(profile_url)
 
   end
 
+  #def self.scrape_index_page(index_url)
+    #doc = Nokogiri::HTML(open(index_url))
+    #students = []
+
+    #doc.css("div.roster-cards-container").each do |student_value|
+    #student = {}
+
+    #binding.pry
+    #student = {
+    #student[:name] => student_value.css("div.student-card").each.search("h4.student-name").text,
+    #student[:location] => student_value.css("div.student-card").each.search("p.student-location").text,
+    #student[:profile_url] => student_value.css("div.student-card").each.search("a href")
+    #}
+#
+    #students<<students
+    #end
+    #students
+  #end
 end
