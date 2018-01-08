7dab44dd14d93df09591118d9cc7fd525c23709f
Araba Taylor <araba.taylor20@gmail.com>
2018-01-08 01:30:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,26 +24,27 @@ end
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-
+    student = {}
     doc.css("div.vitals-container").each do |social_content|
-      student = {}
-        social_content.css("div.social-icon-container,a").each do |social|
-              social_link = social.search("a").attribute("href").value
+        social_content.css("div.social-icon-container a").each do |social|
+              social_link = social.attribute("href").value
               if social_link.include?('twitter')
                 student[:twitter] = social_link
               elsif social_link.include?('linkedin')
                 student[:linkedin] = social_link
-              else social_link.include?('github')
+              elsif social_link.include?('github')
                 student[:github] = social_link
+              else
+                student[:blog] = social_link
             end
-        #binding.pry
           end
-        student
-
         end
+    doc.css("div.vitals-text-container").each do |content|
+      student[:profile_quote] = content.css("div.profile-quote").text
     end
-
-
+      student[:bio] = doc.css("div.details-container").css('p').text
+    student
+    end
 end
 
     #:blog =>
