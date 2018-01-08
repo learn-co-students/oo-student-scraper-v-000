a0bc11179b2de48757001c0d8297e27b1279a36b
Tyler Pulse <tyler3321@gmail.com>
2018-01-08 22:56:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |k, v|
-      self.send("#{k}=", v)
+    attributes_hash.each{ |k, v| self.send("#{k}=", v)}
     end 
 end
 
