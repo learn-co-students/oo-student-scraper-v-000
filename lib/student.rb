c5009213f1b727fdf4148d2d72d5b61d5885b82e
Araba Taylor <araba.taylor20@gmail.com>
2018-01-09 03:23:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
   end
 
   def self.all
