03f7b245486d053aa591475fb84fca9cabd685c1
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-01-12 06:04:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,23 +1,66 @@
 require 'open-uri'
 require 'pry'
-
+#==============================================
 class Scraper
-  # responsible for scraping the index page that lists all of the students
   def self.scrape_index_page(url)
     doc = Nokogiri::HTML(open(url))
-    roster = doc.css("div.student-card")
-    roster.map do |student|
-      {
-      name: student.css(".student-name").text,
+    students = doc.css(".student-card")
+    
+    students.map do |student|
+      { name: student.css(".student-name").text,
       location: student.css(".student-location").text,
-      profile_url: student.css("a").first["href"]
-      }
+      profile_url: student.css("a").first["href"] }
     end
   end
-
-  # responsible for scraping an individual student's profile page
+#==============================================
   def self.scrape_profile_page(url)
-
+    doc = Nokogiri::HTML(open(url))
+    attrs = {}
+    social = doc.css(".social-icon-container a")
+    
+    social.each do |link|
+      case link["href"]
+      when (/^.*twitter.*/)
+        attrs[:twitter] = link["href"]
+      when (/^.*linkedin.*/)
+        attrs[:linkedin] = link["href"]
+      when (/^.*github.*/)
+        attrs[:github] = link["href"]
+      else attrs[:blog] = link["href"]
+      end
+    end
+    attrs[:profile_quote] = doc.css(".profile-quote").text
+    attrs[:bio] = doc.css(".description-holder p").text
+    attrs
   end
-
+#==============================================
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def self.scrape_index_page(url)
+#   doc = Nokogiri::HTML(open(url))
+#   roster = doc.css("div.student-card")
+#   roster.map do |student|N
+#     # binding.pry
+#       name: student.css(".student-name").text
+#       location: student.css(".student-location").text
+#       profile_url: student.css("a").first["href"]
+#   end
+  
+#   student.css("a")
+# end
