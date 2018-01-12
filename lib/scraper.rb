cf075815e595e1be3e22f9d8e5f33ef3250b0507
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-01-12 00:06:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,22 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
-  def self.scrape_index_page(index_url)
-    
+  # responsible for scraping the index page that lists all of the students
+  def self.scrape_index_page(url)
+    doc = Nokogiri::HTML(open(url))
+    roster = doc.css("div.student-card")
+    roster.map do |student|
+      {
+      name: student.css(".student-name").text,
+      location: student.css(".student-location").text,
+      profile_url: student.css("a").first["href"]
+      }
+    end
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  # responsible for scraping an individual student's profile page
+  def self.scrape_profile_page(url)
+
   end
 
 end
-
