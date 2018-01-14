bc02dad15bd1634fecee81bd6696fb2288aaa0fe
Remmy Clay <remmy.lauren@gmail.com>
2018-01-14 03:34:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,7 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
     @@all << self
   end
 
