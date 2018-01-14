d1f603543be80061c9b6daf7ed454ac3b900e749
Remmy Clay <remmy.lauren@gmail.com>
2018-01-14 00:30:21 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,44 @@
 require 'open-uri'
 require 'pry'
+#require 'nokogiri'
 
 class Scraper
+  attr_accessor :name, :location, :profile_url, :twitter_url, :linkedin_url, :github_url, :blog_url, :profile_quote, :bio
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = []
+    doc.css(".student-card").collect do |student|
+      student_hash = {}
+      student_hash[:name] = student.css("h4").text
+      student_hash[:location] = student.css("p").text
+      student_hash[:profile_url] = student.css("a").attr("href").value
+      students << student_hash
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    student_hash = {}
+    doc.css(".vitals-container").each do |student|
+      student_hash[:profile_quote] = student.css(".profile-quote").text
+      student_hash[:bio] = student.css("h6").text
+      student.css(".social-icon-container a").each do |type|
+          link = type.attr("href")
+          if link.include? "twitter"
+            student_hash[:twitter_url] = link
+          elsif link.include? "linkedin"
+            student_hash[:linkedin_url] = link
+          elsif link.include? "github"
+            student_hash[:github_url] = link
+          else
+            student_hash[:blog_url] = link
+          end
+        end
+      end
+      student_hash
+    end
   end
-
-end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,13 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -13,11 +15,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
