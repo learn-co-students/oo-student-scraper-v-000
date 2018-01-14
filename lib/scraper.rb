ffdae391d8acf5316ce1396b618a85666eedde6a
Remmy Clay <remmy.lauren@gmail.com>
2018-01-14 03:14:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,9 @@
 require 'open-uri'
-require 'pry'
-#require 'nokogiri'
 
 class Scraper
-  attr_accessor :name, :location, :profile_url, :twitter_url, :linkedin_url, :github_url, :blog_url, :profile_quote, :bio
 
   def self.scrape_index_page(index_url)
-    html = open(index_url)
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open(index_url))
     students = []
     doc.css(".student-card").collect do |student|
       student_hash = {}
@@ -20,25 +16,22 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    html = open(profile_url)
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open(profile_url))
     student_hash = {}
-    doc.css(".vitals-container").each do |student|
-      student_hash[:profile_quote] = student.css(".profile-quote").text
-      student_hash[:bio] = student.css("h6").text
-      student.css(".social-icon-container a").each do |type|
-          link = type.attr("href")
-          if link.include? "twitter"
-            student_hash[:twitter_url] = link
-          elsif link.include? "linkedin"
-            student_hash[:linkedin_url] = link
-          elsif link.include? "github"
-            student_hash[:github_url] = link
-          else
-            student_hash[:blog_url] = link
-          end
-        end
+    student_hash[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
+    student_hash[:profile_quote] = doc.css(".profile-quote").text
+    doc.css(".social-icon-container a").each do |type|
+      link = type.attr("href")
+      if link.include? "twitter"
+        student_hash[:twitter] = link
+      elsif link.include? "linkedin"
+        student_hash[:linkedin] = link
+      elsif link.include? "github"
+        student_hash[:github] = link
+      else
+        student_hash[:blog] = link
       end
-      student_hash
     end
+    student_hash
   end
+end
--- lib/student.rb
+++ lib/student.rb
@@ -11,14 +11,19 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
   end
 
   def self.all
-
+    @@all
   end
 end
