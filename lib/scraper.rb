865edc588cbba77c0028b12727bc0cc190b1fb3e
PJ Frias <peejfrias@gmail.com>
2018-01-17 16:30:42 +0000

    complete scrape_index_page method in Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,22 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    roster = doc.css(".student-card")
+
+    roster.each do |person|
+      student = {}
+      student[:name] = person.css(".student-name").text
+      student[:location] = person.css(".student-location").text
+      student[:profile_url] = person.css("a").attribute("href").value
+      students << student
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
