2c5f55a70479f362bbc93aec152a3a25135e79d6
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-18 15:19:59 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,38 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    roster = doc.css(".student-card")
+
+    roster.each do |person|
+      student = {}
+      student[:name] = person.css(".student-name").text
+      student[:location] = person.css(".student-location").text
+      student[:profile_url] = person.css("a").attribute("href").value
+      students << student
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    links = doc.css(".social-icon-container a")
+    profile = {}
+    links.each do |link|
+      address = link.attribute("href").value
+      profile[:twitter] = address if address.include?("twitter")
+      profile[:linkedin] = address if address.include?("linkedin")
+      profile[:github] = address if address.include?("github")
+      profile[:blog] =  address if link.css("img").attribute("src").value.include?("rss-icon")
+    end
+    profile[:profile_quote] = doc.css(".profile-quote").text
+    profile[:bio] = doc.css(".bio-content p").text
+    profile
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,25 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student = self
+    student_hash.each {|k,v| student.send("#{k}=", v)}
+    self.class.all << student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| self.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|k,v|self.send("#{k}=",v)}
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
-
