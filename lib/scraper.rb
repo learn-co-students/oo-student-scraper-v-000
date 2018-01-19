a361a304645ed558413d9c69f0af6ef8847dc37a
github username <github email address>
2018-01-19 15:31:28 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,51 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  attr_accessor :name, :location, :profile_url
+
+  # def initialize(index_url)
+  #   doc = Nokogiri::HTML(open(index_url))
+  #
+  #   props = {}
+  #
+  #   props[:name] = doc.search("h1.profile-name")
+  #   props[:location] = doc.search("h2.profile-location")
+  #   props[:profile_url] = doc.search("not-sure")
+  #
+  #   props
+  # end
+
 
   def self.scrape_index_page(index_url)
-    
+    @doc = Nokogiri::HTML(open(index_url))
+    props = {}
+
+    props[:name] = @doc.search("h1.profile-name")
+    props[:location] = @doc.search("h2.profile-location")
+    props[:profile_url] = @doc.search("not-sure")
+
+    props
   end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+
+
+    # @name = doc.css("h1.class.profile_name")
+  #   @name = doc.css(".vitals-container .vitals-text-container .h1.profile-name")
+  # #   @location =doc.css(".vitals-container .vitals-text-container .h2")
+  # #   @profile_url = doc.css(".vitals-container .vitals-text-container .h1") return the actual url in this one so it can be passed through to the self.scrape_profile_page method
+
+
+
+  # self.new_from_example(index_url)
+  #   properties = scraper.new
+  #   properties.each do |k,v|
+  #     scraper.send("#{k}=", v)
+  #   end
+  # end
+
+    def self.scrape_profile_page(profile_url)
+
+    end
+
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,26 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
-    
+   #  properties = Scraper.new
+   #   properties.each do |k,v|
+   #     scraper.send("#{k}=", v)
+   #   end
+   # end
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
