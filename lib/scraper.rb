1719e2340968ce518188427b128f822c2aa5d1ff
github username <github email address>
2018-01-19 17:29:02 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,36 +4,57 @@ require 'pry'
 class Scraper
   attr_accessor :name, :location, :profile_url
 
-  # def initialize(index_url)
-  #   doc = Nokogiri::HTML(open(index_url))
-  #
-  #   props = {}
-  #
-  #   props[:name] = doc.search("h1.profile-name")
-  #   props[:location] = doc.search("h2.profile-location")
-  #   props[:profile_url] = doc.search("not-sure")
-  #
-  #   props
-  # end
-
-
-  def self.scrape_index_page(index_url)
+  def initialize(index_url)
     @doc = Nokogiri::HTML(open(index_url))
-    props = {}
+    binding.pry
+  end
 
-    props[:name] = @doc.search("h1.profile-name")
-    props[:location] = @doc.search("h2.profile-location")
-    props[:profile_url] = @doc.search("not-sure")
+  def call
+    # box.each do |box_doc|
+    #   scrape_box(box_doc)
+  end
 
-    props
+  def box
+    @box ||= @doc.search("div.student-card")
   end
 
 
+  def self.scrape_index_page(index_url)
+    binding.pry
+  #   @doc = Nokogiri::HTML(open(index_url))
+  #
+  # props = {}
+  # props[:name] = @doc.search("h4.student-name")
+  # props[:location] = @doc.search("p.student-location")
+  # props[:profile_url] = @doc.search("div.student-card a")
 
-    # @name = doc.css("h1.class.profile_name")
-  #   @name = doc.css(".vitals-container .vitals-text-container .h1.profile-name")
-  # #   @location =doc.css(".vitals-container .vitals-text-container .h2")
-  # #   @profile_url = doc.css(".vitals-container .vitals-text-container .h1") return the actual url in this one so it can be passed through to the self.scrape_profile_page method
+
+  # <div class="student-card" id="ryan-johnson-card">
+  #           <a href="students/ryan-johnson.html">
+  #             <div class="view-profile-div">
+  #               <h3 class="view-profile-text">View Profile</h3>
+  #             </div>
+  #             <div class="card-text-container">
+  #               <h4 class="student-name">Ryan Johnson</h4>
+  #               <p class="student-location">New York, NY</p>
+  #             </div>
+  #           </a>
+  #         </div>
+  #         ::before
+  # <a href="students/ryan-johnson.html">
+  #             <div class="view-profile-div">
+  #               <h3 class="view-profile-text">View Profile</h3>
+  #             </div>
+  #             <div class="card-text-container">
+  #               <h4 class="student-name">Ryan Johnson</h4>
+  #               <p class="student-location">New York, NY</p>
+  #             </div>
+  #           </a>
+  props
+
+  end
+
+
 
 
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,15 +5,20 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-   #  properties = Scraper.new
-   #   properties.each do |k,v|
-   #     scraper.send("#{k}=", v)
-   #   end
-   # end
+    # student = Student.new
+    #
+    # properties = Scraper.new()
+    #
+    # Scraper.new(students_array)
+    # student.each do |k,v|
+    #   scraper.send("#{k}=", v)
+    # end
+    #
+    # student
   end
 
   def add_student_attributes(attributes_hash)
