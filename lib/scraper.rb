9769bca19e0db3a171ed5ae25848a2aabe167b96
ElleeB <laura.trager@gmail.com>
2018-01-23 10:12:46 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,63 @@
 require 'open-uri'
 require 'pry'
 
+require 'nokogiri' # remove later
+
 class Scraper
 
+  @students_array = []
+
   def self.scrape_index_page(index_url)
-    
+
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card")
+
+    students.each do |student|
+      hash = {:name => nil, :location => nil, :profile_url => nil}
+      hash[:name] = student.css(".student-name").text
+      hash[:location] = student.css(".student-location").text
+      hash[:profile_url] = student.css("a")[0]["href"]
+      @students_array << hash
+    end
+
+    @students_array
+
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    @profile_hash = {:twitter => nil, :linkedin => nil, :github => nil, :blog => nil, :profile_quote => nil, :bio => nil}
+
+
+    student_profile = doc.css(".vitals-container")
+
+# pull url text from any .css("a")
+    social_info = student_profile.css("div.social-icon-container")
+
+    social_info.css("a").each do |code|
+      if code["href"].match(/(twitter)/)
+        @profile_hash[:twitter] = code["href"]
+      elsif code["href"].match(/(linkedin)/)
+        @profile_hash[:linkedin] = code["href"]
+      elsif code["href"].match(/(github)/)
+        @profile_hash[:github] = code["href"]
+      elsif code["href"].match(/(blog)/)
+        @profile_hash[:blog] = code["href"]
+      end
+
+    end
+
+# pull quote text
+    @profile_hash[:profile_quote] = student_profile.css(".vitals-text-container .profile-quote").text
+
+    ### WORKING ON DOUBLE QUOTES AND SLASHES AND SHIT ###
+
+# pull bio text
+    @profile_hash[:bio] = doc.css(".details-container p").text
+
+    @profile_hash
+
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,23 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
-    
+    # The keys of the individual student hashes should be :name, :location and :profile_url.
+    html = open(students_array)
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
