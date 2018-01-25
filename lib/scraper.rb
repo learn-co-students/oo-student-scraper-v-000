99af25d96d7019f040133caacdbfdbeef7025193
John Wojciechowski <jwojo13@gmail.com>
2018-01-25 16:53:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,15 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open('fixtures/student-site/index.html')
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
