b676677d0386eaf1f0a1c5b3dd647409978714c3
Ingrid Wong <yw340@georgetown.edu>
2018-01-27 16:51:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,27 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #return: an array of hashes [{name: , location, profile_url: }, {}]
+      # each hash represents a single student
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card")
+    students_collection = []
+
+    students.each do |student|
+      student_hash = {}
+      student_hash.name = student.css(".student-name").text
+      student_hash.location = student.css(".student-location").text
+      binding.pry
+      students_collection << student_hash
+    end
+
+    #for each student: doc.css(/.student-card)
+      #create a hash with
+        #name:
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
