43ee62d01bedd12002f1f099168208730dd84229
jljardon <github email address>
2018-01-31 00:00:03 -0500

    add scraper and studen class functionality
    Added methods to obtain Students atributes and fill additional information of the students via the scraper.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,28 @@
 class Student
+   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+   @@all = []
 
-  @@all = []
+   def initialize(student_hash)
+      student_hash.each do |key, value|
+         send("#{key}=", value)
+      end
+      @@all.push(self)
+   end
 
-  def initialize(student_hash)
-    
-  end
+   def self.create_from_collection(students_array)
+      students_array.each do |student|
+         student_from_collection = Student.new(student)
+      end
+   end
 
-  def self.create_from_collection(students_array)
-    
-  end
+   def add_student_attributes(attributes_hash)
+      attributes_hash.each do|key, value|
+            send("#{key}=", value)
+      end
+   end
 
-  def add_student_attributes(attributes_hash)
-    
-  end
-
-  def self.all
-    
-  end
+   def self.all
+      @@all
+   end
 end
-
