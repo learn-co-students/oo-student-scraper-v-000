78c63d86739fd9c7517a021c1b7ddbb883fe1830
ode-to-the-code <justcallmethis@gmail.com>
2018-02-01 12:20:01 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,79 @@
 require 'open-uri'
 require 'pry'
 
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+     html = File.read(index_url)
+     noko = Nokogiri::HTML(html)
+     x = noko.search(".student-card")
+    #  binding.pry
+    #  [<div class="card">
+    #   [<h2 id="logo">Title</h2>]
+    #   [<p class="content">Something here</p>]
+    #  <div>]
+      array = []
+     x.each do |person|
+       array << { name: person.css('h4.student-name').text, location: person.search('p.student-location').text, profile_url: person.css('a').first["href"]}
+     end
+     array
   end
+
+
 
   def self.scrape_profile_page(profile_url)
-    
+    html = File.read(profile_url)
+    noko = Nokogiri::HTML(html)
+    # binding.pry
+
+    x = noko.css("div.social-icon-container a")
+
+    @rhash = {}
+
+
+      # binding.pry
+      # x = noko.css("div.profile-quote").text
+      #  noko.css('a').first["href"]
+      # @rhash[:quote] = x
+      x.each do |student|
+        # binding.pry
+        link = student.attributes["href"].value
+        if link.include?("twitter")
+          @rhash[:twitter] = link
+        elsif link.include?("github")
+          @rhash[:github] = link
+        elsif link.include?("linkedin")
+          @rhash[:linkedin] = link
+        else
+          @rhash[:blog] = link
+        end
+
+        # binding.pry
+
+        if noko.css("div.profile-quote")
+          @rhash[:profile_quote] = noko.css("div.profile-quote").text
+        end
+
+        if noko.css("div.description-holder p")
+          @rhash[:bio] = noko.css("div.description-holder p").text
+        end
+
+        # binding.pry
+      end
+
+      # pushing stuff into the hash- interpreting the stuff in x, checking if its
+      # a twtter link for example
+
+      # you can use .include? to check which one it is. if it's none of them, it's
+      # a blog. and add them into @rhash
+
+
+      # @rhash
+
+    # binding.pry
+    @rhash
+
   end
-
+  # binding.pry
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,40 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # binding.pry
+    self.name = student_hash[:name]
+    self.location = student_hash[:location]
+    @@all << self
+
   end
 
   def self.create_from_collection(students_array)
-    
+    # binding.pry
+    students_array.each do |student|
+    Student.new(student)
+      # binding.pry
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # binding.pry
+    self.twitter = attributes_hash[:twitter]
+    self.linkedin = attributes_hash[:linkedin]
+    self.github = attributes_hash[:github]
+    self.profile_url = attributes_hash[:profile_url]
+    self.profile_quote = attributes_hash[:profile_quote]
+    self.blog = attributes_hash[:blog]
+    self.bio = attributes_hash[:bio]
+
+
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
