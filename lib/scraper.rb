9d186057c6ae3c1f71e77087f0edd260b6576781
hushangni <shangnihu@gmail.com>
2018-02-02 10:50:54 -0500

    Finished scrape index page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,29 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require_relative './student.rb'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+  	# Open document
+  	doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+
+  	# name = doc.css(".student-card").css(".student-name")[0].text
+  	# location = doc.css(".student-card").css(".student-location")[0].text
+  	# profile_url = doc.css(".student-card").css("a")[0]['href']
+
+  	students = [];
+
+  	doc.css(".student-card").each do |card|
+  		student = Hash.new
+  		student[:name] = card.css(".student-name").text
+  		student[:location] = card.css(".student-location").text
+  		student[:profile_url] = card.css("a")[0]['href']
+  		students << student
+  	end
+
+  	students
   end
 
   def self.scrape_profile_page(profile_url)
