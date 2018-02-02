62b4d69993fba078c18e35c65d3a4b1d1b367453
hushangni <shangnihu@gmail.com>
2018-02-02 12:10:32 -0500

    Added scrape_profile_page, finished Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,35 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+  	# Open document
+  	doc = Nokogiri::HTML(open(profile_url))
+
+  	#main doc.css(".social-icon-container a").css("img").attribute("src").value
+  	#twitter  == "../assets/img/twitter-icon.png"
+  	#linkedin value == "../assets/img/linkedin-icon.png"
+  	#github value == "../assets/img/github-icon.png"
+  	#blog value == "../assets/img/rss-icon.png"
+
+  	scraped_profile = Hash.new
+
+  	doc.css(".social-icon-container a").each do |social|
+  		if social.css("img").attribute("src").value == "../assets/img/twitter-icon.png"
+  			scraped_profile[:twitter] = social.attribute("href").value
+  		elsif social.css("img").attribute("src").value == "../assets/img/linkedin-icon.png"
+  			scraped_profile[:linkedin] = social.attribute("href").value
+  		elsif social.css("img").attribute("src").value == "../assets/img/github-icon.png"
+  			scraped_profile[:github] = social.attribute("href").value
+  		elsif social.css("img").attribute("src").value == "../assets/img/rss-icon.png"
+  			scraped_profile[:blog] = social.attribute("href").value
+  		end
+  	end
+
+  	#profile doc.css(".profile-quote").text
+  	#bio doc.css(".description-holder").css("p").text
+  	scraped_profile[:profile_quote] = doc.css(".profile-quote").text
+  	scraped_profile[:bio] = doc.css(".description-holder").css("p").text
+
+  	scraped_profile
   end
 
 end
