921653a3c1b225c6809cb97528608d1388a90dbf
Wali Rahman <wali.rahman89@yahoo.com>
2018-02-04 22:10:46 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,13 +15,31 @@ class Scraper
         students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
     end
-    students
+    studentss
   end
 
 
 
   def self.scrape_profile_page(profile_url)
+    profile = Nokogiri::HTML(open(profile_url))
+    student = {}
+    links = profile.css(".social-icon-container").children.css('a').map { |e| e.attribute('href').value }
+    links.each do |link|
+      if link.include?('linkedin')
+        student[:linkedin] = link
+      elsif link.include?('github')
+        student[:github] = link
+      elsif link.include?('twitter')
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
 
+    end
   end
 
 end
+
+
+profile_url = "./fixtures/student-site/students/joe-burgess.html"
+scraped_student = Scraper.scrape_profile_page(profile_url)
