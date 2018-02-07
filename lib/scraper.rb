107a8e8c0af61e9287674875cbf9fb23e41c6b57
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-02-07 19:10:08 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,30 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open('./fixtures/student-site/index.html')
+    doc = Nokogiri::HTML(html)
+    scraped_student = [] 
     
+    doc.css(".student-card").each do |x|
+    student = {
+      :name => x.css("h4").text, 
+      :location => x.css("p").text,
+      :profile_url => x.at_css("a")[:href]
+    }
+    scraped_student << student 
+    end 
+    scraped_student
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+
+    binding.pry 
     
   end
 
