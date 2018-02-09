87049b75db7c8662f5eba39fe38910052cb65746
Katrina Brinson <kbrinso@gmail.com>
2018-02-09 00:28:36 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,24 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  def initialize
+    @index = {name: "", location: "", profile_url: ""}
+    @profiles = {}
+  end
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
     
+    #name
+    binding.pry
+    #name
+    doc.css(".student-name")
+    
+    #location
+    doc.css(".student-location")
+    
+    #profile_url
   end
 
   def self.scrape_profile_page(profile_url)
