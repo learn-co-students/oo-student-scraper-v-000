7ab94fce391787f6a1cd90715dadf4edc867a704
Katrina Brinson <kbrinso@gmail.com>
2018-02-09 21:14:21 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,16 +8,11 @@ class Scraper
   @@all = []
   @@allprofiles = []
   @student_hash = {}
-  @profile_hash = {}
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    # names = doc.css(".student-name")
-    # locations = doc.css(".student-location")
-    # profile_url = doc.css(".student-card").css("a").attribute("href").value
-
-    #name
+    
     allstudents = doc.css(".student-card")
     allstudents.each{ |item| 
       student_hash = {}
@@ -27,47 +22,86 @@ class Scraper
       @@all << student_hash
     }
     
-    #profile_url
-    
-    #location
-    # locations = doc.css(".student-location").text
-    # locations.each{|location|
-    #   @student_hash[:location] = location
-    # }
-    # #profile_url
-    # binding.pry
+    @@all
   end
-  #binding.pry
 
   def self.scrape_profile_page(profile_url)
+    @profile_hash = {}
     #:twitter, :linkedin, :github, :blog, :profile_quote, :bio
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
+    
+    allsocial = doc.css(".social-icon-container a")
+    
+    allsocial.each{|item| 
+      #network = item.css("a").attribute("href").value
+      if item.css("img").attribute("src").value.include?("twitter")
+        @profile_hash[:twitter] = item.attribute("href").value
+      elsif item.css("img").attribute("src").value.include?("linkedin")
+        @profile_hash[:linkedin] = item.attribute("href").value
+      elsif item.css("img").attribute("src").value.include?("github")
+        @profile_hash[:github] = item.attribute("href").value
+      elsif item.css("img").attribute("src").value.include?("rss")
+        @profile_hash[:blog] = item.attribute("href").value
+      end
+    } 
+    
+    @profile_hash[:profile_quote] = doc.css(".profile-quote").text.strip
+    @profile_hash[:bio] = doc.css(".description-holder").first.text.strip
+    @profile_hash
+    
+  end
+    
+    
+    
+    
+    #item.css("a").attribute("href").value[/([^\/])+\./].delete("www").delete(".").to_sym
+    #item.css("a img").attribute("src").value
+    # allsocial.each{ |item| 
+    #   network = item.css("a").attribute("href")
+    #   profile_hash = {}
+    #   network.each{|i|
+    #     binding.pry
+    #     if network.include?("www.")
+    #       name = network[/([^www\.])+\./].chomp(".").to_sym
+    #     else
+    #       name = network[/([^\/])+\./].chomp(".").to_sym
+    #     end
+    #     profile_hash[name] = i.value
+    #   }
+    #   @@allprofiles << profile_hash
+    # }
+    #binding.pry
+    
+    # if network.include?("www.")
+        #   name = network[/([^www\.])+\./].chomp(".").to_sym
+        # else
+        #   name = network[/([^\/])+\./].chomp(".").to_sym
+        # end
+        
     #social = doc.css(".social-icon-container")
     #need to extract each
     # regexex = (/([^\/])+\./).chomp(".")
     # doc.css(".social-icon-container a")[0]["href"]
+    #binding.pry
     
-    allsocial = doc.css(".social-icon-container")
-    #item.css("a").attribute("href").value[/([^\/])+\./].delete("www").delete(".").to_sym
-      
+    # network = item.css("a").attribute("href").value
+    # allsocial = doc.css(".social-icon-container a").attribute("href").value
     
-    profile_hash = {}
-    allsocial.each{ |item| 
-      network = item.css("a").attribute("href").value
-      if network.include?("www.")
-        name = network[/([^www\.])+\./].chomp(".").to_sym
-      else
-        name = network[/([^\/])+\./].chomp(".").to_sym
-      end
-      profile_hash[name] = network
-      @@allprofiles << profile_hash
-    }
-    binding.pry
-    
-    
-    
-  end
+    # if item.css("a img").attribute("src").value.include?("rss")
+    #   @profile_hash[:blog] = doc.css(".social-icon-container a").attribute("href").value
+    # elsif item.css("a img").attribute("src").value.include?("twitter")
+    #   name = :twitter
+    # elsif item.css("a img").attribute("src").value.include?("linkedin")
+    #   name = :linkedin
+    # elsif item.css("a img").attribute("src").value.include?("github")
+    #   name = :github
+    #   # if network.include?("www.")
+    #   #   name = network[/([^www\.])+\./].chomp(".").to_sym
+    #   # else
+    #   #   name = network[/([^\/])+\./].chomp(".").to_sym
+    #   # end
+    # end
 
 end
 
