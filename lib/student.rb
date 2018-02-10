2168ddc1ab4725a9c49a70e2a427159800f21ffa
Jennifer Stoos <jstoos@bitlaw.com>
2018-02-10 17:35:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -38,3 +38,44 @@ class Student
     @@all
   end
 end
+
+# Class Student
+#
+#   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+#
+#   @@all = []
+#
+#   def initialize(student_hash)
+#     @name = student_hash[:name]
+#     @location = student_hash[:location]
+#     @profile_url = student_hash[:profile_url]
+#     @@all << self
+#   end
+#
+#   def self.create_from_collection(students_array)
+#     students_array.each do |student_hash|
+#       #Student.new(student_hash)
+#       s = Student.new(student_hash)
+#       s.send(:initialize, student_hash)
+#     end
+#   end
+#
+#   def add_student_attributes(attributes_hash)
+#     #binding.pry
+#     attributes_hash.each_pair do |key, value|
+#       self.send("#{key}=", value)
+#     end
+#     # @twitter = attributes_hash[:twitter]
+#     # @linkedin = attributes_hash[:linkedin]
+#     # @github = attributes_hash[:github]
+#     # @blog = attributes_hash[:blog]
+#     # @profile_quote = attributes_hash[:profile_quote]
+#     # @bio = attributes_hash[:bio]
+#
+#     self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+# end
