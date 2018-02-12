d5f0def9b84f565fa0ff57e941239abdc1001c35
Emily Zhao <tong.zhao.92@gmail.com>
2018-02-12 23:19:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,10 +6,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     array_index = []
     doc = Nokogiri::HTML(open(index_url))
-
     doc.css(".student-card").each_with_index do |student, i|
       array_index[i]= {}
-      binding.pry
       array_index[i][:name] = student.css(".student-name").text
       array_index[i][:location] = student.css(".student-location").text
       array_index[i][:profile_url] = student.css("a").attr('href').value
@@ -18,7 +16,11 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-
+    doc = Nokogiri::HTML(open(profile_url))
+    student_array = []
+    doc.css(".social-icon-container").each do |a|
+      binding.pry
+    end
   end
 
 end
