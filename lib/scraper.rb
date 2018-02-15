67e9c02182e959adcbd94c923155c15b9cb48396
Derek Haller <dhaller.personal@gmail.com>
2018-02-15 03:12:36 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,37 +2,72 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
   class Scraper
-    attr_accessor :page_loc
-    @page_loc = "http://165.227.31.208:56222/fixtures/student-site/"
-    def self.index_to_param(index_url)
-      doc = Nokogiri::HTML(open(@page_loc))
-      studs_divs = doc.css(".student-card a h4")
-      studs_names = []
-      studs_divs.each do |stud_div|
-        studs_names << stud_div.text
-      end
-      studs_param = []
-      studs_names.each do |name|
-        name.gsub!(" ", "-")
-        name = "#{name}.html"
-        studs_param << name.downcase
-      end
-      studs_param
-    end
+
     def self.scrape_index_page(index_url)
-      studs_param = index_to_param(index_url)
-      students_hashes = []
-      studs_param.each do |param|
-        doc = Nokogiri::HTML(open("#{@page_loc}students/#{param}"))
+      doc = Nokogiri::HTML(open(index_url))
+      student_hashes = []
+      studs_links = doc.css(".student-card a")
+      studs_links.each do |stud_link|
         stu = {}
-        stu[:profile_url] = param
-        stu[:name] = param.gsub("-", " ").gsub(".html", "").split.map(&:capitalize).join(' ')
-        stu[:location] = doc.css(".vitals-text-container h2").text binding.pry
-        students_hashes << stu
+        stu[:name] = stud_link.css("h4").text
+        stu[:profile_url] = stud_link.attr("href")
+        stu[:location] = stud_link.css("p").text
+        student_hashes << stu
       end
-      binding.pry
-      students_hashes
+      student_hashes
+
     end
+
+    #def self.index_to_param(index_url)
+    #  doc = Nokogiri::HTML(open(index_url))
+    #  studs_divs = doc.css(".student-card a h4")
+    #  studs_names = []
+    #  studs_divs.each do |stud_div|
+    #    studs_names << stud_div.text
+    #  end
+    #  studs_param = []
+    #  studs_names.each do |name|
+    #    name.gsub!(" ", "-")
+    #    name = "#{name}.html"
+    #    studs_param << name.downcase
+    #  end
+    #  studs_param
+    #end
+    #def self.scrape_index_page(index_url)
+    #  studs_param = index_to_param(index_url)
+    #  students_hashes = []
+    #  studs_param.each do |param|
+    #    binding.pry
+    #    doc = Nokogiri::HTML(open("./fixtures/student-site/students/#{param}"))
+    #    stu = {}
+    #    stu[:profile_url] = param
+    #    stu[:name] = param.gsub("-", " ").gsub(".html", "").split.map(&:capitalize).join(' ')
+    #    stu[:location] = doc.css(".vitals-text-container h2").text
+    #    students_hashes << stu
+    #    binding.pry
+    #  end
+    #  students_hashes
+    #end
     def self.scrape_profile_page(profile_url)
+      stu = {}
+      doc = Nokogiri::HTML(open(profile_url))
+      stu[:profile_quote] = doc.css(".profile-quote").text.strip
+      stu[:bio] = doc.css(".description-holder p").text
+      soc_links = doc.css(".social-icon-container a")
+      if soc_links
+      soc_links.each do |soc_link|
+        a = soc_link.attr("href")
+          if a.include?("twitter")
+            stu[:twitter] = soc_link.attr("href")
+          elsif a.include?("linkedin")
+            stu[:linkedin] = soc_link.attr("href")
+          elsif a.include?("github")
+            stu[:github] = soc_link.attr("href")
+          else
+            stu[:blog] = soc_link.attr("href")
+          end
+        end
+      end
+      stu
     end
   end
--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,23 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
+    if student_hash[:name] then @name = student_hash[:name]
+    if student_hash[:location] then @location = student_hash[:location]
+    if student_hash[:twitter] then @twitter = student_hash[:twitter]
+    if student_hash[:linkedin] then @linkedin = student_hash[:linkedin]
+    if student_hash[:github] then @github = student_hash[:github]
+    if student_hash[:blog] then @blog = student_hash[:blog]
+    if student_hash[:profile_quote] then @profile_quote = student_hash[:profile_quote]
+    if student_hash[:bio] then @bio = student_hash[:bio]
+    if student_hash[:profile_url] then @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
+    binding.pry
+    students_array.each do |stud|
+      Student.initialize(stud)
+    end
 
   end
 
