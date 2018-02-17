2fe39fdcb1fa1a3e66a0436f38ddbf482b2f396f
Petya Djanovska <petya.djanovska@gmail.com>
2018-02-17 01:36:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,7 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
     @@all << self
   end
 
@@ -17,13 +18,16 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.twitter = attributes_hash[:twitter] if attributes_hash[:twitter]
-    self.linkedin = attributes_hash[:linkedin] if attributes_hash[:linkedin]
-    self.github = attributes_hash[:github] if attributes_hash[:github]
-    self.blog = attributes_hash[:blog] if attributes_hash[:blog]
-    self.profile_quote = attributes_hash[:profile_quote] if attributes_hash[:profile_quote]
-    self.bio = attributes_hash[:bio] if attributes_hash[:bio]
-    self.profile_url = attributes_hash[:profile_url] if attributes_hash[:profile_url]
+    # self.twitter = attributes_hash[:twitter] if attributes_hash[:twitter]
+    # self.linkedin = attributes_hash[:linkedin] if attributes_hash[:linkedin]
+    # self.github = attributes_hash[:github] if attributes_hash[:github]
+    # self.blog = attributes_hash[:blog] if attributes_hash[:blog]
+    # self.profile_quote = attributes_hash[:profile_quote] if attributes_hash[:profile_quote]
+    # self.bio = attributes_hash[:bio] if attributes_hash[:bio]
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
   end
 
   def self.all
