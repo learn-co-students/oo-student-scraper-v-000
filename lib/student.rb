99290a55ba9ce85fe91f0f91c0308b5153ec8029
Chely Ho <adept_inept@yahoo.com>
2018-02-21 20:24:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |student|
-      self.new(student)
-    end
+    students_array.each {|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
