aed9f5e62c3d1d285170c95859592799ad05f91d
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-22 21:27:22 +0000

    Profile 1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,39 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = page.css(".student-card")
+
+    student_array = []
+    students.each do |student|
+      s_name = student.css(".student-name").text
+      s_location = student.css(".student-location").text
+      s_profile = student.css("a").attribute("href").value
+      student_array << {name: s_name, location: s_location, profile_url: s_profile}
+    end
+    student_array
   end
 
+  # twitter url, linkedin url, github url, blog url, profile quote, and bio.
+
   def self.scrape_profile_page(profile_url)
-    
+    page = Nokogiri::HTML(open(profile_url))
+    details = {}
+    details[:bio] = page.css(".bio-content p").text
+    details[:profile_quote] = page.css(".profile-quote").text
+    socials = []
+    social = page.css(".social-icon-container").children.select(&:element?)
+    social.each{|s| socials << s.attribute("href").value}
+    socials.each do |s|
+      details[:linkedin] = s if s.match('linkedin')
+      details[:github] = s if s.match('github')
+      details[:twitter] = s if s.match('twitter')
+    end
+
+    t = page.css("social-icon-container").("img").src("../assets/img/rss-icon.png")
+
+    binding.pry
+
   end
 
 end
-
