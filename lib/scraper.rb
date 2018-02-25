9098527fa00771223124bea2aa49b194809cec25
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-25 20:49:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,17 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-
+    doc = Nokogiri::HTML(open(profile_url))
+    profile_info = {}
+    profile_vitals = doc.css(".vitals-container").css(".social-icon-container")
+    profile_info[:twitter] = profile_vitals.css("a")[0]["href"]
+    profile_info[:linkedin] = profile_vitals.css("a")[1]["href"]
+    profile_info[:github] = profile_vitals.css("a")[2]["href"]
+    profile_info[:blog] = profile_vitals.css("a")[3]["href"]
+    profile_info[:profile_quote] =  doc.css(".profile-quote").text
+    progfile_info[:bio] = doc.css(".details-container").css(".bio-block").css(".bio-content").css(".description-holder").css("p").text
+    binding.pry
   end
+
 
 end
