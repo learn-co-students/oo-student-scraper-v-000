9501c0a0dcaa38fd920f152e7d67a99bf19919a5
Bryan Hooper <bryan@lindsayfarm.net>
2018-02-26 16:23:32 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,14 +20,26 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     # Scrapes a students profile page and returns a hash of attributes describing an indivual student
     social_urls = []
+    student = {}
     page = Nokogiri::HTML(open(profile_url)) # use Nokogiri to grab the web page into an HTML object
-    socials = page.css('.social-icon-container a')
-    socials.each do |social|
-      social_urls << social['href']
+    socials = page.css('.social-icon-container a') # scrape the social icons and links
+    socials.each do |social|  # iterate over the social links
+      url = social['href']
+      if url["twitter"]  # figure out if the specific link is twitter, linkedin, github or blog
+        student[:twitter] = url
+      elsif url["linkedin"]
+        student[:linkedin] = url
+      elsif url["github"]
+        student[:github] = url
+      else
+        student[:blog] = url
+      end
     end
     quote = page.css('.profile-quote').text
-    bio = page.css('.description-holder').text
-    binding.pry 
+    bio = page.css('.bio-content p').text.split.join(" ") # clean up the bio paragraph
+    student[:profile_quote] = quote 
+    student[:bio] = bio
+    student # return the built out hash
   end
 
 end
