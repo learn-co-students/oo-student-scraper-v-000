59375ce4f4695daf3d4144093dab2b6da090974f
github username <github email address>
2018-02-28 10:30:44 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,25 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
+  def self.all
+    @@all
+  end
+
   def initialize(student_hash)
-    
+    self.name = student_hash[:name]
+    self.location = student_hash[:location]
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.instance_variable_set("@#{key}", value)}
   end
 
-  def self.all
-    
-  end
 end
-
