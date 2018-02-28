39cf221b94ac63b2530e29430040f6e2fb363f02
Kim Gonzales <kimgo589@gmail.com>
2018-02-28 21:44:39 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,18 +7,39 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    my_hash = doc.css('.student-card').collect do |student|
+     doc.css('.student-card').collect do |student|
       {:name => student.css('h4').text, :location => student.css('p').text, :profile_url => student.css('a')[0]['href']}
     end
-    #student name? = doc.css('.student-card').first.css('h4').text TRUE
-    #student location? = doc.css('.student-card').first.css TRUE
-    #student profile_url?  = doc.css('.student-card').first.css('a')[0]['href'] TRUE
-
-
-
   end
 
   def self.scrape_profile_page(profile_url)
+    student = {}
+
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    social_node = doc.css('.social-icon-container a')
+
+    student[:bio] = doc.css('p').text
+    student[:profile_quote] = doc.css(".profile-quote").text
+
+    social_node.each do |element|
+      if element['href'].include?("twitter")
+        student[:twitter] = element['href']
+      elsif element['href'].include?("linkedin")
+        student[:linkedin] = element['href']
+      elsif element['href'].include?("github")
+        student[:github] = element['href']
+      else
+        student[:blog] = element['href']
+      end
+    end
+
+      student
+    # student[:twitter]= social_node[0]['href']
+    # student[:linkedin]=social_node[1]['href']
+    # student[:github]=social_node[2]['href']
+    # student[:blog]= social_node[3]['href']
+    # student
 
   end
 
