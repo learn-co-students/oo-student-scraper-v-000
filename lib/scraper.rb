c8d666971634b938021a3db100cacc5be3f1e9a4
Branden Mosley <branden.mosley1337@gmail.com>
2018-03-04 19:49:39 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,10 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     students = doc.css(".student-card")
+    i = 0
     students.collect do |student|
-      {:name => student.css(".student-name").text, :location => student.css(".student-location").text, :profile_url => students.css("a")[0]["href"]}
+      {:name => student.css(".student-name").text, :location => student.css(".student-location").text, :profile_url => students.css("a")[i]["href"]}
+      i += 1
     end
     #binding.pry
   end
