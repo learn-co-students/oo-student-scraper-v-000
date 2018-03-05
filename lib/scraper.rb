fdaf79c59ef6d79043f9f4d14af6cf7df53124f8
Eden Mesfun <emesfun@gmail.com>
2018-03-05 17:14:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,43 +27,29 @@ class Scraper
       html = File.read(open(profile_url))
       profile_page = Nokogiri::HTML(html)
 
+      all_profiles = {}
 
       profile_page.css("div.social-icon-container a").each do |profile|
 
-        twitter = profile_page.css("div.social-icon-container a").attribute("href").value
-        linkedin = profile_page.css("div.social-icon-container a")[1].attribute("href").value
-        github = profile_page.css("div.social-icon-container a")[2].attribute("href").value
-        blog = profile_page.css("div.social-icon-container a")[3].attribute("href").value
+        link = profile.attribute("href").value
+
+        if link.include?("twitter")
+          all_profiles[:twitter] = link
+        elsif link.include?("linkedin")
+          all_profiles[:linkedin] = link
+        elsif link.include?("github")
+          all_profiles[:github] = link
+        else
+          all_profiles[:blog] = link
+        end
+      end #each
         profile_quote = profile_page.css(".profile-quote").text
         bio = profile_page.css(".details-container p").children.text
+        all_profiles[:profile_quote] = profile_quote
+        all_profiles[:bio] = bio
 
-        all_profiles = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog, :profile_quote => profile_quote, :bio => bio}
+        all_profiles
 
-        if profile_page.css("div.social-icon-container a").attribute("href").value
-          all_profiles[:twitter] = twitter
-        end
-        if profile_page.css("div.social-icon-container a")[1].attribute("href").value
-          all_profiles[:linkedin] = linkedin
-        end
-        if profile_page.css("div.social-icon-container a")[2].attribute("href").value
-          all_profiles[:github] = github
-        end
-        if profile_page.css("div.social-icon-container a")[3].attribute("href").value
-          all_profiles[:blog] = blog
-        end
-        if profile_page.css(".profile-quote").text
-          all_profiles[:profile_quote] = profile_quote
-        end
-        if profile_page.css(".details-container p").children.text
-          all_profiles[:bio] = bio
-        end
-        #binding.pry
-
-      end
-    end
-
-
-
-
+    end #method
 
 end #class
