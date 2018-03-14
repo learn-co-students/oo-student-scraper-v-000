31c1574ee72825a1884f1459e5bd13a96b70626c
Ryan Manchester <ryanm17@gmail.com>
2018-03-14 00:00:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,18 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+    binding.pry
+    #name : doc.css(".student-name").text
+    #location: doc.css(".student-location")
+    #profile_url: doc.css(".student-card").children[1].attributes["href"].value
+
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
