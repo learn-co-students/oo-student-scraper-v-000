621435c836f0103572e0e222d9107bae0e30d4a3
Denise Pendleton <dpendl1@hotmail.com>
2018-03-18 03:51:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,12 +5,32 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    # s = Student.new(student_hash)
+    # s.name = student_hash[:name]
+    # s.location = student_hash[:location]
+    # s.twitter = student_hash[:twitter]
+    # s.linkedin = student_hash[:linkedin]
+    # s.github = student_hash[:github]
+    # s.blog = student_hash[:blog]
+    # s.quote = student_hash[:quote]
+    # s.bio = student_hash[:bio]
+    # s.profile_url = student_hash[:profile_url]
+    #
+    # @@aall << s
 
   end
 
-  def self.create_from_collection(students_array)
+  def self.create_from_collection(students)
+      s = Student.new(students)
 
-  end
+      s = students.collect do |student|
+        s.name=student[:name]
+        s.location=student[:location]
+        s
+        binding.pry
+        @@all << s
+      end
+    end
 
   def add_student_attributes(attributes_hash)
 
