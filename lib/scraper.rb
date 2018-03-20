8dd626a513ba2bb2a64bd68706623b8a0397dbec
Kriti Rai <kriti.rai@gmail.com>
2018-03-20 05:29:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,18 +5,18 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
-
     scraped_students = []
-    student_hash = {}
-    student_hash[:name] = doc.css(".student-card").css("h4").css(".student-name").map{|name| name.text}
-    student_hash[:location] = doc.css(".student-card").css("p").css(".student-location").map{|location| location.text}
-    student_hash[:profile_url] = doc.css(".student-card").css("a").map{|link| link.attribute('href').to_s}
-    student_hash.map do |k,v|
-      scraped_students << Student.new(student_hash).send("#{k}=",v)
+    student_profile = doc.css(".student-card")
+    # binding.pry
+    student_profile.each do |student|
+      student = {
+      :name => student.css("h4.student-name").text,
+      :location => student.css("p.student-location").text,
+      :profile_url => student.css("a").attribute('href').to_s
+    }
+      scraped_students << student
     end
     scraped_students
-    binding.pry
-    # scraped_students
     # binding.pry
   end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,14 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    # student_hash = {}
-    # student_hash.each do |k,v|
-    #   self.send("#{k}=",v)
-    # end
+    student_hash = {}
+    student_hash.map do |k,v|
+      @@all << self.send("#{k}=",v)
+    end
   end
 
   def self.create_from_collection(students_array)
-
+    student_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -20,6 +22,6 @@ class Student
   end
 
   def self.all
-
+    @@all
   end
 end
