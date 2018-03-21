f60ffae89f145fc2ce1d1ad46e62aa6167cbffe9
Emmy White <emmalinerosewhite@gmail.com>
2018-03-21 10:21:16 -0400

    Testing, learning.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
+#This is a test. Beep-bop-a-looh-bah, ah-boom-bam-boom!
 end
-
