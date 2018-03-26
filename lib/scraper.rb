ba3d36fc1c63b8417fef1eab0629f1316d067be3
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-26 17:26:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    binding.pry
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
