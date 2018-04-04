2667fb4a35ab0fbbe2ea1e61efd4be5b4823c2b4
Nathan  Visak <nathan.visak@gmail.com>
2018-04-04 18:27:46 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,24 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open("index_url"))
+    student = [{}]
+
+    student[:name] =
+    student[:location] =
+    student[:profile_url] = 
+
+    end
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
