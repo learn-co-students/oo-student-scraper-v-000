eb1c0d0708c2d6c9134bea7973febbea761f128c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-11 21:45:55 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,18 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open("./fixtures/student-site/index.html")
-    doc = Nokogiri::HTML(html)
-  end
+    index_url = open("./fixtures/student-site/index.html")
+    doc = Nokogiri::HTML(index_url)
+    students = doc.css(".roster-cards-container")
+    scraped_students = []
+    
+    students.map do |student|
+      student_hash = {name: student.css(".student-name").text, location: student.css(".student-location").text, profile_url: student.css(".view-profile-div").text}
+       scraped_students << student_hash 
+    end
+    scraped_students
+    
+    end
 
   def self.scrape_profile_page(profile_url)
     
