0f952ee9204a9c7084851dad958323be60046e04
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-11 21:10:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("./fixtures/student-site/index.html")
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
