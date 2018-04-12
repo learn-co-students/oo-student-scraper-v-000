01c2bea7bfff8a1e5751f892a8a06e314c83d752
Vanessa Toro <vanetoro84@gmail.com>
2018-04-12 16:13:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,8 +2,21 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  
+  attr_accessor :name, :location, :page_url
+  
+  # :name=> index_page.css('h4').children.text
+  # :location => index_page.css('.student-location').children.text
+  # :page_url => index_page.css('.student-card a').attribute('href').value
 
   def self.scrape_index_page(index_url)
+    html = open('./fixtures/student-site/index.html')
+    index_page = Nokogiri::HTML(html)
+    binding.pry
+    
+    students = {}
+    
+    index_page.css
     
   end
 
