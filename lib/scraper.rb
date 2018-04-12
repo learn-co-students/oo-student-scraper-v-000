970f9a300c1afd5cb9cbab311d623e0b9e581774
Victoria Meng <nights1ststar@protonmail.com>
2018-04-12 20:59:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,29 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+    profiles = doc.css(".student-card")
+    profiles.each do |profile|
+      fields = {}
+      fields[:name] = profile.css(".student-name").text
+      fields[:location] = profile.css(".student-location").text
+      fields[:profile_url] = profile.css('a')[0]['href']
+      students << fields
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    social_media = []
+    doc = Nokogiri::HTML(open(profile_url))
+    links = doc.css(".social-icon-container")
+    links.each do |link|
+      fields = {}
+      fields[:linkedin] = profile.css('a')[0]['href']
+      binding.pry
+    end
+
   end
 
 end
-
