5cfe2586cc7735874efb94721925a1a1918fd590
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-12 17:13:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,14 +11,18 @@ class Scraper
     students = doc.css(".student-card")
     scraped_students = []
     students.map do |student|
-      student_hash = {name: student.css(".student-name").text, location: student.css(".student-location").text, profile_url: student.css(".view-profile-div")}
+      student_hash = {name: student.css(".student-name").text, location: student.css(".student-location").text, profile_url: student.css("a")[0]["href"]}
        scraped_students << student_hash
      end
     scraped_students
     end
 
   def self.scrape_profile_page(profile_url)
-    
+    array = self.scrape_index_page(open("./fixtures/student-site/index.html"))
+    profiles = array.map do |students|
+      students[:profile_url]
+    end
+    profiles
   end
 
 end
