c4da7f6e3ebd2f1bce88c9b2e586bb79ed1d113c
Vanessa Toro <vanetoro84@gmail.com>
2018-04-12 16:38:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,15 +10,18 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open('./fixtures/student-site/index.html')
     index_page = Nokogiri::HTML(html)
-    binding.pry
     
-    students = {}
     
-    index_page.css('roster-body-wrapper').each do |student|
-        :name=> index_page.css('h4').children.text,
+    students = []
+    
+    index_page.css('.roster-body-wrapper').each do |student|
+    
+        :name => index_page.css('h4').children.text,
         :location => index_page.css('.student-location').children.text,
         :page_url => index_page.css('.student-card a').attribute('href').value
-    
+    end    
+    binding.pry
+    students
   end
 
   def self.scrape_profile_page(profile_url)
