038147865b882192c95e1c18a71a517976c4654e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-04-19 14:33:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,33 @@
+require  'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    scraper_doc   = doc.css(".roster-cards-container")
+
+  #  binding.pry
+       scraper_doc.each do |student|
+
+         name = student.css(".student-name").text
+         location = student.css(".student-location").text
+          profile_url = student.css("href")
+          puts "name"  + name
+          puts  "location"  + location
+          puts  profile_url
+
+#
+    end
+#
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+
   end
 
 end
 
+Scraper.scrape_index_page("fixtures/student-site/index.html")
