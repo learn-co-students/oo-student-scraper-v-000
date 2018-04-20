27d33a24c218f10c6a5e6b6f6ef3794fdcbae4bb
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-04-20 16:54:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -11,14 +11,28 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
-      student.new(student_hash)
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    self.new(attributes_hash)
+    attributes_hash.each {|key, value| self.send("#{key}=", value)}
   end
 
   def self.all
     @@all
   end
 end
+student_hash  =  {:twitter=>"http://twitter.com/flatironschool",
+      :linkedin=>"https://www.linkedin.com/in/flatironschool",
+      :github=>"https://github.com/learn-co",
+      :blog=>"http://flatironschool.com",
+      :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+      :bio=> "I'm a school"
+     }
+
+student = Student.new(student_hash)
+puts "twitter"
+student.twitter= "ssssssssssssssss"
+
+Student.all
