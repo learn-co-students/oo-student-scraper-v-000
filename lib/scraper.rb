e1d079e3503a3d2acc077efa84d6b3aa36d0cae4
Diego Yanez <diegoyanez@MacBook-Pro-2.local>
2018-04-26 16:23:09 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,16 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    binding.pry
+    doc = Nokogiri::HTML(open(fixtures/student-site/index.html))
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
