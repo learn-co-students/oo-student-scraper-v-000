4e647d2515d440a20dc6cc72375a444b79fd7749
Chris Dundon <chris.dundon@gmail.com>
2018-04-27 04:58:11 +0000

    Done.

--- config.rb
+++ config.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require_relative "./lib/student.rb"
 require_relative "./lib/scraper.rb"
 require_relative "./lib/command_line_interface.rb"
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,25 @@
-require 'open-uri'
-require 'pry'
+require_relative '../config.rb'
+
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url = "http://192.241.157.192:47859/fixtures/student-site/")
+    rtn = []
+
+    page = Nokogiri::HTML(open(index_url))
+    cards = page.css(".student-card")
+    cards.each do |card|
+      add = {}
+      add[:name] = card.css(".student-name").text
+      add[:location] = card.css(".student-location").text
+      add[:url] = card.css("a").url
+      rtn << add
+    end
+    rtn
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
