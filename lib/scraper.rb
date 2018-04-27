e11c842f8a939042fdd5ea9ac7a91e64f946f44d
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-27 22:00:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,13 +7,21 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    doc.css("div .student-card")
-      puts doc
+     name_container = doc.css("div .roster-cards-container")
+     name = doc.css("div .student-card").first.css("h4").text
+     name_container.map do |profile|
+     puts name
+   end
+   end
 
-  end
+
 
   def self.scrape_profile_page(profile_url)
-
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+     name = doc.css("div .student-card").first.css("h4").text
+     from = doc.css("div .student-card").first.css("p").text
+      puts doc
   end
 
 end
