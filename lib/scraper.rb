944cfdc26f8bbb445fa00020d28b36ca78e11cde
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-27 01:36:16 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
 require 'pry'
 
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css("div .student-card")
+      puts doc
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
