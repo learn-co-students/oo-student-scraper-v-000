898ba8cfdf23247631af23d6efe2ae5789e01643
Ally Netzel <netzelally@gmail.com>
2018-04-29 15:47:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,13 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  attr_accessor :name, :location, :profile_url
+  # <h4 class="student-name">Ryan Johnson</h4>
+  # doc.css(".card-text-container").first.css("h4")
   def self.scrape_index_page(index_url)
+    hash = Hash.new  
     
   end
 
