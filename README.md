a668b93123cdeac7702a7c2c43b5e95a081ba0be
Ally Netzel <netzelally@gmail.com>
2018-04-30 13:04:43 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-I'm # Object Oriented Student Scraper
+HiI'm # Object Oriented Student Scraper
 
 ## Objectives
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,8 +11,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = self.get_page
     array = []
-    students = {}
-  doc.css(".student-card").each do |student|
+    doc.css(".student-card").each do |student|
     hash_student = {
       :name => student.css("h4").text,
       :location => student.css("p").text,
@@ -23,7 +22,20 @@ class Scraper
   array
 end
   def self.scrape_profile_page(profile_url)
-    
+    binding.pry
+    doc = Nokogiri::HTML(open("./fixtures/student-site/students/joe-burgess.html"))
+    doc.css(".student-card").each do |student|
+      hash_profile = {
+        :linkedin => student.css("a").attribute("href").value
+
+        # ("https://linkedin.com"),
+        # :github => student.css("https://github.com"),
+        # :blog => student.css("http://flatironschool.com"),
+        # :profile_quote => student.css("p").text,
+        # :bio => student.css("p").text
+      }
+    end
+hash_profile
   end
 
 end
