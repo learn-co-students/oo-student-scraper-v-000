cf0b3dca404713c87096d67749570810a8ea1ed4
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-01 00:10:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,27 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    #retreive student page
+    html = File.read(profile_url)
+    #access HTML of stuent page
+    infos = Nokogiri::HTML(html)
 
+    #initializes student_profile as a has
+    scraped_student = {}
+
+    #scrapes the bio and assigns to student
+    scraped_student[:bio] = infos.css('.details-container').css('p').text
+
+    #assign all the social media attributes
+    infos.css('.vitals-container').each do |attribute|
+
+      scraped_student[:profile_quote] = attribute.css('.profile-quote').text
+      scraped_student[:twitter] = attribute.css('a')[0]['href']
+      scraped_student[:linkedin] = attribute.css('a')[1]['href']
+      scraped_student[:github] = attribute.css('a')[2]['href'] unless attribute.css('a')[2] == nil
+      scraped_student[:blog] = attribute.css('a')[3]['href'] unless attribute.css('a')[3] == nil
+    end
+    scraped_student
   end
 
 end
