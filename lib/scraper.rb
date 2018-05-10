cad18ad1ae07579991ac03c880a23193114c65a4
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-05-10 19:05:25 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,26 +22,28 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     page = Nokogiri::HTML(html)
-    social_icon_container = page.css(".social-icon-container")
-      social_icon_container.each do |social_link|
-        social_links = {
-          :twitter => social_link.css("a").map{ |a| if a['href'].match("/twitter/")
-          }
-        
-    # page.css(".vitals-container").each do |item|
-    #   item.css(".social-icon-container") do |social_link|
+    social_links = page.css("div.social-icon-container a")
+    social_links.each do |social_link|
+      social_link_hash = {
+        :twitter => social_link.css("a").select{|link| link["href"].include?("twitter")}
+        :linkedin => social_link.css("a").select{|link| link["href"].include?("linkedin")}
+        :github => social_link.css("a").select{|link| link["href"].include?("github")}
+      }
+    end
+    
+      binding.pry
+    end
+  end
         
       
       
-    #   social_links = {
+    #     social_links = 
     #     :twitter =>
     #     :linkedin =>
     #     :github =>
     #     :blog =>
     #     :profile_quote =>
     #     :bio =>
-    
-  end
 
 end
 
