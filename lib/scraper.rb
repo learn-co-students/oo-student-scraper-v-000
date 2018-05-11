8e1c27f7ca26527da5fc50c74eb7789f557b104a
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-05-11 18:41:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,17 +24,26 @@ class Scraper
     page = Nokogiri::HTML(html)
     social_links = page.css("div.social-icon-container a")
     social_link_hash = {}
-    social_links.each do |social_link|
-      if social_link.attribute('href').value.include?("twitter")
-        social_link_hash[:twitter] = social_link.attribute('href').value
-      elsif social_link.attribute('href').value.include?("linkedin")
-        social_link_hash[:linkedin] = social_link.attribute('href').value
-      elsif social_link.attribute('href').value.include?("github")
-        social_link_hash[:github] = social_link.attribute('href').value
+    page.css(".social-icon-container")
+    student_name = page.css("div.vitals-text-container h1")
+    i = 0
+    while i < social_links.css("a").length
+      social_links.each do |social_link|
+        if social_link.attribute('href').value.include?("twitter")
+          social_link_hash[:twitter] = social_link.attribute('href').value
+        elsif social_link.attribute('href').value.include?("linkedin")
+          social_link_hash[:linkedin] = social_link.attribute('href').value
+          #binding.pry
+        elsif social_link.attribute('href').value.include?("github")
+          social_link_hash[:github] = social_link.attribute('href').value
+        elsif social_link.attribute('href').value.include?(student_name)
+          social_link_hash[:blog] = social_link.attribute('href').value
+        end
+        i += 1
       end
     end
-    social_link_hash[:profile_quote] = page.css("div.vitals-text-container div")
-    social_link_hash[:bio] = page.css("div.description-holder p")
+    social_link_hash[:profile_quote] = page.css("div.vitals-text-container div").text
+    social_link_hash[:bio] = page.css("p").text
     social_link_hash
     #binding.pry
   end
