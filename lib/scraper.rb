3508adbf177457dc100d2635c9e888c1a8322b26
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-05-11 19:54:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,20 +25,16 @@ class Scraper
     social_links = page.css("div.social-icon-container a")
     social_link_hash = {}
     page.css(".social-icon-container")
-    i = 0
-    while i < social_links.css("a").length
-      social_links.each do |social_link|
-        if social_link.attribute('href').value.include?("twitter")
-          social_link_hash[:twitter] = social_link.attribute('href').value
-        elsif social_link.attribute('href').value.include?("linkedin")
-          social_link_hash[:linkedin] = social_link.attribute('href').value
-          #binding.pry
-        elsif social_link.attribute('href').value.include?("github")
-          social_link_hash[:github] = social_link.attribute('href').value
-        elsif social_link.attribute('href').value.include?("div.vitals-text-container h1")
-          social_link_hash[:blog] = social_link.attribute('href').value
-        end
-        i += 1
+    social_links.each do |social_link|
+      if social_link.attribute('href').value.include?("twitter")
+        social_link_hash[:twitter] = social_link.attribute('href').value
+      elsif social_link.attribute('href').value.include?("linkedin")
+        social_link_hash[:linkedin] = social_link.attribute('href').value
+        #binding.pry
+      elsif social_link.attribute('href').value.include?("github")
+        social_link_hash[:github] = social_link.attribute('href').value
+      else
+        social_link_hash[:blog] = social_link.attribute('href').value
       end
     end
     social_link_hash[:profile_quote] = page.css("div.vitals-text-container div").text
--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.create_from_collection(students_array)
