cdf939a3fb848b71cc081bbd929806dd0c16c4ab
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-05-12 14:39:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,6 @@ class Scraper
     social_links = page.css("div.social-icon-container a")
     social_link_hash = {}
     page.css(".social-icon-container")
-<<<<<<< HEAD
     social_links.each do |social_link|
       if social_link.attribute('href').value.include?("twitter")
         social_link_hash[:twitter] = social_link.attribute('href').value
@@ -36,22 +35,6 @@ class Scraper
         social_link_hash[:github] = social_link.attribute('href').value
       else
         social_link_hash[:blog] = social_link.attribute('href').value
-=======
-    i = 0
-    while i < social_links.css("a").length
-      social_links.each do |social_link|
-        if social_link.attribute('href').value.include?("twitter")
-          social_link_hash[:twitter] = social_link.attribute('href').value
-        elsif social_link.attribute('href').value.include?("linkedin")
-          social_link_hash[:linkedin] = social_link.attribute('href').value
-          #binding.pry
-        elsif social_link.attribute('href').value.include?("github")
-          social_link_hash[:github] = social_link.attribute('href').value
-        elsif social_link.attribute('href').value.include?("div.vitals-text-container h1")
-          social_link_hash[:blog] = social_link.attribute('href').value
-        end
-        i += 1
->>>>>>> 797b1a3c4446fd7d7e3ac505380d18014510dc83
       end
     end
     social_link_hash[:profile_quote] = page.css("div.vitals-text-container div").text
