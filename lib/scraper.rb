a6743efa835c240c231a72d79ab4230db2302437
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-13 03:27:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,37 +4,28 @@ require 'nokogiri'
 
 class Scraper
 
-  # def self.get_page
-  #   doc = Nokogiri::HTML(open(index_url))
-  # end
-
   def self.scrape_index_page(index_url)
     results = []
     doc = Nokogiri::HTML(open(index_url))
     students = doc.css("div.student-card")
-    # names = doc.css(".student-card").css(".student-name").text
-    # locations = doc.css(".student-card").css(".student-location").text
-    # profile_url= doc.css(".student-card").css(".href").text
-
-
     students.each do |student|
-      results << {:name => student.css("h4.student-name").text,
-      :location => student.css("p.student-location").text,
-     :profile_url => "http://192.241.157.192:41735/fixtures/student-site/#{student.css("a").first["href"]}"}
-    #  "http://127.0.0.1:4000/fixtures/student-site/#{i.css("a").first["href"]}"
-    # :location => student.css("a").css(".a").text}
-
-    # name = doc.css(".student-card").css(".student-name").text
-    # location = doc.css(".student-card").css(".student-location").text
-    # profile_url= doc.css(".student-card").css(".href").text
-
-
-end
-results
+      results << {:name => student.css(".student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").first["href"]}
+    end
+    results
   end
 
   def self.scrape_profile_page(profile_url)
+    social_profile = []
+    profile = Nokogiri::HTML(open(profile_url))
+    social = profile.css(".social-icon-container").css("a")
 
+      social.each do |link|
+        
+
+    binding.pry
+  end
   end
 
 end
