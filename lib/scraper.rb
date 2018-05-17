f218fd9502411fe3d340e8acce7628aeb2c4a21f
Nicholas Powers <njpowers7915@gmail.com>
2018-05-17 18:47:59 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,46 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = File.read(index_url)
+    file_text = Nokogiri::HTML(html)
     
+    student_hash_array = []
+    
+    file_text.css("div.student-card").each do |profile|
+      student_hash = {
+        :name => profile.css("h4.student-name").text,
+        :location => profile.css("p.student-location").text,
+        :profile_url => profile.css("a").attribute("href").value
+      }
+      student_hash_array << student_hash
+    end
+  student_hash_array
   end
 
   def self.scrape_profile_page(profile_url)
+    html = File.read(profile_url)
+    file_text = Nokogiri::HTML(html)
     
+    @scraped_student = {}
+    
+    @scraped_student[:profile_quote] = file_text.css("div.profile-quote").text
+    @scraped_student[:bio] = file_text.css("div.bio-content.content-holder").css("p").text
+    file_text.css("div.social-icon-container").css("a").each do |link|
+      if link.attribute("href").value.include?("twitter.com")
+        @scraped_student[:twitter] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("linkedin.com")
+        @scraped_student[:linkedin] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("github.com")
+        @scraped_student[:github] = link.attribute("href").value
+      else 
+        @scraped_student[:blog] = link.attribute("href").value
+      end
+    end
+    @scraped_student
   end
-
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +7,33 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location] 
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio] 
+    @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all
   end
 
   def self.all
-    
+    @@all
   end
 end
 
