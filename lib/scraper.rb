be7cfb4de72a48b4bbdec7d7676e99fe86b3c55f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-17 19:10:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read('./fixtures/student-site/index.html')
+    student_scraper = Nokogiri::HTML(html)
+
+    scraped_students = []
+
+    student_scraper.css().each do |student|
+      name = student.css().text
+      scraped_students[name.to_sym] = {}
+    end
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
