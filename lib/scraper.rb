7a32c170c16019e00a00d3e7488a3d88039f3eac
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-18 05:32:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,15 @@ require 'pry'
 
 class Scraper
 
+  attr_accessor :name, :location, :profile_url
+
   def self.scrape_index_page(index_url)
+    student_index_array = {}
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     students = doc.css("div .roster-cards-container")
     students.each do |student_card|
+      student_card.css("div a").attribute("href").value
      binding.pry
     end
 
