157416f6a3b8a3f263bb3d00606ab06cccb872d8
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-18 03:22:08 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,23 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css("div .roster-cards-container")
+    students.each do |student_card|
+     binding.pry
+    end
+
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
