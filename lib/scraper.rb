e5eb23308af432a0f6dfa7e626e5b33ae37cf374
Shea Kusiak <shkusiak@gmail.com>
2018-05-20 19:34:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,51 @@
 require 'open-uri'
 require 'pry'
 
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css("div.roster-cards-container").each do |roster_card|
+      roster_card.css(".student-card a").each do |student|
+        student_name = student.css('.student-name').text
+        student_location = student.css('.student-location').text
+        student_link = student['href']
+
+        students << {name: student_name, location: student_location, profile_url: student_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
+
+    links = doc.css(".social-icon-container a").map { |link| link['href']}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+
+    if doc.css(".profile-quote")
+      student[:profile_quote] = doc.css(".profile-quote").text
+    end
+
+    if doc.css("div.bio-content.content holder div.description-holder p")
+      student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
+    end
+
+    student
   end
 
 end
-
