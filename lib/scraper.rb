23771c791e742bc48574b01ad08bd929407c1e9a
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-21 22:44:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,22 @@
-require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url = "http://165.227.16.205:49280/fixtures/student-site/")
-    # html = open(index_url)
-     html = File.read('fixtures/student-site/index.html')
+  def self.scrape_index_page(index_url)
+    html = open(index_url)
+    #  html = File.read('fixtures/student-site/index.html')
      doc = Nokogiri::HTML(html)
-     binding.pry
-     doc.css("").each do |student|
+     doc.css("div.roster-cards-container").each do |student_card|
+       binding.pry
+       student_card.css(".student_card a").each do |student|
+         name = student.css(".student-name").text
+         location = student.css(".student-location").text
+         profile_url
+       end
      end
-  end
+   end
+
 
   def self.scrape_profile_page(profile_url)
 
