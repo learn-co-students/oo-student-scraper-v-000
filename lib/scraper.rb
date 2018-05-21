1d045a0e3352abda0a7730e55705be6002ee59a7
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-21 21:04:50 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,15 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url = "http://165.227.16.205:49280/fixtures/student-site/")
+     html = open('http://165.227.16.205:49280/fixtures/student-site/')
+     doc = Nokogiri::HTML(html)
+     binding.pry
+     doc.css("").each do |student|
+     end
   end
 
   def self.scrape_profile_page(profile_url)
