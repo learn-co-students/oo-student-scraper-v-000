bc8da71bddbb892487ea812bdd3d850cf0d9181b
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-21 21:12:43 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url = "http://165.227.16.205:49280/fixtures/student-site/")
-     html = open('http://165.227.16.205:49280/fixtures/student-site/')
+    # html = open(index_url)
+     html = File.read('fixtures/student-site/index.html')
      doc = Nokogiri::HTML(html)
      binding.pry
      doc.css("").each do |student|
