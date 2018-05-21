1aa438ea4bf69f7fb7297e2f7e867f32a794c83c
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-05-21 23:00:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,18 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  attr_accessor :location, :name
 
   def self.scrape_index_page(index_url)
-    
+    index_url = "./fixtures/student-site/index.html"
+    scraped_students = [{:location}
+
+    ]
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
