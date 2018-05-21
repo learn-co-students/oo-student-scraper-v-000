fd258cf1c00abbbd152dc47037a0c844039db6b1
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-21 20:55:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(http://165.227.16.205:49280/fixtures/student-site/)
+  def self.scrape_index_page(index_url = "http://165.227.16.205:49280/fixtures/student-site/")
   end
 
   def self.scrape_profile_page(profile_url)
