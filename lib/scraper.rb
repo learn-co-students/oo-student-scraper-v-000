9d78e97853519df099d1d65057a8cbd641216db1
Anu Khambete <anukhambete@gmail.com>
2018-05-22 21:32:55 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,20 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     student_prof = Nokogiri::HTML(open(profile_url))
+    #student_prof.css("div.social-icon-container a").attribute("href").value -- gives us "https://twitter.com/jmburges"
+    # student_prof.css("div.social-icon-container").css("a")    .attribute("href").value
+    
+    #student_prof.css("div.social-icon-container a").attribute("href").value.gsub(/http(s)?:\/\/(www.)?|.(com|net|co.uk|us)+.*/,"")
+    
+    #student_prof.css("div.social-icon-container").css("a").each do |s_icon|
+    #s_icon.attribute("href").value #link
+    #s_icon.attribute("href").value.gsub(/http(s)?:\/\/(www.)?|.(com|net|co.uk|us)+.*/,"") #title to symbol
+      
+      
+    #end
+    
+    
+    
     binding.pry
   end
 
