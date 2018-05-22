cacf60572c56e7cb789d09c3f0b9102353460df0
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-22 19:23:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,17 +13,29 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
-    links = doc.css('div.social-icon-container')
-    linkedin = links.xpath("a[contains(@href, 'linkedin')]")[0]['href']
-    github = links.xpath("a[contains(@href, 'github')]")[0]['href']
-    #blog =
-    profile_quote = doc.css('div.profile-quote').text
-    bio = doc.css('div.description-holder p').text
-    binding.pry
+    profile = {}
+    profile_urls = "./fixtures/student-site/students/kris-henderson.html"
+    doc = Nokogiri::HTML(open(profile_urls))
+    links = []
+    doc.css('div.social-icon-container a').each {|link| links << link['href']}
+      linkedin = links.detect {|l| l.include?"linkedin"}
+      github = links.detect {|l| l.include?"github"}
+      twitter  = links.detect {|l| l.include?"twitter"}
+      blog = links.detect {|l| l != linkedin && l != github && l != twitter}
+      binding.pry
+      
+      #l = xpath("a[contains(@href, 'linkedin')]")[0]['href']
+      #g = links.xpath("a[contains(@href, 'github')]")[0]['href']
+      #b = links.xpath("a[contains(@href, 'http:')]")[0]['href']
+      #t = links.xpath("a[contains(@href, 'twitter')]")[0]['href']
+      
+        profile[:linkedin] = linkedin if linkedin != nil
+        profile[:blog] = blog if blog != nil
+        profile[:twitter] = twitter if twitter !=nil
+        profile[:profile_quote] = doc.css('div.profile-quote').text
+        profile[:bio] = doc.css('div.description-holder p').text
+        
+    
   end
 
 end
-
-#roster-cards-container
-
