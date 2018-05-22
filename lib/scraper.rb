5fc7a86c6763159a60bfcadcc35a0030202e973a
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-05-22 00:27:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,10 +6,14 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     index_url = "./fixtures/student-site/index.html"
-    scraped_students = [{:location}
-
-    ]
-
+    doc = Nokogiri::HTML(open(index_url))
+    scraped_students = []
+    binding.pry
+    # doc.css("div.roster-cards-container").each do |card|
+      # students = doc.css("div.student-card div.card-text-container")
+      # name = doc.css("div h4.student-name").text
+      # location = doc.css("div p.student-location").text
+      # profile_url =
   end
 
   def self.scrape_profile_page(profile_url)
