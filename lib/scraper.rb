cc23d10781717e6084baae31e308e091d7d92da0
Anu Khambete <anukhambete@gmail.com>
2018-05-22 20:01:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,8 +6,18 @@ class Scraper
   def self.scrape_index_page(index_url)
    learn_ver = Nokogiri::HTML(open(index_url)) 
    
+   students = []
    
-   
+     learn_ver.css("div.student-card").each do |student|
+       student_hash = {}
+       student_hash ={
+        :name => 
+        :location =>
+        :profile_url => 
+      }
+      students << student_hash
+    end
+   students
   end
 
   def self.scrape_profile_page(profile_url)
