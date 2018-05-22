8bc16fc92a7a075dee411275e7ada2aaadd1d1eb
Anu Khambete <anukhambete@gmail.com>
2018-05-22 19:55:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+   learn_ver = Nokogiri::HTML(open(index_url)) 
+   
+   
+   
   end
 
   def self.scrape_profile_page(profile_url)
