2f197d6f78f98efa0f6acdb0e5a993e6fd026195
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-25 04:43:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,11 +37,11 @@ class Scraper
       else
         profile_hash[:blog] = link
 
-        profile_hash[:profile_quote] = doc.css(".profile-quote").text
-        profile_hash[:bio] = doc.css(".description-holder p").text
         #binding.pry
        end
      end
+    profile_hash[:profile_quote] = doc.css(".profile-quote").text
+    profile_hash[:bio] = doc.css(".description-holder p").text
     profile_hash
   end
 
