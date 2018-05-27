5874e0ff91f00ffcb61ed7f598d5bda2fd52a376
Christiana Vandermale <christianavdm@gmail.com>
2018-05-27 21:05:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,11 +10,12 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| Student.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    self
   end
 
   def self.all
