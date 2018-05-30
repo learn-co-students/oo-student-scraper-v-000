ef57e8f3916e56c31462761c19d1d0e61fdbd7f6
sgibson47 <sgibson47@gmail.com>
2018-05-30 18:00:40 -0500

    identified css selctors for #self.scrape_profile_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,11 +15,10 @@ class Scraper
     #profile_url: student.css("a").attribute("href").value
 
     index_page.css(".student-card").each do |student|
-      binding.pry
       students << {
-        :name => student.css("selector").text,
-        :location => student.css("selector").text,
-        :profile_url => student.css("selector").attribute("href").value
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attribute("href").value
       }
     end
     students
@@ -27,6 +26,13 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
+    binding.pry
+    #twitter: profile_page.css(".social-icon-container a").attribute("href").value
+    #linkedin: profile_page.css(".social-icon-container a +a").attribute("href").value
+    #github: profile_page.css(".social-icon-container a +a +a").attribute("href").value
+    #blog: profile_page.css(".social-icon-container a +a +a +a").attribute("href").value
+    #profile_quote: profile_page.css(".profile-quote").text
+    #bio: profile_page.css(".description-holder p").text
 
     student = {
       :twitter => profile_page.css("selector").text,
