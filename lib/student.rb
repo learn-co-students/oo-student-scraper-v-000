48b6d34e8c9b48b2242b11c153586252c07af0b1
Shirley Collins <shirley@m104.us>
2018-05-30 20:42:57 -0700

    Added methods to the Student class (#initialize, .create_from_collection, #add_student_attributes, .all)

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
@@ -5,18 +7,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    @bio = attributes_hash[:bio]
+    @blog = attributes_hash[:blog]
+    @linkedin = attributes_hash[:linkedin]
+    @profile_quote = attributes_hash[:profile_quote]
+    @twitter = attributes_hash[:twitter]
   end
 
   def self.all
-
+    @@all
   end
 end
