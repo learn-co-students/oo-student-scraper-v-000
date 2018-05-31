f6b7a1668ff6d8750f26344125e4f1e509dc7e38
sgibson47 <sgibson47@gmail.com>
2018-05-31 12:37:05 -0500

    added self.create_from_collection

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
