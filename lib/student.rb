99ea248f69238203ce7b79ef695998210ac0677d
sgibson47 <sgibson47@gmail.com>
2018-05-31 12:34:08 -0500

    refactor Student.new to iterate over hash & use .send

--- lib/student.rb
+++ lib/student.rb
@@ -5,13 +5,15 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name]
-    @location = student_hash[:location]
+    student_hash.each do |k,v|
+      self.send("#{k}=", v)
+    end
+
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
