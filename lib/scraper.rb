24f6817612656825d170026beea2840488957fde
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-01 22:51:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,25 +1,30 @@
 require 'open-uri'
-require 'pry'
 require 'nokogiri'
+require 'pry'
+
 
 class Scraper
 
   def self.scrape_index_page(index_url)
   doc = Nokogiri::HTML(open("fixtures/student-site/index.html"))
-    doc.css(".roster-cards-container").each do |key, value|
-      student-name = doc.css(".roster-cards-container").first.css(".student-name").first.text
-      student-location = doc.css(".roster-cards-container").first.css(".student-location").first.text
-      card =  index_url.css(".student-card a").first
-      link = card.attr("href")
+    student_card = doc.css(".student-card")
+    array = []
+    student_card.each do |student|
+      array << {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attr("href").value
+      }
 
-      puts "{:name => #{student-name}, :location => #{student-location}, :profile-url => #{link} }"
-   #index_url = doc.css(".roster-cards-container").first
-
+      end
+      array
     end
-  end
 
-  def self.scrape_profile_page(profile_url)
 
-  end
+    def self.scrape_profile_page(profile_url)
+
+      doc = Nokogiri::HTML(open("fixtures/student-site/students/ryan-johnson.html"))
+        binding.pry
+    end
 
 end
