9010e196dbbf7610b56766758845d178e61f4f32
theresamorelli <theresa.l.morelli@gmail.com>
2018-06-04 23:01:10 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    fs = Nokogiri::HTML(File.read(index_url))
-    
+    index = Nokogiri::HTML(open(index_url))
     students = []
 
-    fs.css("div.student-card").each do |student|  
+    index.css("div.student-card").each do |student|
       student_info = {:name => student.css("h4.student-name").text, :location => student.css("p.student-location").text, :profile_url => student.css("a").attribute("href").value}
 
       students << student_info
@@ -19,30 +18,27 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)    
-    student = Nokogiri::HTML(File.read(profile_url))
+    student = {}
+    page = Nokogiri::HTML(File.read(profile_url))
+    student_info = page.css("div.profile")
+    urls = student_info.css("div.social-icon-container a").collect {|i| i.attribute("href").value}
 
-    student_info = {}
-
-    info = student.css("div.profile")
-
-    social = info.css("div.social-icon-container a").collect {|i| i.attribute("href").value}
-
-    social.each do |s|
+    urls.each do |s|
       case
       when s.include?("linkedin")
-        student_info[:linkedin] = s
+        student[:linkedin] = s
       when s.include?("twitter")
-        student_info[:twitter] = s
+        student[:twitter] = s
       when s.include?("github")
-        student_info[:github] = s
+        student[:github] = s
       else
-        student_info[:blog] = s
+        student[:blog] = s
       end
     end
 
-    student_info[:profile_quote] = info.css("div.profile-quote").text
-    student_info[:bio] = info.css("div.bio-content p").text
+    student[:profile_quote] = student_info.css("div.profile-quote").text if student_info.css("div.profile-quote")
+    student[:bio] = student_info.css("div.bio-content p").text if student_info.css("div.bio-content p")
 
-    student_info
+    student
   end
 end
