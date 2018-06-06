16e473f70e06211988e4e43d514d2e1682be1571
Conor Hamilton <conhamil19@gmail.com>
2018-06-06 10:49:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    student_hash.each{|a,b| self.send("#{a}=", b)}
+    student_hash.each{|c,b| self.send("#{c}=", b)}
     @@all << self
   end
 
