ed7844be92e28d9cf1c9796c197cb7b3e4ff51f4
David Moore <hey@iamdavidmoore.com>
2018-06-06 15:40:38 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,12 @@ require 'pry'
 class Scraper
   def self.scrape_index_page(index_url)
     student_ary = []
-    doc = Nokogiri::HTML(open(index_url))
-    index_page = doc.css(".roster-cards-container .card-text-container")
-    index_page.each_with_index do |student, a_tag_num|
+    index_page = Nokogiri::HTML(open(index_url))
+    index_page.css(".roster-cards-container .card-text-container").each_with_index do |student, a_tag_num|
       hash = {}
       hash[:location] = student.css("p.student-location").text
       hash[:name] = student.css("h4.student-name").text
-      hash[:profile_url] = doc.css("a")[a_tag_num+1]["href"]
+      hash[:profile_url] = index_page.css("a")[a_tag_num+1]["href"]
       student_ary << hash
     end
     student_ary
@@ -18,8 +17,8 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     profile = {}
-    doc = Nokogiri::HTML(open(profile_url))
-    urls = doc.css(".social-icon-container a")
+    page = Nokogiri::HTML(open(profile_url))
+    urls = page.css(".social-icon-container a")
     urls.each do |link|
       if  link["href"].include?("twitter")
         profile[:twitter] = link["href"]
@@ -31,8 +30,8 @@ class Scraper
         profile[:blog] = link["href"]
       end
     end
-    profile[:profile_quote] = doc.css(".vitals-text-container div.profile-quote").text
-    profile[:bio] = doc.css(".details-container .description-holder p").text
+    profile[:profile_quote] = page.css(".vitals-text-container div.profile-quote").text
+    profile[:bio] = page.css(".details-container .description-holder p").text
     profile
   end
 end
