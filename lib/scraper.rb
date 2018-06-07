2b77c725843f80d4605dcd652cce85cd8f1ba1b6
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-07 21:04:53 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,9 +22,27 @@ class Scraper
 
 
     def self.scrape_profile_page(profile_url)
+   profile_hash = {}
+   html = open(profile_url)
+   doc = Nokogiri::HTML(html)
+   student = doc.css("div .social-icon-container").children.css("a").map{|tag| tag.attribute("href").value}
 
-      doc = Nokogiri::HTML(open("fixtures/student-site/students/ryan-johnson.html"))
-        binding.pry
+   student.each do |link|
+     if link.include?("twitter")
+       profile_hash[:twitter] = link
+     elsif link.include?("linkedin")
+       profile_hash[:linkedin] = link
+     elsif link.include?("github")
+       profile_hash[:github] = link
+     else
+       profile_hash[:blog] = link
+
+       #binding.pry
+      end
     end
+   profile_hash[:profile_quote] = doc.css(".profile-quote").text
+   profile_hash[:bio] = doc.css(".description-holder p").text
+   profile_hash
+ end
 
 end
