e3c562222637e4c407739b2322f9b94bca6631bc
Carrie Savinell <carrielane89@gmail.com>
2018-06-07 19:48:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each {}|student| Student.new(student)}
+    students_array.each {|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
