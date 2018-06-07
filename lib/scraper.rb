141ea3208e0d9a68b3323b351591691fe91177c6
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-07 20:31:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,27 +5,35 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     index = Nokogiri::HTML(open(index_url))
-   binding.pry
+   
     students = []
-    
-    #name:index.css('div.card-text-container h4.student-name').text
-    #location:index.css('div.card-text-container p.student-location').text
-    #html:index.css('div.student-card a').attribute('href').value
-    
-    index.css('div.roster-cards-container').each do |student|
+  
+    index.css('div.student-card').each do |student|
       student = {
-        :name => index.css('div.card-text-container h4.student-name').text,
-        :location => index.css('div.card-text-container p.student-location').text,
-        :profile_url => index.css('div.student-card a').attribute('href').value
+        :name => student.css('h4.student-name').text,
+        :location => student.css('p.student-location').text,
+        :profile_url => student.css('a').attribute('href').value
       }
       students << student
     end
-    
     students
-  
   end
 
   def self.scrape_profile_page(profile_url)
+    profile = Nokogiri::HTML(open(profile_url))
+    
+    attributes = {}
+   
+    profile.css('div.social-icon-container').each do |attr|
+      attributes = {
+        :twitter => attr.css('div.social-icon-container a')[0].attribute('href').value,
+        :linkedin => attr.css('div.social-icon-container a')[1].attribute('href').value,
+        :github => attr.css('div.social-icon-container a')[2].attribute('href').value,
+        :blog => attr.css('div.social-icon-container a')[3].attribute('href').value,
+        :profile_quote => attr.css('div.profile-quote').text,
+        :bio => attr.css('div.description-holder p').text
+      }
+    end
     
   end
 
