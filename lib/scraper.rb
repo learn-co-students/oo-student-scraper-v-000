827802bf09490012e468f5493c252a2744ad4f4a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-07 16:47:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = File.read('fixtures/student-site/index.html')
+    index = Nokogiri::HTML(html)
     
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
