aefd7362ed91119dddeed1fe4b0629d198e016e7
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-08 17:51:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,6 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     index = Nokogiri::HTML(open(index_url))
-   
     students = []
   
     index.css('div.student-card').each do |student|
@@ -22,18 +21,43 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = Nokogiri::HTML(open(profile_url))
     attributes = {}
-    profile = html.css('div.vitals-container')
+    social_link = html.css('div.social-icon-container')
     
-    attributes.tap do |attr|
-      attr[:twitter] = profile.css('a')[0].attribute('href').value if profile.css('a')[0].attribute('href').value
-        #:twitter => profile.css('a')[0].attribute('href').value,
-      attr[:linkedin] = profile.css('a')[1].attribute('href').value if profile.css('a')[1].attribute('href').value
-      attr[:github] = profile.css('a')[2].attribute('href').value if profile.css('a')[2].attribute('href').value
-      attr[:blog] = profile.css('a')[3].attribute('href').value if profile.css('a')[3].attribute('href').value
-      attr[:profile_quote] = profile.css('div.profile-quote').text if profile.css('div.profile-quote').text
-      attr[:bio] = html.css('div.description-holder p').text if html.css('div.description-holder p').text
-    }
+    link_index = 0
+    
+    if social_link.css('a')[link_index]
+      scraped_link = social_link.css('a')[link_index].attribute('href').value
+      if scraped_link.include?('twitter')
+        attributes[:twitter] = social_link.css('a')[link_index].attribute('href').value
+        link_index += 1 
+      end
+    end
+    
+    if social_link.css('a')[link_index]
+      scraped_link = social_link.css('a')[link_index].attribute('href').value
+      if scraped_link.include?('linkedin')
+        attributes[:linkedin] = social_link.css('a')[link_index].attribute('href').value
+        link_index += 1
+      end
+    end
+   
+    if social_link.css('a')[link_index]
+      scraped_link = social_link.css('a')[link_index].attribute('href').value
+      if scraped_link.include?('github')
+        attributes[:github] = social_link.css('a')[link_index].attribute('href').value
+        link_index += 1
+      end
+    end
+    
+    if social_link.css('a')[link_index]
+      attributes[:blog] = social_link.css('a')[link_index].attribute('href').value
+    end
+    
+    attributes[:profile_quote] = html.css('div.profile-quote').text
+    attributes[:bio] = html.css('div.description-holder p').text
+    
     attributes
+    
   end
 
 end
