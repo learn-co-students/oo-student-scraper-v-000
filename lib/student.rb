2fcc9e248ae9603ad5fb8d6b747742b56bfd3ea4
Richard Melchior <raustin.melchior@gmail.com>
2018-06-09 20:44:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    add_student_attributes(student_hash);
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio]
+    @profile_url = student_hash[:profile_url]
+
     @@all << self
   end
 
@@ -16,15 +25,15 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    @name = attributes_hash[:name]
-    @location = attributes_hash[:location]
+    #@name = attributes_hash[:name]
+    #@location = attributes_hash[:location]
     @twitter = attributes_hash[:twitter]
     @linkedin = attributes_hash[:linkedin]
     @github = attributes_hash[:github]
     @blog = attributes_hash[:blog]
     @profile_quote = attributes_hash[:profile_quote]
     @bio = attributes_hash[:bio]
-    @profile_url = attributes_hash[:profile_url]
+    #@profile_url = attributes_hash[:profile_url]
   end
 
   def self.all
