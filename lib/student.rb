c3afd8aef69336896acefb26d62ec8ae25fd151c
Dale Terrell <tragicid@gmail.com>
2018-06-12 12:56:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,19 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    Student class @@all = []
   end
 
   def self.create_from_collection(students_array)
-    
+    Student.create_from_collection
   end
 
   def add_student_attributes(attributes_hash)
-    
+    Student.add_student_attributes
   end
 
   def self.all
-    
+    @@all << self
   end
 end
 
