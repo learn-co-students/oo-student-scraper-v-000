b54339c5127badba7d9001ed3dc0e7e70361b234
Scott Weinstein <sweinstein27@gmail.com>
2018-06-12 18:33:11 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
 
-    student_cards = doc.css(".student_card")
-    student = []
+    student_card = doc.css(".student_card")
+    students = []
     student_card.collect do |student_card_xml|
       students << {
       :name => student_card_xml.css("h4.student-name").text,
@@ -19,10 +19,29 @@ class Scraper
       :profile_url => "./fixtures/student-site/" + student_card_xml.css("a").attribute("href").value
     }
     end
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
+     profile_html = open(profile_url)
+     profile_doc = Nokogiri::HTML(profile_html)
+     attributes = {}
+     profile_doc.css("div.social-icon-container a").each do |link_xml|
+       case link_xml.attribute("href").value
+       when /twitter/
+         attributes[:twitter] = link_xml.attribute("href").value
+       when /github/
+         attributes[:github] = link_xml.attribute("href").value
+       when /linkedin/
+         attributes[:linkedin] = link_xml.attribute("href").value
+       else
+         attributes[:blog] = link_xml.attribute("href").value
+       end
+     end
+     attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
+     attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
+     attributes
+    end
+
 
   end
-
-end
