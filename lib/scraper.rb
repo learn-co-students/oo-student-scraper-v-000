d899a891c885d99a3009de3d2972715c7e5b9c57
Heidi Engstrom <hengstrom25@gmail.com>
2018-06-13 19:18:12 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,38 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    student_array = []
+    index = Nokogiri::HTML(open(index_url))
+    index.css("div.roster-cards-container").each do |container|
+      container.css(".student-card a").each do |student|
+        name = student.css('.student-name').text
+        location = student.css('.student-location').text
+        profile_url = "#{student.attr('href')}"
+        student_array << {name: name, location: location, profile_url: profile_url}
+      end
+    end
+      student_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+    students_profile = {}
+    social_media = profile.css("div.social-icon-container a").collect {|a| a.attribute("href").value}
+    social_media.each do |link|
+      case
+      when link.include?("twitter")
+        students_profile[:twitter] = link
+      when link.include?("linkedin")
+        students_profile[:linkedin] = link
+      when link.include?("github")
+        students_profile[:github] = link
+      else
+        students_profile[:blog] = link
+      end
+        students_profile[:profile_quote] = profile.css("div.profile-quote").text
+        students_profile[:bio] = profile.css(".bio-block .description-holder p").text
+    end
+    students_profile
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,20 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| Student.new(student_hash)}
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all
   end
 end
 
