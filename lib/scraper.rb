fa8e29fc04ff20206b9e905f2e2ca8b4791ddb1d
Monica Seaman <monica.l.seaman@gmail.com>
2018-06-16 17:50:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,6 @@ class Scraper
     students
   end
 
-
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     twitter_handle = ""
--- lib/student.rb
+++ lib/student.rb
@@ -5,18 +5,27 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-     
+    student_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    #shouldn't this check that the student doesn't already exist, and if it does, at the attributes to the correct student?
+    attributes_hash.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    self
   end
 
   def self.all
-
+    @@all
   end
 end
