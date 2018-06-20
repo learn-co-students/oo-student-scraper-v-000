d1c3c5d5fa220f91bd0e1191ce8b167a0e2e427f
Joey Cho <cjy12182000@naver.com>
2018-06-20 20:18:11 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,56 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  def initialize(student_hash={})
+
+    @name = student_hash[:name] || nil
+    @location =  student_hash[:location] || nil
+    @twitter = student_hash[:twitter] || nil
+    @linkedin = student_hash[:linkedin] || nil
+    @github = student_hash[:github] || nil
+    @blog = student_hash[:blog] || nil
+    @profile_quote = student_hash[:profile_quote] || nil
+    @bio = student_hash[:bio] || nil
+    @profile_url = student_hash[:profile_url] || nil
+
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+
+  #  binding.pry
+    students_array.each do |ele|
+      student = self.new
+      student.name = ele[:name]
+      student.location = ele[:location]
+#      binding.pry
+#      @@all << student
+    end
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
+      self.bio = attributes_hash[:bio]
+
+      self.twitter = attributes_hash[:twitter]
+
+      self.blog = attributes_hash[:blog]
+
+      self.github = attributes_hash[:github]
+
+      self.profile_quote = attributes_hash[:profile_quote]
+
+      self.linkedin = attributes_hash[:linkedin]
+
+
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
