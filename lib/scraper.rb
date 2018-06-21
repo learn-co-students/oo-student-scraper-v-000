a579182c334a16fa466621e5ff523ebfa892af72
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-21 17:26:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,23 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     students_array = []
-    students_hash = {}
+    students_array[1] = []
+    students_array[2] = []
+
     html = open("./fixtures/student-site/index.html")
     doc = Nokogiri::HTML(html)
-    student_area = doc.css(".card-text-container h4")
+    # student_area = doc.css(".card-text-container h4")
+    student_area = doc.css(".card-text-container")
      student_area.each do |x|
-       binding.pry
-       students_hash["name"] = x.child.text
-       students_array << students_hash
+       students_name_hash = Hash.new
+       students_location_hash = Hash.new
+       students_name_hash[:name] = x.css("h4").text
+       students_location_hash[:location] = x.css("p").text
+       students_array[1] << students_name_hash
+       students_array[2] << students_location_hash
      end
-  students_array
+   students_array
+   binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
