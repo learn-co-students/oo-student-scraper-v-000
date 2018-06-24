9a1360a9aeb4e450dd16b2c29fafb1980a890200
Brent Bauman <brentbauman86@gmail.com>
2018-06-24 23:55:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,37 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-      html = File.read("fixtures/student-site/index.html")
-      student_scraper = Nokogiri::HTML(html)
-
+      student_scraper = Nokogiri::HTML(open(index_url))
       student = []
-
-      student << students = {:name => student_scraper.css(".student-name").first.css("h4").text,
-      :location => student_scraper.css(".student-location").first.text,
-      :profile_url => student_scraper.css(".fixtures/student-site/index.html").first.text}
+      student_scraper.css("div.roster-cards-container").each do |info|
+        info.css(".student-card a").each do |link|
+          name = link.css(".student-name").text
+          location = link.css(".student-location").text
+          profile_url = "#{link.attr("href")}"
+          student << {:name => name, :location => location, :profile_url => profile_url}
+        end
+      end
+      student
   end
 
   def self.scrape_profile_page(profile_url)
-
+    student_profile = Nokogiri::HTML(open(profile_url))
+    profile = {}
+    links = student_profile.css(".social-icon-container").children.css("a").map { |info| info.attribute("href").value}
+    links.each do |link|
+        if link.include?("linkedin")
+          profile[:linkedin] = link
+        elsif link.include?("github")
+          profile[:github] = link
+        elsif link.include?("twitter")
+          profile[:twitter] = link
+        else
+          profile[:blog] = link
+    end
   end
+    profile[:profile_quote] = student_profile.css(".profile-quote").text if student_profile.css(".profile-quote")
+    profile[:bio] = student_profile.css("div.bio-content.content-holder div.description-holder p").text if student_profile.css("div.bio-content.content-holder div.description-holder p")
 
-end
+    profile
+    end
+  end
