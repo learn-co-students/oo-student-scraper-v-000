032babfb375e3129cb5070f10bf9c1ad4e85353b
Hayden Betts <haydenbetts@gmail.com>
2018-06-30 20:06:53 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,25 +18,32 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    # :twitter>, :linkedin, :github, :blog, :profile_quote, :bio
+
     html = File.read(profile_url)
     profile_page = Nokogiri::HTML(html)
+    profile_details = {}
 
     social_link_tags = profile_page.css(".social-icon-container").css("a")
 
-    profile_details = {
-      :twitter => social_link_tags[0].attr("href"),
-      :linkedin => social_link_tags[1].attr("href"),
-      :github => social_link_tags[2].attr("href"),
-      :profile_quote => profile_page.css(".profile-quote").text,
-      :bio => profile_page.css(".bio-block").css("p").text
-    }
 
-    # i want to do something clean for this. Dynamically add
-    # symbols for the social links if they exist
-    # if social_link_tags[3].attr("href")
-    #   profile_details[:blog] = social_link_tags[3].attr("href")
-    # end
+    social_link_tags.each do |social_link_tag|
+      social_site_url = social_link_tag.attr("href") # here is the url
+      social_site_name = url.match(/\/\/[w]{3}?\.?(\w+).com/).captures[0] # site name
+
+      if ['github', 'linkedin', 'twitter'].includes?(social_site_name)
+        profile_details[social_site_name] = social_site_url
+      else
+        profile_details[:blog] = social_site_url
+      end
+
+    end
+
+    profile_details[:profile_quote] = profile_page.css(".profile-quote").text
+    profile_details[:bio] = profile_page.css(".bio-block").css("p").text
+    return profile_details
+
   end
 
 end
+
+Scraper.scrape_profile_page('./fixtures/student-site/students/david-kim.html')
