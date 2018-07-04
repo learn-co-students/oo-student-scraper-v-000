b3dc105615c5f8f806a6666bf3f547e910adc6d2
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-07-04 20:33:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = File.open(index_url)
     profiles = Nokogiri::HTML(html)
-    students = []
+    students_array = []
 
     profiles.css("div.student-card").each do |student|
       student =
@@ -15,9 +15,9 @@ class Scraper
           :location => student.css("p.student-location").text,
           :profile_url => student.css("a").attribute("href").value
         }
-      students << student
+      students_array << student
     end
-    students
+    students_array
     # students: profiles.css("div.student-card")
     # name: student.css("h4.student-name").text
     # location: student.css("p.student-location").text
@@ -29,26 +29,27 @@ class Scraper
     profile = Nokogiri::HTML(html)
     profile_details = {}
 
-    profile.css("body").each do |profile_detail|
-      # how can I get the below to work with the same syntax as above? i.e. :profile_quote => profile_detail.css("div.profile-quote").text
-      profile_details[:profile_quote] = profile_detail.css("div.profile-quote").text
-      profile_details[:bio] = profile_detail.css("div.bio-content.content-holder p").text
+    profile_details[:profile_quote] = profile.css("div.profile-quote").text
+    profile_details[:bio] = profile.css("div.bio-content.content-holder p").text
 
-      profile_detail.css("div.social-icon-container a").each do |link|
-        if link.attribute("href").value.include?("twitter")
-          profile_details[:twitter] = link.attribute("href").value
-        elsif link.attribute("href").value.include?("linkedin")
-          profile_details[:linkedin] = link.attribute("href").value
-        elsif link.attribute("href").value.include?("github")
-          profile_details[:github] = link.attribute("href").value
-        elsif link.attribute("href").value.include?("http")
-          profile_details[:blog] = link.attribute("href").value
-        end
+    links = profile.css("div.social-icon-container a").map do |link|
+      link.attribute("href").value
+    end
+
+    links.each do |link|
+      if link.include?("twitter")
+        profile_details[:twitter] = link
+      elsif link.include?("linkedin")
+        profile_details[:linkedin] = link
+      elsif link.include?("github")
+        profile_details[:github] = link
+      else
+        profile_details[:blog] = link
       end
     end
     profile_details
-    # links: profile_detail.css("div.social-icon-container a")
-    # profile_quote: profile_detail.css("div.profile-quote").text
-    # bio: profile_detail.css("div.bio-content.content-holder p").text
+    # links: profile.css("div.social-icon-container a")
+    # profile_quote: profile.css("div.profile-quote").text
+    # bio: profile.css("div.bio-content.content-holder p").text
   end
 end
