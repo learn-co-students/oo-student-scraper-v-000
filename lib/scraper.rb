6fd905092bff9435b64da13560bc9be048c57883
Richard Cho <richardcho100@gmail.com>
2018-07-12 00:00:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
   #profile_url: index_page.css("div.student-card a").attribute("href").value
 
   def self.scrape_index_page(index_url)
+
     index_page = Nokogiri::HTML(open(index_url))
     students = []
     index_page.css("div.student-card").each do |student|
@@ -20,6 +21,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+
     # :twitter=>  profile_page.css("div.social-icon-container a").attribute("href").value
     # :linkedin=>  profile_page.css("div.social-icon-container a")[1].attribute("href").value
     # :github=>   profile_page.css("div.social-icon-container a")[2].attribute("href").value
@@ -27,34 +29,19 @@ class Scraper
     # :profile_quote=>  profile_page.css("div.profile-quote").text
     # :bio=>  profile_page.css("div.bio-content.content-holder div.description-holder p").text
 
-    # Passed 1st test with the following code for #scrape_profile_page method
-
-    # profile_page = Nokogiri::HTML(open(profile_url))
-    # student_info = {}
-    # profile_page.css("div.social-icon-container").each {|profile| student_info[:twitter] = profile.css("a").attribute("href").value}
-    # profile_page.css("div.social-icon-container").each {|profile| student_info[:linkedin] = profile.css("a")[1].attribute("href").value}
-    # profile_page.css("div.social-icon-container").each {|profile| student_info[:github] = profile.css("a")[2].attribute("href").value}
-    # profile_page.css("div.social-icon-container").each {|profile| student_info[:blog] = profile.css("a")[3].attribute("href").value}
-    # student_info[:profile_quote] = profile_page.css("div.profile-quote").text
-    # student_info[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text
-    # student_info
-
-    #trying to refactor to pass 2nd test for #scrape_profile_page method
-
     profile_page = Nokogiri::HTML(open(profile_url))
     student_info = {}
-    if profile_page.css("div.social-icon-container a").attribute("href").value
-      profile_page.css("div.social-icon-container").each {|profile| student_info[:twitter] = profile.css("a").attribute("href").value}
-    end
-    if profile_page.css("div.social-icon-container a")[1].attribute("href").value
-      profile_page.css("div.social-icon-container").each {|profile| student_info[:linkedin] = profile.css("a")[1].attribute("href").value}
-    end
-
-    if profile_page.css("div.social-icon-container a")[2].attribute("href").value
-      profile_page.css("div.social-icon-container").each {|profile| student_info[:github] = profile.css("a")[2].attribute("href").value}
-    end
-    if profile_page.css("div.social-icon-container a")[3].attribute("href").value
-      profile_page.css("div.social-icon-container").each {|profile| student_info[:blog] = profile.css("a")[3].attribute("href").value}
+    profile_page.css("div.social-icon-container a").each do |element|
+      url = element.attribute("href").value
+      if url.include?("twitter")
+        student_info[:twitter] = url
+      elsif url.include?("linkedin")
+        student_info[:linkedin] = url
+      elsif url.include?("github")
+        student_info[:github] = url
+      else
+        student_info[:blog] = url
+      end
     end
 
     student_info[:profile_quote] = profile_page.css("div.profile-quote").text
