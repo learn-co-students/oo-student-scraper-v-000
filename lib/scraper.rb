16622a9f70e5520337fdf1076795b73024ff8870
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-07-13 23:25:31 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,4 @@
+
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
@@ -5,7 +6,6 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
     doc = Nokogiri::HTML(open(index_url)) #grabs the HTML that
     #makes up index_url and then uses the Nokogiri::HTML method
     #to convert it to a NodeSet that we can use. Save it in doc.
@@ -20,17 +20,28 @@ class Scraper
     end
 
   def self.scrape_profile_page(profile_url)
-    profile_page = Nokogiri::HTML(open(profile_url))
+    doc = Nokogiri::HTML(open(profile_url))
+    profile_info = {}
 
-    profile_info = []
+    profile_info[:bio] = doc.css("p").text
+    profile_info[:profile_quote] = doc.css(".profile-quote").text
 
-    student_info = doc.css("#student_info .vitals-container .vitals-text-container .description-holder .profile-quote")
-    student_info.each do |information|
-      puts "Linkedin: " + student_info.css("a href").value
-      puts "github: " + student_info.css("a href").value
-      puts "blog: " + student_info.css("a href").value
-      puts "Profile Quote: " + profile-quote.css.text
-      puts "Bio: " + discription-holder.css.text
+    media = doc.css(".vitals-container .social-icon-container").children.css("a").collect {|social| social.attribute("href").value}
+      #binding.pry
+      media.each do |social|
+        #binding.pry
+     if social.include?("twitter")
+       profile_info[:twitter] = social
+       #binding.pry
+     elsif social.include?("linkedin")
+       profile_info[:linkedin] = social
+     elsif social.include?("github")
+       profile_info[:github] = social
+     else
+       profile_info[:blog] = social
+      # binding.pry
+      end
     end
+      profile_info
   end
 end
