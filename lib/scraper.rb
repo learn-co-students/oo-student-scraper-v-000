15b2da4d9e2098e7dac7eeb7880a69d1af5c2439
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-16 02:53:35 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,24 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+
+# require_relative './fixtures/student-site/index.html'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    binding.pry
+    index_url = File.open("./fixtures/student-site/index.html"){ |f| Nokogiri::HTML(f) }
+
+
+
+    student = []
+    puts index_url.css(".student-location").text
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
