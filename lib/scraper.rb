0badcd06265d3257f0ac74164cabf93e623581ab
Kazumi Karbowski <kazkazinus@gmail.com>
2018-07-17 02:28:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ class Scraper
 
 
   def self.scrape_index_page(index_url)
+    pry
     index_page = Nokogiri::HTML(open(index_url))
     students = []
     index_page.css("div.roster-cards-container").each do |card|
@@ -21,6 +22,11 @@ class Scraper
 
 
   def self.scrape_profile_page(profile_url)
+    profile_page = Nokogiri::HTML(open(profile_url))
+    student_info = {}
+    profile_page.css(".social-icon-container").each do |social_icon|
+      social_icon.css(".social-icon").each do |icon|
+        twitter =
 
   end
 
