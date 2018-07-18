ab98731d9a681a11334e33e4de2c28442af5523b
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-18 00:35:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,18 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
     
+    students = {}
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
