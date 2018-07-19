aec372f9f920daced52750b25ec3ada7f7e990db
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-19 18:22:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,13 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    
+
     students = {}
-
+    doc.css(".student-card").first
+    #locations = doc.css("p.student-location").first.text
+    binding.pry
   end
-
+  #names = doc.css("h4.student-name").text
   def self.scrape_profile_page(profile_url)
 
   end
