8704b6548568976a17a1cacd4f21cfd449727e97
Heidi Hlavinka <hchlav@gmail.com>
2018-07-19 01:28:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,41 +1,45 @@
-require 'open-uri'
-require 'pry'
+## Worked on by Maurice Argoetti, Srija Kumar and Brandon Green, Lynie McClimon, Heidi Hlavinka
+## PAIR PROGRAMMING FTW
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
-    scraped_students = []
+  profiles = []
 
-    index_page.css("div.roster-cards-container").each do |cards| # div .roster-cards-container .student-card .card-text-container h4.student-name
-      cards.css(".student-card a").each do |student|
-        student_profile = "#{student.attr('href')}"
-        student_location = student.css(".student-location").text
-        student_name = student.css(".student-name").text
-        scraped_students << {name: student_name, location: student_location, profile_url: student_profile}
-      end
-    end
-    scraped_students
+  ind = Nokogiri::HTML(open(index_url))
+  students = ind.css(".student-card")
+
+  students.each do |profile|
+    name = profile.css(".student-name").text
+    location = profile.css(".student-location").text
+    profile_url = profile.css("a").attribute("href").value
+    profiles << {name: name,
+      location: location,
+      profile_url: profile_url}
   end
-
-  def self.scrape_profile_page(profile_url)
-    profile_page = Nokogiri::HTML(open(profile_url))
-    scraped_profile = {}
-
-    links = profile_page.css(".social-icon-container").children.css("a").map do |element|
-        element.attribute("href").value
-    end
-    twitter = links[0]
-    linkedin = links[1]
-    github = links[2]
-    blog = links[3]
-
-   #scraped_profile
- end
-
+  profiles
 end
 
-# go into div .main-wrapper profile - returned 0 DONT USE
-# div .vitals-container = 0 div .social-icon-container a - social links
-# div .details-container div .bio-block details-block - contains bio
-# div .bio-content content-holder div .description-holder - for bio text
+
+  def self.scrape_profile_page(profile_url)
+    student = {}
+
+    prof = Nokogiri::HTML(open(profile_url))
+
+    links = prof.css(".social-icon-container a").collect {|icon| icon.attribute("href").value}
+    links.each do |link|
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?(".com")
+        student[:blog] = link
+      end
+    end
+    student[:profile_quote] = prof.css(".profile-quote").text
+    student[:bio] = prof.css("div.description-holder p").text
+    student
+  end
+
+end
