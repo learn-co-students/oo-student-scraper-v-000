cf1a931cfc111a3a48699a622bfa7bdf2b78062d
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-07-19 14:53:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value 
+    send("#{key}=", value)
   end
 
   def self.create_from_collection(students_array)
