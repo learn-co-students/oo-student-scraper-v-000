2163a094a76354d178198bb4b6f44a2a74c405ab
Jamie Brown <jmbrowncow@gmail.com>
2018-07-19 14:31:59 +0000

    index working

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,36 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_scrape = Nokogiri::HTML(open(index_url)).css("div.student-card")
+    student_hash_index = []
+    index_scrape.each do |student|
+      student_hash_index<< {
+        name: student.css("div.card-text-container h4.student-name").text,
+        location: student.css("div.card-text-container p.student-location").text,
+        profile_url: student.css("a").first["href"]
+      }
+    end
+    student_hash_index
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_scrape = Nokogiri::HTML(open(profile_url)).css("div.main-wrapper.profile")
+
+    student_hash_profile = []
+    profile_scrape.each do |profile|
+        student_hash_profile <<{
+          linkedin: "something",
+          github: "something",
+          blog: "something",
+          profile_quote: "something",
+          bio: profile.css("div.vitals-text-container.profile-quote").text
+        }
+        binding.pry
+      end
   end
 
 end
-
