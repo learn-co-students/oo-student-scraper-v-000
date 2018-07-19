6c944eca79bafe603fffccb64a8b4247f1c6e252
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-07-19 19:45:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,18 +12,20 @@ class Student
   end 
 
 
-
-
   def self.create_from_collection(students_array)
+  students_array.each { |name_and_location| self.new(name_and_location) }
+  end 
     
-  end
-
+    
+    
   def add_student_attributes(attributes_hash)
-    
+  attributes_hash.each { |key, value| send("#{key}=", value) }
   end
 
+ 
   def self.all
-    
-  end
+    @@all 
+    end
+
 end
 
