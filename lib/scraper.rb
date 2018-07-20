1dfd4703c295bc3a595b64f9efccce77a45571f2
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-20 01:55:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,10 +6,21 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
+    #doc.css("div.student-card").each do |student|
+    #doc.css("a href").value
 
-    students = {}
-    doc.css(".student-card").first
+    #end
+      students = {}
+      #:name => student.css("h4.student-name").text,
+
+      #:location => student.css("p.student-location").text,
+
+
+
+    #doc.css("div.student-card a").attribute("href").value
     #locations = doc.css("p.student-location").first.text
+
+    doc.css("div.student-card")
     binding.pry
   end
   #names = doc.css("h4.student-name").text
