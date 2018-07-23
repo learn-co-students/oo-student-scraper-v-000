bbada3bd457a1cc51e3abe39ca786eb768ba5a38
Madeline Stark <madeline.stark1@gmail.com>
2018-07-23 21:17:21 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,40 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    new_array = []
+    doc.css(".student-card").each_with_index do |student, index|
+      student_name = doc.css(".student-name")[index].text
+      location = doc.css(".student-location")[index].text
+      url = doc.css("a")[index + 1].attributes["href"].value #+1 b/c first one is logo, not link
+      new_array << {:name => student_name, :location => location, :profile_url => url}
+    end
+    new_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    #class method b/c not storing anything
+    doc = Nokogiri::HTML(open(profile_url))
+    students = {}
+    links = doc.css("a")
+    links.each do |link|
+      if link.attributes["href"].value.include?("twitter") #don't need index after link b/c it's already one iteration
+        students[:twitter] = link.attributes["href"].value
+      elsif link.attributes["href"].value.include?("linkedin")
+        students[:linkedin] = link.attributes["href"].value
+      elsif link.attributes["href"].value.include?("github")
+        students[:github] = link.attributes["href"].value
+      elsif link.attributes["href"].value.scan(/\w/) != [ ] #trying to scan for letters and leave out .../
+         students[:blog] = link.attributes["href"].value
+      end
+      students[:profile_quote] = doc.css(".profile-quote").text
+      students[:bio] = doc.css("p").text #outside of loop b/c don't need to check for these 
+    end
+    students
   end
 
-end
 
+
+
+
+end
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,31 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
+#these methods shouldn't be dependent on scraper class, just be able to take in any info
   def initialize(student_hash)
-    
+      student_hash.each {|key, value| self.send(("#{key}="), value)}
+      #much quicker than defining each individually
+
+      @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+      students_array.each do |student|
+        student = Student.new(student)
+      end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    #here you're not creating new students, youre adding new attributes to students you created above
+    self
   end
 
   def self.all
-    
+    @@all
   end
-end
 
+end
