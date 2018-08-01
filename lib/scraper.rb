88d988152f80947bbef6f5b6885f175cc469d75e
Amit Singh <amit_nyc@live.com>
2018-08-01 19:58:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,8 +26,27 @@ class Scraper
     students
    end
 
-  def self.scrape_profile_page(profile_url)
 
-  end
 
-end
+   def self.scrape_profile_page(profile_url)
+     #Scrapes URL passed in
+     profile = Nokogiri::HTML(open(profile_url))
+     #prolife_links is a variable that equals to the container that stores the profile links of each profile
+     profile_links = profile.css("div.social-icon-container a").collect do |link|
+       link.attr("href")
+     end
+     #detect  link and checks if it includes the arg(keyword)
+     profile_hash = {
+       :twitter => profile_links.detect{|link| link.include?('twitter')},
+       :linkedin => profile_links.detect{|link| link.include?('linkedin')},
+       :github => profile_links.detect{|link| link.include?('github')},
+       :blog => profile_links.detect{|link| !link.include?('twitter') && !link.include?('linkedin') && !link.include?('github')},
+       :profile_quote => profile.css("div.profile-quote").text,
+       :bio => profile.css("div.bio-content p").text
+     }
+     profile_hash.delete_if{|k,v| v.nil?}
+     #removing empty elements from hash
+   end
+
+
+ end
