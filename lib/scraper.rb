39e317e9f05bd71593cbed24336b5d06dc066e36
Amit Singh <amit_nyc@live.com>
2018-08-01 17:52:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,33 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+
+
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+     #Scrapes URL passed in
+     doc = Nokogiri::HTML(open(index_url))
+
+     #Creates student array  to store hash
+     students = []
+
+     #Enters into container holding student cards
+     doc.css("div.student-card").each do|student|
+         #Saves student link, location, and name
+         student_link = student.css("a").attr("href").text
+         student_name = student.css("h4").text
+         student_location = student.css("p").text
+         #Puts those saved variables into hash, and saves into student array
+         students << {name: student_name, location: student_location, profile_url: student_link}
+     end
+     #Returns student array
+    students
+   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
