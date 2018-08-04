cfc89b3e2870a502a243beb3a9b71f8bb2bbf8a3
Ana Bebic <anabebic.x@gmail.com>
2018-08-04 07:06:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,6 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
-
-class Scraper
-
-  def self.scrape_index_page(index_url)
-    
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
+def data_scraper(url)
+    Nokogiri::HTML(open(url))
 end
-
