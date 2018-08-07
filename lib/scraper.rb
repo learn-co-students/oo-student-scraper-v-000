ca9e0a4bdf58c2c87908441393414760122aded8
Laura Messa Travin <laura.travin@gmail.com>
2018-08-07 22:13:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,26 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read(index_url)
+    htmlcode= Nokogiri::HTML(html)
+    profiles= []
+    binding.pry
+    htmlcode.css('.student-card').each { |dat|
+
+                    hash = {  :profile_url => dat.css('a').attribute('href').value   }
+
+                    profiles.push(hash)
+    }
+  #    htmlcode.css('.student-card a').attribute('href').value
+   binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
