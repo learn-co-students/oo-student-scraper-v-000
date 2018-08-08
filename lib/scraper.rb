3dc6ff5ea937344ef650584c26f00ea84a6d662f
Laura Messa Travin <laura.travin@gmail.com>
2018-08-08 22:20:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,19 +8,64 @@ class Scraper
     html = File.read(index_url)
     htmlcode= Nokogiri::HTML(html)
     profiles= []
-    binding.pry
     htmlcode.css('.student-card').each { |dat|
 
-                    hash = {  :profile_url => dat.css('a').attribute('href').value   }
+                    hash = {  :name => dat.css("h4").text,
+                              :location => dat.css("p").text,
+                              :profile_url => dat.css('a').attribute('href').value   }
 
                     profiles.push(hash)
     }
-  #    htmlcode.css('.student-card a').attribute('href').value
-   binding.pry
+    return profiles
   end
 
   def self.scrape_profile_page(profile_url)
+    html = File.read(profile_url)
+    htmlcode= Nokogiri::HTML(html)
+    profile_url= []
 
+    #get all the values
+    htmlcode.css('.profile').each { |dat|
+                       twitter = ' '
+                       ink = ' '
+                       gith = ' '
+                       blog = ' '
+
+                   #get social status
+                      dat.css(".vitals-container .social-icon-container").each { |chr|
+                              # puts chr.css("a").attribute("href").value
+
+                              if chr.css("a").attribute("href").value.include?("twitter.com")
+                                 twitter = chr.css("a").attribute("href").value
+                              end
+                              if chr.css("a").attribute("href").value.include?("linkedin.com")
+                                 ink = chr.css("a").attribute("href").value
+                              end
+                              if chr.css("a").attribute("href").value.include?("github.com")
+                                gith = chr.css("a").attribute("href").value
+                              end
+                              if !chr.css("a").attribute("href").value.include?("twitter.com") &&  !chr.css("a").attribute("href").value.include?("linkedin.com") && !chr.css("a").attribute("href").value.include?("github.com")
+                                blog = chr.css("a").attribute("href").value
+                              end
+                          }
+
+
+
+          hash = {
+                    :twitter =>  twitter,
+                    :linkedin => ink,
+                    :github=>  gith,
+                    :blog=> blog,
+
+                    :profile_quote => " p",
+                    :bio=>    "p "          }
+          hash.delete_if{|k,v| v.nil? }
+
+                 binding.pry
+         return hash
+
+
+        }#get all the values
   end
 
 end
