7d13474ef26c0e69e523a86f10c7e034a990a1b5
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-08-10 04:01:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,19 +6,18 @@ class Student
 
   def initialize(student_hash)
     student_hash.keys.each do |key|
-        m = "#{key}="
-        self.send( m, hash[key] ) if self.respond_to?( m )
+        send( "#{key}=", student_hash[key] )
     end
     @@all << self
   end
 
   def self.create_from_collection(students_array)
     
-    students_array.each {|student| @@all << Self.new(student)}
+    students_array.each {|h| @@all << self.new(h)}
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each {|key, value| self[key] = value }
+    attributes_hash.each {|key, value| send( "#{key}=", value ) }
   end
 
   def self.all
