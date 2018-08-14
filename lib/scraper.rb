aeb62f1e28dfc239fde78754379f2500ef51bf76
Adam Barton <adam.barton333@gmail.com>
2018-08-14 04:35:14 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = (open("http://165.227.31.208:52075/fixtures/student-site/"))
     doc = Nokogiri::HTML(html)
-    profiles = doc.css(".student-card .card-text-container").text
-    names = doc.css(".student-card .card-text-container .student-name").text
-    locations = doc.css(".student-card .card-text-container .student-location").text
+
+
+    # profiles = doc.css(".student-card .card-text-container").text
+    # names = doc.css(".student-card .card-text-container .student-name").text
+    # locations = doc.css(".student-card .card-text-container .student-location").text
+    # url = doc.css('.student-card a').first.attribute('href').value
 
 
     binding.pry
