784de90f12f42b33fbffe6d4cf6345fd37278f55
Reno Sorensen <renojames19@gmail.com>
2018-08-15 04:29:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    binding.pry
+    # roster = doc.css(".student-card")
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+Scraper.scrape_index_page("http://165.227.31.208:53117/fixtures/student-site/")
