2e3c93b7a4484e1c06601a473ce60951d996a282
Reno Sorensen <renojames19@gmail.com>
2018-08-16 04:22:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,6 @@ class Scraper
   def self.scrape_index_page(index_url)
     students = []
     doc = Nokogiri::HTML(open(index_url))
-    #binding.pry
     # doc.css(".student-card") => provides the individual student objects
     # doc.css(".student-card").first.css(".student-name").text => provides the student's name
     # doc.css(".student-card").first.css(".student-location").text => provides the student's location
@@ -21,15 +20,20 @@ class Scraper
       students << student_hash
       #binding.pry
     end
-
     students
-
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
 
+    # doc.css(".social-icon-container a").map {|link| link['href']}[0] => Twitter Profile link, string
+    # doc.css(".social-icon-container a").map {|link| link['href']}[1] => Linkedin Profile link, string
+    # doc.css(".social-icon-container a").map {|link| link['href']}[2] => Github Profile link, string
+    # doc.css(".social-icon-container a").map {|link| link['href']}[3] => Youtube Profile link, string
+
+    binding.pry
   end
 
 end
 
-Scraper.scrape_index_page("http://165.227.31.208:53117/fixtures/student-site/")
+Scraper.scrape_profile_page("http://165.227.16.205:49641/fixtures/student-site/students/ryan-johnson.html")
