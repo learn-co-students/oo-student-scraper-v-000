74843c543f1f5589e2bb67b2244c65d1dfe632b0
Adam Barton <adam.barton333@gmail.com>
2018-08-16 01:54:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,10 +19,35 @@ class Scraper
           student_profiles << student_profile
         end
         student_profiles
-        #binding.pry
       end
 
   def self.scrape_profile_page(profile_url)
+    html = (open("http://165.227.16.205:46452/fixtures/student-site/students/matt-preiser.html"))
+    doc = Nokogiri::HTML(html)
+    student_links = []
+    student_data = {}
+      doc.css('.social-icon-container a').each do |link|
+      if link.attributes['href'].value.include?("twitter")
+            student_data[:twitter] = link.attributes['href'].value
+      end
+      if link.attributes['href'].value.include?("linkedin")
+            student_data[:linkedin] = link.attributes['href'].value
+      if  link.attributes['href'].value.include?("github")
+           student_data[:github] = link.attributes['href'].value
+        end
+      end
+
+        binding.pry
+
+      end
+
+
+
+    # linkedin = doc.css('social-icon-container a')
+    # github =
+    # blog =
+    # profile_quote = doc.css(".vitals-container .vitals-text-container .profile-quote").text
+    # bio = doc.css(".description-holder p").text
 
   end
 
