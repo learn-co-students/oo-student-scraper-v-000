cfe17bb25e3768f86bd8d40b4b4ce3b01191d3bf
Reno Sorensen <renojames19@gmail.com>
2018-08-21 03:52:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
