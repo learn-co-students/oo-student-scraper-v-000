f57a20d1c4459f01a5f03695b485bc7a25408157
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-21 01:47:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'nokogiri'
 #all cards: div.roster-cards-container
 #names: div.card-text-container h4
-# doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+# doc = Nokogiri::HTML(open("./fixtures/student-site/students/jenny-yamada.html"))
 # binding.pry
 class Scraper
 
@@ -22,7 +22,23 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    info = {
+      :profile_quote => doc.css("div.profile-quote").text,
+      :bio => doc.css("div.bio-content.content-holder p").text
+    }
+    doc.css('div.social-icon-container a').each do |soc|
+      if soc.attribute("href").value.include?('github')
+        info[:github] = soc.attribute("href").value
+      elsif soc.attribute("href").value.include?('twitter')
+        info[:twitter] = soc.attribute("href").value
+      elsif soc.attribute("href").value.include?('linkedin')
+        info[:linkedin] = soc.attribute("href").value
+      else
+        info[:blog] = soc.attribute("href").value
+      end
+    end
+    info
   end
 
 end
