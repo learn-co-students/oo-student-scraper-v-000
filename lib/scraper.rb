572ef49419dd153cbb8abcdacb578da96e883eab
Tao Liu <taoliudesign@gmail.com>
2018-08-29 07:16:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,43 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    students= Nokogiri::HTML(open(index_url)).css(".student-card")
     
+    students.collect do |student|
+      { :name => "#{student.css(".student-name").text}",
+        :location => "#{student.css(".student-location").text}",
+        :profile_url => "#{student.css("a").attribute("href").value}" }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
+    profile= Nokogiri::HTML(open(profile_url))
+    social_links = profile.css(".social-icon-container a").collect do |nodeset|
+      nodeset.attribute("href").value
+    end
     
+    profile_hash = {}
+    
+    social_links.each do |link|
+      if link.include? "twitter"
+        profile_hash[:twitter] = link
+      elsif link.include? "linkedin"
+        profile_hash[:linkedin] = link
+      elsif link.include? "github"
+        profile_hash[:github] = link
+      elsif link.include? "facebook"
+        profile_hash[:facebook] = link
+      else 
+        profile_hash[:blog] = link
+      end
+    end
+    
+    profile_hash[:profile_quote] = "#{profile.css(".profile-quote").text}"
+    profile_hash[:bio] = "#{profile.css(".description-holder p").text}"
+    
+    profile_hash
   end
 
 end
 
+          
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,29 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key,value|
+      key = key.to_s + "="
+      self.send(key, value)      
+    end
+
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.collect do |student_hash|
+      self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key,value|
+      key = key.to_s + "="
+      self.send(key, value)      
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
