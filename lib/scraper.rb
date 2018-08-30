1aefe1714095ab335bcf6c4f728862ed74da36a3
Azhar Shah <azharshahstl@yahoo.com>
2018-08-30 19:38:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
+
     doc.css(".student-card").collect do |student_info|
       student_index_hash = {}
       student_index_hash[:name] = student_info.css("h4").text
@@ -17,12 +18,22 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    doc.css(".social-icon-container").css("a").attribute("href").value
-    doc.css(".social-icon-container").css("a")[1].attribute("href").value
-    doc.css(".social-icon-container").css("a")[2].attribute("href").value
-    doc.css(".social-icon-container").css("a")[3].attribute("href").value
-    doc.css(".vitals-text-container").css(".profile-quote").text
-    doc.css(".description-holder").css("p").text
-
-end
+    student_profile_hash = {}
+      doc.css(".social-icon-container").css("a").each do |social_account|
+        if social_account[:twitter]
+           student_profile_hash[:twitter] = doc.css(".social-icon-container").css("a")[0].attribute("href").value
+        elsif social_account[:linkedin]
+           student_profile_hash[:linkedin] = doc.css(".social-icon-container").css("a")[1].attribute("href").value
+        elsif social_account[:github]
+           student_profile_hash[:github] = doc.css(".social-icon-container").css("a")[2].attribute("href").value
+        elsif social_account[:blog]
+           student_profile_hash[:blog] = doc.css(".social-icon-container").css("a")[3].attribute("href").value
+        end
+       end
+    
+    student_profile_hash[:profile_quote] = doc.css(".vitals-text-container").css(".profile-quote").text
+    student_profile_hash[:bio] = doc.css(".description-holder").css("p").text
+    student_profile_hash
+  end
+end 
 
