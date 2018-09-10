45d789e49a8bfd50488dc2d5dab0fdce302dff4c
Kitty Davies <kittyfdavies@gmail.com>
2018-09-10 00:20:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    hash_ary = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
