32156cc97d8da389a4c3800fe447dec6ab844d23
Kitty Davies <kittyfdavies@gmail.com>
2018-09-10 01:16:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,13 @@ class Scraper
     hash_ary = []
     html = open(index_url)
     doc = Nokogiri::HTML(html)
+    students = doc.css(".roster-cards-container .student-card")
+    students.each do |student|
+      hash = {:name => student.css(".card-text-container h4").text, :location => student.css(".card-text-container p").text, :profile_url => student.css("div.student-card a").text}
+      hash_ary << hash
+    end
+    hash_ary
+    #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
