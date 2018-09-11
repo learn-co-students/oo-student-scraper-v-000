e0ffee99694dde8e804cde056e5191ecf6619664
Daniel Schoch <d.schoch90@gmail.com>
2018-09-11 02:57:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,16 @@
 require 'open-uri'
+require 'Nokogiri'
 require 'pry'
 
 class Scraper
+  attr_accessor :name, :location, :profile_url
 
   def self.scrape_index_page(index_url)
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
