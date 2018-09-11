a8567c02e97fbcd8449ff2aa7b1bf39c7de0ffa9
Melissa Real <melissa.real@hotmail.com>
2018-09-11 16:22:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each{|dude| Student.new(dude)}
+    students_array.each{|dude| self.new(dude)}
   end
 
   def add_student_attributes(attributes_hash)
