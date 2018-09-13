c1c8277eb147372c45beaeb96dcd3923fc1051f4
Melissa Real <melissa.real@hotmail.com>
2018-09-13 16:55:41 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,45 +6,38 @@ class Scraper
   def self.scrape_index_page(index_url)
     kicks =  Nokogiri::HTML(open(index_url))
 
-    students = []
+    # students = []
     kicks.css(".student-card").collect { |student|
-      students << {
+      {
         :name => student.css(".student-name").text,
         :location => student.css(".student-location").text,
         :profile_url => student.css("a").attr("href").value
       }
      }
-     students
+     # students
   end
 
   def self.scrape_profile_page(profile_url)
     kicks = Nokogiri::HTML(open(profile_url))
-    profiles = []
+    profile = {}
     social_profiles = kicks.css(".social-icon-container").css("a").map{ |link| link.attribute("href").value}
+
     social_profiles.collect{|s|
       if s.include?("twitter")
-      profiles << {
-        :twitter => s
-      } elsif s.include?("github")
-      profiles << {
-        :github => s
-      } elsif s.include?("linkedin")
-      profiles << {
-        :linkedin => s
-      } else s.include?("blog")
-    profiles << {
-      :blog => s
-    }
-    end
+        profile[:twitter] = s
+       elsif s.include?("github")
+        profile[:github] = s
+       elsif s.include?("linkedin")
+        profile[:linkedin] = s
+       else s.include?("blog")
+        profile[:blog] = s
+      end
     }
 
-    kicks.css(".vitals-container").collect{ |profile|
-      profiles << {
-        :profile_quote => profile.css(".vitals-text-container div.profile-quote").text,
-        :bio => kicks.css(".description-holder").css("div p").text
-        }
-      }
-      profiles.reduce Hash.new, :merge
+    profile[:profile_quote] = kicks.css(".vitals-container").css(".vitals-text-container div.profile-quote").text
+    profile[:bio] = kicks.css(".description-holder").css("div p").text
+
+    profile
 
   end
 
