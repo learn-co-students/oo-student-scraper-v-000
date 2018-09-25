e244d77c68155a403996ff63ba6051da42d4159e
Harley Rayner <harleymteden@gmail.com>
2018-09-25 00:06:12 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,23 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+      html = open(index_url)
+      doc = Nokogiri::HTML(html)
+      doc.css(".roster-cards-container").collect do |student|
+
+        # name = project.css(".student-card")
+      binding.pry
+      # students.each do |student| student_array << student
+      #   # student_array
+      end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
