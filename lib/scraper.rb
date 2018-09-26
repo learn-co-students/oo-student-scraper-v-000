2a0e2c4386da60f281e4c4d44e0fa975b34ee4d9
Rafael Lopez <lpz.raf@gmail.com>
2018-09-26 17:08:47 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,26 +23,33 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
-    doc = Nokogiri::HTML(html)
+    profile = Nokogiri::HTML(open(profile_url))
     scraped_students = {}
-    doc.css(".social-icon-container a").each do |link|
-       if link.include?("twitter")
-         scraped_students[:twitter] = link
-       elsif link.include?("linkedin")
-         scraped_students[:linkedin] = link
-       elsif link.include?("github")
-         scraped_students[:github] = link
-       elsif link.include?("youtube")
-         scraped_students[:youtube] = link
+    social_link = profile.css(".social-icon-container").children.css("a").map  {|link| link.attribute("href").value}
+       social_link.each do |link_2|
+       if link_2.include?("twitter")
+         scraped_students[:twitter] = link_2
+       elsif link_2.include?("linkedin")
+         scraped_students[:linkedin] = link_2
+       elsif link_2.include?("github")
+         scraped_students[:github] = link_2
+       elsif link_2.include?("youtube")
+         scraped_students[:youtube] = link_2
        else
-         scraped_students[:blog] = link
+         scraped_students[:blog] = link_2
        end 
-      
-       
-     
     end
-   scraped_students 
-  end
-
+  
+         
+         scraped_students[:bio] = profile.css("div.bio-content.content-holder div.description-holder p").text
+         if profile.css("div.bio-content.content-holder div.description-holder p").text
+           scraped_students[:profile_quote] = profile.css(".profile-quote").text
+         if profile.css(".profile-quote").text
+  
+   scraped_students
 end
+end
+end
+end
+
 
