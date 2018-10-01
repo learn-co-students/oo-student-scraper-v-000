2a31226eaaef9d232e576daed0fce5cf7ba4118c
Sharmaine Requerme <srqrme@gmail.com>
2018-10-01 20:09:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,11 +12,15 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end
   end
 
   def self.all
