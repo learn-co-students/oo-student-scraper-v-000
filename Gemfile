bb7211c1410f657ecd462936b2e95cf620abea70
Heshie Brody <4hbrody@gmail.com>
2018-10-07 17:22:11 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,10 @@ source "https://rubygems.org"
 gem 'pry'
 gem 'nokogiri', '1.6.6.2'
 gem 'rspec'
+
+gem 'jekyll'
+
+group :jekyll_plugins do
+  gem 'jekyll-feed'
+  gem 'jekyll-seo-tag'
+end
--- _site/lib/scraper.rb
+++ _site/lib/scraper.rb
@@ -1,16 +1,15 @@
-require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
-    binding.pry
+    
   end
 
   def self.scrape_profile_page(profile_url)
-
+    
   end
 
 end
+
