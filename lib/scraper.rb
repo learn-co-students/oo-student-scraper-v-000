28ce68636b54265986fa4e0e33cf816578b82996
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-07 21:37:37 -0400

    Set up scraper for .scrape_profile_page method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,10 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  @@student_info = []
 
   def self.scrape_index_page(index_url)
     student_site = Nokogiri::HTML(open(index_url))
-    student_info = []
-    
-    # binding.pry
-
     
     student_site.css(".student-card").each do |student|
       i = 0
@@ -18,16 +15,23 @@ class Scraper
         :location => student.css(".card-text-container").css(".student-location").text,
         :profile_url => student.css("a")[i]["href"]
       }
-      student_info << student_hash
+      @@student_info << student_hash
       i += 1
     end
-    student_info
-
-
+    # binding.pry
+    @@student_info
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    # @@student_info.each do |student|
+    #   i = 0
+    #   profile = student[i][:profile_url]
+    #   # all method code in here
+    #   i += 1
+    # end
+    # profile_url = "./fixtures/student-site/#{profile}"
+
+    student_profile = Nokogiri::HTML(open(profile_url))
   end
 
 end
