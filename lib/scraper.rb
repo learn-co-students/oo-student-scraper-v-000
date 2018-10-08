ac682a10b1045ecd3a76845324f69ee74417085e
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-07 21:28:33 -0400

    Scraper iterates through all students on the index page and returns info in correct form

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,18 +7,21 @@ class Scraper
   def self.scrape_index_page(index_url)
     student_site = Nokogiri::HTML(open(index_url))
     student_info = []
+    
     # binding.pry
 
     
     student_site.css(".student-card").each do |student|
+      i = 0
       student_hash = {
         :name => student.css(".card-text-container").css(".student-name").text,
         :location => student.css(".card-text-container").css(".student-location").text,
-        :profile_url => student.css("a")[0]["href"]
+        :profile_url => student.css("a")[i]["href"]
       }
       student_info << student_hash
+      i += 1
     end
-    
+    student_info
 
 
   end
