d6f7dae94ac039b576a8a2f19fda4d6cc475c9ec
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-07 20:34:56 -0400

    Defined html doc using Nokogiri and OpenURI

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,13 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    student_site = Nokogiri::HTML(open(index_url))
+    binding.pry
+    student_info = {}
     
   end
 
