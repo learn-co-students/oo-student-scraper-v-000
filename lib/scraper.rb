045da1e3c0eec3c18fd33897d5be44b006116416
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-07 21:24:17 -0400

    Can extract values from site and add as separate hashes to the student_info array

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,9 +6,21 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     student_site = Nokogiri::HTML(open(index_url))
-    binding.pry
-    student_info = {}
+    student_info = []
+    # binding.pry
+
     
+    student_site.css(".student-card").each do |student|
+      student_hash = {
+        :name => student.css(".card-text-container").css(".student-name").text,
+        :location => student.css(".card-text-container").css(".student-location").text,
+        :profile_url => student.css("a")[0]["href"]
+      }
+      student_info << student_hash
+    end
+    
+
+
   end
 
   def self.scrape_profile_page(profile_url)
