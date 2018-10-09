2bb51ce8f0a87cb60a19b7948bebfc270844884f
Kitty Davies <kittyfdavies@gmail.com>
2018-10-09 16:15:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,10 +20,10 @@ class Scraper
     hash_ary_single = {}
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    binding.pry
+    #binding.pry
     icons = doc.css(".vitals-container .social-icon-container @href")
-    bio = doc.css(".details-container .bio-block .bio-content .description-holder").text
-    quote = doc.css(".vitals-container .vitals-text-container .profile-quote p").text
+    bio = doc.css(".details-container .bio-block .bio-content .description-holder p").text
+    quote = doc.css(".vitals-container .vitals-text-container .profile-quote").text
     hash_ary_single = {:twitter => icons[0].text, :linkedin => icons[1].text, :github => icons[2].text, :blog => icons[3].text, :bio => bio, :profile_quote => quote}
     hash_ary_single
   end
