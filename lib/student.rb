ffcd168f8fb8dbf1722f5f98c18cc8a0b98ca259
meg-gutshall <meghan.gutshall@gmail.com>
2018-10-09 14:40:20 -0400

    Able to initialize a new student from a hash

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @twitter = student_hash[:twitter]
+    @linkedin = student_hash[:linkedin]
+    @github = student_hash[:github]
+    @blog = student_hash[:blog]
+    @profile_quote = student_hash[:profile_quote]
+    @bio = student_hash[:bio]
+    @profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
