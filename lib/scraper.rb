484c4e1ee7c78113275d9863b10044d1975cc15c
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-10 19:57:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,19 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def self.scrape_index_page(index_url = "./fixtures/student-site/index.html")
+
+    html = open(index_url)
     
+
+
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_profile_page(profile_url = "./fixtures/student-site/index.html")
+
   end
 
 end
-
