e9e818d773d92fd2fdcc5f8ebab42854ffe3ef99
Rachel Wyatt <rw126@evansville.edu>
2018-10-15 12:02:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("http://159.89.225.105:56893/fixtures/student-site/")
+    doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+
+#names doc.css("h4").text
+#location doc.css("p").text
