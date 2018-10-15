84b115b7146685c967a0f09210f1a760fd1014c1
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-15 22:10:26 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,30 +10,51 @@ class Scraper
   
       html = open(index_url)
       doc = Nokogiri::HTML(html)
-      student_cards = doc.css(".student-card")
       
-      student_cards.each do |card|
-         student_name = card.css(".student-name").text 
-         student_location = card.css(".student-location").text 
-         first_name = student_name.split(" ")[0].downcase
-         last_name = student_name.split(" ")[1].downcase 
-         student_url = "students/#{first_name}-#{last_name}.html"
-         new_hash = {
-           :name => student_name,
-           :location => student_location,
-           :profile_url => student_url 
-         } 
-         
-         #students_array.push(new_hash)
-        binding.pry 
-          end 
-      
-
+     doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_link = "#{student.attr('href')}"
+        student_name = student.css('.student-name').text
+        student_location =  student.css('.student-location').text 
+        new_hash = {
+          :name => student_name, 
+          :location => student_location,
+          :profile_url => student_link
+        }
+        students_array << new_hash
+      end 
+    end 
+    students_array
   end
    
   def self.scrape_profile_page(profile_url)
     
- end
-
-end
-
+    student_hash = {
+     
+    }
+    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+     student_name = doc.xpath("//h1 [@class='profile-name']").text 
+     last_name = student_name.split(" ")[1]
+     last_name = last_name.downcase 
+    all_links = doc.xpath("//a")
+     all_links.each do |link|
+       if link.attr('href').include?('twitter')
+         student_hash[:twitter] = link.attr('href')
+       elsif link.attr('href').include?('linkedin')
+         student_hash[:linkedin] = link.attr('href')
+       elsif link.attr('href').include?('github')
+         student_hash[:github] = link.attr('href')
+       elsif link.attr('href').include?(last_name)
+         student_hash[:blog] = link.attr('href')
+       end 
+     end 
+     bio = doc.xpath("//div [@class='description-holder']/p").text 
+     student_hash[:bio] = bio
+     profile_quote = doc.xpath("//div [@class='profile-quote']").text 
+     student_hash[:profile_quote] = profile_quote
+     student_hash
+  end 
+end 
+  
