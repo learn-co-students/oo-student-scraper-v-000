ea5cc00c80068e36246050064da80066234ded91
Rachel Wyatt <rw126@evansville.edu>
2018-10-19 14:31:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,10 +5,9 @@ require 'nokogiri'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open(index_url)
-    doc = Nokogiri::HTML(html)
-    binding.pry
-  end
+      doc = Nokogiri::HTML(open(index_url))
+     	binding.pry
+    end
 
   def self.scrape_profile_page(profile_url)
 
