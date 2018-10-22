a79f272f3a8fa08c442b6b3f2da02233602f97c6
Nick Romans <nicholas.romans@gmail.com>
2018-10-22 20:35:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,25 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    html = profile_url
+    profile_page = Nokogiri::HTML(open(html))
+    student = {}
+
+      social_icon_container = profile_page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+      social_icon_container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
+      end
+      student[:profile_quote] = profile_page.css(".profile-quote").text
+      student[:bio] = profile_page.css("div.description-holder p").text
+      student
 
   end
-
 end
