ec3a8c90b93c5b8dca72f21873bdc95201be628b
Justin Kenney <justinkenney13@gmail.com>
2018-10-22 22:57:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
@@ -17,15 +18,24 @@ class Scraper
     profile = Nokogiri::HTML(open(profile_url))
     websites = profile.css(".social-icon-container a")
     h2 = {}
-    binding.pry
+    h2[:bio] = profile.css(".bio-block p").text
+    h2[:profile_quote] = profile.css(".vitals-text-container .profile-quote").text
+
     websites.each {|s|
-      # v=URI.parse(s.attribute("href").value).host.gsub(".com", "")
-      url=s.attribute("href").value
+      url = s.attribute("href").value
       v = URI.parse(url).host.gsub(".com", "").gsub("www.", "")
-      # binding.pry
-      h2[v.to_sym] = url
+      binding.pry
+
+      # v.include? "twitter", h2[v.to_sym] = url
       }
       h2
-  end
 
+    # h2.each {|k,v|
+    #   v.include?(profile.css(".profile-name").text.downcase.split.first) ? k = :blog : nil}
+    #
+    # binding.pry
+
+
+
+  end
 end
