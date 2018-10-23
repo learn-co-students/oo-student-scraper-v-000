848105bfbe2a791c185dd5e0498d67c1e698a29e
Erin Green <erin.y.green@gmail.com>
2018-10-23 23:37:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,17 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+   index_url = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+   
+   #roster = index_url.css(".student-card")
+   #name = index_url.css(".student-name").text
+   #location = index_url.css(".student-location").text 
+   #profile_url = index_url.css("div").css(".student-card").css("a").text
+   binding.pry 
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +19,6 @@ class Scraper
   end
 
 end
+
+Scraper.scrape_index_page("./fixtures/student-site/index.html")
 
