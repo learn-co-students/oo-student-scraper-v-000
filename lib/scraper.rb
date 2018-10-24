642bf18863bfd726084c5fabaf9fbfd252f2a904
Erin Green <erin.y.green@gmail.com>
2018-10-24 00:39:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,8 @@ class Scraper
    #roster = index_url.css(".student-card")
    #name = index_url.css(".student-name").text
    #location = index_url.css(".student-location").text 
-   #profile_url = index_url.css("div").css(".student-card").css("a").text
-   binding.pry 
+   #profile_url = index_url.css("div").css(".student-card a href")
+   
   end
 
   def self.scrape_profile_page(profile_url)
