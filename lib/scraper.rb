91f6a00805d0fed2cddb552808cfd840840fd965
Ashley Wong <ashwong04@gmail.com>
2018-10-27 17:31:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,12 +7,13 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
     html_card = html.css(".student-card")
-    binding.pry
+    # binding.pry
     html_card.collect do |person|
-      student = Student.new
-      student.name = person.css(".student-name").text
-      student.location = person.css(".student-location").text
-      student.url = person.css("a").href
+      student = Hash.new
+      student[:name] = person.css(".student-name").text
+      student[:location] = person.css(".student-location").text
+      student[:profile_url] = person.css("a href").text
+      student
     end
   end
 
