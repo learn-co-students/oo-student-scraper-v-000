e93a4096e4c0a2a62cbb235166a1f3766aad106d
Ashley Wong <ashwong04@gmail.com>
2018-10-27 16:35:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
+    html_card = html.css(".student-card")
+    binding.pry
+    html_card.collect do |person|
+      student = Student.new
+      student.name = person.css(".student-name").text
+      student.location = person.css(".student-location").text
+      student.url = person.css("a").href
+    end
   end
 
   def self.scrape_profile_page(profile_url)
