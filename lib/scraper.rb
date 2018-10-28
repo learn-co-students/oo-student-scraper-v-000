7f690e269c733454cc32280511d3f1f67da2743e
taisaoko <ttd3calvin@gmail.com>
2018-10-28 02:25:38 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,13 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = index_url
+    doc = Nokogiri::HTML(html)
+    student_list = {}
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +16,31 @@ class Scraper
 
 end
 
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+def create_project_hash
+  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  doc = Nokogiri::HTML(html)
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+  
+  projects
+end
+
+
+create_project_hash
