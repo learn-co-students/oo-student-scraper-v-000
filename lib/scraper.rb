f836ea56596e98ccd1f454b26e27712ade1beb47
taisaoko <ttd3calvin@gmail.com>
2018-10-29 02:13:38 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,18 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = index_url
+    html = open(index_url)
     doc = Nokogiri::HTML(html)
-    student_list = {}
+    student_list = []
+    
+      doc.css("div.student_card a").each do |student|
+        student_list <<
+        {:name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").first["href"] }
+    
+    end
+    student_list
   end
 
   def self.scrape_profile_page(profile_url)
@@ -23,6 +32,7 @@ end
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
+=begin
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
@@ -41,6 +51,5 @@ def create_project_hash
   
   projects
 end
-
-
 create_project_hash
+=end
