fd07606ba2c32d23673535c3e99b77b33e797bfb
taisaoko <ttd3calvin@gmail.com>
2018-10-30 14:43:46 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,25 +5,29 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open(index_url)
-    doc = Nokogiri::HTML(html)
+    html = File.read('fixtures/student_site/index.html')
+    kickstarter = Nokogiri::HTML(html)
     student_list = []
     
       doc.css("div.student_card a").each do |student|
         student_list <<
         {:name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
-        :profile_url => student.css("a").first["href"] }
-    
-    end
+        :profile_url => student.attribute("href").value}
+      end
     student_list
+    binding.pry
   end
+  
 
   def self.scrape_profile_page(profile_url)
     
   end
 
 end
+
+page = Scraper.new
+page.scrape_index_page('fixtures/student_site/index.html')
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
