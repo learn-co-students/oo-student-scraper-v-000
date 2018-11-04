8c9ba862248bfbd5ba8839d3e842c591dc97aa08
Cheryl K <cherylkelson@icloud.com>
2018-11-04 15:25:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,27 +5,34 @@ require 'pry'
 
 class Scraper
     attr_accessor :parse_page
- 
+
   def initialize 
     doc = open-uri.get("./fixtures/student-site/index.html")
     @parse_page = Nokogiri::HTML(html)  
   end 
 
-  names = parse_page.css()
-  
+  # names = parse_page.css()
+  # location = parse_page.css()
+  # profile_url = parse_page.css()
+
   def self.scrape_index_page(index_url)
-    {:name => <h4 class, :location => <p class, :student_url => <a href}
+    something.css(name, location, profile_url).something.map
     
-    #<h4 class="student-name">Ryan Johnson</h4>   Name
-    #<p class="student-location">Glenelg, MD</p>  Location
-    #<a href="students/eric-chu.html">          Student-url
-              
-                     
+  end
+    
+    {:name => <h4 class, 
+    :location => <p class, 
+    :student_url => <a href}
+
+     
+    
+                   
     
     scraped_students = [] 
     @student = student
     @@all << self
     index_url = "./fixtures/student-site/index.html"
+    
     html = open(https://flatironschool.com)
     
     
