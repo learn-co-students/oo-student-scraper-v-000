5a750089b201b60e8d55826dc4e5913f3c5beb08
Sheena Bonilla <sheena.myb@gmail.com>
2018-11-05 16:28:15 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,38 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    parsed_page = Nokogiri::HTML(html)
+    all_students = parsed_page.css('div.student-card')
     
+    all_students.each do |key, value|
+      binding.pry
+      student 
+        student[:name] = student.css('h4.student-name').text
+        student[:location] = student.css('p.student-location').text
+  end
+  
+          # profile_url: student.xpath('//div/a/@href').text.strip
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    #=> responsible for scraping an individual student's profile page to get further info about that student
   end
 
 end
 
+
+
+    
+#     def make_courses
+      
+#       all_courses = self.get_courses
+#       all_courses.each do |noko_course|
+#         course = Course.new
+#         course.title = noko_course.css("h2").text
+#         course.schedule = noko_course.css(".date").text
+#         course.description = noko_course.css("p").text
+#       end 
+#     end
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,11 +7,11 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+  
   end
 
   def self.create_from_collection(students_array)
-    
+ 
   end
 
   def add_student_attributes(attributes_hash)
