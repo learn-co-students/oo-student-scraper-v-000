5022b7346e6b91cb58335b34c564f9ac31ef935c
Sheena Bonilla <sheena.myb@gmail.com>
2018-11-05 17:07:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,15 +8,18 @@ class Scraper
     parsed_page = Nokogiri::HTML(html)
     all_students = parsed_page.css('div.student-card')
     
-    all_students.each do |key, value|
-      binding.pry
-      student 
-        student[:name] = student.css('h4.student-name').text
-        student[:location] = student.css('p.student-location').text
+    all_students.collect do |student_card|
+      #binding.pry
+      student = {
+        :name => student_card.css('h4.student-name').text,
+        :location => student_card.css('p.student-location').text,
+        :profile_url => student_card.css('a').attr('href').value
+      }
+      student
+    end
   end
   
-          # profile_url: student.xpath('//div/a/@href').text.strip
-  end
+      
 
   def self.scrape_profile_page(profile_url)
     #=> responsible for scraping an individual student's profile page to get further info about that student
