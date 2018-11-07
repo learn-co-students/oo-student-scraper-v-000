a654476713e8361983b7fdf1ce976c9df962e74c
Stef Claus <stef.claus@gmail.com>
2018-11-07 21:31:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,17 +12,17 @@ attr_accessor :name, :location, :profile_url
       html = open(index_url)
       index = Nokogiri::HTML(html)
       student_card = index.css(".student-card")
-      student_name = student_card.css(".student-name").text
-      student_location = student_card.css(".student-location").text
-      student_url = student_card.css("a").attribute("href").text
+    #  student_name = student_card.css(".student-name").text
+    #  student_location = student_card.css(".student-location").text
+    #  student_url = student_card.css("a").attribute("href").text
+
+
 
       student_card.each do |student|
-        student_name
-        student_location
-        student_url
-        #students << { name location etc...}
-        binding.pry
-
+        student_name = student_card.css(".student-name").text
+        student_location = student_card.css(".student-location").text
+        student_url = student_card.css("a").attribute("href").text
+        students << {name: student_name, location: student_location, profile_url: student_url}
       end
       end
 
