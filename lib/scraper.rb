e2b4bf4f639831fac60506642405d3ee1e4e98bd
Elias Ventura <eliasventura23@gmail.com>
2018-11-07 17:43:31 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,53 +3,35 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index)
+  def self.scrape_index_page(index_url)
     scraped_students = []
-    index_page = Nokogiri::HTML(open(index))
-    index_page.css("div .roster-cards-container").each do |card|
-      card.css(".student-card a").each do |student|
-        student_location = student.css(".student-location").text
-        student_name = student.css(".student-name").text
-        student_profile = "./fixtures/student-site/#{student.attr('href')}"
-        scraped_students << {name: student_name, location: student_location, profile_url: student_profile}
+    index_page = Nokogiri::HTML(open(index_url))
+    index_page.css("div .roster-cards-container").each do |student_card|
+      student_card.css(".student-card a").each do |student|
+        profile_url = "./fixtures/student-site/#{student.attr("href")}"
+        scraped_students << {:location => student.css(".student-location").text, :name => student.css(".student-name").text, :profile_url => profile_url}
+      end
     end
-  end
-  scraped_students
-end
-
-
-  def self.scrape_profile_page(profile_url)
-    scraped_student = {}
-    profile_page = Nokogiri::HTML(open(profile_url))
-    social_icons = profile_page.css(".vitals-container .social-icon-container a")
-    social_text = profile_page.css(".vitals-container .vitals-text-container")
-    student_info = profile_page.css(".details-container")
-
-    if social_icons.any? do |icon|
-      if icon['href'].include?("twitter")
-      scraped_student[:twitter] = icon['href']
-    end
-    end
+        scraped_students
   end
 
-  if social_icons.any? do |icon|
-    if icon['href'].include?("linkedin")
-      scraped_student[:linkedin] = icon['href']
-    end
-  end
-end
+  def self.scrape_profile_page(student_profile_url)
+    links = {}
+    student_profile = Nokogiri::HTML(open(student_profile_url))
+    social_icons = student_profile.css("div.social-icon-container a").map {|link| link['href']}
+    profile_quote = student_profile.css("div.vitals-text-container .profile-quote").text
+    bio =  student_profile.css(".bio-content p").text
+    twitter = social_icons.find {|icon| icon.include?("twitter")}
+    linked_in = social_icons.find {|icon| icon.include?("linkedin")}
+    github = social_icons.find {|icon| icon.include?("github")}
+    blog = (social_icons.reject {|icon| icon.include?("https")}).first
 
-if social_icons.any? do |icon|
-  if icon['href'].include?("github")
-    scraped_student[:github] = icon['href']
-  end
-end
-end
-
-    scraped_student[:blog] = social_icons[3].attribute("href").value if social_icons[3]
-    scraped_student[:profile_quote] = social_text.css(".profile-quote").text
-    scraped_student[:bio] = student_info.css("div.bio-content.content-holder div.description-holder p").text
-    scraped_student
-
+    links[:twitter] = twitter unless !twitter
+    links[:linkedin] = linked_in unless !linked_in
+    links[:github] = github unless !github
+    links[:blog] = blog unless !blog
+    links[:profile_quote] = profile_quote
+    links[:bio] = bio
+    links
   end
 end
