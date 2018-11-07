a7b40b9c4d612d3530b437a7024287e65a016db5
Stef Claus <stef.claus@gmail.com>
2018-11-07 19:37:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,18 @@ require 'pry'
 
 class Scraper
 
+attr_accesor :name, :location, :profile_url
+
+
+
   def self.scrape_index_page(index_url)
-    
+      Nokogiri::HTML(open(index_url))
+      binding.pry
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
