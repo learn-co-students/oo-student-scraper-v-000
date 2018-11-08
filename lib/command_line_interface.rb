161b798cf494915613ee86e044930bba37ee3ed8
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-11-08 02:26:17 +0000

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -14,27 +14,36 @@ class CommandLineInterface
 
   def make_students
     students_array = Scraper.scrape_index_page(BASE_PATH + 'index.html')
+  #calls #scrape_index_page on Scraper class to scrape student name, location
+  #and profile link from student site and stores all info on stuents_array variable
     Student.create_from_collection(students_array)
+  #calls #create_from_collection to create a new instances of the Student class using
+  #the students_array data scraped from student site. saves each instance in the @@all student class variable.
+
   end
 
   def add_attributes_to_students
-    Student.all.each do |student|
+    Student.all.each do |student| # iterates through each instane of Student class
       attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
+  # calls #scrape_profile_page on Scraper class to scrape student info from each
+  #student profile link and saves the info in attributes hash variable.
       student.add_student_attributes(attributes)
+  # calls #add_student_attritubes on each instance of a student class to add the
+  # additional information retrevied from their profile link.
     end
   end
 
   def display_students
-    Student.all.each do |student|
-      puts "#{student.name.upcase}".colorize(:blue)
-      puts "  location:".colorize(:light_blue) + " #{student.location}"
-      puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
+    Student.all.each do |student| #iterates through each instance of student class
+      puts "#{student.name.upcase}".colorize(:blue) # formats and prints Student name in all caps and blue font
+      puts "  location:".colorize(:light_blue) + " #{student.location}" # puts header "location" in blue and student location info in default color.
+      puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}" #prints header "profile quote" in blue and student profile quote in default color.
       puts "  bio:".colorize(:light_blue) + " #{student.bio}"
       puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
       puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
       puts "  github:".colorize(:light_blue) + " #{student.github}"
-      puts "  blog:".colorize(:light_blue) + " #{student.blog}"
-      puts "----------------------".colorize(:green)
+      puts "  blog:".colorize(:light_blue) + " #{student.blog}" # all above code prints header for student info in blue and info in default color.
+      puts "----------------------".colorize(:green) # prints a green border to divide and organize student profile info. 
     end
   end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,65 @@
+
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    names = []
+    doc.search('h4.student-name').each do |name|
+      names << name.text
+    end
+    locations = []
+    doc.search('p.student-location').each do |location|
+      locations << location.text
+    end
+    profile_links = []
+    doc.search('a').each do |link|
+      profile_links << link['href']
+    end
+    profile_links = profile_links.drop(1)
+
+    students = []
+    names.each { |name| students << {:name => name}}
+    l = 0
+    locations.each {|location| students[l][:location] = location; l += 1}
+    u = 0
+    profile_links.each {|profile_url| students[u][:profile_url] = profile_url; u += 1}
+    students
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+    social = []
+    profile.search('a').each do |link|
+      social << link['href']
+    end
+    social = social.drop(1)
+    student_info = {}
+    twitter = nil
+    linkedin = nil
+    github = nil
+    blog = nil
+    social.each do |social_link|
+      if social_link.include?("twitter") == true
+        twitter = social_link
+      elsif social_link.include?("linkedin") == true
+        linkedin = social_link
+      elsif social_link.include?("github") == true
+        github = social_link
+      else
+        blog = social_link
+      end
+    end
+    student_info[:twitter] = twitter unless twitter == nil
+    student_info[:linkedin] = linkedin unless linkedin == nil
+    student_info[:github] = github unless github == nil
+    student_info[:blog] = blog unless blog == nil
+    student_info[:profile_quote] = profile.css(".profile-quote").text
+    student_info[:bio] = profile.css("p").text
+    student_info
   end
 
 end
-
