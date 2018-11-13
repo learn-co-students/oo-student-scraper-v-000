5103d7fc480c5c434c35ae28974c1810295d13af
Karen Flores <karen.ag.flores@gmail.com>
2018-11-13 02:25:46 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,21 +9,50 @@ class Scraper
     html = File.read(index_url)
     roster = Nokogiri::HTML(html)
     students = {}
-    roster.css(".roster-cards-container").each do |student|
-      student_header = student.css(".student-card").text
-      students[student_header.to_sym] = {
-        :name => variable.css(".roster-cards-container").css("h4").text,
-        :location => variable.css(".roster-cards-container").css("p").text,
-        :profile_url => variable.css(".roster-cards-container").css("a").attribute("href").value
-        binding.pry
+    i = 1
+    #roster.css(".roster-cards-container").each do |student|
+    roster.css(".student-card").each do |student|
+      student_header = i#student.css("div.id").text
+      #binding.pry
+      students[student_header] = {
+        :name => student.css("h4").text,
+        :location => student.css("p").text,
+        :profile_url => student.css("a").attribute("href").value
       }
+      i+=1
     end
     students
+    #binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-
+    #This is a class method that should take in an argument of a student's profile URL. It should use
+    #Nokogiri and Open-URI to access that page. The return value of this method should be a hash in which
+    #the key/value pairs describe an individual student. Some students don't have a twitter or some other
+    #social link. Be sure to be able to handle that.
+    #twitter url, linkedin, gituhub url, blog url, profile quote, bio
+    html = File.read(profile_url)
+    roster = Nokogiri::HTML(html)
+    profile = {}
+    i = 1
+    #roster.css(".roster-cards-container").each do |student|
+    #roster.css(".student-card").each do |student|
+      #student_header = i#student.css("div.id").text
+      #binding.pry
+      profile = {
+        :twitter => ,
+        :linkedin => ,
+        :github => ,
+        :blog => ,
+        :profile_quote => ,
+        :bio => 
+        binding.pry
+      }
+      #i+=1
+    end
+    profile
+    #binding.pry
   end
 
 end
-Scraper.scrape_index_page('./fixtures/student-site/index.html')
+Scraper.scrape_profile_page('./fixtures/student-site/students/ryan-johnson.html')
