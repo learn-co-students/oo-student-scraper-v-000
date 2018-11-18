f0b9b7a908e268760db65d7656d1ea8f49d1754d
Malcome Fein <mfein90@gmail.com>
2018-11-18 15:00:06 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,26 +10,32 @@ class Student
   def initialize(student_hash)
   @name = student_hash[:name]
   @location = student_hash[:location]
-  @twitter = student_hash[:twitter]
-  @linkedin = student_hash[:linkedin]
-  @github = student_hash[:github]
-  @blog = student_hash[:blog]
-  @profile_quote = student_hash[:profile_quote]
-  @bio = student_hash[:bio]
-  @profile_url = student_hash[:profile_url]
+  # @twitter = student_hash[:twitter]
+  # @linkedin = student_hash[:linkedin]
+  # @github = student_hash[:github]
+  # @blog = student_hash[:blog]
+  # @profile_quote = student_hash[:profile_quote]
+  # @bio = student_hash[:bio]
+  # @profile_url = student_hash[:profile_url]
   @@all << self
   end
 
   def self.create_from_collection(students_array)
-    Scraper.scrape_index_page(index_url)
-    binding.pry
+    # students_array.each do |student|
+    #   Studnet.new(student)
+    # end
+    students_array.map{|student_hash|Student.new(student_hash)}
+    #binding.pry
   end
 
   def add_student_attributes(attributes_hash)
-
+    # attributes_hash.
+  #  binding.pry
+  attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
     @@all
   end
+
 end
