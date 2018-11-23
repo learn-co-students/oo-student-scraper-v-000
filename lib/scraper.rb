c535af87fef1b36fa30f97e83075c369c3e44f5e
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-23 20:14:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,31 +23,23 @@ class Scraper
     profile_page = Nokogiri::HTML(open(profile_url))
     student = []
 
-    links = profile_page.css("div.vitals-container").each do |element|
-      element.css
-    twitter_url:
-    linkedin_url:
-    github_url:
-    blog_url:
-    profile_quote:
-    bio:
+    profile_page.css(".social-icon-container").children.css("a").collect do |element| #takes the social icon container, and iterates through its children
+      links = element.attribute('href').value #collects element attributes that match href and returns the value.
+      links.each do |link|
+        if link.include?("twitter")
+          student[twitter_url:] = link
+        elsif link.include?("linkedin")
+          student[linkedin_url:] = link
+        elsif link.include?("github")
+          student[github_url:] = link
+        elsif link.include?("blog")
+          student[blog_url:] = link
+        end
+      end
+    end
+student[profile_quote:] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+student[bio:] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
 
+student
   end
-
-  def self.scrape_profile_page(profile_slug)
-   student = {}
-   profile_page = Nokogiri::HTML(open(profile_slug))
-   links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
-   links.each do |link|
-     if link.include?("linkedin")
-       student[:linkedin] = link
-     elsif link.include?("github")
-       student[:github] = link
-     elsif link.include?("twitter")
-       student[:twitter] = link
-     else
-       student[:blog] = link
-     end
-   end
-
 end
