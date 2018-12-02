fd73230c12d167686a24bfb51a9aed2228b60639
github username <github email address>
2018-12-02 17:31:50 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,19 +6,18 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    student_data = doc.css("div.roster-cards-container")
-    student_data.each do |card|
-    	student_hash = Hash.new
- 		# card.css(".student-card a")
- 		student_hash[:name] = card.css(".student-card a").css("div:nth-child(2)").css(".student-name").first.text
- 		student_hash[:location] = card.css(".student-card a").css("div:nth-child(2)").css(".student-location").first.text
- 		binding.pry
-    end
-    # student_hash = Hash.new 
-    # students.each do |student|
-    # 	student_hash[:name] = student
-    # end
+    students = Array.new
+    student_data = doc.css("div.roster-cards-container .student-card a")
     # binding.pry
+    student_data.each do |card|
+    	# binding.pry 
+ 		student_name = card.css("div:nth-child(2)").css(".student-name").first.text
+ 		location = card.css("div:nth-child(2)").css(".student-location").first.text
+ 		profile_url  = card.attribute("href").value
+ 		student_hash ={:name => student_name, :location => location, :profile_url => profile_url}
+ 		students << student_hash
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -39,3 +38,12 @@ end
 
 
        # student_locations = doc.css("div.roster-cards-container").first.css("p").text
+
+       		# students << student_hash[:name] = student_name
+ 		# students << student_hash[:location] = location 
+ 		# students << student_hash[:profile_url] = profile_url
+ 		# binding.pry
+
+ 		# 		student_name = card.css(".student-card a").css("div:nth-child(2)").css(".student-name").first.text
+ 		# location = card.css(".student-card a").css("div:nth-child(2)").css(".student-location").first.text
+ 		# profile_url  = card.css(".student-card a").attribute("href").value
