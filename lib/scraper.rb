a0c1c8140dcdb5b633d1db3ae80574aa2fe88574
tgray017 <tgray017@gmail.com>
2018-12-04 01:20:31 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
