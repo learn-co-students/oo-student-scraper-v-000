9f43a7bad2650f721e12157ed0cbadf494d8f7df
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-12-20 14:40:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,17 @@
+rewuire 'Nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css("roster-cards-container")
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
