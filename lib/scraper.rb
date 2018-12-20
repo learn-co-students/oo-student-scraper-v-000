5a20af2e10168d458487fa99255cb1129c1ed7f7
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-12-20 16:30:45 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,24 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
+    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+    student_profile
+  end
 
   end
 
-end
