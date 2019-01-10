de37386eb12c5639915736da4c34f664f5da0b60
Andrew Trent <andrewtrentdesign@gmail.com>
2019-01-10 02:06:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,34 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # mass assign attributes from hash
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    # save new Student instances to @@all array
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    # iterate through students array and instance Students
+    students_array.each do |hsh|
+      self.new(hsh)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # allow Student object to iterate over a hash of new attributes
+      attributes_hash.each do |key, value|
+        self.send("#{key}=", value)
+      end
   end
 
   def self.all
-    
+    # saving method
+    @@all
   end
 end
-
