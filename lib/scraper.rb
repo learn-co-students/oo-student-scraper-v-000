69058e4776267c0d3b8c7f4e1649a97435e5533d
Ashleigh Chun <ashleigh.chun@wework.com>
2019-01-11 18:13:56 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,18 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card(".")
+      binding.pry # for example
+      
+    end
+end
   end
 
   def self.scrape_profile_page(profile_url)
