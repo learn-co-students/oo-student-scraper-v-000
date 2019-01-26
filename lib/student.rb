aedf138d4e1278d2fd5ee126e2b6e8ca07b6125c
Sara Walker <sarawall1187@gmail.com>
2019-01-26 23:05:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student.each do |k,v|
+      end 
+    end
   end
 
   def add_student_attributes(attributes_hash)
