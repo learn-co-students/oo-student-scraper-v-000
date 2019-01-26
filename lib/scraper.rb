d0cd7a8e2d6e591cd2f9a46cc01e74b139ec3111
Sara Walker <sarawall1187@gmail.com>
2019-01-26 20:07:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,16 +4,23 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open('./fixtures/student-site/index.html'))
-    student = {}
-    student[:name] = doc.css("h1#profile-name").text
-    student[:location] = doc.css("h2#profile-location").text
-    student[:profile_url] = doc.css("div#student-card a href").text
-         binding.pry    
-  end
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+     doc.css(".roster-cards-container").each do |card| 
+      card.css("a").each do |student|
+        student_name = student.css("h4.student-name").text
+        student_location = student.css("p.student-location").text
+        student_url = student.attr("href")
+        students << {name: student_name, location: student_location, profile_url: student_url}
+      end
+     end
+   students
+ end
+  
 
   def self.scrape_profile_page(profile_url)
-    
+     doc = Nokogiri::HTML(open(profile_url))
+     student_profile = []
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+
   end
 
   def self.create_from_collection(students_array)
