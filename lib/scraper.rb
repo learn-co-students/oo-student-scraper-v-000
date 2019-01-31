df3aae8e0aa62dcdcf4ca94a69d7f70f72652042
ktochow1 <ktochow1@binghamton.edu>
2019-01-31 18:49:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,55 @@
 require 'open-uri'
 require 'pry'
 
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+
+    index_page = Nokogiri::HTML(open(index_url))
+    student_arr = []
+    index_page.css(".roster-cards-container").each do |student|
+      student.css(".student-card a").each do |info|
+        student_name = info.css(".student-name").text
+        student_location = info.css(".student-location").text
+        student_pro_url = "#{info.attr('href')}"
+        
+        student_arr << {:name => student_name, :location => student_location, :profile_url => student_pro_url}
+      end 
+    end
+    student_arr
   end
 
+  
   def self.scrape_profile_page(profile_url)
-    
+    scrape_page = Nokogiri::HTML(open(profile_url))
+    profile_hash = {}
+    profile_page = scrape_page.css(".social-icon-container a").collect do |icon| 
+      icon.attribute("href").value
+    end
+       profile_page.each do |link| 
+        if link.include?("twitter")
+         profile_hash[:twitter] = link
+          
+          elsif link.include?("linkedin")
+            profile_hash[:linkedin] = link 
+          
+          elsif link.include?("github")
+            profile_hash[:github] = link
+          
+          elsif link.include?(".com")
+            profile_hash[:blog] = link
+            
+          elsif link.include?("profile-quote")
+          profile_hash[:profile_quote] = link 
+          
+          elsif link.include?("bio")
+          profile_hash[:bio] = link
+        end
+      end 
+  profile_hash[:profile_quote] = scrape_page.css(".profile-quote").text 
+  profile_hash[:bio] = scrape_page.css("div.description-holder p").text
+  profile_hash
   end
-
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,36 @@
 class Student
-
+require 'pry'
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
 
   def initialize(student_hash)
-    
+    self.send("name=", student_hash[:name])
+    self.send("location=", student_hash[:location])
+    # self.send("profile_url=", student_hash[:profile_url])
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #students_array is an array of hashes with name, location of each student 
+    #look at students_array and pic each student hash and create a new student from that student_hash
+    #how does the scraper class ineract???
+    students_array.each do |student_hash|
+     Student.new(student_hash) 
+    end
   end
 
   def add_student_attributes(attributes_hash)
+    attributes_hash.each do |k,v| 
+      self.send("#{k}=", v)
+    end
+      @@all << self
     
   end
 
   def self.all
-    
+    @@all
   end
 end
+
 
