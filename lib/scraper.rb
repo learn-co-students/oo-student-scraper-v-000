4c032470d50bac1b5af737d34e691cb397a28284
Jane Justiz <janejustiz@gmail.com>
2019-02-10 23:13:44 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,17 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    names = []
+    doc = Nokogiri::HTML(open(index_url))
+    students = doc.css(".student-card")
+
+    students.each.tap do |name|
+      names << name.css("h4").text
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
