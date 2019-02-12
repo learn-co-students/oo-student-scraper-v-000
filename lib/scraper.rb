a755e6a265476173c6d4c2e3667156e771a4078e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-02-12 21:59:17 +0000

    working on scrape_profile_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,19 +9,43 @@ class Scraper
     roster = Nokogiri::HTML(html)
     students = []
     roster.css(".student-card").each do |student_card|
+  
     students << {
       :name => student_card.css(".student-name").text,
       :location => student_card.css(".student-location").text,
       :profile_url => student_card.css("a").attribute("href").value
     }
+    
     end
     students
   end
 
   def self.scrape_profile_page(profile_url)
+    html = File.read(profile_url)
+    profile_page = Nokogiri::HTML(html)
+    student_profile = []
+    profile_page.css(".profile").each do |div|
+      # binding.pry
+      twitter = div.css(".social-icon-container a[href*='twitter']").attribute("href").value
+      linkedin = div.css(".social-icon-container a[href*='linkedin']").attribute("href").value
+      github = div.css(".social-icon-container a[href*='github']").attribute("href").value
+      profile_quote = div.css(".profile-quote").text
+      bio = div.css(".bio-content description-holder").text
+      
+
+      student_profile << {
+        # :twitter => twitter unless twitter != undefined,
+        # :linkedin => linkedin unless linkedin != undefined,
+        # :github => github unless github != undefined,
+        :profile_quote => profile_quote,
+        :bio => bio
+      }
+    end
+    
     
   end
   
-  self.scrape_index_page("./fixtures/student-site/index.html")
+  # self.scrape_index_page("./fixtures/student-site/index.html")
 
 end
+
