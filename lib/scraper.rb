ed024c9c7b597f9f10ba82da19081a2cce60ea62
Sarah Paz <sarah.paz@gmail.com>
2019-02-12 05:13:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,20 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = File.read("./fixtures/student-site/index.html")
+    learn_students = Nokogiri::HTML(doc)
+
+    students = {}
+
+    learn_students.css('student-card').div
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
