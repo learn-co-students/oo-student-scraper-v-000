aca1ef0b5543839102e5b3067412355ddc2cfcb1
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-02-13 18:56:07 +0000

    need blog attribute

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,27 +23,29 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = File.read(profile_url)
     profile_page = Nokogiri::HTML(html)
-    student_profile = []
+    student_profile = {}
     profile_page.css(".profile").each do |div|
       # binding.pry
       twitter = div.css(".social-icon-container a[href*='twitter']").attribute("href").value
       linkedin = div.css(".social-icon-container a[href*='linkedin']").attribute("href").value
       github = div.css(".social-icon-container a[href*='github']").attribute("href").value
+      binding.pry
+      # blog = div.css(".social-icon-container a[src*='rss']").attribute("href").value
       profile_quote = div.css(".profile-quote").text
-      bio = div.css(".bio-content description-holder").text
+      bio = div.css(".bio-content .description-holder p").text
       
 
-      student_profile << {
+      student_profile = {
         #need to figure out how to select attributes only if they exist
-        # :twitter => twitter unless twitter != undefined,
-        # :linkedin => linkedin unless linkedin != undefined,
-        # :github => github unless github != undefined,
-        
+        :twitter => twitter,
+        :linkedin => linkedin,
+        :github => github,
+        # :blog => blog,
         :profile_quote => profile_quote,
         :bio => bio
       }
     end
-    
+    student_profile
     
   end
   
