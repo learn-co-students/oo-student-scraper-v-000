399e09a861c0241be9ab379343fcd492379e2432
jontenthoff <jontenthoff@hotmail.com>
2019-02-20 14:39:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
+  def Scraper.scrape_index_page(index_url)
    students = []
     html = open(index_url)
     index = Nokogiri::HTML(html)
