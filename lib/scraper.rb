634613d667a7a0bade951b6fbe492ce703ae9e02
Karly Mareka <karly.mareka@gmail.com>
2019-03-03 22:21:34 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = File.read(index_url)
     learn_webpage = Nokogiri::HTML(html)
-    student_info = [] 
-    #my_array << {:residential => "false"}
+    @student_info = [] 
     learn_webpage.css().each do |student|
-      name = student.css().text 
-      location = student.css().text 
+      name = student.css().text
+      @student_info << {:name => name}
+      location = student.css().text
+      @student_info[:location] = location 
     end
     learn_webpage.css(). each do |student|
       profile_url = student.css().text 
