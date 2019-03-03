e8dd04973be77aba551e474edd6697ccc15d78ef
Karly Mareka <karly.mareka@gmail.com>
2019-03-03 22:10:26 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,8 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = File.read(index_url)
+    learn_webpage = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
