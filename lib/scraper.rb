5e07396fe8f37d6f271ed61d1a1facb4b9a70bd9
Karly Mareka <karly.mareka@gmail.com>
2019-03-04 21:30:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,6 @@ class Scraper
     html = File.read(profile_url)
     profile_webpage = Nokogiri::HTML(html)
     @social_hash = Hash.new
-    keys = [:twitter, :linkedin, :github, :blog, :profile_quote, :bio]
     @values = []
     profile_webpage.css("div.social-icon-container a").each do |social_icon|
       @values << social_icon['href']
@@ -28,22 +27,18 @@ class Scraper
     @social_hash[:twitter] = @values.find {|icon| icon.include?"twitter"}
     @social_hash[:linkedin] = @values.find {|icon| icon.include?"linkedin"}
     @social_hash[:github] = @values.find {|icon| icon.include?"github"}
-    unless @values[3].empty? 
+    unless @values[3] == nil  
       @social_hash[:blog] = @values[3]
+    end
+    profile_quote = profile_webpage.css("div.profile-quote").text 
+    unless profile_quote == nil 
+      @social_hash[:profile_quote] = profile_quote
+    end
+    bio = profile_webpage.css("div.description-holder p").text 
+    unless bio == nil 
+      @social_hash[:bio] = bio 
     end 
-    binding.pry 
+    @social_hash 
   end 
-=begin 
-    i = 0
-    keys.each do |key|
-      @social_hash[key] = values[i]
-      i += 1 
-    end 
-    @social_hash[:profile_quote] = profile_webpage.css("div.profile-quote").text 
-    @social_hash[:bio] = profile_webpage.css("div.description-holder p").text 
-    @social_hash
-  end
-=end 
-
 end
 
