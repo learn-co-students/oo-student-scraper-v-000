ee1273b1bb96b595e4f02610a50f748f48e02680
J Fern <jfern20@gmail.com>
2019-03-14 04:27:03 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,12 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    student_hash.each {|k, v| self.send(("#{k}="), v )}
+    @@all << self 
  
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      self.new(student_hash)
   end
+end 
 
   def add_student_attributes(attributes_hash)
     
