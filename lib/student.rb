8d9e9f5ea9a8bb69bc9efcdd821b68a206a9c75e
Shanise Barona <shanisebarona@gmail.com>
2019-04-18 18:13:03 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,12 +4,19 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash)
-    
+  #The #initialize method should take in an argument of a hash and use metaprogramming to assign the newly created student attributes and values in accordance with the key/value pairs of the hash. Use the #send method to achieve this.
+  def initialize(student_hash) 
+    student_hash.each do |key, value|
+      self.send(("#{key}="), value) 
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #binding.pry
+    # iterate over students_array
+    # instantiate new student and assign attributes using "student_array"
+    # thassit
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +24,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
