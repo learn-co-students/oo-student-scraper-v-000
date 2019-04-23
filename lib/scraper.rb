68d11754f1c29de87386a694ed7e42ddce273b2a
Carden Z <ephemere@protonmail.com>
2019-04-23 18:50:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,23 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
+
+    names = doc.css('h4.student-name')
+    # names.each { |n| puts "#{n.text}" }
+    locations =  doc.css('.student-location')
+    urls =  doc.css('.student-card a').attr('href')
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
   end
 
 end
-
