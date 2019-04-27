de55d3e93152234eb7cf928197e9f7b0e24e7564
Sara Harvey <saraharveynyc@gmail.com>
2019-04-27 18:29:35 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,22 +4,33 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page('./fixtures/student-site/index.html')
-    doc = Nokogiri::HTML(open('./fixtures/student-site/index.html'))
-    student_index_array = []
-    doc.css(".student-card").each do |student|
-    name = student.css("h4.student-name")
-    location = student.css("p.student-location")
-    profile_url = student.css("href")
-    student_index_array << {name: name, location: location, profile_url: profile_url}
+  def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css('div.student-card').each do |student|
+      students << {
+        :name => student.css("h4.student-name").text,
+        :location => student.css("p.student-location").text,
+        :profile_url => student.css("a").first['href']
+      }
+    end
+    students
   end
-    student_index_array
-  end
-  
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    profiles = {}
     
-  end
-
+    links = doc.css('.social-icon-container a').collect {|link| link.attr('href')}
+    links.each do |link|
+      link.include?('twitter')? profiles[:twitter] = link : ""
+      link.include?('github')? profiles[:github] = link : ""
+      link.include?('linkedin')? profiles[:linkedin] = link : ""
+      !link.include?('twitter') && !link.include?('github') && !link.include?('linkedin')? profiles[:blog] = link : ""
+    end 
+      profiles[:profile_quote] = doc.css('.profile-quote').text
+      profiles[:bio] = doc.css('.description-holder p').text
+      profiles
+    end
 end
 
