848f888068f25dfd7a133f37d79cc46366c0c076
Toni-Ty <antonique2479@yahoo.com>
2019-05-06 23:48:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
 require 'pry'
 
+
+
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc.css(."student-card").each do
   end
+end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
