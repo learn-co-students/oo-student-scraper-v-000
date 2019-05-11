0d4da0d9c15cc00e2876bddda06ddabb5cb2af1b
pmknyc <pmknyc@optimum.net>
2019-05-11 12:20:47 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,18 +3,24 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+ 
 # index_url = '.fixtures/student-site.index.html'
+# top layer page scrape, make student, get these attributes:
+# students_array (array of indiv student hashes) = 
+#   [{:name => "Abby Smith",:location => "Brooklyn, NY",:profile_url => "students/abby-smith.html"}, 
+#    {:name => } ]
+
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(File.read(index_url))
-
     students_array = []
-# [# {:name => "Abby Smith", :location => "Brooklyn, NY",
-# :profile_url => "students/abby-smith.html"} ]
+  # ?? not iterate, just shovel whole list 
+    students_array << doc.css("roster-cards-container")
+    binding.pry
+  end    
+    # .each do |student_card|  students_array << 
 
-    index.css("div.roster-cards-container.student_card").each do |student_card|
-      students_array << 
-
+# second layer scrape indiv student profile pages:
+# 
   def self.scrape_profile_page(BASE_PATH + student.profile_url)
 
 
--- lib/student.rb
+++ lib/student.rb
@@ -10,23 +10,22 @@ class Student
 # Example metaprogramming lesson as guide: 
 #  https://tinyurl.com/metaprog-example
 
-  def initialize(student_hash)
-    # use .send 
-    student_hash.each {|key, value| self.send(("#{key}="), value)}
+  def initialize
     @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
   def self.create_from_collection(students_array)
-    
+    #Student.new
   end
-
+ 
   def add_student_attributes(attributes_hash)
-    
+    # use .send 
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
-end
+end 
 
