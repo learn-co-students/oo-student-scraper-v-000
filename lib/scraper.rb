2ec202b087bad898e94ba7b189044476e31998d7
Niusha H <niusha.hadziomerovic@gmail.com>
2019-05-20 06:22:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,10 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index_url = "./fixtures/student-site/index.html"
     
+    
+    #return value should be an array of hashes with :name, :location, :profile_url
   end
 
   def self.scrape_profile_page(profile_url)
