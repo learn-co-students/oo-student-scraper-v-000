5d4c854f340261c9335cacfbd1fae3301993ad64
pmknyc <pmknyc@optimum.net>
2019-05-28 16:22:49 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,9 @@ require 'pry'
 class Scraper
  
 # .scrape_index_page => 
-#   top layer page scrape used to make new student, 
-#   get attributes as array of indiv student hashes: 
+#   top layer page scrape grabs 3 attributes from 
+#   all students; will be used to make each student object, 
+#   attributes saved as array of indiv student hashes: 
 #    [{:name => "Abby Smith",:location => "Brooklyn, NY",
 #      :profile_url => "students/abby-smith.html"}, 
 #     {:name => etc.} ]
@@ -14,12 +15,11 @@ class Scraper
   def self.scrape_index_page(index_url)
     # index_url = '.fixtures/student-site.index.html'
     doc = Nokogiri::HTML(File.read(index_url))
-    student_index_array = doc.css('div.student-card').map do |student|
-       {:name => student.css('h4').text, 
-        :location => student.css('p').text,
-        :profile_url => student.css('a').attribute('href').value}
+    doc.css('div.student-card').map do |student|
+      {:name => student.css('h4').text, 
+       :location => student.css('p').text,
+       :profile_url => student.css('a').attribute('href').value}
      end
-      student_index_array
   end    
  
 # second layer scrape indiv student profile pages. 
@@ -27,25 +27,25 @@ class Scraper
 # :twitter, :linkedin, :github, :blog, :profile_quote, :bio
   def self.scrape_profile_page(profile_url)
     profile = Nokogiri::HTML(File.read(profile_url))
-      # social_icons = []
-      social_icons = profile.css("div.social-icon-container")
-      quote = profile.css("div.profile-quote")
-      bio = profile.css("div.bio-content content-holder")
-      # social_icons << icon.css('a').attribute('href').value.include?('twitter')
-      # p social_icons
-  end
-    #   {
-    #     :twitter => 
-    #     :linkedin => icon.css('')  , 
-    # :github    =>   , 
-    # :blog =>        , 
-    # :profile_quote =>  ,
-    # } 
-    #  p profile_url.css('.bio-block.details-block p').text
+    attributes = {}
+    social = profile.css('div.social-icon-container')
+      social_info = social.css('a').attribute('href').each do |ref|
+        if ref.value.include?('twitter')
+          attributes[:twitter] = social_info
+        elsif ref.value.include?('linkedin')
+          attributes[:linkedin] = social_info
+        elsif ref.value.include?('github')
+          attributes[:github] = social_info      
+        elsif ref.value.include?('github')
+          attributes[:blog] = social_info
+        end
+      binding.pry
+      end  
+    attributes[:profile_quote] = profile.css("div.profile-quote").text
+   # attributes[:bio] = 
+    test = profile.css("div.bio-content.content-holder")#.children.each {|child| 'p').text
 
-  #    {:bio => "test" 
-  #   }
-  #   end
-  # end
-# 
+    #attributes
+  end
+  
 end # class end
