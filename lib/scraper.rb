e7be9421ed662ed52ca68fd06503062bec268ead
pmknyc <pmknyc@optimum.net>
2019-05-28 13:32:47 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,33 +4,33 @@ require 'pry'
 
 class Scraper
  
-# index_url = '.fixtures/student-site.index.html'
-# top layer page scrape, make student, get these attributes:
-# students_array (array of indiv student hashes) = 
-#   [{:name => "Abby Smith",:location => "Brooklyn, NY",:profile_url => "students/abby-smith.html"}, 
-#    {:name => } ]
-
-  def initialize(path)
-
-  end
-
+# .scrape_index_page => 
+#   top layer page scrape used to make new student, 
+#   get attributes as array of indiv student hashes: 
+#    [{:name => "Abby Smith",:location => "Brooklyn, NY",
+#      :profile_url => "students/abby-smith.html"}, 
+#     {:name => etc.} ]
+  
   def self.scrape_index_page(index_url)
+    # index_url = '.fixtures/student-site.index.html'
     doc = Nokogiri::HTML(File.read(index_url))
-      doc.css('div.student-card').map do |student|
+    student_index_array = doc.css('div.student-card').map do |student|
        {:name => student.css('h4').text, 
         :location => student.css('p').text,
         :profile_url => student.css('a').attribute('href').value}
      end
+      student_index_array
   end    
-
-# second layer scrape indiv student profile pages. Add these attributes:
+ 
+# second layer scrape indiv student profile pages. 
+# Add these attributes:
 # :twitter, :linkedin, :github, :blog, :profile_quote, :bio
   def self.scrape_profile_page(profile_url)
     profile = Nokogiri::HTML(File.read(profile_url))
       # social_icons = []
-      profile.css('div.social.psychology')
-
-            
+      social_icons = profile.css("div.social-icon-container")
+      quote = profile.css("div.profile-quote")
+      bio = profile.css("div.bio-content content-holder")
       # social_icons << icon.css('a').attribute('href').value.include?('twitter')
       # p social_icons
   end
--- lib/student.rb
+++ lib/student.rb
@@ -3,14 +3,18 @@ require 'pry'
 
 class Student
 
-  attr_accessor :name, :location, :profile_url, :twitter, :linkedin, :github, :blog, :profile_quote, :bio 
-
+  attr_accessor :name, :location, :profile_url, 
+      :twitter, :linkedin, :github, :blog, :profile_quote, :bio
   @@all = []
 
 # Example metaprogramming lesson as guide: 
 #  https://tinyurl.com/metaprog-example
 
-  def initialize
+  # .initialize called by .create_from_collection method
+  # assigns attributes: @name, @location, @profile_url
+  # and adds new objects to @@all collection
+  def initialize(student_hash)
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
     @@all << self
   end
 
@@ -18,14 +22,19 @@ class Student
     @@all
   end
 
+# .create_from_collection
+# uses scraped data from Scraper.scrape_index_page method
   def self.create_from_collection(students_array)
-    #Student.new
+    students_array.each do |student_hash|
+      Student.new(student_hash) #initializes student object
+    end  
   end
  
+# .add_student_attributes
+# uses second layer scraped data from Scraper.scrape_profile_page
+# of individual student profile to find social media, quote & bio
+# attributes
   def add_student_attributes(attributes_hash)
-    # use .send 
     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
-
-end 
-
+end # class end
