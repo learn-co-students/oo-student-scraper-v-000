f2873417d315f82a3984e2b43f93234f43eae1e5
Kimberly Aviles <01010110.v@gmail.com>
2019-06-10 01:37:54 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,14 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) # scrape the INDEX page that lists all students
+    # return value should be an array of hashes - each hash is a single student
+    # keys ---- :name, :location, :profile_url
+
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+  def self.scrape_profile_page(profile_url) # scrape INDIVIDUAL student's profile
+
   end
 
 end
-
