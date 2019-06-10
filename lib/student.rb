1b6654f4e2a1f0b8eb12bbb479e860d7109abd06
Kimberly Aviles <01010110.v@gmail.com>
2019-06-10 02:00:15 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,25 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    # use metaprogramming - assign newly created attributes and values
+    # use SEND method
+
   end
 
   def self.create_from_collection(students_array)
-    
+    # iterate over STUDENTS_ARRAY of hashes
+    # create a new individual student using each hash
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
