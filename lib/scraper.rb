4174affcac32e47d5ac41ce7eb6117ff5e9bcafc
Kimberly Aviles <01010110.v@gmail.com>
2019-07-01 17:48:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,18 @@
 require 'nokogiri'
 require 'open-uri' #<---- Ruby module of methods for HTTP requests
 
-html = open("URL - plz put in site")
-doc = Nokogiri::HTML(html)
+
 
 class Scraper # does not store information- just scrapes
 
   def self.scrape_index_page(index_url) # scrape the INDEX page that lists all students
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    Student.name = <h4 class="student-name">blahblahguy</h4>
+    Student.location = <p class="student-location">blahplace</p>
+    Student.profile_url = <a href="students/blahblahname-lastname.html">..</a>
+
     # return value should be an array of hashes - each hash is a single student
     # keys ---- :name, :location, :profile_url
     # [:name => "Arthur Carter", :location => "California", :profile_url => "www.him.com"]
