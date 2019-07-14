6c25d6d738f0863bf1fe47e5ab22a3150689f3d5
Gigi M <mmgamao1@gmail.com>
2019-07-14 20:20:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,31 +8,24 @@ require 'pry'
 
 # The #scrape_index_page Method - responsible for scraping the index page that lists all of the students.  Class method.  should take in an argument of the URL of the index page, for the purposes of our test the URL will be "./fixtures/student-site/index.html". 
 
-class Scraper
+
+class Scraper  
 
   def self.scrape_index_page(index_url)
-    
-    # binding.pry
-    # It should use Nokogiri and Open-URI to access that page.
-    
-    index_page = Nokogiri::HTML(open(index_url))
-    
-    # The return value of this method should be an array of hashes in which each hash represents a single student.
-    
-    students = []
-    index_page.css("div.roster-cards-container").each do |card|
+
+    doc = Nokogiri::HTML(open(index_url))
+    students = Array.new
+    doc.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
-        
-        # The keys of the individual student hashes should be :name, :location and :profile_url.
-        
-        student_profile_link = "./fixtures/student-site/#{student.attr('href')}"
-        student_location = student.css('.student-location').text
-        student_name = student.css('.student-name').text
-        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+        name = student.css(".student-name").text
+        location = student.css(".student-location").text
+        profile_url = "#{student.attr("href")}"
+        students << {name: name, location: location, profile_url: profile_url}
       end
     end
     students
-  end
+  end	
+
   
   # The #scrape_profile_page method is responsible for scraping an individual student's profile page to get further information about that student. This is a class method that should take in an argument of a student's profile URL. It should use Nokogiri and Open-URI to access that page. The return value of this method should be a hash in which the key/value pairs describe an individual student. Some students don't have a twitter or some other social link. 
 
--- lib/student.rb
+++ lib/student.rb
@@ -3,22 +3,31 @@ class Student
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
 
   @@all = []
+  
+  
+# The #initialize method should take in an argument of a hash and use metaprogramming to assign the newly created student attributes and values in accordance with the key/value pairs of the hash. Use the #send method to achieve this. 
+# This method should also add the newly created student to the Student class' @@all array of all students. You'll need to create this class variable and set it equal to an empty array at the top of your class. Push self into the array at the end of the #initialize method.
 
   def initialize(student_hash)
-    
+    student_hash.each do |attribute, value|
+    self.send("#{attribute}=", value)
+end
     @@all << self  
   end
 
-  def self.create_from_collection(students_array)
-    
-  end
+ def self.create_from_collection(students_array)
+    students_array.each { |student_hash| self.new(student_hash) }
+  end	  
+  
 
-  def add_student_attributes(attributes_hash)
-    @all 
-  end
+def add_student_attributes(attributes_hash)
+    attributes_hash.each_pair { |key, value| self.send(("#{key}="), value) 
+    }
+end
 
   def self.all
-    
+    @@all  
   end
 end
+
 
