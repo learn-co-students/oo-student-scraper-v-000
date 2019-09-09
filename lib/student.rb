c3ceb409b34efaac6927419f8a0389de5e5151d5
alejandra cantu <alejandra.cantu13@gmail.com>
2019-09-09 01:25:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
+  # student_hash.each {|key, value| self.send(("#{key}="), value)} 
   self.name = student_hash[:name]
   self.location = student_hash[:location]
 
@@ -14,15 +14,30 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    binding.pry
+    #binding.pry
+    students_array.each do |student|
+      Student.new(student)
+    end
 
   end
 
   def add_student_attributes(attributes_hash)
+# binding.pry
+# attributes_hash.each {|key, value| self.send(("#{key}="), value)} 
 
-  end
+# attributes_hash.each do |e|
+#   self.e = attributes_hash[:e]
+
+self.twitter = attributes_hash[:twitter]
+self.linkedin = attributes_hash[:linkedin]
+self.github = attributes_hash[:github]
+self.blog = attributes_hash[:blog]
+self.profile_quote = attributes_hash[:profile_quote]
+self.bio = attributes_hash[:bio]
+    @@all << attributes_hash
+   end
 
   def self.all
-
+@@all
   end
 end
