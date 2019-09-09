411bee471dc3a3f906c893c97ad6f03f3711bbb7
alejandra cantu <alejandra.cantu13@gmail.com>
2019-09-09 00:38:39 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,49 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
+    students_hash = []
+    html = Nokogiri::HTML(open(index_url))
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+    html.css(".student-card").collect do |student|
+      hash = {
+        name: student.css("h4.student-name").text,
+        profile_url: student.css("a").attribute("href").value,
+        location: student.css("p.student-location").text
+      }
+      students_hash << hash
+     end
+     students_hash
+   end
 
-end
+    def self.scrape_profile_page(profile_url)
 
+      scraped_student = {}
+
+      html = Nokogiri::HTML(open(profile_url))
+
+        html.css(".social-icon-container a").each do |student|
+        url = student.attributes["href"].value
+        # scraped_student[:bio] = html.css("div.description-holder p").text
+        # scraped_student[:quote] = html.css("div.profile-quote").text
+            if url.include?("twitter")
+              scraped_student[:twitter] = url
+            elsif url.include?("github")
+              scraped_student[:github] = url
+            elsif  url.include?("linkedin")
+              scraped_student[:linkedin] = url
+            elsif url.include?("youtube")
+              scraped_student[:youtube_url] = url
+            else
+              scraped_student[:blog] = url
+
+            end
+          end
+          scraped_student[:profile_quote] = html.css("div.profile-quote").text
+          scraped_student[:bio] = html.css("div.description-holder p").text
+# binding.pry
+          scraped_student
+
+      end
+
+
+    end
