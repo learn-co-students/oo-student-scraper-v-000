6b3d67276cad1dccfb3ef682715a75c0681bd860
Olivia Kesler <oakesler@protonmail.ch>
2019-10-29 14:54:04 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,41 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    locations_array = []
+    url_array = []
+    names_array = []
+    final_array = []
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    doc = Nokogiri::HTML(html)
     
-  end
+    ################# name #####################
+    names = doc.css(".student-name")
+    names.each do |item|
+      names_array << item.text.strip
+    end
+    ####################################
+  
+    #############location#####################
+    locations = doc.css(".student-location")
+    locations.each do |item|
+      locations_array << item.text.strip
+    end
+    #######################################
+    
+    x = 0
+    y = names_array.length 
+    
+    while x < y 
+      binding.pry
+      temp_hash = Hash.new 
+      temp_hash[:name] = "#{names_array[x - 1]}"
+      temp_hash[:location] = "#{locations_array[x - 1]}"
+      temp_hash[:profile_url] = ".students/#{names_array[x - 1]}.html"
+      final_array << temp_hash
+      x += 1
+    end
+    final_array
+  end 
 
   def self.scrape_profile_page(profile_url)
     
