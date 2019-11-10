8d3052cee628b5b3515b17c561ff92c29e50fd92
Paris Dolfman <parisdolfman@gmail.com>
2019-11-10 22:20:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,9 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
 
-    student_name = doc.css(".student-name").text.strip
+    #student_name = doc.css(".student-name").text.strip
+    #location = doc.css(".student-location").text.strip
+    profile_url = doc.css("div.student-card").attr("href")
     binding.pry
   end
 
@@ -19,3 +21,7 @@ class Scraper
 
 end
 Scraper.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+
+
+# student names: doc.css(".student-name").text.strip  #whitespace removed with .strip
+# location: location = doc.css(".student-location").text.strip
