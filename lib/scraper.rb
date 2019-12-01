7d24015674ae1b59a83aac64a1831eee560fdd66
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-01 22:17:07 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,13 +5,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    # html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
-    # doc = Nokogiri::HTML(html)
-
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
     array = doc.css(".roster-cards-container")
     hash = Hash[array.collect { |key, value| [name, name.css(".student-name")] }
-
   end
 
   def self.scrape_profile_page(profile_url)
