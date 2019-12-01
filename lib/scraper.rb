66f3da23a3bf52175388a3a2b98298adaedb4cfa
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-01 18:48:23 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,16 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    doc = Nokogiri::HTML(html)
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
