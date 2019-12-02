0e256d623af5d0739aab6a146833439dff6e1bec
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-02 03:37:42 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,12 +6,13 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-    array = doc.css(".roster-cards-container")
+    # array = doc.css(".roster-cards-container").first
     # hash = Hash[array.collect { |key, value| [name, name.css(".student-name")] }
     # hash = Hash[*array.flatten]
     # hash = Hash[array.map {|key, value| [name, name.css(".student-name")]}]
     # a3 = [ ['apple', 1], ['banana', 2], [['orange','seedless'], 3] ]
     # h3 = Hash[*a3.flatten]
+    binding.pry
   end
   # binding.pry
 
