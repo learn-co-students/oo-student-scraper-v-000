a515b332ff863eb00cf16c4ed85addeb64bb1df5
Kimberly Aviles <01010110.v@gmail.com>
2019-12-03 16:58:21 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,18 @@
-require 'nokogiri'
-require 'open-uri'
+require 'nokogiri' #"fine-toothed-saw"
+require 'open-uri' #Ruby module that shipped with ruby but needs to imported..i think..?
+
+index_page_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
 
  class Scraper
 
-   def scrape_index_page
+   def scrape_index_page(index_page_url)
+     html_grab = open(index_page_url)
+     #uses OPEN-URImodule in Ruby to grab the HTML of the site
+     noko_html_grab = Nokogiri::HTML(html_grab)
+     #uses HTML method in the NOKOGIRIgem to translate into  nested nodes for easier scraping
+     :name =>
+     :location =>
+     :profile_url =>
    end
 
    def scrape_profile_page
