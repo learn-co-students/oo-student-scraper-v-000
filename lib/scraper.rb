d6a635d27079a825e5df86bc8cd627de4edb3757
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-10 19:47:37 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,33 +7,40 @@ class Scraper
   def self.scrape_index_page(index_url)
 
     doc = Nokogiri::HTML(open(index_url))
-    # binding.pry
+
     array = []
     students = doc.css(".roster-cards-container")
 
     students.each do |student|
+      hash = {}
+      student.css(".student-name").each do |name|
+      name = name.text
 
-      student.css(".student-name").each do |item|
-        item = { :name => name.text, :location => location.text }
-        array << hash
-        binding.pry
+      binding.pry
+      # hash = { :name => student.css(".student-name").each { |name| name.text }, :location => student.css(".student-location").each { |location| location.text } }
       end
 
       student.css(".student-location").each do |location|
-        hash = { :location => location.text }
-        array << hash
-        # binding.pry
+      location.text
+      location
       end
 
-      # student.css(".student-card a").map { |links| links['href'] }
-      #   links.each do |link|
-      #     binding.pry
-      #   hash = { :profile_url => link[0] }
-      #   array << hash
-      #
-      #   end
-      # end
+      student.css(".student-card a").map do |link|
+      link['href']
+      link
+      end
+
+      # hash = { :name => name.text, :location => location.text, :profile_url => link['href'] }
+      # binding.pry
+      end
+
       # profile_url = doc.css(".roster-cards-container").css(".student-card a").map { |link| link['href'] }
+
+      # index_url.collect do |name, location, profile_url|
+      #   hash = { :name => name[0].text, :location => location[0].text, :profile_url => profile_url[0] }
+      #   array << hash
+        # binding.pry
+      # end
     end
 
 
@@ -55,7 +62,7 @@ class Scraper
     # end
     # array
         # binding.pry
-  end
+
 
     # :student_index_array) {[{:name=>"Joe Burgess", :location=>"New York, NY", :profile_url=>"students/joe-burgess.html"},
     #                              {:name=>"Mathieu Balez", :location=>"New York, NY", :profile_url=>"students/mathieu-balez.html"},
