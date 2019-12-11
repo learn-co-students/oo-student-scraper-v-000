7bc753178776172acf0b96a2a446346a75ab48e9
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-11 03:38:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,25 +11,29 @@ class Scraper
     students = doc.css(".roster-cards-container")
 
     students.each do |student|
-      hash = {}
+
       student.css(".student-name").each do |name|
-      name = name.text
-      hash[:name] = name
+      @name = name.text
+      # hash[:name] = name
       end
 
       student.css(".student-location").each do |location|
-      location = location.text
-      hash[:location] = location
+      @location = location.text
+      # hash[:location] = location
       end
 
       student.css(".student-card a").map do |link|
-      link = link['href']
-      hash[:profile_url] = link
+      @link = link['href']
+      # hash[:profile_url] = link
       end
+
+      hash = { :name => @name, :location => @location, :profile_url => @link }
+
        array << hash
+      #  binding.pry
       end
-      array
-      binding.pry
+       array
+      # binding.pry
     end
 
 
