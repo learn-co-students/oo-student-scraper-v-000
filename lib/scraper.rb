b26dd31f7ee1eb438c559dc04da06d3a4eef6950
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-13 22:53:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,27 +3,27 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
-
-
 # the return value of this method should be an array of hashes in which each hash represents
 # a single student.
   def self.scrape_index_page(index_url)
-    # hash = {}
+    doc = Nokogiri::HTML(open(index_url))
+    # students = doc.css(".roster-cards-container")
+    students = doc.css(".student-card")
+    # binding.pry
     scraped_students = []
 
-    doc = Nokogiri::HTML(open(index_url))
-
-    students = doc.css(".roster-cards-container div")
-    binding.pry
     students.each do |student|
+      # hash = { :name => student.css(".student-name")[0].text,
+      #   :location => student.css(".student-location")[0].text,
+      #   :profile_url => student.css(".student-card a")[0]['href']}
 
-      hash = { :name => student.css(".student-name")[0].text,
-        :location => student.css(".student-location")[0].text,
-        :profile_url => student.css(".student-card a")[0]['href'] }
+        hash = { :name => student.css(".student-name")[0].text,
+          :location => student.css(".student-location")[0].text,
+          :profile_url => student.css(".student-card").first}
 
+        binding.pry
       scraped_students << hash
-      # binding.pry
-      #  artist_count[artist] += 1
+
       end
       scraped_students
       # binding.pry
