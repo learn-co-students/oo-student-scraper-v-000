54710eb1cdb80cfc753fd2c80842c38ee98c8f3c
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-14 21:41:31 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
+
 # the return value of this method should be an array of hashes in which each hash represents
 # a single student.
   def self.scrape_index_page(index_url)
@@ -16,18 +17,36 @@ class Scraper
 
       hash = { :name => student.css(".student-name")[0].text,
         :location => student.css(".student-location")[0].text,
-        :profile_url => student.css("a")[0]['href']}
+        :profile_url => student.css("a")[0]['href'] }
         # binding.pry
       scraped_students << hash
-
       end
       scraped_students
       # binding.pry
     end
 
-
+    # The return value
+    # of this method should be a hash in which the key/value pairs describe an
+    # individual student
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    students = doc.css(".social-icon-container")
 
+    social_url = {}
+
+    students.each do |student|
+      
+
+    binding.pry
   end
+
+  # {
+  #   :twitter=>"http://twitter.com/flatironschool",
+  #   :linkedin=>"https://www.linkedin.com/in/flatironschool",
+  #   :github=>"https://github.com/learn-co",
+  #   :blog=>"http://flatironschool.com",
+  #   :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+  #   :bio=> "I'm a school"
+  # }
 
 end
