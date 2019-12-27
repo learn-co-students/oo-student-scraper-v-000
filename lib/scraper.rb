57af701d698b4749a4697fe017f884c081dee743
Dan Suarez <suarez.daniel101@gmail.com>
2019-12-27 16:22:13 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,6 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
       # social_urls = Hash.new("Not available")
-
       social_urls = { twitter: doc.css(".social-icon-container").css("a")[0]['href'],
         linkedin: doc.css(".social-icon-container").css("a")[1]['href'],
         github: doc.css(".social-icon-container").css("a")[2]['href'],
@@ -36,13 +35,27 @@ class Scraper
         profile_quote: doc.css(".vitals-text-container").css(".profile-quote").text,
         bio: doc.css(".details-container").css("p").text }
 
+      if doc.css(".social-icon-container").css("a") != nil ||
+        doc.css(".vitals-text-container").css(".profile-quote") != nil ||
+        doc.css(".details-container").css("p") != nil
+        social_urls
+      end
+      # binding.pry
+
+      # social_urls = { twitter: doc.css(".social-icon-container").css("a")[0]['href'],
+      #   linkedin: doc.css(".social-icon-container").css("a")[1]['href'],
+      #   github: doc.css(".social-icon-container").css("a")[2]['href'],
+      #   blog: doc.css(".social-icon-container").css("a")[3]['href'],
+      #   profile_quote: doc.css(".vitals-text-container").css(".profile-quote").text,
+      #   bio: doc.css(".details-container").css("p").text }
+        # binding.pry
         # Coach said to try this concept for each link:
         # use if on this section  of "doc.css(".social-icon-container").css("a")" and check that it is not nil.
         # if not nil you can then take the whole link above and add it to the key.
         # if doc.css(".social-icon-container").css("a") != nil
         #   then social_urls = { twitter: doc.css(".social-icon-container").css("a")[0]['href'] }
         # social_urls.each do |key, value|
-        social_urls.reject { |k,v| v.nil? }
+        # social_urls.reject { |k,v| v.nil? }
         # binding.pry
         # end
         # binding.pry
