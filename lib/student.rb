8465f198c679e5d01bdf568719c8b02da5496954
Dan Suarez <suarez.daniel101@gmail.com>
2020-01-10 16:48:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,14 +5,12 @@ class Student
   @@all = []
 
   def name=(name)
-    # @name = name
     @name = @student_hash[:name]
-    # binding.pry
   end
 
-  # def name
-  #   @name
-  # end
+  def location=(location)
+    @location = @student_hash[:location]
+  end
 
   # use meta-programming(#send) to assign the newly created student attributes and values per
   # the key/value pairs of the hash
@@ -20,23 +18,24 @@ class Student
   # using the key/value pairs of that hash.
   def initialize(student_hash)
     @student_hash = student_hash
-    @name = @student_hash[:name]
-    @location = @student_hash[:location]
-    # @student_hash.send(:name)
-    # @student_hash.send(:name)
-    # binding.pry
+    # @name = @student_hash[:name]
+    # @location = @student_hash[:location]
+    send(:name=, name)
+    send(:location=, location)
+    # @student_hash.send(:name=, name)    "This didn't work with @student_hash in the beginning of the send method"
+    # @student_hash.send(:location=, location)    "This didn't work with @student_hash in the beginning of the send method"
     @@all << self
   end
 
-  # The
-  # `#create_from_collection` method should iterate over the array of hashes and
+  # The `#create_from_collection` method should iterate over the array of hashes and
   # create a new individual student using each hash. This brings us to the
   # `#initialize` method on our `Student` class.
 
   def self.create_from_collection(students_array)
-    # students_array.each do |student|
-    #   student.send(new(@student_hash))
-    # end
+    students_array.each do |student|
+      student = Student.new(@student_hash)
+      # student.send(:new, @student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -44,6 +43,7 @@ class Student
   end
 
   def self.all
-
+    @@all
   end
+  
 end
