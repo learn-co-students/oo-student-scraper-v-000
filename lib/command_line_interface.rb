f89d582bd2a7ba9cb5d44b5fefc348ba0578441e
Dan Suarez <suarez.daniel101@gmail.com>
2020-01-15 02:00:01 +0000

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -29,6 +29,7 @@ class CommandLineInterface
 
   def display_students
     Student.all.each do |student|
+      binding.pry
       puts "#{student.name.upcase}".colorize(:blue)
       puts "  location:".colorize(:light_blue) + " #{student.location}"
       puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
