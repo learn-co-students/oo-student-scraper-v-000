5e098db799786c5177b166d2f8f6afae10727753
andy-aguilar <aaguil3@gmail.com>
2020-12-02 17:38:46 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,42 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = []
+    doc.css('.student-card').each do |card|
+      students << {
+        name: card.css('.student-name').text,
+        location: card.css('.student-location').text,
+        profile_url: card.css('a').attribute('href').value
+      }
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    student = {}
+    student[:profile_quote] = doc.css('.profile-quote').text
+    student[:bio] = doc.css('.description-holder p').text    
+    doc.css('.social-icon-container a').each do |tag|
+      url = tag.attribute('href').value
+      if url.include?("twitter")
+        student[:twitter] = url
+      elsif url.include?('linkedin')
+        student[:linkedin] = url
+      elsif url.include?('github')
+        student[:github] = url
+      else
+        student[:blog] = url
+      end
+    end
+    student
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +6,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
   end
 
   def self.all
-    
+    @@all
   end
 end
 
