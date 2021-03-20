c005dc43295999b3e5e5aeb3e6d9b5750ec67a42
Valencia Caldwell <vkcaldwell@gmail.com>
2021-03-20 16:53:57 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,24 @@
+require 'pry'
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+    # binding.pry
+    @@all
   end
 end
-
