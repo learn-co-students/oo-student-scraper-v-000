69eda4e133808d7c4b117cda530b2c5431b850d6
Valencia Caldwell <vkcaldwell@gmail.com>
2021-03-22 12:29:12 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,21 +17,49 @@ class Scraper
 
     # binding.pry
     students = []
-    page.css("div.student-card").each do |student|
+    page.css("div.roster-cards-container").each do |card|
+      card.css("div.student-card a").each do |student|
 
-      students << {
-        :name => student.css("div.card-text-container h4").text,
-        :location => student.css("div.card-text-container p").text,
-        # :profile_url => student.css("div.student-card a").attribute("href")
+        student_href = "#{student.attr("href")}"
+        student_location = student.css(".student-location").text
+        student_name = student.css('.student-name').text
         # binding.pry
-      }
-
+        students << {
+          :name => student_name,
+          :location => student_location,
+          :profile_url => student_href
+        # binding.pry
+          }
+      end
     end
-      binding.pry
+      students
   end
+
 
   def self.scrape_profile_page(profile_url)
+    profile_page = Nokogiri::HTML(open(profile_url))
+    student = {}
 
+    links = profile_page.css("social-icon-container").children.css("a").map{ |e| e.atrribute("href").value}
+    links.each do |link|
+
+      if link.include?("twitter")
+        student[:twitter] = link
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      if link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+        binding.pry
+      end
+  end
+    student[:profile-quote] = profile_page.css(".profile-quote").text if page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.description-holder p").text if page.css("div.description-holder p")
+    sbinding.pry
+    student
+    end
   end
 
+  end
 end
